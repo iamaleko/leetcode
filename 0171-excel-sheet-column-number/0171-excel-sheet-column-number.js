db3d07d3ab571e480d5f81e9b414724569052215
Alexander Kolobov <a@da.am>
2023-08-13 23:58:10 +0400

    Time: 58 ms (88.78%), Space: 42.8 MB (92.86%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
@@ -0,0 +1,34 @@
+const map = {
+        'A': 1,
+        'B': 2,
+        'C': 3,
+        'D': 4,
+        'E': 5,
+        'F': 6,
+        'G': 7,
+        'H': 8,
+        'I': 9,
+        'J': 10,
+        'K': 11,
+        'L': 12,
+        'M': 13,
+        'N': 14,
+        'O': 15,
+        'P': 16,
+        'Q': 17,
+        'R': 18,
+        'S': 19,
+        'T': 20,
+        'U': 21,
+        'V': 22,
+        'W': 23,
+        'X': 24,
+        'Y': 25,
+        'Z': 26,
+    },
+      titleToNumber = (s) => {
+    let res = 0;
+    s = s.split('').reverse();
+    for (let i = 0; i < s.length;) res += map[s[i]] * Math.pow(26, i++);
+    return res;
+};
