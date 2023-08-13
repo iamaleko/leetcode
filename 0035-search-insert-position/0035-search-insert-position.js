14399d088809c3984988ebe165a4a5eeae179b26
Alexander Kolobov <a@da.am>
2023-08-13 23:46:06 +0400

    Time: 51 ms (81.17%), Space: 41.7 MB (84.62%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,15 @@
+const searchInsert = (nums, target) => {
+    let l = 0,
+        r = nums.length - 1;
+    while (l <= r) {
+        const c = l + Math.ceil((r - l) / 2)
+        if (nums[c] === target) {
+            return c;
+        } else if (target < nums[c]) {
+            r = c - 1;
+        } else {
+            l = c + 1;
+        }
+    }
+    return r + 1;
+};
