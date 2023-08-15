bf1f70b484ea21024860890746c164a624788772
Alexander Kolobov <a@da.am>
2023-08-15 23:40:08 +0400

    Time: 60 ms (89.44%), Space: 46.3 MB (57.69%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -1,17 +1,30 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {boolean}
+//  */
+// const canJump = (nums, i = 0) => {
+//   if (nums[i] === true || nums[i] === false) return nums[i];
+//   if (i >= nums.length - 1) return nums[i] = true;
+//   let jump = nums[i];
+//   while (jump) {
+//     if (canJump(nums, i + jump)) {
+//       return true;
+//     }
+//     --jump;
+//   }
+//   return nums[i] = false;
+// };
+
 /**
  * @param {number[]} nums
  * @return {boolean}
  */
-const canJump = (nums, i = 0) => {
-  if (nums[i] === true || nums[i] === false) return nums[i];
-  if (i === nums.length - 1) return nums[i] = true;
-  if (i > nums.length - 1) return nums[i] = false;
-  let jump = nums[i];
-  while (jump) {
-    if (i + jump < nums.length && canJump(nums, i + jump)) {
-      return true;
-    }
-    --jump;
+const canJump = (nums) => {
+  let max = 0;
+  for (let i = 0; i < nums.length; ++i) {
+    if (i + nums[i] > max) max = i + nums[i];
+    if (max >= nums.length - 1) return true;
+    if (max === i && !nums[i]) return false;
   }
-  return nums[i] = false;
+  return false;
 };
