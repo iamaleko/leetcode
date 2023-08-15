a464eb8ec3b6951e01230608d083bf8f69b2df8b
Alexander Kolobov <a@da.am>
2023-08-16 01:09:38 +0400

    Time: 88 ms (21.24%), Space: 52.1 MB (5.1%) - LeetHub

---
+++ 0135-candy/0135-candy.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} ratings
+ * @return {number}
+ */
+var candy = function(children) {
+  const candys = {};
+  const rating = {};
+
+  for (const i in children) {
+    candys[i] = 1;
+    rating[i] = children[i];
+    children[i] = i;
+  }
+
+  children.sort((a, b) => rating[a] - rating[b]);
+
+  let candy = 0;
+
+  for (const i of children) {
+    if (rating[i - 1] !== undefined && rating[i] > rating[i - 1]) candys[i] = Math.max(candys[i - 1] + 1, candys[i]);
+    if (rating[+i + 1] !== undefined && rating[i] > rating[+i + 1]) candys[i] = Math.max(candys[+i + 1] + 1, candys[i]);
+    candy += candys[i];
+  }
+
+  return candy;
+};
