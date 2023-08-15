e5aedd0451c8ce6b45612be01b1eed7ee941e228
Alexander Kolobov <a@da.am>
2023-08-15 23:09:17 +0400

    Time: 1083 ms (13.1%), Space: 52.2 MB (7.89%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const canJump = (nums, i = 0, mem = {}) => {
+  if (i in mem) return mem[i];
+  if (i === nums.length - 1) return mem[i] = true;
+  if (i > nums.length - 1) return mem[i] = false;
+  let jump = nums[i];
+  while (jump) {
+    if (canJump(nums, i + jump--, mem)) return true;
+  }
+  return mem[i] = false;
+};
