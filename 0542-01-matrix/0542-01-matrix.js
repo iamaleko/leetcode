9b29aca4bc8e2ec1fbcc8cce06369de7ed366492
Alexander Kolobov <a@da.am>
2023-08-17 18:59:37 +0400

    Time: 151 ms (82.29%), Space: 59.1 MB (59.53%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.js
+++ 0542-01-matrix/0542-01-matrix.js
@@ -25,26 +25,108 @@
 //   return out;
 // };
 
+// const updateMatrix = (mat) => {
+//   const out = Array.from(Array(mat.length), () => []);
+//   const queue = [];
+
+//   for (const y in mat) {
+//     for (const x in mat[y]) {
+//       if (mat[y][x] === 0) queue.push([+x, +y, 0]);
+//     }
+//   }
+
+//   while (queue.length) {
+//     const [x, y, s] = queue.shift();
+//     if (out[y][x] === undefined) {
+//       out[y][x] = s;
+//       if (x > 0)                 queue.push([x - 1, y, s + 1])
+//       if (x < mat[y].length - 1) queue.push([x + 1, y, s + 1])
+//       if (y > 0)                 queue.push([x, y - 1, s + 1])
+//       if (y < mat.length - 1)    queue.push([x, y + 1, s + 1])
+//     }
+//   }
+
+//   return out;
+// };
+
+// const updateMatrix = (mat) => {
+//   const out = Array.from(Array(mat.length), () => []);
+//   const queue = [];
+
+//   for (const y in mat) {
+//     for (const x in mat[y]) {
+//       if (mat[y][x] === 0) {
+//         out[y][x] = 0;
+//         queue.push([+x, +y]);
+//       } else {
+//         out[y][x] = Number.MAX_VALUE;
+//       }
+//     }
+//   }
+
+//   while (queue.length) {
+//     const [x, y] = queue.shift();
+
+//     if (x > 0 && out[y][x - 1] > out[y][x]) {
+//       out[y][x - 1] = out[y][x] + 1;
+//       queue.push([x - 1, y]);
+//     }
+
+//     if (y > 0 && out[y - 1][x] > out[y][x]) {
+//       out[y - 1][x] = out[y][x] + 1;
+//       queue.push([x, y - 1]);
+//     }
+
+//     if (x < mat[y].length - 1 && out[y][x + 1] > out[y][x]) {
+//       out[y][x + 1] = out[y][x] + 1;
+//       queue.push([x + 1, y]);
+//     }
+
+//     if (y < mat.length - 1 && out[y + 1][x] > out[y][x]) {
+//       out[y + 1][x] = out[y][x] + 1;
+//       queue.push([x, y + 1]);
+//     }
+//   }
+
+//   return out;
+// };
+
 const updateMatrix = (mat) => {
-  const out = Array.from(Array(mat.length), () => []);
   const queue = [];
 
   for (const y in mat) {
     for (const x in mat[y]) {
-      if (mat[y][x] === 0) queue.push([+x, +y, 0]);
+      if (mat[y][x] === 0) {
+        queue.push([+x, +y]);
+      } else {
+        mat[y][x] = Number.MAX_VALUE;
+      }
     }
   }
 
   while (queue.length) {
-    const [x, y, s] = queue.shift();
-    if (out[y][x] === undefined) {
-      out[y][x] = s;
-      if (x > 0)                 queue.push([x - 1, y, s + 1])
-      if (x < mat[y].length - 1) queue.push([x + 1, y, s + 1])
-      if (y > 0)                 queue.push([x, y - 1, s + 1])
-      if (y < mat.length - 1)    queue.push([x, y + 1, s + 1])
+    const [x, y] = queue.shift();
+
+    if (x > 0 && mat[y][x - 1] > mat[y][x]) {
+      mat[y][x - 1] = mat[y][x] + 1;
+      queue.push([x - 1, y]);
+    }
+
+    if (y > 0 && mat[y - 1][x] > mat[y][x]) {
+      mat[y - 1][x] = mat[y][x] + 1;
+      queue.push([x, y - 1]);
+    }
+
+    if (x < mat[y].length - 1 && mat[y][x + 1] > mat[y][x]) {
+      mat[y][x + 1] = mat[y][x] + 1;
+      queue.push([x + 1, y]);
+    }
+
+    if (y < mat.length - 1 && mat[y + 1][x] > mat[y][x]) {
+      mat[y + 1][x] = mat[y][x] + 1;
+      queue.push([x, y + 1]);
     }
   }
 
-  return out;
+  return mat;
 };
