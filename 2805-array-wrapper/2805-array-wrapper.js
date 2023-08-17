2a331d76ef7400ca5c461636659c35c2d4e8ba19
Alexander Kolobov <a@da.am>
2023-08-18 01:38:27 +0400

    Time: 62 ms (32.85%), Space: 43.8 MB (64.16%) - LeetHub

--- 2805-array-wrapper/2805-array-wrapper.js
+++ 2805-array-wrapper/2805-array-wrapper.js
@@ -1,11 +1,15 @@
 class ArrayWrapper {
   constructor(arr) {
-    this.arr = arr;
+    // this.arr = arr;
+    this.a = arr.reduce((a, v) => a += v, 0);
+    this.b = `[${arr.join`,`}]`;
   }
   valueOf() {
-    return this.arr.reduce((a, v) => a += v, 0);
+    return this.a
+    // return this.arr.reduce((a, v) => a += v, 0);
   }
   toString() {
-    return `[${this.arr.join`,`}]`;
+    return this.b
+    // return `[${this.arr.join`,`}]`;
   }
 }
