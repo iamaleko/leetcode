be9973d00dee0bc31dc3efd8bab65f620ea92322
Alexander Kolobov <a@da.am>
2023-08-20 19:23:06 +0400

    Time: 39 ms (99.03%), Space: 42.1 MB (47.96%) - LeetHub

--- 1791-richest-customer-wealth/1791-richest-customer-wealth.js
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.js
@@ -1,8 +1,9 @@
 const maximumWealth = function(accounts) {
-  let max = 0;
-  for (const account of accounts) {
-    let i = account.length, sum = 0;
-    while (i--) sum += account[i];
+  let max = 0, a = accounts.length, b, sum;
+  while (a--) {
+    b = accounts[a].length
+    sum = 0;
+    while (b--) sum += accounts[a][b];
     if (max < sum) max = sum;
   }
   return max;
