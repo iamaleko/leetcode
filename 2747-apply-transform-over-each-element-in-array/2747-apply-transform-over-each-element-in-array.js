308665791aaa7d3c3c3d69a2d4f54a9c7c950049
Alexander Kolobov <a@da.am>
2023-08-21 22:34:38 +0400

    Time: 54 ms (61.58%), Space: 42.1 MB (23.94%) - LeetHub

---
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+const map = (arr, fn) => {
+  for (let i = 0; i < arr.length; ++i) arr[i] = fn(arr[i], i);
+  return arr;
+};
