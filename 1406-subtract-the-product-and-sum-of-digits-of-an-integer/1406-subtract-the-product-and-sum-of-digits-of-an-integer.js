619437fa7b1b620588637752b032a05f4d2f835e
Alexander Kolobov <a@da.am>
2023-08-23 18:50:57 +0400

    Time: 55 ms (55.53%), Space: 41.6 MB (91.93%) - LeetHub

---
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/1406-subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const subtractProductAndSum = function(n) {
+  let prod = 1, sum = 0;
+  while (n) {
+    const digit = n % 10;
+    n = Math.floor(n / 10);
+    prod *= digit;
+    sum += digit;
+  }
+  return prod - sum;
+};
