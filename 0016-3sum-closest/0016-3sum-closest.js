b12af5e2166112b98e0b635908b098dde0fc88e9
Alexander Kolobov <a@da.am>
2023-08-30 01:10:49 +0400

    Time: 72 ms (65.4%), Space: 43.7 MB (71%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.js
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -7,18 +7,16 @@ const threeSumClosest = (nums, target) => {
   nums.sort((a,b) => a - b);
   let closest_sum,
       sum,
-      min_diff = Infinity,
-      diff;
+      min_diff = Infinity;
   for (let a = 0; a < nums.length - 2; ++a) {
     let b = a + 1,
         c = nums.length - 1;
     while (b < c) {
       sum = nums[a] + nums[b] + nums[c];
       if (sum === target) return sum;
-      diff = Math.abs(target - sum);
       sum < target ? ++b : --c;
-      if (min_diff > diff) {
-        min_diff = diff;
+      if (min_diff > Math.abs(target - sum)) {
+        min_diff = Math.abs(target - sum);
         closest_sum = sum;
       }
     }
