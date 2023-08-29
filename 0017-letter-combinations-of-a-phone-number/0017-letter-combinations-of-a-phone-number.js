92f51fdaba16ec8e1269d9b9e6a5d29a00bf2e46
Alexander Kolobov <a@da.am>
2023-08-30 01:45:26 +0400

    Time: 53 ms (51.07%), Space: 41.5 MB (91.47%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -2,18 +2,14 @@
  * @param {string} digits
  * @return {string[]}
  */
-const letterCombinations = function(digits) {
+const letterCombinations = (digits) => {
   const res = [];
   const dict = [,, 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
   const build = (prefix, digits) => {
-    const digit = digits % 10;
+    const str = dict[digits % 10];
     digits = Math.floor(digits / 10);
-    for (const char of dict[digit]) {
-      if (digits) {
-        build(char + prefix, digits);
-      } else {
-        res.push(char + prefix);
-      }
+    for (const char of str) {
+      digits ? build(char + prefix, digits) : res.push(char + prefix);
     }
   }
   digits && build('', digits);
