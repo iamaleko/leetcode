41d19ed9f6dd4a134f1f0fbcdbd45031c787d725
Alexander Kolobov <a@da.am>
2023-08-30 23:37:20 +0400

    Time: 115 ms (33.67%), Space: 48.7 MB (17.35%) - LeetHub

--- 1939-queries-on-number-of-points-inside-a-circle/1939-queries-on-number-of-points-inside-a-circle.js
+++ 1939-queries-on-number-of-points-inside-a-circle/1939-queries-on-number-of-points-inside-a-circle.js
@@ -5,18 +5,18 @@
  */
 const countPoints = (points, queries) => {
   const answers = [];
-  const map = new Map();
-  const getKey = (px,py,qx,qy,qr) => px+','+py+','+qx+','+qy+','+qr;
+  // const map = new Map();
+  // const getKey = (px,py,qx,qy,qr) => px+','+py+','+qx+','+qy+','+qr;
   for (const i in queries) {
     const [qx,qy,qr] = queries[i];
     answers[i] = 0;
     for (const [px,py] of points) {
-      const key = getKey(px,py,qx,qy,qr);
-      let cache = map.get(key);
-      if (cache === undefined) {
-        cache = py < qy - qr || py > qy + qr || px < qx - qr || px > qx + qr ? false : Math.sqrt(Math.pow(qx - px, 2) + Math.pow(qy - py, 2)) <= qr;
-        map.set(key, cache);
-      }
+      // const key = getKey(px,py,qx,qy,qr);
+      // let cache = map.get(key);
+      // if (cache === undefined) {
+        const cache = py < qy - qr || py > qy + qr || px < qx - qr || px > qx + qr ? false : Math.sqrt(Math.pow(qx - px, 2) + Math.pow(qy - py, 2)) <= qr;
+        // map.set(key, cache);
+      // }
       if (cache) ++answers[i]
     }
   }
