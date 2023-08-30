9c306a7dc9ebddbb32d6666f0c39c9580e4df451
Alexander Kolobov <a@da.am>
2023-08-30 23:31:39 +0400

    Time: 2114 ms (5.05%), Space: 169.6 MB (5.05%) - LeetHub

---
+++ 1939-queries-on-number-of-points-inside-a-circle/1939-queries-on-number-of-points-inside-a-circle.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} points
+ * @param {number[][]} queries
+ * @return {number[]}
+ */
+const countPoints = (points, queries) => {
+  const answers = [];
+  const map = new Map();
+  const getKey = (px,py,qx,qy,qr) => px+','+py+','+qx+','+qy+','+qr;
+  for (const i in queries) {
+    const [qx,qy,qr] = queries[i];
+    answers[i] = 0;
+    for (const [px,py] of points) {
+      const key = getKey(px,py,qx,qy,qr);
+      let cache = map.get(key);
+      if (cache === undefined) {
+        cache = Math.sqrt(Math.pow(qx - px, 2) + Math.pow(qy - py, 2)) <= qr;
+        map.set(key, cache);
+      }
+      if (cache) ++answers[i]
+    }
+  }
+  return answers;
+};
