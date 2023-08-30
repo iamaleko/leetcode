7b7c674c33234b047ce7168f7ded077b0333166e
Alexander Kolobov <a@da.am>
2023-08-31 00:38:38 +0400

    Time: 120 ms (24.49%), Space: 47.6 MB (26.53%) - LeetHub

--- 1939-queries-on-number-of-points-inside-a-circle/1939-queries-on-number-of-points-inside-a-circle.js
+++ 1939-queries-on-number-of-points-inside-a-circle/1939-queries-on-number-of-points-inside-a-circle.js
@@ -5,20 +5,11 @@
  */
 const countPoints = (points, queries) => {
   const answers = [];
-  const map = new Map();
-  const cantor = (a,b) => (a + b) * (a + b + 1) / 2 + b;
-  const getKey = (px,py,qx,qy,qr) => cantor(cantor(cantor(px,py), cantor(qx, qy)), qr);
   for (const i in queries) {
     const [qx,qy,qr] = queries[i];
     answers[i] = 0;
     for (const [px,py] of points) {
-      const key = getKey(px,py,qx,qy,qr);
-      let cache = map.get(key);
-      if (cache === undefined) {
-        cache = py < qy - qr || py > qy + qr || px < qx - qr || px > qx + qr ? false : Math.sqrt(Math.pow(qx - px, 2) + Math.pow(qy - py, 2)) <= qr;
-        map.set(key, cache);
-      }
-      if (cache) ++answers[i]
+      if (Math.sqrt(Math.pow(qx - px, 2) + Math.pow(qy - py, 2)) <= qr) ++answers[i];
     }
   }
   return answers;
