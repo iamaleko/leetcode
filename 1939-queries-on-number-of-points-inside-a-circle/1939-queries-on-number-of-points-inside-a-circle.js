13e2a4db8a5f7b1b25ea02eec5541e7fa41988fe
Alexander Kolobov <a@da.am>
2023-08-31 00:40:19 +0400

    Time: 112 ms (41.84%), Space: 47.2 MB (33.67%) - LeetHub

--- 1939-queries-on-number-of-points-inside-a-circle/1939-queries-on-number-of-points-inside-a-circle.js
+++ 1939-queries-on-number-of-points-inside-a-circle/1939-queries-on-number-of-points-inside-a-circle.js
@@ -9,7 +9,7 @@ const countPoints = (points, queries) => {
     const [qx,qy,qr] = queries[i];
     answers[i] = 0;
     for (const [px,py] of points) {
-      if (Math.sqrt(Math.pow(qx - px, 2) + Math.pow(qy - py, 2)) <= qr) ++answers[i];
+      if (Math.pow(qx - px, 2) + Math.pow(qy - py, 2) <= qr*qr) ++answers[i];
     }
   }
   return answers;
