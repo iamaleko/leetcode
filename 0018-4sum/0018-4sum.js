251dcbfafb6e448a8b8cf1d25d1722c8176cbd24
Alexander Kolobov <a@da.am>
2023-09-03 20:22:46 +0400

    Time: 4269 ms (5.05%), Space: 48 MB (26.17%) - LeetHub

---
+++ 0018-4sum/0018-4sum.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+const fourSum = function(nums, target) {
+  nums.sort((a,b) => a - b);
+  const tree = {};
+  const res = [];
+  all: for (let a = 0; a < nums.length - 3; a += 1) {
+    for (let b = a + 1; b < nums.length - 2; ++b) {
+      for (let c = b + 1; c < nums.length - 1; ++c) {
+        for (let d = c + 1; d < nums.length; ++d) {
+          if (nums[a] + nums[b] + nums[c] + nums[d] > target) {
+            // if (d - a === 3) break all;
+            break;
+          }
+          if (nums[a] + nums[b] + nums[c] + nums[d] === target) {
+            if (!tree[nums[a]]) {
+              tree[nums[a]] = {};
+            }
+            if (!tree[nums[a]][nums[b]]) {
+              tree[nums[a]][nums[b]] = {};
+            }
+            if (!tree[nums[a]][nums[b]][nums[c]]) {
+              tree[nums[a]][nums[b]][nums[c]] = {};
+            }
+            if (!tree[nums[a]][nums[b]][nums[c]][nums[d]]) {
+              tree[nums[a]][nums[b]][nums[c]][nums[d]] = true;
+              res.push([nums[a], nums[b], nums[c], nums[d]]);
+            }
+          }
+        }   
+      }
+    } 
+  }
+  return res;
+};
