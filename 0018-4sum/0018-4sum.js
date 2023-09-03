63b9fb945a1bb0b28116a6a3243bfcd30e6298e0
Alexander Kolobov <a@da.am>
2023-09-03 20:31:14 +0400

    Time: 4512 ms (5.05%), Space: 47.4 MB (27.4%) - LeetHub

--- 0018-4sum/0018-4sum.js
+++ 0018-4sum/0018-4sum.js
@@ -7,11 +7,10 @@ const fourSum = function(nums, target) {
   nums.sort((a,b) => a - b);
   const tree = {};
   const res = [];
-  for (let a = 0; a < nums.length - 3; a += 1) {
+  for (let a = 0; a < nums.length - 3; ++a) {
     for (let b = a + 1; b < nums.length - 2; ++b) {
       for (let c = b + 1; c < nums.length - 1; ++c) {
         for (let d = c + 1; d < nums.length; ++d) {
-          if (nums[a] + nums[b] + nums[c] + nums[d] > target) break;
           if (nums[a] + nums[b] + nums[c] + nums[d] === target) {
             if (!tree[nums[a]]) {
               tree[nums[a]] = {};
