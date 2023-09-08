f97ab0192f5a0b81432ba786e2fdb5fa7211008b
Alexander Kolobov <a@da.am>
2023-09-08 12:48:26 +0400

    Time: 69 ms (5.13%), Space: 41.4 MB (92.19%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.js
+++ 0290-word-pattern/0290-word-pattern.js
@@ -4,17 +4,17 @@
  * @return {boolean}
  */
 const wordPattern = (pattern, s) => {
+  s = s.split` `;
+  if (pattern.length !== s.length) return false;
   const r = new Map(), l = new Map();
-  s = s.split` `.map((word, i) => {
+  for (const i in s) {
+    const word = s[i];
     if (!r.has(word) && !l.has(pattern[i])) {
       r.set(word, pattern[i]);
       l.set(pattern[i], word);
-      return pattern[i];
-    } else if (r.get(word) === pattern[i] && l.get(pattern[i]) === word) {
-      return pattern[i];
-    } else {
-      return '.';
+    } else if (r.get(word) !== pattern[i] || l.get(pattern[i]) !== word) {
+      return false;
     }
-  }).join``;
-  return s === pattern;
+  };
+  return true;
 };
