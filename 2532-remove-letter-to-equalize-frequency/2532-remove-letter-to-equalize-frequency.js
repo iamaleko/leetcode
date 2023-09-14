deaa48bc12b6b632104a6e1fafe31344eac515e1
Alexander Kolobov <a@da.am>
2023-09-14 23:17:21 +0400

    Time: 76 ms (5.1%), Space: 42.2 MB (48.47%) - LeetHub

---
+++ 2532-remove-letter-to-equalize-frequency/2532-remove-letter-to-equalize-frequency.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+const equalFrequency = function(word) {
+  const map = {};
+  for (let i = 0; i < word.length; ++i) {
+    if (word[i] in map) {
+      ++map[word[i]];
+    } else {
+      map[word[i]] = 1;
+    }
+  }
+  const freq = {};
+  for (char in map) {
+    if (freq[map[char]]) {
+      ++freq[map[char]]
+    } else {
+      freq[map[char]] = 1;
+    }
+  }
+  const entries = Object.entries(freq);
+
+  console.log(entries);
+
+  if (
+    entries.length === 1 &&
+    (entries[0][0] === '1' || entries[0][1] === 1)
+  ) return true;
+  if (
+    entries.length === 2 && (
+      (entries[0][0] - entries[1][0] === 1 && entries[0][1] === 1) ||
+      (entries[1][0] - entries[0][0] === 1 && entries[1][1] === 1)
+    )
+  ) return true;
+  if (
+    entries.length === 2 && 
+    (entries[0][0] === '1' && entries[0][1] === 1 || entries[1][0] === '1' && entries[1][1] === 1)
+  ) return true;
+
+  return false;
+};
