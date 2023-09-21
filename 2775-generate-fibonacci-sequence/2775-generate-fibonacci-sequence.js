49492d0f9d95e9cd0989f4f561f8c0f3efa50c71
Alexander Kolobov <a@da.am>
2023-09-21 18:57:28 +0400

    Time: 65 ms (5.94%), Space: 42 MB (50.24%) - LeetHub

--- 2775-generate-fibonacci-sequence/2775-generate-fibonacci-sequence.js
+++ 2775-generate-fibonacci-sequence/2775-generate-fibonacci-sequence.js
@@ -2,11 +2,9 @@
  * @return {Generator<number>}
  */
 const fibGenerator = function*() {
-  let a = undefined, b = undefined;
-  if (a === undefined) yield a = 0;
-  if (b === undefined) yield b = 1;
+  let a = 0, b = 1;
   while(true) {
-    yield a + b;
+    yield a;
     [a,b] = [b, a + b];
   }
 };
