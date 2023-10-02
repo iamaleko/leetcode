4bb2fddf996b97cba66c4c804ab95bb8bd046884
Alexander Kolobov <a@da.am>
2023-10-02 21:29:12 +0200

    Time: 90 ms (8.21%), Space: 48.3 MB (6.12%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const longestValidParentheses = (s) => {
+  const stack = [], index = [], intervals = [];
+  for (let i = 0; i < s.length; ++i) {
+    const b = s[i];
+    if (b === '(') {
+      stack.push(b);
+      index.push(i);
+    } else if (b === ')' && stack.at(-1) === '(') {
+      stack.pop();
+      const at = index.pop();
+      while (intervals.length && intervals.at(-1)[0] > at) {
+        intervals.pop();
+      }
+      if (intervals.length && intervals.at(-1)[1] === at - 1) {
+        intervals.at(-1)[1] = i;
+      } else {
+        intervals.push([at, i]);
+      }
+    } else {
+      stack.slice(0, i);
+      index.slice(0, i);
+    }
+  }
+
+  console.log(intervals);
+  let max = 0;
+  for ([from, to] of intervals) {
+    if (max < to - from + 1) max = to - from + 1;
+  }
+  return max;
+};
