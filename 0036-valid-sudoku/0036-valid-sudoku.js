f106bf747b33afcd2c888f68dac819f724442aac
Alexander Kolobov <a@da.am>
2023-10-05 20:30:45 +0200

    Time: 66 ms (62.59%), Space: 44.5 MB (81.95%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.js
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -6,17 +6,17 @@ const isValidSudoku = function(board) {
   const set = new Set();
   for (const row in board) {
     for (const col in board[row]) {
-      // const val = board[row][col];
-      if (board[row][col] !== '.') {
-        const box = (row / 3 | 0) * 3 + (col / 3 | 0) + 1;
+      const val = board[row][col];
+      if (val !== '.') {
+        const box = (row / 3 | 0) * 3 + (col / 3 | 0) + 1 << 12 | val;
         if (
-          set.has(row + 1 << 4 | board[row][col]) ||
-          set.has(col + 1 << 8 | board[row][col]) ||
-          set.has(box << 12 | board[row][col])
+          set.has(row + 1 << 4 | val) ||
+          set.has(col + 1 << 8 | val) ||
+          set.has(box)
         ) return false;
-        set.add(row + 1 << 4 | board[row][col]);
-        set.add(col + 1 << 8 | board[row][col]);
-        set.add(box << 12 | board[row][col]);
+        set.add(row + 1 << 4 | val);
+        set.add(col + 1 << 8 | val);
+        set.add(box);
       }
     } 
   } 
