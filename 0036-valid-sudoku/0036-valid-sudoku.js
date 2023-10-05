79d5327ca52067925502f706fb990434c91382d2
Alexander Kolobov <a@da.am>
2023-10-05 20:29:10 +0200

    Time: 66 ms (62.59%), Space: 44.9 MB (65.68%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.js
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -8,15 +8,15 @@ const isValidSudoku = function(board) {
     for (const col in board[row]) {
       const val = board[row][col];
       if (val !== '.') {
-        // const box = (row / 3 | 0) * 3 + (col / 3 | 0) + 1;
+        const box = (row / 3 | 0) * 3 + (col / 3 | 0) + 1;
         if (
           set.has(row + 1 << 4 | val) ||
           set.has(col + 1 << 8 | val) ||
-          set.has((row / 3 | 0) * 3 + (col / 3 | 0) + 1 << 12 | val)
+          set.has(box << 12 | val)
         ) return false;
         set.add(row + 1 << 4 | val);
         set.add(col + 1 << 8 | val);
-        set.add((row / 3 | 0) * 3 + (col / 3 | 0) + 1 << 12 | val);
+        set.add(box << 12 | val);
       }
     } 
   } 
