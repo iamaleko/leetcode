e99e5d7feab115cde07ab0339a87d17a54a96d83
Alexander Kolobov <a@da.am>
2023-10-06 22:13:10 +0200

    Time: 227 ms (6.74%), Space: 92.6 MB (10.36%) - LeetHub

---
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} edges
+ * @return {number}
+ */
+const findCenter = (edges) => {
+  const map = {};
+  let max = 0, out = 0;
+  for (const edge of edges) {
+    for (const val of edge) {
+      map[val] = map[val] === undefined ? 1 : map[val] + 1;
+      if (map[val] > max) {
+        max = map[val];
+        out = val;
+      } 
+    }
+  }
+  console.log(map, max, out);
+  return out;
+};
