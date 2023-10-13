b54b57ddfcdc44cd20b042f4624a469cec1231ff
Alexander Kolobov <a@da.am>
2023-10-13 19:26:36 +0200

    Time: 51 ms (80.24%), Space: 44.1 MB (76.64%) - LeetHub

--- 2798-chunk-array/2798-chunk-array.js
+++ 2798-chunk-array/2798-chunk-array.js
@@ -5,8 +5,8 @@
  */
 const chunk = (arr, size) => {
   const res = [];
-  for (let i = 0; i < arr.length; ++i) {
-    if ((i + 1) % size === 0) res.push(arr.slice(i - size + 1, i + 1))
+  for (let i = 1; i <= arr.length; ++i) {
+    if (i % size === 0) res.push(arr.slice(i - size, i))
   }
   if (arr.length % size !== 0) res.push(arr.slice(arr.length - arr.length % size));
   return res;
