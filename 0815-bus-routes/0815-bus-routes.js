351335918e01d26f57177c9d36afb8c599843f39
Alexander Kolobov <a@da.am>
2023-11-16 20:28:08 +0100

    Time: 1260 ms (26.64%), Space: 108 MB (38.13%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.js
+++ 0815-bus-routes/0815-bus-routes.js
@@ -8,34 +8,6 @@ var numBusesToDestination = function(routes, source, target) {
   if (source === target) return 0;
   
   const graph = new Map();
-  const find = (from, to) => {
-    const queue = [];
-    const checked = new Map();
-    
-    for (const joint of graph.get(from)) {
-      queue.push(joint);
-      checked.set(joint, 1);
-    }
-    
-    while (queue.length) {
-      const joint = queue.shift();
-      const val = checked.get(joint);
-      
-      for (const stop of joint) {
-        if (stop === to) return val;
-        
-        for (const _joint of graph.get(stop)) {
-          const _val = checked.get(_joint);
-          if (!_val || _val > val + 1) {
-            queue.push(_joint);
-            checked.set(_joint, val + 1);
-          }
-        }
-      }
-    }
-    
-    return -1;
-  }
   
   // build graph
   for (const route of routes) {
@@ -47,7 +19,28 @@ var numBusesToDestination = function(routes, source, target) {
     }
   }
   
-  // console.log(graph)
+  const queue = [];
+  const val = Symbol();
+
+  for (const joint of graph.get(source)) {
+    queue.push(joint);
+    joint[val] = 1;
+  }
+
+  while (queue.length) {
+    const joint = queue.shift();
+
+    for (const stop of joint) {
+      if (stop === target) return joint[val];
+
+      for (const _joint of graph.get(stop)) {
+        if (_joint[val] === undefined || _joint[val] > joint[val] + 1) {
+          queue.push(_joint);
+          _joint[val] = joint[val] + 1;
+        }
+      }
+    }
+  }  
   
-  return find(source, target);
+  return -1;
 };
