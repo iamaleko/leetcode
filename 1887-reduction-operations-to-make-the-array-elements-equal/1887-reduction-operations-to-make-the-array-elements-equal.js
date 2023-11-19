6c0b7bb50a37301c980b4b3022c3342e502b1dcf
Alexander Kolobov <a@da.am>
2023-11-19 22:18:02 +0100

    Time: 200 ms (58.33%), Space: 54.6 MB (58.33%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.js
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.js
@@ -1,35 +1,24 @@
 const reductionOperations = (nums) => {
   nums.sort((a,b) => b - a);
   
-  let ops = 0,
-      l = 0,
-      r = nums.length - 1;
+  let ops = 0, l = 0, r = nums.length - 1;
   
   if (nums[l] === nums[r]) return 0;
   
-  if (nums.length < 50) {
-    for (let i = 1; i < nums.length; ++i) {
-      if (nums[i] !== nums[i - 1]) {
-        ops += i;
+  let c, t = nums[l];
+  while (t !== nums[r]) {
+    while (l < r) {
+      c = (l + r) / 2 | 0;
+      if (nums[c] === t) {
+        l = c + 1;
+      } else {
+        r = c;
       }
     }
-  } else {
-    let c,
-        t = nums[l];
-    while (t !== nums[r]) {
-      while (l < r) {
-        c = (l + r) / 2 | 0;
-        if (nums[c] === t) {
-          l = c + 1;
-        } else {
-          r = c;
-        }
-      }
-      ops += l;
-      t = nums[l];
-      r = nums.length - 1;
-    } 
-  }
+    ops += l;
+    t = nums[l];
+    r = nums.length - 1;
+  } 
   
   return ops;
 };
