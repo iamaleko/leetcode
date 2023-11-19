b094cbdb9f3a16b65d91a04f54086407b2df1bf6
Alexander Kolobov <a@da.am>
2023-11-19 22:07:26 +0100

    Time: 191 ms (79.17%), Space: 54.8 MB (58.33%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.js
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.js
@@ -1,11 +1,10 @@
 const reductionOperations = (nums) => {
-  // if all elements already the same
-  // if (new Set(nums).size === 1) return 0;
-  
-  // if elements differ
   nums.sort((a,b) => b - a);
   
   let ops = 0, l = 0, r = nums.length - 1, a, b, c;
+  
+  if (nums[l] === nums[r]) return 0;
+  
   while (nums[l] !== nums[r]) {
     a = l;
     b = r;
