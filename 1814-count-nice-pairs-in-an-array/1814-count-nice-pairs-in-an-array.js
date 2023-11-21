bf835dfe76ae9715e7eba6d6b21da20fe49e7c58
Alexander Kolobov <a@da.am>
2023-11-21 19:58:01 +0100

    Time: 81 ms (100.00%), Space: 51.1 MB (96.00%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.js
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.js
@@ -5,16 +5,17 @@
 const countNicePairs = (nums) => {
   const map = new Map();
   
-  let pairs = 0;
-  for (let num of nums) {
-    let diff = num, rev = 0;
+  let pairs = 0, diff, rev, num, count;
+  for (num of nums) {
+    diff = num;
+    rev = 0;
     while (num) {
       rev *= 10;
       rev += num % 10;
       num = num / 10 | 0;
     }
     diff -= rev;
-    let count = map.get(diff) | 0;
+    count = map.get(diff) | 0;
     pairs += count;
     map.set(diff, count + 1);
   }
