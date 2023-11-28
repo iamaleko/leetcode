7d463bb43e2778c5a5d14104f63b358821adc9c5
Alexander Kolobov <a@da.am>
2023-11-28 20:06:06 +0100

    Time: 74 ms (100.00%), Space: 52.4 MB (75.00%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.js
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.js
@@ -3,29 +3,26 @@
  * @return {number}
  */
 const numberOfWays = function(corridor) {
-  let seats_pairs = 0;
-  let vars = 1;
+  let i = corridor.indexOf('S');
+  if (i < 0) return 0;
   
-  let seats = 0, plants = 0;
-  for (const i in corridor) {
-    const item = corridor[i];
-    if (item === 'S') {
+  let vars = 1, seats = 0, plants = 0;
+  for (;i < corridor.length; ++i) {
+    if (corridor[i] === 'S') {
       ++seats;
       if (seats === 2) {
         if (plants) {
           vars = (vars * (plants+1)) % (1e9+7)
           plants = 0;
         }
-        ++seats_pairs;
         seats = 0;
       }
     } else {
-      if (seats_pairs && seats === 0) {
+      if (seats === 0) {
         ++plants;
       }
     }
   }
   
-  if (seats > 0 || seats_pairs === 0) return 0;
-  return vars;
+  return seats > 0 ? 0 : vars;
 };
