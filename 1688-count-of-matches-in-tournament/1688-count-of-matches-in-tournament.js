bbe27a22b72be3581764a42c6cbcb0ea2eb2cfa9
Alexander Kolobov <a@da.am>
2023-12-05 22:27:40 +0100

    Time: 53 ms (44.96%), Space: 42 MB (22.56%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
@@ -2,11 +2,4 @@
  * @param {number} n
  * @return {number}
  */
-const numberOfMatches = (n) => {
-  let cnt = 0;
-  while (n > 1) {
-    cnt += Math.floor(n / 2);
-    n = Math.ceil(n / 2);
-  }
-  return cnt;
-};
+const numberOfMatches = (n) => n - 1;
