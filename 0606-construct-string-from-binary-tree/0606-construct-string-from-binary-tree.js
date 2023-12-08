9e7d0c8a202f9d4a08a4c773c86815f996b11c91
Alexander Kolobov <a@da.am>
2023-12-08 19:25:01 +0100

    Time: 76 ms (7.24%), Space: 46.6 MB (83.55%) - LeetHub

---
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.js
@@ -0,0 +1,6 @@
+const tree2str = (node) => {
+  if (node.left && node.right) return `${node.val}(${tree2str(node.left)})(${tree2str(node.right)})`;
+  if (node.left) return `${node.val}(${tree2str(node.left)})`;
+  if (node.right) return `${node.val}()(${tree2str(node.right)})`;
+  return node.val + '';
+};
