f0bbcf591f76cc226b25338a263fd9d5a897e122
Alexander Kolobov <a@da.am>
2023-12-08 19:58:14 +0100

    Time: 106 ms (12.70%), Space: 52.4 MB (43.86%) - LeetHub

--- 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -1,5 +1,5 @@
 const checkIfInstanceOf = (obj, constructor) => {
-  return constructor instanceof Object && constructor instanceof Function && (
+  return constructor instanceof Function && (
      obj instanceof constructor ||
      obj?.constructor === constructor ||
      obj?.constructor instanceof constructor
