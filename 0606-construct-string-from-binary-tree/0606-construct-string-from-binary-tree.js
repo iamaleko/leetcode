89da492fa955ef95c43ea6e384d2b2b420c16d74
Alexander Kolobov <a@da.am>
2023-12-08 19:38:54 +0100

    Time: 69 ms (28.95%), Space: 46.9 MB (65.13%) - LeetHub

--- 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.js
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.js
@@ -1,12 +1,24 @@
-const tree2str = (node) => {
+const tree2str = (node, b = '', a = '') => {
   if (node.left && node.right) {
-    return node.val + '(' + tree2str(node.left) + ')(' + tree2str(node.right) + ')';
+    return tree2str(node.left, b + node.val + '(', tree2str(node.right, ')(', ')' + a));
   }
   if (node.left) {
-    return node.val + '(' + tree2str(node.left) + ')';
+    return tree2str(node.left, b + node.val + '(', ')' + a);
   }
   if (node.right) {
-    return node.val + '()(' + tree2str(node.right) + ')';
+    return tree2str(node.right, b + node.val + '()(', ')' + a);
   }
-  return node.val + '';
+  return b + node.val + a;
+  
+  
+  // if (node.left && node.right) {
+  //   return node.val + '(' + tree2str(node.left) + ')(' + tree2str(node.right) + ')';
+  // }
+  // if (node.left) {
+  //   return node.val + '(' + tree2str(node.left) + ')';
+  // }
+  // if (node.right) {
+  //   return node.val + '()(' + tree2str(node.right) + ')';
+  // }
+  // return node.val + '';
 };
