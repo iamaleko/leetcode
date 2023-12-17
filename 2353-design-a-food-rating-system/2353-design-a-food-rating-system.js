749198ec86474b78db076823c5c9e125613dfa1f
Alexander Kolobov <a@da.am>
2023-12-17 21:08:35 +0100

    Time: 485 ms (63.64%), Space: 110 MB (36.36%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.js
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.js
@@ -1,60 +1,60 @@
 class Heap {
-    constructor(data, comparator) {
-        this.comparator = (a, b) => {
-          return comparator(this.heap[a], this.heap[b]);
-        };
-        this.map = new Map();
-        data.sort((a,b) => comparator(a, b));
-        this.heap = data;
-        for (const i in this.heap) {
-            this.map.set(this.heap[i], i);
+  constructor(data, comparator) {
+    this.comparator = (a, b) => {
+      return comparator(this.heap[a], this.heap[b]);
+    };
+    this.map = new Map();
+    data.sort((a,b) => comparator(a, b));
+    this.heap = data;
+    for (const i in this.heap) {
+        this.map.set(this.heap[i], i);
+    }
+  }
+  update(el) {
+    const i = this.map.get(el);
+    this.repair(i);
+  }
+  repair(i) {
+      const pi = this.parent(i);
+      if (i && this.comparator(i, pi) < 0) {
+        this.swap(i, pi);
+        this.repair(pi);
+      } else {
+        const ri = this.right(i);
+        const li = this.left(i);
+        const si = ri in this.heap && li in this.heap && this.comparator(li, ri) >= 0 ? ri : li;
+        if (si in this.heap && this.comparator(i, si) > 0) {
+          this.swap(i, si);
+          this.repair(si);
         }
-    }
-    update(el) {
-      const i = this.map.get(el);
-      this.repair(i);
-    }
-    repair(i) {
-        const pi = this.parent(i);
-        if (i && this.comparator(i, pi) < 0) {
-          this.swap(i, pi);
-          this.repair(pi);
-        } else {
-          const ri = this.right(i);
-          const li = this.left(i);
-          const si = ri in this.heap && li in this.heap && this.comparator(li, ri) >= 0 ? ri : li;
-          if (si in this.heap && this.comparator(i, si) > 0) {
-            this.swap(i, si);
-            this.repair(si);
-          }
-        }
-    }
-    parent(i) {
-        return i>>>1;
-    }
-    left(i) {
-        return i * 2;
-    }
-    right(i) {
-        return i * 2 + 1;
-    }
-    swap(a,b) {
-        this.map.set(this.heap[a], b);
-        this.map.set(this.heap[b], a);
-        const tmp = this.heap[a];
-        this.heap[a] = this.heap[b];
-        this.heap[b] = tmp;
-    }
-    peek() {
-        return this.heap[0];
-    }
+      }
+  }
+  parent(i) {
+    return i>>>1;
+  }
+  left(i) {
+    return i * 2;
+  }
+  right(i) {
+    return i * 2 + 1;
+  }
+  swap(a,b) {
+    this.map.set(this.heap[a], b);
+    this.map.set(this.heap[b], a);
+    const tmp = this.heap[a];
+    this.heap[a] = this.heap[b];
+    this.heap[b] = tmp;
+  }
+  peek() {
+    return this.heap[0];
+  }
 }
 
 class FoodRatings {
+  // O(n log n)
   constructor(foods, cuisines, ratings) {
     this.cm = new Map();
     this.fm = new Map();
-    const comparator = (a,b) => b[1] === a[1] ? (b === a ? 0 : b < a ? 1 : -1) : b[1] - a[1];
     
     for (const i in foods) {
       const item = [foods[i], ratings[i], cuisines[i]];
@@ -66,17 +66,20 @@ class FoodRatings {
       }
     }
     
+    const comparator = (a,b) => b[1] === a[1] ? (b === a ? 0 : b < a ? 1 : -1) : b[1] - a[1];
     for (const [key, val] of this.cm) {
       this.cm.set(key, new Heap(val, comparator));
     }
   }
   
+  // O(log n)
   changeRating(food, rating) {
     const item = this.fm.get(food);
     item[1] = rating;
     this.cm.get(item[2]).update(item);
   }
   
+  // O(1)
   highestRated(cuisine) {
     return this.cm.get(cuisine).peek()[0];
   }
