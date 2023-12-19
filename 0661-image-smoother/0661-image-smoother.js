b92aef7472c837e955ef4ccd280254156cd4af56
Alexander Kolobov <a@da.am>
2023-12-19 18:37:25 +0100

    Time: 105 ms (62.96%), Space: 50.8 MB (11.11%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.js
+++ 0661-image-smoother/0661-image-smoother.js
@@ -1,8 +1,43 @@
 const imageSmoother = (img) => {
-  const res = [], w = img[0].length - 1, h = img.length - 1;
+  const w = img[0].length - 1, h = img.length - 1;
+  if (w === 0 && h === 0) return img;
+  
+  const res = Array.from({length: h + 1}, () => Array.from({length: w + 1}));
+  
+  if (w > 1 && h > 1) {
+    let y, x;
+    
+    // corners
+    res[0][0] = (img[0][0] + img[0][1] + img[1][1] + img[1][0]) / 4 | 0;
+    res[h][0] = (img[h][0] + img[h][1] + img[h-1][1] + img[h-1][0]) / 4 | 0;
+    res[0][w] = (img[0][w] + img[0][w-1] + img[1][w-1] + img[1][w]) / 4 | 0;
+    res[h][w] = (img[h][w] + img[h][w-1] + img[h-1][w-1] + img[h-1][w]) / 4 | 0;
+
+    // edges
+    for (x = 1; x < w; ++x) {
+      res[0][x] = (img[0][x] + img[0][x-1] + img[0][x+1] + img[1][x-1] + img[1][x+1] + img[1][x]) / 6 | 0;
+      res[h][x] = (img[h][x] + img[h][x-1] + img[h][x+1] + img[h-1][x-1] + img[h-1][x+1] + img[h-1][x]) / 6 | 0;
+    }
+    for (y = 1; y < h; ++y) {
+      res[y][0] = (img[y][0] + img[y-1][0] + img[y+1][0] + img[y-1][1] + img[y+1][1] + img[y][1]) / 6 | 0;
+      res[y][w] = (img[y][w] + img[y-1][w] + img[y+1][w] + img[y-1][w-1] + img[y+1][w-1] + img[y][w-1]) / 6 | 0;
+    }
+
+    // center 
+    for (y = 1; y < h; ++y) {
+      for (x = 1; x < w; ++x) {
+        res[y][x] = (
+          img[y][x] + img[y][x-1] + img[y][x+1] +
+          img[y-1][x] + img[y-1][x-1] + img[y-1][x+1] +
+          img[y+1][x] + img[y+1][x-1] + img[y+1][x+1]
+        ) / 9 | 0;
+      }
+    }
+    
+    return res;
+  }
   
   for (let y = 0; y < h + 1; ++y) {
-    res[y] = [];
     for (let x = 0; x < w + 1; ++x) {
       let sum = img[y][x], cnt = 1;
       
