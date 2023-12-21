ef262f1da0ddc11197f0bdea1a992d5c0ccb0c41
Alexander Kolobov <a@da.am>
2023-12-21 01:40:45 +0100

    Time: 71 ms (52.39%), Space: 49.7 MB (41.67%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -3,25 +3,27 @@
  * @return {string}
  */
 const reverseVowels = (s) => {
-    const vovels = new Set(['a','A','e','E','i','I','o','O','u','U']), res = new Array(s.length);
-    let l = 0, r = s.length - 1, li, ri;
+    const vovels = new Set(['a','A','e','E','i','I','o','O','u','U']);
+    let a = '', b = '', l = 0, r = s.length - 1, li, ri;
     while (l <= r || li && ri) {
         if (li) {
             if (ri) {
-                res[l++] = ri;
-                res[r--] = li;
+                a += ri;
+                if (l !== r) b = li + b;
                 li = undefined;
                 ri = undefined;
+                ++l;
+                --r;
             } else if (vovels.has(s[r])) {
                 ri = s[r];
             } else {
-                res[r] = s[r--];
+                b = s[r--] + b;
             }
         } else if (vovels.has(s[l])) {
             li = s[l];
         } else {
-            res[l] = s[l++];
+            a += s[l++];
         }
     }
-    return res.join('');
+    return a + b;
 };
