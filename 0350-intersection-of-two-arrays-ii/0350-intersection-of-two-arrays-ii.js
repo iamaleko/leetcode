8f9fcb515ba5dac31e436c7a37a17785e0477a6e
Alexander Kolobov <a@da.am>
2023-12-22 19:14:43 +0100

    Time: 57 ms (54.52%), Space: 44.6 MB (24.68%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -4,9 +4,8 @@
  * @return {number[]}
  */
 const intersect = (a, b) => {
-    let n;
-    const m = {}, r = [];
+    let n, m = {}, r = [];
     for (n of b) n in m ? ++m[n] : m[n] = 1;
-    for (n of a) if (n in m && --m[n] > -1) r.push(n);
+    for (n of a) n in m && --m[n] > -1 && r.push(n);
     return r;
 };
