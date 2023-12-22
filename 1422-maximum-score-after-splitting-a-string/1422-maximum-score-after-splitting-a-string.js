1f7198430cbdbd5f38a5f95f2440633fb15a38cc
Alexander Kolobov <a@da.am>
2023-12-22 18:06:19 +0100

    Time: 51 ms (89.09%), Space: 42.4 MB (60.00%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.js
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.js
@@ -3,12 +3,10 @@
  * @return {number}
  */
 const maxScore = (s) => {
-    const arr = new Array(s.length + 1).fill(0);
-    let m = 0;
-    for (let a = 0, b = 0, i = 0, j = s.length; i < s.length; ++i, --j) {
-        // if (m < (arr[i] += s[i] === '0' ? a++ : a)) m = arr[i];
-        // if (m < (arr[j] += s[j - 1] === '1' ? b++ : b)) m = arr[j];
-        m = Math.max(m, arr[i] += s[i] === '0' ? a++ : a, arr[j] += s[j - 1] === '1' ? b++ : b)
+    const a = new Array(s.length + 1).fill(0);
+    let m = 0, l = 0, r = 0, i = 0, j = s.length;
+    for (; i < s.length; ++i, --j) {
+        m = Math.max(m, a[i] += s[i] === '0' ? l++ : l, a[j] += s[j - 1] === '1' ? r++ : r)
     }
     return m;
 };
