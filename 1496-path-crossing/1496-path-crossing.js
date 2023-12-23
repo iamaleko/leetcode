78c0fc238c7d4e116915ca6ea7d29e8773b57560
Alexander Kolobov <a@da.am>
2023-12-23 20:51:34 +0100

    Time: 51 ms (62.69%), Space: 42.1 MB (64.18%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.js
+++ 1496-path-crossing/1496-path-crossing.js
@@ -4,8 +4,8 @@
  */
 const isPathCrossing = (path) => {
     let x = 10000, y = 10000;
-    const map = new Map();
-    map.set((x << 15) + y, null);
+    const map = {};
+    map[(x << 15) + y] = null;
     
     for (const d of path) {
         switch (d) {
@@ -15,8 +15,8 @@ const isPathCrossing = (path) => {
             case 'E': ++x; break;
         }
         
-        if (map.has((x << 15) + y)) return true;
-        map.set((x << 15) + y, null);
+        if ((x << 15) + y in map) return true;
+        map[(x << 15) + y] = null;
     }
     
     return false;
