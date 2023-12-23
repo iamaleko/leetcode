6be815def4ad40ff607b4a01f949864b3276dbbb
Alexander Kolobov <a@da.am>
2023-12-23 21:17:53 +0100

    Time: 94 ms (47.32%), Space: 48.8 MB (73.21%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -2,19 +2,21 @@
  * @param {number[][]} intervals
  * @return {number[][]}
  */
-const merge = (intervals) => {
-    intervals.sort((a,b) => a[0] - b[0]);
-    const res = [];
-    let p = -1;
-    for (const int of intervals) {
-        if (p > -1 && res[p][1] >= int[0]) {
-            if (res[p][1] < int[1]) {
-                res[p][1] = int[1];
-            }
+const merge = (int) => {
+    if (int.length <= 1) return int;
+    
+    int.sort((a, b) => a[0] - b[0]);
+    
+    let p = 0;
+    
+    for (let i = 1; i < int.length; ++i) {
+        if (int[p][1] >= int[i][0]) {
+            if (int[p][1] < int[i][1]) int[p][1] = int[i][1];
+            int[i] = undefined;
         } else {
-            res.push(int);
-            ++p;
+            p = i;
         }
     }
-    return res;
+    
+    return int.filter(e => e);
 };
