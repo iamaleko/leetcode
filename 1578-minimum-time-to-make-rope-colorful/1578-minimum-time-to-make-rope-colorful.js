470f8aff4a0ebedb6ae662f19d290a9e16d2179b
Alexander Kolobov <a@da.am>
2023-12-27 16:55:15 +0100

    Time: 68 ms (97.96%), Space: 52.6 MB (69.39%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.js
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.js
@@ -1,7 +1,7 @@
 const minCost = (colors, time) => {
-    let max = 0, res = 0;
+    let res = 0;
     
-    for (let i = 1; i < colors.length; ++i) {
+    for (let max = 0, i = 1; i < colors.length; ++i) {
         if (colors[i - 1] === colors[i]) {
             if (!max) max = time[i - 1];
             if (max < time[i]) {
