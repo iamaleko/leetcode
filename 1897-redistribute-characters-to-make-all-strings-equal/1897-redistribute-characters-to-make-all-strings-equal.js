bb0e6786f685144b332fa246b72b3b1734eb8a83
Alexander Kolobov <a@da.am>
2023-12-30 14:59:29 +0100

    Time: 90 ms (11.27%), Space: 44 MB (91.55%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.js
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.js
@@ -7,7 +7,6 @@ const makeEqual = (words) => {
     words = words.join('');
     let a = 0;
     for (; a < words.length; ++a) ++ind[words.charCodeAt(a) - 97];
-    console.log(words, ind)
     for (a = 0; a < 26; ++a) if (ind[a] && ind[a] % len !== 0) return false;
     
     return true;
