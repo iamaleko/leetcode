a1628a03c6ed5ac7e76f540d560c708eb81ffb12
Alexander Kolobov <a@da.am>
2023-12-30 14:51:27 +0100

    Time: 117 ms (5.63%), Space: 43.6 MB (95.77%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.js
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.js
@@ -6,7 +6,7 @@ const makeEqual = (words) => {
     const ind = new Array(26).fill(0);
     let a = 0, b = 0;
     for (; a < words.length; ) {
-        if (words[a][b]) {
+        if (b < words[a].length) {
             ++ind[words[a][b].charCodeAt(0) - 97];
             ++b;
         } else {
