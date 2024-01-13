f0b6f5c1a4cf5da0a0f69d20ad43ed6e737c2ed6
Alexander Kolobov <a@da.am>
2024-01-13 11:26:10 +0100

    Time: 171 ms (22.09%), Space: 48.2 MB (37.21%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.js
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.js
@@ -3,7 +3,7 @@ const minSteps = (s, t) => {
     let steps = 0, n;
     
     for (n of s) n in a ? ++a[n] : a[n] = 1;
-    for (n of t) if (n in a && b[n] !== a[n]) b[n] = b[n] ? b[n] + 1 : 1;
+    for (n of t) if (n in a && b[n] !== a[n]) n in b ? ++b[n] : b[n] = 1;
     for (n in a) steps += n in b ? a[n] - b[n] : a[n];
     
     return steps;
