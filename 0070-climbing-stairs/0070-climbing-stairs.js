4b1389229c9ebc54ba290ef4ee128152774327ff
Alexander Kolobov <a@da.am>
2024-01-18 18:59:17 +0100

    Time: 50 ms (66.82%), Space: 41.5 MB (81.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -1,20 +1,7 @@
-// /**
-//  * @param {number} n
-//  * @return {number}
-//  */
-// const climbStairs = (n) => {
-//     let v = 1, p = 0;
-//     const f = n => { 
-//         let s = n;
-//         while(--n) s *= n;
-//         return s;
-//     }
-//     while (n > 1) {
-//         n = n - 2;
-//         ++p;
-//         v += n ? f(n + p) / (f(n) * f(p)) : 1
-//     }
-//     return v;
-// };
-
-climbStairs=d=>(m={},p=d=>m[d]||(m[d]=d<4?d:p(d-1)+p(d-2)))(d)
+const climbStairs = (n, a = 1, b = 2) => {
+    if (--n) {
+        while (--n) a = (b += a) - a;
+        return b;
+    }
+    return a;
+}
