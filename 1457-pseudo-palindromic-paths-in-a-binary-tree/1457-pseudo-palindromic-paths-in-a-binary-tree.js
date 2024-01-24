57bb87c0f997555575745147940757c0aecc2705
Alexander Kolobov <a@da.am>
2024-01-24 19:01:59 +0100

    Time: 297 ms (40%), Space: 90.3 MB (100%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.js
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.js
@@ -1,13 +1,12 @@
 const pseudoPalindromicPaths  = (root) => {
   let res = 0, node, i, n;
-  root.mem = [0,0,0,0,0,0,0,0,0];
+  root.mem = 0;
   const queue = [root];
   while (queue.length) {
     node = queue.pop();
-    node.mem[node.val - 1] ? --node.mem[node.val - 1] : ++node.mem[node.val - 1];
+    node.mem ^= 1 << node.val;
     if (node.left && node.right) {
-      node.left.mem = node.mem;
-      node.right.mem = node.mem.slice();
+      node.left.mem = node.right.mem = node.mem;
       queue.push(node.left);
       queue.push(node.right);
     } else if (node.left) {
@@ -16,10 +15,15 @@ const pseudoPalindromicPaths  = (root) => {
     } else if (node.right) {
       node.right.mem = node.mem;
       queue.push(node.right);
+    } else if (node.mem === 0) {
+      ++res;
     } else {
       n = 0;
-      for (i = 0; i < node.mem.length; n += node.mem[i++]);
-      if (n < 2) ++res;
+      while (node.mem) {
+        if (node.mem & 1) ++n;
+        node.mem = node.mem >> 1;
+      }
+      if (n === 1) ++res;
     }
     delete node.mem;
   }
