a6f5fab3d1e68d321f441e1a5628206d75220f4f
Alexander Kolobov <a@da.am>
2024-01-25 18:13:09 +0100

    Time: 169 ms (23.35%), Space: 84.3 MB (22.01%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -2,12 +2,8 @@ const longestCommonSubsequence = (a, b) => {
   const mem = [];
   const dp = (ap, bp) => {
     if (ap === a.length || bp === b.length) return 0;
-    if (ap < mem.length) {
-      if (bp in mem[ap]) return mem[ap][bp];
-    } else {
-      mem[ap] = [];
-    }
-
+    if (mem[ap]?.[bp] !== undefined) return mem[ap][bp];
+    if (!mem[ap]) mem[ap] = [];
     return mem[ap][bp] = a[ap] === b[bp] ? 1 + dp(ap + 1,bp + 1) : Math.max(dp(ap, bp + 1), dp(ap + 1, bp));
   }
   return dp(0,0);
