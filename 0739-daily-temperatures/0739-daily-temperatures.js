8d708c3a0a2088cabbc507040c2922e57b6b6b99
Alexander Kolobov <a@da.am>
2024-01-31 18:21:00 +0100

    Time: 220 ms (57.5%), Space: 71.5 MB (43.89%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -2,7 +2,7 @@ const dailyTemperatures = (t) => {
   const stack = [];
   let i = 0, p = -1;
   for (; i < t.length; ++i) {
-    while (t[stack[p]] < t[i]) {
+    while (p > -1 && t[stack[p]] < t[i]) {
       t[stack[p]] = i - stack[p--];
     }
     stack[++p] = i;
