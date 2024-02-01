e0e1308f35126560d5ce3d7647db165c3a1472da
Alexander Kolobov <a@da.am>
2024-02-01 19:27:13 +0100

    Time: 366 ms (19.36%), Space: 81.9 MB (91.75%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.js
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.js
@@ -1,9 +1,9 @@
 const divideArray = (nums, k) => {
   nums.sort((a,b) => a - b);
-  const res = [], l = nums.length;
+  const res = new Array(nums.length/3), l = nums.length;
   for (let i = 0; i < l; ) {
     if (nums[i + 2] - nums[i] > k) return [];
-    res.push([nums[i++], nums[i++], nums[i++]]);
+    res[i/3] = [nums[i++], nums[i++], nums[i++]];
   }
   return res;
 };
