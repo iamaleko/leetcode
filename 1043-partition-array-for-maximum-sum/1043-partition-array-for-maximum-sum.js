87c0374b075d3d9fcc336f63a87ec72a1222b244
Alexander Kolobov <a@da.am>
2024-02-03 17:50:45 +0100

    Time: 75 ms (44.44%), Space: 49.9 MB (6.94%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.js
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.js
@@ -1,14 +1,13 @@
 const maxSumAfterPartitioning = (arr, k) => {
-  const mem = [arr[0]];
+  const dp = new Array(arr.length).fill(0);
+  dp[0] = arr[0];
   for(let i = 1; i < arr.length; ++i) {
-    let n = 1, m = Number.MIN_SAFE_INTEGER;
-    mem[i] = Number.MIN_SAFE_INTEGER;
-    while (n <= Math.min(i + 1, k)) {
-      if (m < arr[i - n + 1]) m = arr[i - n + 1];
-      let val = (mem[i - n]||0) + m * n;
-      if (mem[i] < val) mem[i] = val;
-      ++n;
+    let j = 1, m = 0, h = Math.min(i + 1, k) + 1;
+    while (j < h) {
+      m = Math.max(m, arr[i - j + 1]);
+      dp[i] = Math.max(dp[i], (dp[i - j] || 0) + m * j);
+      ++j;
     }
   }
-  return mem.at(-1);
+  return dp.at(-1);
 };
