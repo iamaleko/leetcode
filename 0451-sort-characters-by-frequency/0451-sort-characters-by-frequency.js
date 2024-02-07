00ca057d82d8dcccf8e13e3150eb8b60554f4672
Alexander Kolobov <a@da.am>
2024-02-07 21:35:23 +0100

    Time: 59 ms (95.99%), Space: 60.2 MB (6.95%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.js
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.js
@@ -1,5 +1,5 @@
 const frequencySort = (s) => {
-  const arr = new Array(122).fill('');
+  const arr = new Array(123).fill('');
   for (let i = 0; i < s.length; arr[s.charCodeAt(i)] += s[i++]);
   return arr.sort((a, b) => b.length - a.length).join``;
 };
