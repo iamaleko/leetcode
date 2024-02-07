76c6af9c4c29e494ac1217593877017058c8d932
Alexander Kolobov <a@da.am>
2024-02-07 22:18:45 +0100

    Time: 107 ms (14.96%), Space: 59.2 MB (5.28%) - LeetHub

---
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.js
@@ -0,0 +1,12 @@
+const rowAndMaximumOnes = (rows) => {
+  let row = 0, ones = 0, i = -1, j, count;
+  while (++i < rows.length) {
+    count = j = 0;
+    while (j < rows[i].length) if (rows[i][j++]) ++count;
+    if (count > ones) {
+      row = i;
+      ones = count;
+    }
+  }
+  return [row, ones];
+};
