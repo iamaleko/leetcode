3110579e265d3113f8a79974f87e0bba633f7203
Alexander Kolobov <a@da.am>
2024-02-07 22:10:22 +0100

    Time: 55 ms (53.08%), Space: 49.7 MB (20.92%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.js
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.js
@@ -1,5 +1,5 @@
 const prefixCount = (words, pref) => {
   let res = 0, i = 0;
-  for (; i < words.length; words[i].startsWith(pref) && ++res, ++i);
+  for (; i < words.length; words[i++].startsWith(pref) && ++res);
   return res;
 };
