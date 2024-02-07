dfe01cb1b7d2cf6992bb7c730761ddff283483be
Alexander Kolobov <a@da.am>
2024-02-07 21:52:26 +0100

    Time: 52 ms (69.27%), Space: 48.9 MB (18.78%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.js
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.js
@@ -1,9 +1,9 @@
 const maxDepth = (s) => {
   let max = 0, level = 0, i = 0;
   for (; i < s.length; ++i) {
-    switch (s.charCodeAt(i)) {
-      case 40: if (++level > max) max = level; break;
-      case 41: --level; break;
+    switch (s[i]) {
+      case '(': if (++level > max) max = level; break;
+      case ')': --level; break;
     }
   }
   return max;
