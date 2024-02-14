d134846785dd93a15f18ce98697ec28750894134
Alexander Kolobov <a@da.am>
2024-02-14 21:50:54 +0100

    Time: 68 ms (100%), Space: 57.7 MB (5.22%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.js
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.js
@@ -7,13 +7,17 @@ const checkArithmeticSubarrays = (nums, l, r) => {
       if (nums[p] < min) min = nums[p];
       if (nums[p] > max) max = nums[p];
     }
-    const diff = (max - min) / (r[i] - l[i]);
-    const arr = new Array(r[i] - l[i] + 1).fill(0);
-    for (let p = l[i]; p <= r[i]; ++p) {
-      const ind = (nums[p] - min) / diff;
-      if (ind % 1 || ++arr[ind] > 1) break;
+    if (min === max) {
+      res.push(true)
+    } else {
+      const diff = (max - min) / (r[i] - l[i]);
+      const arr = new Array(r[i] - l[i] + 1).fill(0);
+      for (let p = l[i]; p <= r[i]; ++p) {
+        const ind = (nums[p] - min) / diff;
+        if (ind % 1 || ++arr[ind] > 1) break;
+      }
+      res.push(!~arr.indexOf(0));
     }
-    res.push(!~arr.indexOf(0));
   }
   return res;
 };
