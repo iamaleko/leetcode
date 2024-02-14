fc77c4e40bd8161ac1a7d3b3b1fb1b2b341369ee
Alexander Kolobov <a@da.am>
2024-02-14 22:50:01 +0100

    Time: 63 ms (92.09%), Space: 51.8 MB (27.21%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.js
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.js
@@ -1,5 +1,4 @@
-const sortByBits = (arr) => arr.sort((a, b, aa = 0, bb = 0, aaa, bbb) => {
-  aaa = a, bbb = b;
+const sortByBits = (arr) => arr.sort((a, b, aa = 0, bb = 0, aaa = a, bbb = b) => {
   while (a) a &= a - 1, ++aa;
   while (b) b &= b - 1, ++bb;
   return aa - bb || aaa - bbb;
