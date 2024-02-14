c707d048de6ad0fb470d0f971bea6ddda992eb73
Alexander Kolobov <a@da.am>
2024-02-14 22:28:54 +0100

    Time: 338 ms (22.18%), Space: 94.4 MB (67.15%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.js
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.js
@@ -1,13 +1,5 @@
 const rearrangeArray = (nums) => {
   const res = new Array(nums.length);
-  for (let pi = 0, ni = 1, i = 0; i < nums.length; ++i) {
-    if (nums[i] < 0) {
-      res[ni] = nums[i];
-      ni += 2;
-    } else {
-      res[pi] = nums[i];
-      pi += 2;
-    }
-  }
+  for (let pi = -2, ni = -1, i = 0; i < nums.length; res[nums[i] < 0 ? ni += 2 : pi += 2] = nums[i++]);
   return res;
 };
