28640be7bb72b6a03e5ec94cd2e6d2c3c3d07340
Alexander Kolobov <a@da.am>
2024-02-14 22:25:32 +0100

    Time: 314 ms (39.22%), Space: 93.2 MB (75.15%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.js
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.js
@@ -1,7 +1,14 @@
 const rearrangeArray = (nums) => {
-  const ps = [], ms = [];
-  let i = 0;
-  while (i < nums.length) (nums[i] > 0 ? ps : ms).push(nums[i++]);
-  while (i) nums[--i] = (i % 2 ? ms : ps).pop();
-  return nums;
+  const res = [];
+  let pi = 0, ni = 1;
+  for (let i = 0; i < nums.length; ++i) {
+    if (nums[i] < 0) {
+      res[ni] = nums[i];
+      ni += 2;
+    } else {
+      res[pi] = nums[i];
+      pi += 2;
+    }
+  }
+  return res;
 };
