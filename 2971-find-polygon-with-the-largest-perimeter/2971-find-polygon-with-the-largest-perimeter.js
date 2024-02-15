ab8764b0b842100fa7577972513dd9566360291a
Alexander Kolobov <a@da.am>
2024-02-15 19:01:23 +0100

    Time: 219 ms (23.73%), Space: 66.4 MB (12.99%) - LeetHub

--- 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.js
+++ 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.js
@@ -1,6 +1,6 @@
 const largestPerimeter = (nums) => {
   nums.sort((a, b) => a - b);
-  let max = -1, i = 2, sum = nums[1] + nums[0];
-  for (; i < nums.length; ++i) sum > nums[i] ? max = sum += nums[i] : sum += nums[i];
+  let max = -1, i = 1, sum = nums[1] + nums[0];
+  while (i < nums.length) sum > nums[++i] ? max = sum += nums[i] : sum += nums[i];
   return max;
 };
