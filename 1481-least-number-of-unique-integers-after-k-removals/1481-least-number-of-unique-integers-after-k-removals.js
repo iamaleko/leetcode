dc3f6add41ed97253d84c09a6f1413bf506c93e9
Alexander Kolobov <a@da.am>
2024-02-17 00:03:22 +0100

    Time: 91 ms (95.48%), Space: 71.5 MB (66.33%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.js
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.js
@@ -1,8 +1,9 @@
 const findLeastNumOfUniqueInts = (nums, k) => {
   const map = new Map();
-  for (const num of nums) map.set(num, (map.get(num) || 0) + 1);
-  nums = Array.from(map.entries()).map((e) => e[1]).sort((a, b) => a - b);
   let i = 0;
+  while (i < nums.length) map.set(nums[i], (map.get(nums[i++]) || 0) + 1);
+  nums = Array.from(map.values()).sort((a, b) => a - b);
+  i = 0;
   while (nums[i] <= k) k -= nums[i++];
   return nums.length - i;
 };
