150d9ec80e05ac32e14896d720af9c3eca128b04
Alexander Kolobov <a@da.am>
2024-02-19 23:32:36 +0100

    Time: 47 ms (87.34%), Space: 49 MB (30.2%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -1,32 +1,7 @@
-/**
- * @param {number[]} nums1
- * @param {number} m
- * @param {number[]} nums2
- * @param {number} n
- * @return {void} Do not return anything, modify nums1 in-place instead.
- */
-var merge = function(nums1, m, nums2, n) {
-    const buffer = [];
-    for (let i = 0; i < m + n; i++) {
-        if (i < m) {
-            if (nums1[i] > nums2[0]) {
-                if (nums2[0] > buffer[0]) {
-                    buffer.push(nums1[i]);
-                    nums1[i] = buffer.shift(); 
-                } else {
-                    buffer.push(nums1[i]);
-                    nums1[i] = nums2.shift();   
-                }
-            } else if (nums1[i] > buffer[0]) {
-                buffer.push(nums1[i]);
-                nums1[i] = buffer.shift();  
-            }
-        } else {
-            if (!nums2.length || buffer.length && nums2[0] > buffer[0]) {
-                nums1[i] = buffer.shift();
-            } else {
-                nums1[i] = nums2.shift();
-            }   
-        }
-    }
+const merge = (a, m, b, n) => {
+  let i = m + n;
+  --m;
+  --n;
+  while (i--) a[i] = n < 0 || a[m] > b[n] ? a[m--] : b[n--];
+  return a;
 };
