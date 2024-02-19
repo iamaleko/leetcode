89da0e1406a319b0250f3ac95b15958a1c36073b
Alexander Kolobov <a@da.am>
2024-02-19 23:04:37 +0100

    Time: 73 ms (50.1%), Space: 53.9 MB (6.4%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.js
+++ 0231-power-of-two/0231-power-of-two.js
@@ -1,7 +1,11 @@
 // const isPowerOfTwo = (n) => Math.log2(n) % 1 === 0;
 // const isPowerOfTwo = (n) => n > 0 && !(n & n - 1);
 // const isPowerOfTwo = (n) => n > 0 && !(n & --n);
-const isPowerOfTwo = (n, b = 0) => {
-  while (n > 0) n &= n - 1, ++b;
-  return b === 1;
+// const isPowerOfTwo = (n, b = 0) => {
+//   while (n > 0) n &= n - 1, ++b;
+//   return b === 1;
+// }
+const isPowerOfTwo = (n) => {
+  if (n) while ((n = n / 2) % 1 === 0);
+  return n === 0.5;
 }
