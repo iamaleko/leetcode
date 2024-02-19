72891995594ed67d084b4e0268a0ac97f4f120aa
Alexander Kolobov <a@da.am>
2024-02-19 23:57:54 +0100

    Time: 51 ms (71.48%), Space: 49.1 MB (18.18%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -1,3 +1,3 @@
-const merge = (a, m, b, n, i = m-- + n--) => {
-  while (i--) a[i] = n < 0 || a[m] > b[n] ? a[m--] : b[n--];
+const merge = (a, m, b, n, i = --m + n--) => {
+  while (n >= 0) a[i--] = n < 0 || a[m] > b[n] ? a[m--] : b[n--];
 }
