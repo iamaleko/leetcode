938ab4bebb09f154f81996cdb899e03563c63b8b
Alexander Kolobov <a@da.am>
2024-02-22 00:14:57 +0600

    Time: 127 ms (51.71%), Space: 55.5 MB (13.66%) - LeetHub

---
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.js
@@ -0,0 +1,15 @@
+const rangeBitwiseAnd = (l, r) => {
+  // deal with power of two
+  if (!l || !r) return 0;
+  const maxPower = 2 ** Math.floor(Math.log2(r));
+  const minPower = 2 ** Math.ceil(Math.log2(l));
+  if (maxPower > l) return 0;
+  if (maxPower >= l && maxPower !== minPower) return 0;
+  if (maxPower >= l && maxPower === minPower) return maxPower;
+
+  // deal with l and r between powers of 2
+  let res = l++;
+  while (res && l <= r) res &= l++;
+
+  return res; 
+};
