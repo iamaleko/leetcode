567dfef27cb846ff0d936d286f634b945896c175
Alexander Kolobov <a@da.am>
2024-02-23 02:27:43 +0600

    Time: 79 ms (86.84%), Space: 56.6 MB (56.96%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.js
+++ 0997-find-the-town-judge/0997-find-the-town-judge.js
@@ -2,9 +2,9 @@ const findJudge = (n, trust) => {
   if (trust.length === 0) return n === 1 ? 1 : -1;
 
   const mem = new Array(n + 1).fill(1);
-  let res = -1;
+  let res = -1, i = 0;
 
-  for (let i = 0; i < trust.length; ++i) {
+  for (; i < trust.length; ++i) {
     mem[trust[i][0]] = 0;
     if (res === trust[i][0]) res = -1;
     if (mem[trust[i][1]] && ++mem[trust[i][1]] === n) res = trust[i][1];
