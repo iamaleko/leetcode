90309e8c9327b35e0c15cbb3411f5ed45cae89fb
Alexander Kolobov <a@da.am>
2024-02-23 23:52:06 +0600

    Time: 135 ms (13.13%), Space: 57.6 MB (36.59%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.js
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.js
@@ -1,33 +1,32 @@
-const findCheapestPrice = (cities, links, from, to, stops) => {
+const findCheapestPrice = (_, links, from, to, stops) => {
   // build graph
   const graph = new Map();
-  for (const [to, from, cost] of links) {
+  for (const [from, to, cost] of links) {
     let map;
-    if (graph.has(from)) {
-      map = graph.get(from);
+    if (graph.has(to)) {
+      map = graph.get(to);
     } else {
       map = new Map();
-      graph.set(from, map);
+      graph.set(to, map);
     }
-    map.set(to, cost);
+    map.set(from, cost);
   }
-  // console.log(graph);
 
   // bfs
   const mem = new Map(), queue = [[to, 0, 0]];
   let res = -1;
   while (queue.length) {
     const [city, costSum, stopsSum] = queue.pop();
-    // console.log(city, costSum, stopsSum);
     mem.set(city, costSum);
     if (city === from) {
       if (res > costSum || res === -1) res = costSum;
     } else if (stopsSum <= stops) {
-      const links = graph.get(city);
-      if (links) for (const [city, cost] of links) {
-        let key = city;
-        if (!mem.has(key) || mem.get(key) > cost + costSum) {
-          queue.push([city, cost + costSum, stopsSum + 1]);
+      if (graph.has(city)) {
+        const links = graph.get(city);
+        for (const [city, cost] of links) {
+          if (!mem.has(city) || mem.get(city) > cost + costSum) {
+            queue.push([city, cost + costSum, stopsSum + 1]);
+          }
         }
       }
     }
