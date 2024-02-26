4a49cea9226bec7c66e1a3047f0a4d88dd79dd73
Alexander Kolobov <a@da.am>
2024-02-27 02:06:22 +0600

    Time: 45 ms (95.07%), Space: 51 MB (24.45%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -1,5 +1,5 @@
 const reverseWords = (s) => {
   let l = '', r = '', i = 0;
   for (; i < s.length; i++) if (s[i] !== ' ') s[i - 1] === ' ' ? (r = l + ' ' + r, l = s[i]) : l += s[i];
-  return l + ' ' + r.trim();
+  return (l + ' ' + r).trim();
 };
