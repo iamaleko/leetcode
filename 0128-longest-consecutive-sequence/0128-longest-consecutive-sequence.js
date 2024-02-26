393d8e4ab2892f8f97020788c7960d264af13aa3
Alexander Kolobov <a@da.am>
2024-02-27 04:19:14 +0600

    Time: 1151 ms (32.31%), Space: 81.1 MB (8.63%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -0,0 +1,50 @@
+const longestConsecutive = (nums) => {
+  if (!nums.length) return 0;
+
+  let max = 0;
+
+  const minmap = new Map(),
+        maxmap = new Map(),
+        merge = (l, r) => {
+          console.log('merge', l, r)
+          maxmap.delete(l[1]);
+          minmap.delete(r[0]);
+          l[1] = r[1];
+          maxmap.set(r[1], l);
+          return l;
+        },
+        expandRight = (l) => {
+          console.log('exp right', l)
+          maxmap.delete(l[1]);
+          maxmap.set(++l[1], l);
+          return minmap.has(l[1] + 1) ? merge(l, minmap.get(l[1] + 1)) : l;
+        },
+        expandLeft = (r) => {
+          console.log('exp left', r)
+          minmap.delete(r[0]);
+          minmap.set(--r[0], r);
+          return maxmap.has(r[0] - 1) ? merge(maxmap.get(r[0] - 1), r) : r;
+        };
+  
+  for (const num of nums) {
+    if (minmap.has(num) || maxmap.has(num)) continue;
+
+    const l = maxmap.get(num - 1),
+          r = minmap.get(num + 1);
+
+    // console.log(num, l, r)
+
+    const n = l && r ? merge(l, r) : l ? expandRight(l) : r ? expandLeft(r) : [num, num];
+
+    if (n[0] < n[1]) {
+      if (n[1] - n[0] > max) max = n[1] - n[0];
+    } else {
+      minmap.set(num, n);
+      maxmap.set(num, n);
+    }
+
+    // console.log(n, minmap, maxmap)
+  }
+
+  return max + 1;
+};
