d15588595132f625f628cf0421715cb4cc5c9ae4
Alexander Kolobov <a@da.am>
2024-02-27 04:21:04 +0600

    Time: 142 ms (50.26%), Space: 83.9 MB (5.31%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -6,7 +6,6 @@ const longestConsecutive = (nums) => {
   const minmap = new Map(),
         maxmap = new Map(),
         merge = (l, r) => {
-          console.log('merge', l, r)
           maxmap.delete(l[1]);
           minmap.delete(r[0]);
           l[1] = r[1];
@@ -14,13 +13,11 @@ const longestConsecutive = (nums) => {
           return l;
         },
         expandRight = (l) => {
-          console.log('exp right', l)
           maxmap.delete(l[1]);
           maxmap.set(++l[1], l);
           return minmap.has(l[1] + 1) ? merge(l, minmap.get(l[1] + 1)) : l;
         },
         expandLeft = (r) => {
-          console.log('exp left', r)
           minmap.delete(r[0]);
           minmap.set(--r[0], r);
           return maxmap.has(r[0] - 1) ? merge(maxmap.get(r[0] - 1), r) : r;
@@ -30,20 +27,15 @@ const longestConsecutive = (nums) => {
     if (minmap.has(num) || maxmap.has(num)) continue;
 
     const l = maxmap.get(num - 1),
-          r = minmap.get(num + 1);
+          r = minmap.get(num + 1),
+          n = l && r ? merge(l, r) : l ? expandRight(l) : r ? expandLeft(r) : [num, num];
 
-    // console.log(num, l, r)
-
-    const n = l && r ? merge(l, r) : l ? expandRight(l) : r ? expandLeft(r) : [num, num];
-
-    if (n[0] < n[1]) {
+    if (n[0] !== n[1]) {
       if (n[1] - n[0] > max) max = n[1] - n[0];
     } else {
       minmap.set(num, n);
       maxmap.set(num, n);
     }
-
-    // console.log(n, minmap, maxmap)
   }
 
   return max + 1;
