6287fcda207d80c09cc020610a06ba78efea79f3
Alexander Kolobov <a@da.am>
2024-02-27 02:37:26 +0600

    Time: 58 ms (46.54%), Space: 54.8 MB (5.07%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -1,5 +1,19 @@
 const reverseWords = (s) => {
-  let l = '', r = '', i = 0;
-  for (; i < s.length; i++) if (s[i] !== ' ') s[i - 1] === ' ' ? (r = l + ' ' + r, l = s[i]) : l += s[i];
-  return (l + ' ' + r).trim();
+  let m = s.length, l = 0, r = 0;
+  while (r < m) {
+    if (s[l] === ' ') { ++l; ++r; } else
+    if (s[r] !== ' ') { ++r; } else
+    {
+      s = s.slice(r, m) + ' ' + s.slice(l, r) + s.slice(m);
+      m -= r;
+      l = r = 0;
+    }
+  }
+  return s.trim();
 };
+
+// const reverseWords = (s) => {
+//   let l = '', r = '', i = 0;
+//   for (; i < s.length; i++) if (s[i] !== ' ') s[i - 1] === ' ' ? (r = l + ' ' + r, l = s[i]) : l += s[i];
+//   return (l + ' ' + r).trim();
+// };
