7cebfab4e444fba6580320dc76286db3f3ef0fa1
Alexander Kolobov <a@da.am>
2024-02-28 02:54:25 +0600

    Time: 69 ms (20.94%), Space: 52.8 MB (6.08%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.js
+++ 0071-simplify-path/0071-simplify-path.js
@@ -2,15 +2,16 @@ const simplifyPath = (path) => {
   let s = '';
   path += '/';
   for (const chr of path) {
-    s += chr;
-    if (s.endsWith('/./')) {
-      s = s.slice(0, -2);
-    } else if (s.endsWith('/../')) {
-      const slash = s.length === '/../' ? -1 : s.lastIndexOf('/', s.length - 5);
-      s = ~slash ? s.slice(0, slash + 1) : s.slice(0, -3);
-    } else if (s.endsWith('//')) {
-      s = s.slice(0, -1);
+    if (chr === '/') {
+      if (s.endsWith('/.')) {
+        s = s.slice(0, -2);
+      } else if (s.endsWith('/..')) {
+        s = s.slice(0, s.lastIndexOf('/', s.length - 4));
+      } else if (s.at(-1) === '/') {
+        s = s.slice(0, -1);
+      }
     }
+    s += chr;
   }
   return s.slice(0, -1) || '/';
 };
