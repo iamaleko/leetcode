e63035b00717089e2e6022b380da981d585c3643
Alexander Kolobov <a@da.am>
2024-03-01 02:13:10 +0600

    Time: 168 ms (92.76%), Space: 63.5 MB (20.71%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.js
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.js
@@ -4,11 +4,11 @@ class RecentCounter {
     this.ts = [];
   }
   ping(t) {
-    this.ts.push(t);
+    this.ts.push(t + 3000);
     let l = 0, r = this.ts.length - 2, c;
     while (l <= r) {
       c = l + r >> 1;
-      if (this.ts[c] < t - 3000) {
+      if (this.ts[c] < t) {
         l = c + 1;
       } else {
         r = c - 1;
