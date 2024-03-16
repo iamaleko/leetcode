6dcd38f174ce55578a00a59fd41b78e4c218a67a
Alexander Kolobov <a@da.am>
2024-03-17 04:50:36 +0600

    Time: 97 ms (47.36%), Space: 56.2 MB (89.85%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.js
+++ 0525-contiguous-array/0525-contiguous-array.js
@@ -2,13 +2,11 @@ const findMaxLength = (nums) => {
   const map = new Map();
   map.set(0, -1);
   let level = 0, max = 0, i = 0;
-  for (; i < nums.length; i++) {
-    nums[i] ? level++ : level--;
-    if (map.has(level)) {
-      max = Math.max(max, i - map.get(level));
-      //if (max <  i - map.get(level)) max = i - map.get(level);
+  while (i < nums.length) {
+    if (map.has(nums[i] ? ++level : --level)) {
+      max = Math.max(max, i++ - map.get(level));
     } else {
-      map.set(level, i);
+      map.set(level, i++);
     }
   }
   return max;
