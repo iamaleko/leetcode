748c2ed61787a4c16dcba9e5d9a37a6d0c48e054
Alexander Kolobov <a@da.am>
2024-03-17 06:45:08 +0600

    Time: 65 ms (99.37%), Space: 56.5 MB (85.41%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.js
+++ 0525-contiguous-array/0525-contiguous-array.js
@@ -1,23 +1,25 @@
 const findMaxLength = (nums) => {
-  let level = 0, max = 0;
-  for (let i = 0; i < nums.length; i++) {
+  let level = 0, max = -1, i = 0;
+  while (i < nums.length) {
     nums[i] ? level++ : level--;
     nums[i] = 0;
     if (level === 0) {
-      max = i + 1;
+      max = i++;
     } else if (level < 0) {
-      if (nums[-level-1] & 4294901760) {
-        if (max < (i + 1) - ((nums[-level-1] & 4294901760) >>> 16)) max = (i + 1) - ((nums[-level-1] & 4294901760) >>> 16);
+      if (nums[-level - 1] & 4294901760) {
+        if (max < i - ((nums[-level - 1] & 4294901760) >>> 16)) max = i - ((nums[-level - 1] & 4294901760) >>> 16);
+        i++;
       } else {
-        nums[-level-1] |= (i + 1) << 16;
+        nums[-level - 1] |= ++i << 16;
       }
     } else if (level > 0) {
-      if (nums[level-1] & 65535) {
-        if (max < (i + 1) - (nums[level-1] & 65535)) max = (i + 1) - (nums[level-1] & 65535);
+      if (nums[level - 1] & 65535) {
+        if (max < i - (nums[level - 1] & 65535)) max = i - (nums[level - 1] & 65535);
+        i++;
       } else {
-        nums[level-1] |= (i + 1);
+        nums[level - 1] |= ++i;
       }
     }
   }
-  return max;
+  return max + 1;
 };
