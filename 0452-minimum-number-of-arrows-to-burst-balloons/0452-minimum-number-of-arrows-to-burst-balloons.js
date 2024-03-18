ac3a8d6af03d4b2c10b450d11b0af274789748ec
Alexander Kolobov <a@da.am>
2024-03-19 03:52:13 +0600

    Time: 247 ms (8.75%), Space: 73.3 MB (35.12%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.js
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.js
@@ -1,13 +1,13 @@
 const findMinArrowShots = (points) => {
   if (!points.length) return 0;
   points.sort((a, b) => a[0] - b[0]);
-  let arrows = 1, r = points[0][1];
-  for (const point of points) {
-    if (point[0] > r) {
-      r = point[1];
+  let arrows = 1;
+  for (let i = 1, r = points[0][1]; i < points.length; i++) {
+    if (points[i][0] > r) {
+      r = points[i][1];
       arrows++;
-    } else if (point[1] < r) {
-      r = point[1];
+    } else if (points[i][1] < r) {
+      r = points[i][1];
     }
   }
   return arrows;
