94bd70857af2072f67af7169d7251c98e93c0fc2
Alexander Kolobov <a@da.am>
2024-03-29 04:28:53 +0500

    Time: 62 ms (96.8%), Space: 56.8 MB (91.37%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -1,15 +1,13 @@
 const findDuplicate = (nums) => {
   let s = nums[0], f = nums[0];
-  while (true) {
+  do {
     s = nums[s];
     f = nums[nums[f]];
-    if (s === f) break;
-  }
-  f = nums[0];
+  } while (s !== f);
+  s = nums[0];
   while(f !== s) {
     s = nums[s]
     f = nums[f]
   }
-
   return s;
 };
