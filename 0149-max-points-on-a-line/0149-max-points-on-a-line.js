480f29b18e76ac9e146fc2708cb445b39512926e
Alexander Kolobov <a@da.am>
2024-03-29 02:41:21 +0500

    Time: 115 ms (61.05%), Space: 61.1 MB (36.92%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.js
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.js
@@ -1,15 +1,13 @@
 const maxPoints = (points) => {
   if (points.length === 1) return 1;
   let max = 0;
-  const fn = (x1, y1, x2, y2) => (x1 - x2)/(y1 - y2);
   points.sort((a, b) => a[0] - b[0] || a[1] - b[1]);
   for (let i = 0; i < points.length; i++) {
     const map = {};
-    for (let j = 0; j < points.length; j++) {
+    for (let j = i + 1; j < points.length; j++) {
       if (i === j) continue;
-      const diff = fn(points[i][0], points[i][1], points[j][0], points[j][1]);
-      if (!map.hasOwnProperty(diff)) map[diff] = 1;
-      map[diff]++;
+      const diff = (points[i][0] - points[j][0]) / (points[i][1] - points[j][1]);
+      map.hasOwnProperty(diff) ? map[diff]++ : map[diff] = 2;
       if (max < map[diff]) max = map[diff];
     }
   }
