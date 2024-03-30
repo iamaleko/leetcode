2a868fdff14221b2bdff12a7e4a1f592c118102f
Alexander Kolobov <a@da.am>
2024-03-30 06:17:46 +0500

    Time: 51 ms (82.25%), Space: 52.9 MB (5.29%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.js
+++ 0414-third-maximum-number/0414-third-maximum-number.js
@@ -2,14 +2,18 @@ class Heap {
   constructor(comp) {
     this.heap = [];
     this.comparator = comp;
+    this.min = 0;
   }
   push(n) {
     this.heap.push(n);
     this.heapify(this.heap.length - 1);
   }
   pop() {
-    let res = this.heap.pop();
-    this.heap.push(this.heap.shift())
+    if (this.min === this.heap.length) return undefined;
+    let res = this.heap[this.heap.length - 1];
+    this.heap[this.heap.length - 1] = this.heap[this.min];
+    this.heap[this.min] = undefined;
+    this.min++;
     this.heapify(this.heap.length - 1);
     return res;
   }
@@ -18,8 +22,8 @@ class Heap {
     let l = this.heap.length - (((this.heap.length - i - 1) * 2) + 1) - 1,
         r = this.heap.length - (((this.heap.length - i - 1) * 2) + 2) - 1,
         s = i;
-    if (l >= 0 && this.comparator(this.heap[l], this.heap[s])) s = l;
-    if (r >= 0 && this.comparator(this.heap[r], this.heap[s])) s = r;
+    if (l >= this.min && this.comparator(this.heap[l], this.heap[s])) s = l;
+    if (r >= this.min && this.comparator(this.heap[r], this.heap[s])) s = r;
     if (s !== i) {
       [this.heap[i], this.heap[s]] = [this.heap[s], this.heap[i]];
       this.heapify(s);
