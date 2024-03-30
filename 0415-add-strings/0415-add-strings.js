ee9803a32e4e6f851159878b69429248b97eae7a
Alexander Kolobov <a@da.am>
2024-03-31 03:39:13 +0500

    Time: 57 ms (67.8%), Space: 50 MB (74.63%) - LeetHub

--- 0415-add-strings/0415-add-strings.js
+++ 0415-add-strings/0415-add-strings.js
@@ -1,7 +1,7 @@
 const addStrings = (a, b) => {
   let res = '', mem = 0, ap = a.length - 1, bp = b.length -1, sum;
   while (ap > -1 || bp > -1 || mem) {
-    sum = (+a[ap--] || 0) + (+b[bp--] || 0) + mem;
+    sum = mem + (a[ap--] | 0) + (b[bp--] | 0);
     if (sum > 9) {
       sum -= 10;
       mem = 1;
