7319dc3ae7bd5d753e7988d03387dd073db83580
Alexander Kolobov <a@da.am>
2024-03-31 03:35:21 +0500

    Time: 62 ms (46.24%), Space: 51.9 MB (32.2%) - LeetHub

--- 0415-add-strings/0415-add-strings.js
+++ 0415-add-strings/0415-add-strings.js
@@ -1,15 +1,15 @@
 const addStrings = (a, b) => {
   let res = '', mem = 0;
-  while (a || b || mem) {
-    let sum = (+a.at(-1) || 0) + (+b.at(-1) || 0) + mem;
+  a = a.split('').map(n => +n);
+  b = b.split('').map(n => +n);
+  while (a.length || b.length || mem) {
+    let sum = (a.length ? a.pop() : 0) + (b.length ? b.pop() : 0) + mem;
     if (sum > 9) {
       sum -= 10;
       mem = 1;
     } else {
       mem = 0;
     }
-    a = a.slice(0, -1);
-    b = b.slice(0, -1);
     res = sum + '' + res;
   }
   return res;
