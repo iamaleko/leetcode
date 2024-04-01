00ed9de2031d3a6cd1802db587ce344be8313a8f
Alexander Kolobov <a@da.am>
2024-04-01 13:54:26 +0500

    Time: 46 ms (83.49%), Space: 48.3 MB (94.08%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -1,11 +1,1 @@
-/**
- * @param {string} s
- * @return {number}
- */
-const lengthOfLastWord = (s) => {
-  let l, r = s.length;
-  while (s[--r] === ' ');
-  l = r;
-  while (l >= 0 && s[--l] !== ' ');
-  return r - l;
-};
+const lengthOfLastWord=s=>s.trim``.split` `.at(-1).length;
