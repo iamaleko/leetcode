3c6774453204a27157784d10ed95490d8d0acdf4
Alexander Kolobov <a@da.am>
2024-04-01 13:56:19 +0500

    Time: 54 ms (40.31%), Space: 48.9 MB (50.18%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -1,1 +1,9 @@
 const lengthOfLastWord=s=>s.trim``.split` `.at(-1).length;
+
+// const lengthOfLastWord = (s) => {
+//   let l, r = s.length;
+//   while (s[--r] === ' ');
+//   l = r;
+//   while (l >= 0 && s[--l] !== ' ');
+//   return r - l;
+// };
