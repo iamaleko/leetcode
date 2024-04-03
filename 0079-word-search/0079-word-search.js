b38ab3d5cd7a9ddf692632b885d6ebf63db2f976
Alexander Kolobov <a@da.am>
2024-04-03 19:45:53 +0500

    Time: 839 ms (34.23%), Space: 48.4 MB (97.29%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -1,25 +1,22 @@
 const exist = (board, word) => {
-  word = word.split('').reverse();
-  const dfs = (y, x, word) => {
-    if (board[y]?.[x] === word.at(-1)) {
-      const bchar = board[y][x];
-      const wchar = word.pop();
+  const dfs = (y, x, p) => {
+    if (board[y] && board[y][x] === word[p]) {
+      const char = board[y][x];
       board[y][x] = null;
       if (
-        word.length === 0 ||
-        dfs(y - 1, x, word) ||
-        dfs(y + 1, x, word) ||
-        dfs(y, x + 1, word) ||
-        dfs(y, x - 1, word)
+        ++p === word.length ||
+        dfs(y - 1, x, p) ||
+        dfs(y + 1, x, p) ||
+        dfs(y, x + 1, p) ||
+        dfs(y, x - 1, p)
       ) return true;
-      board[y][x] = bchar;
-      word.push(wchar);
+      board[y][x] = char;
     }
     return false;
   }
   for (let y = 0; y < board.length; y++) {
     for (let x = 0; x < board[0].length; x++) {
-      if (dfs(y, x, word)) return true;
+      if (dfs(y, x, 0)) return true;
     }
   }
   return false;
