140967150d1cc4364c3a832d30c65ccb53995b0e
Alexander Kolobov <a@da.am>
2024-04-07 20:05:55 +0500

    Time: 54 ms (45.13%), Space: 48.8 MB (65.34%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.js
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.js
@@ -1,29 +1,43 @@
 /**
- * Time limit exeeded on cases with multiple adjacent *
  * @param {string} s
  * @return {boolean}
  */
 const checkValidString = (s) => {
-  const mem = {};
-  const pair = (a,b) => (a + b) * (a + b + 1) / 2 + b; 
-  const req = (i, level) => {
-    const key = pair(i, level);
-    if (mem.hasOwnProperty(key)) return mem[key];
-
-    while (i < s.length) {
-      if (s[i] === '(') {
-        level++;
-        i++;
-      } else if (s[i] === ')') {
-        level--;
-        if (level < 0) return mem[key] = false;
-        i++;
-      } else if (s[i] === '*') {
-        i++;
-        return mem[key] = req(i, level) || req(i, level + 1) || level && req(i, level - 1);
-      }
-    }
-    return mem[key] = level === 0;
-  };
-  return req(0, 0);
+  let l = 0, r = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') { l++; r++; }
+    else if (s[i] === ')') { l--; r--; }
+    else if (s[i] === '*') { l--; r++; }
+    if (r < 0) return false;
+    if (l < 0) l = 0;
+  }
+  return l === 0;
 };
+
+// /**
+//  * @param {string} s
+//  * @return {boolean}
+//  */
+// const checkValidString = (s) => {
+//   const mem = {},
+//         req = (i, level) => {
+//           const key = (level << 7) + i;
+//           if (mem.hasOwnProperty(key)) return mem[key];
+
+//           while (i < s.length) {
+//             if (s[i] === '(') {
+//               level++;
+//               i++;
+//             } else if (s[i] === ')') {
+//               level--;
+//               if (level < 0) return mem[key] = false;
+//               i++;
+//             } else if (s[i] === '*') {
+//               i++;
+//               return mem[key] = req(i, level) || req(i, level + 1) || level && req(i, level - 1);
+//             }
+//           }
+//           return mem[key] = level === 0;
+//         };
+//   return req(0, 0);
+// };
