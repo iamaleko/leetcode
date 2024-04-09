4f850120f372312224ce2a5ed25ee5faca02e7c9
Alexander Kolobov <a@da.am>
2024-04-09 20:05:55 +0500

    Time: 64 ms (58%), Space: 50.8 MB (44.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -6,8 +6,10 @@
 
 const twoSum = (nums, target) => {
   const map = {};
-  for (let i = 0; i < nums.length; i++) map[nums[i]] = i;
   for (let i = 0; i < nums.length; i++)
-    if (map.hasOwnProperty(target - nums[i]) && map[target - nums[i]] !== i)
+    if (map.hasOwnProperty(target - nums[i]) && map[target - nums[i]] !== i) {
       return [i, map[target - nums[i]]];
+    } else {
+      map[nums[i]] = i;
+    }
 };
