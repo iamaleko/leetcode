da3a4346384176823d86057756e7f8f12ddeebf0
Alexander Kolobov <a@da.am>
2024-04-09 20:03:20 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,5 +1,11 @@
+// const twoSum = (nums, target) => {
+//   for (let a = 0; a < nums.length - 1; a++)
+//     for (let b = a + 1; b < nums.length; b++)
+//       if (nums[a] + nums[b] === target) return [a, b];
+// };
+
 const twoSum = (nums, target) => {
-  for (let a = 0; a < nums.length - 1; a++)
-    for (let b = a + 1; b < nums.length; b++)
-      if (nums[a] + nums[b] === target) return [a, b];
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) map.set(nums[i], i);
+  for (let i = 0; i < nums.length; i++) if (i !== map.get(target - nums[i])) return [i, map.get(target - nums[i])];
 };
