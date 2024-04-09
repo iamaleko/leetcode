89bf19cf85dbfe9a7f20f11d8bf1729cf2f23aea
Alexander Kolobov <a@da.am>
2024-04-09 19:46:36 +0500

    Time: 51 ms (35.04%), Space: 16.7 MB (5.73%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -1,7 +1,15 @@
+# class Solution:
+#   def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+#     seconds: int = 0
+#     for i, val in enumerate(tickets):
+#       if i > k and val >= tickets[k]: seconds -= 1
+#       seconds += min(val, tickets[k])
+#     return seconds
+
 class Solution:
   def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
     seconds: int = 0
-    for i, val in enumerate(tickets):
-      if i > k and val >= tickets[k]: seconds -= 1
-      seconds += min(val, tickets[k])
+    for i in range(len(tickets)):
+      if i > k and tickets[i] >= tickets[k]: seconds -= 1
+      seconds += min(tickets[i], tickets[k])
     return seconds
