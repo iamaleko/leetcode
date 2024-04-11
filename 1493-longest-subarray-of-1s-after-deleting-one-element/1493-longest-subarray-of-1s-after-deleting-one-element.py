19c146abf997abbdac3605d5658d42437fb350c8
Alexander Kolobov <a@da.am>
2024-04-12 02:17:47 +0500

    Time: 423 ms (89.9%), Space: 19.6 MB (78.07%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -18,11 +18,12 @@ class Solution:
   def longestSubarray(self, nums: List[int]) -> int:
     l = 0
     z = 0
-    for r in range(len(nums)):
+    n = len(nums)
+    for r in range(n):
       if not nums[r]:
         z += 1
       if z > 1: # при таком подходе левый указатель за весь проход отстанет от правого на максимальную длинну на успешном участке
         if not nums[l]:
           z -= 1
         l += 1
-    return len(nums) - l - 1
+    return n - l - 1
