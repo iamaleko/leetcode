ea76b1992ca56e89d9215d43d3605c9818a5f059
Alexander Kolobov <a@da.am>
2024-04-12 01:27:48 +0500

    Time: 2139 ms (5.03%), Space: 39 MB (7.31%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -0,0 +1,21 @@
+class Solution:
+  def maxOperations(self, nums: List[int], k: int) -> int:
+    map = {}
+    for i, num in enumerate(nums):
+      if num not in map:
+        map[num] = set()
+      map[num].add(i)
+    print(map)
+    cnt = 0
+    for i, num in enumerate(nums):
+      key = k - num
+      if key in map and i in map[num]:
+        for j in map[key]:
+          if j != i:
+            cnt += 1
+            map[key].remove(j)
+            if i in map[num]:
+              map[num].remove(i)
+            break
+    return cnt
+        
