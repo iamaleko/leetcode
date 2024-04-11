e09c12f35f0f2631d38a5c0fed4e51fa4a3eecbf
Alexander Kolobov <a@da.am>
2024-04-12 01:44:16 +0500

    Time: 117 ms (66.11%), Space: 17.2 MB (90.74%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,17 +1,33 @@
+# class Solution:
+#   def maxVowels(self, s: str, k: int) -> int:
+#     vovels = set(['a','e','i','o','u'])
+#     cnt = 0
+#     for i in range(min(k, len(s))):
+#       if s[i] in vovels:
+#         cnt += 1
+#     p = 0
+#     max = cnt
+#     while p + k < len(s):
+#       if s[p] in vovels:
+#         cnt -= 1
+#       if s[p + k] in vovels:
+#         cnt += 1
+#       if cnt > max:
+#         max = cnt
+#       p += 1
+#     return max
+
 class Solution:
   def maxVowels(self, s: str, k: int) -> int:
     vovels = set(['a','e','i','o','u'])
     cnt = 0
-    for i in range(min(k, len(s))):
-      if s[i] in vovels:
-        cnt += 1
+    max = 0
     p = 0
-    max = cnt
-    while p + k < len(s):
+    while p < len(s):
       if s[p] in vovels:
-        cnt -= 1
-      if s[p + k] in vovels:
         cnt += 1
+      if p - k > -1 and s[p - k] in vovels:
+        cnt -= 1
       if cnt > max:
         max = cnt
       p += 1
