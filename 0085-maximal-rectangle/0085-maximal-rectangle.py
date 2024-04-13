6fc57bc402aaf868094f89ebce95264f975cc0bb
Alexander Kolobov <a@da.am>
2024-04-14 00:28:31 +0500

    Time: 594 ms (18.57%), Space: 17.9 MB (76.17%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -2,21 +2,19 @@ class Solution:
   def maximalRectangle(self, matrix: List[List[str]]) -> int:
     h = len(matrix)
     w = len(matrix[0])
-    sum = [None for i in range(h)]
 
     for y in range(h):
-      sum[y] = [0 for i in range(w)]
       for x in range(w):
-        t = int(matrix[y - 1][x]) if y > 0 else 0
-        v = int(matrix[y][x])
-        sum[y][x] = v + (sum[y - 1][x] if t else 0) if v else 0
+        matrix[y][x] = int(matrix[y][x])
+        if matrix[y][x] and y > 0:
+          matrix[y][x] += matrix[y - 1][x]
 
     max = 0
     y = h - 1
     x = w - 1
     while True:
-      if sum[y][x]:
-        val = self.calc(y,x,sum)
+      if matrix[y][x]:
+        val = self.calc(y,x,matrix)
         if val > max:
           max = val
       x -= 1
