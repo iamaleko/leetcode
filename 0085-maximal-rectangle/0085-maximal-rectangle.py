5a3766729b85a9089e93698a720a0d26fc041e87
Alexander Kolobov <a@da.am>
2024-04-14 00:19:46 +0500

    Time: 622 ms (18.13%), Space: 18 MB (34.16%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -5,21 +5,17 @@ class Solution:
     sum = [None for i in range(h)]
 
     for y in range(h):
-      sum[y] = [[] for i in range(w)]
+      sum[y] = [0 for i in range(w)]
       for x in range(w):
-        l = int(matrix[y][x - 1]) if x > 0 else 0
         t = int(matrix[y - 1][x]) if y > 0 else 0
         v = int(matrix[y][x])
-        sum[y][x] = [
-          v + (sum[y][x - 1][0] if l else 0) if v else 0, # x
-          v + (sum[y - 1][x][1] if t else 0) if v else 0, # y
-        ]
+        sum[y][x] = v + (sum[y - 1][x] if t else 0) if v else 0
 
     max = 0
     y = h - 1
     x = w - 1
     while True:
-      if sum[y][x][0] and sum[y][x][1]:
+      if sum[y][x]:
         val = self.calc(y,x,sum)
         if val > max:
           max = val
@@ -32,13 +28,13 @@ class Solution:
     return max
 
   def calc(self, y, x, sum):
-    max = sum[y][x][1]
+    max = sum[y][x]
     ys = max
     xs = 1
-    while x > 0 and sum[y][x - 1][0]:
+    while x > 0 and sum[y][x - 1]:
       xs += 1
-      if ys > sum[y][x - 1][1]:
-        ys = sum[y][x - 1][1]
+      if ys > sum[y][x - 1]:
+        ys = sum[y][x - 1]
       if max < ys * xs:
         max = ys * xs
       x -= 1
