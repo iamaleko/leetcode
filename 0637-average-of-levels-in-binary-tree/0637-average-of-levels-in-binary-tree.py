b077bb1b70f4e97f94b271e2314919aabaa14f33
Alexander Kolobov <a@da.am>
2024-04-14 23:53:05 +0500

    Time: 46 ms (51.2%), Space: 18.7 MB (6.73%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
@@ -1,22 +1,38 @@
+# class Solution:
+#   def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+#     if not root:
+#       return []
+#     cnt = []
+#     sum = []
+#     def traverse(node, level):
+#       if level == len(cnt):
+#         cnt.append(0)
+#         sum.append(0)
+#       cnt[level] += 1
+#       sum[level] += node.val
+#       if node.left:
+#         traverse(node.left, level + 1)
+#       if node.right:
+#         traverse(node.right, level + 1)
+#     traverse(root, 0)
+#     for i in range(len(cnt)):
+#       if cnt[i]:
+#         sum[i] /= cnt[i]
+#     return sum
+
 class Solution:
   def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
-    if not root:
-      return []
-    cnt = []
-    sum = []
-    def traverse(node, level):
-      if level == len(cnt):
-        cnt.append(0)
-        sum.append(0)
-      cnt[level] += 1
-      sum[level] += node.val
-      if node.left:
-        traverse(node.left, level + 1)
-      if node.right:
-        traverse(node.right, level + 1)
-    traverse(root, 0)
-    for i in range(len(cnt)):
-      if cnt[i]:
-        sum[i] /= cnt[i]
-    return sum
+    res = []
+    queue = [root]
+    while queue:
+      sum = 0
+      _queue = []
+      for node in queue:
+        sum += node.val
+        if node.left: _queue.append(node.left)
+        if node.right: _queue.append(node.right)
+      res.append(sum / len(queue))
+      queue = _queue
+    return res
+
         
