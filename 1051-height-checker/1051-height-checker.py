5970a54846b165c0c2001329f297f44fba1cee77
Alexander Kolobov <a@da.am>
2024-04-15 02:15:47 +0500

    Time: 41 ms (46.66%), Space: 16.7 MB (7.88%) - LeetHub

--- 1051-height-checker/1051-height-checker.py
+++ 1051-height-checker/1051-height-checker.py
@@ -1,11 +1,28 @@
+# O(n log n) with sorting
+# class Solution:
+#   def heightChecker(self, heights: List[int]) -> int:
+#     expected = heights[:]
+#     expected.sort()
+#     cnt = 0
+#     for i in range(len(heights)):
+#       if expected[i] != heights[i]:
+#         cnt += 1
+#     return cnt
+
+# O(n) with bucket sort
 class Solution:
   def heightChecker(self, heights: List[int]) -> int:
-    expected = heights[:]
-    expected.sort()
+    expected = [0] * 101
+    for heigh in heights:
+      expected[heigh] += 1
     cnt = 0
-    for i in range(len(heights)):
-      if expected[i] != heights[i]:
-        cnt += 1
+    i = 0
+    for j in range(101):
+      for _ in range(expected[j]):
+        if j != heights[i]:
+          cnt += 1
+        i += 1
     return cnt
+
 
         
