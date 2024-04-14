52271df18c999be19358f9c1d58ba4da07353e85
Alexander Kolobov <a@da.am>
2024-04-14 18:52:07 +0500

    Time: 41 ms (22.13%), Space: 16.7 MB (47.79%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -1,14 +1,25 @@
+# class Solution:
+#   def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+#     sum = 0
+#     queue = [root]
+#     while queue:
+#       node = queue.pop()
+#       if node.left:
+#         if not node.left.left and not node.left.right:
+#           sum += node.left.val
+#         queue.append(node.left)
+#       if node.right:
+#         queue.append(node.right)
+#     return sum
+
 class Solution:
-  def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+  def sumOfLeftLeaves(self, node: Optional[TreeNode]) -> int:
     sum = 0
-    queue = [root]
-    while queue:
-      node = queue.pop()
-      if node.left:
-        if not node.left.left and not node.left.right:
-          sum += node.left.val
-        queue.append(node.left)
-      if node.right:
-        queue.append(node.right)
+    if node.left:
+      if not node.left.left and not node.left.right:
+        sum += node.left.val
+      sum += self.sumOfLeftLeaves(node.left)
+    if node.right:
+      sum += self.sumOfLeftLeaves(node.right)
     return sum
         
