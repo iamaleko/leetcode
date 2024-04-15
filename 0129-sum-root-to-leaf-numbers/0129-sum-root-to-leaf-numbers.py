3b13d3286be32a8104f66a2de67dc08012aba0c0
Alexander Kolobov <a@da.am>
2024-04-16 00:10:07 +0500

    Time: 38 ms (40.03%), Space: 16.4 MB (81%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -16,10 +16,10 @@
 class Solution:
   def sumNumbers(self, root: Optional[TreeNode]) -> int:
     def traverse(node, n):
+      n = n * 10 + node.val
       if not node.left and not node.right:
-        return n * 10 + node.val
+        return n
       else:
-        n = n * 10 + node.val
         return (traverse(node.left, n) if node.left else 0) + (traverse(node.right, n) if node.right else 0)
     return traverse(root, 0)
         
