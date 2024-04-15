2ee498d6dc09480a5691eb2abd40b59d103a303c
Alexander Kolobov <a@da.am>
2024-04-16 01:00:28 +0500

    Time: 30 ms (80.64%), Space: 16.6 MB (17.41%) - LeetHub

---
+++ 1185-day-of-the-week/1185-day-of-the-week.py
@@ -0,0 +1,3 @@
+class Solution:
+  def dayOfTheWeek(self, day: int, month: int, year: int) -> str:
+    return ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"][date(year, month, day).weekday()]
