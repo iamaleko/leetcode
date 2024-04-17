753c8c1c74886311aea3c2613af95e0e79275f0e
Alexander Kolobov <a@da.am>
2024-04-18 01:13:35 +0500

    Time: 59 ms (9.47%), Space: 16.7 MB (99.97%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,13 +1,30 @@
+# class Solution:
+#   def isPalindrome(self, s: str) -> bool:
+#     voc = 'abcdefghijklmnopqrstuvwxyz0123456789'
+#     s = s.lower()
+#     l = 0
+#     r = len(s) - 1
+#     while l < r:
+#       if s[l] not in voc:
+#         l += 1
+#       elif s[r] not in voc:
+#         r -= 1
+#       elif s[l] == s[r]:
+#         l += 1
+#         r -= 1
+#       else:
+#         return False
+#     return True
+
 class Solution:
   def isPalindrome(self, s: str) -> bool:
-    voc = 'abcdefghijklmnopqrstuvwxyz0123456789'
     s = s.lower()
     l = 0
     r = len(s) - 1
     while l < r:
-      if s[l] not in voc:
+      if not s[l].isalnum():
         l += 1
-      elif s[r] not in voc:
+      elif not s[r].isalnum():
         r -= 1
       elif s[l] == s[r]:
         l += 1
