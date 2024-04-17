2570177d81b0fbf1e370d47390eaa216751d8701
Alexander Kolobov <a@da.am>
2024-04-17 20:14:56 +0500

    Time: 38 ms (85%), Space: 17.8 MB (34.37%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
@@ -1,7 +1,23 @@
+# class Solution:
+#   def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+#     res = None
+#     queue = [(root, '')]
+#     while queue:
+#       node, s = queue.pop()
+#       s = chr(97 + node.val) + s
+#       if node.left or node.right:
+#         if node.left:
+#           queue.append((node.left, s))
+#         if node.right:
+#           queue.append((node.right, s))
+#       elif not res or res > s:
+#         res = s
+#     return res
+
 class Solution:
   def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
     res = None
-    queue = [(root, '')]
+    queue = deque([(root, '')])
     while queue:
       node, s = queue.pop()
       s = chr(97 + node.val) + s
