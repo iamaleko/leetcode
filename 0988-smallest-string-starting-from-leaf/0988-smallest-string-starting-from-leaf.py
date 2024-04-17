b0374bd9792e42b3028644cfe00a34d2250c632d
Alexander Kolobov <a@da.am>
2024-04-17 20:14:46 +0500

    Time: 44 ms (53.33%), Space: 17.6 MB (99.38%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
@@ -1,30 +1,30 @@
-# class Solution:
-#   def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
-#     res = None
-#     queue = [(root, '')]
-#     while queue:
-#       node, s = queue.pop()
-#       s = chr(97 + node.val) + s
-#       if node.left or node.right:
-#         if node.left:
-#           queue.append((node.left, s))
-#         if node.right:
-#           queue.append((node.right, s))
-#       elif not res or res > s:
-#         res = s
-#     return res
-
 class Solution:
-  def tr(self, node, s):
-    s = chr(97 + node.val) + s
-    if node.left or node.right:
-      if node.left:
-        self.tr(node.left, s)
-      if node.right:
-        self.tr(node.right, s)
-    elif not self.res or self.res > s:
-      self.res = s
   def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
-    self.res = None
-    self.tr(root, '')
-    return self.res
+    res = None
+    queue = [(root, '')]
+    while queue:
+      node, s = queue.pop()
+      s = chr(97 + node.val) + s
+      if node.left or node.right:
+        if node.left:
+          queue.append((node.left, s))
+        if node.right:
+          queue.append((node.right, s))
+      elif not res or res > s:
+        res = s
+    return res
+
+# class Solution:
+#   def tr(self, node, s):
+#     s = chr(97 + node.val) + s
+#     if node.left or node.right:
+#       if node.left:
+#         self.tr(node.left, s)
+#       if node.right:
+#         self.tr(node.right, s)
+#     elif not self.res or self.res > s:
+#       self.res = s
+#   def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+#     self.res = None
+#     self.tr(root, '')
+#     return self.res
