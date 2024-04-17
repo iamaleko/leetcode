c4c8c96ea23bc5628fa7659992aec72a8807a7ba
Alexander Kolobov <a@da.am>
2024-04-17 20:57:39 +0500

    Time: 68 ms (67.65%), Space: 55.8 MB (47.06%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.js
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.js
@@ -1,40 +1,41 @@
-// class ListNode {
-//   constructor(node, s = '', next = null) {
-//     this.node = node;
-//     this.s = s;
-//     this.next = next;
-//   }
-// }
-
-// const smallestFromLeaf = (root) => {
-//   let res = '',
-//       head = new ListNode(root);
-//   while (head) {
-//     const node = head.node,
-//           s = String.fromCharCode(97 + node.val) + head.s;
-//     head = head.next;
-//     if (node.left || node.right) {
-//       if (node.left) head = new ListNode(node.left, s, head);
-//       if (node.right) head = new ListNode(node.right, s, head);
-//     } else if (!res || res > s) {
-//       res = s;
-//     }
-//   }
-//   return res;
-// };
-
+// faster because of using linked list
+class ListNode {
+  constructor(node, s = '', next = null) {
+    this.node = node;
+    this.s = s;
+    this.next = next;
+  }
+}
 const smallestFromLeaf = (root) => {
-  let res = '';
-  const queue = [[root, '']];
-  while (queue.length) {
-    let [node, s] = queue.pop();
-    s = String.fromCharCode(97 + node.val) + s;
+  let res = '',
+      head = new ListNode(root);
+  while (head) {
+    const node = head.node,
+          s = String.fromCharCode(97 + node.val) + head.s;
+    head = head.next;
     if (node.left || node.right) {
-      if (node.left) queue.push([node.left, s]);
-      if (node.right) queue.push([node.right, s]);
+      if (node.left) head = new ListNode(node.left, s, head);
+      if (node.right) head = new ListNode(node.right, s, head);
     } else if (!res || res > s) {
       res = s;
     }
   }
   return res;
 };
+
+// slower because of using array as queue
+// const smallestFromLeaf = (root) => {
+//   let res = '';
+//   const queue = [[root, '']];
+//   while (queue.length) {
+//     let [node, s] = queue.pop();
+//     s = String.fromCharCode(97 + node.val) + s;
+//     if (node.left || node.right) {
+//       if (node.left) queue.push([node.left, s]);
+//       if (node.right) queue.push([node.right, s]);
+//     } else if (!res || res > s) {
+//       res = s;
+//     }
+//   }
+//   return res;
+// };
