c471858a78f989ef6821137d9059ea78ca14a482
Alexander Kolobov <a@da.am>
2024-04-17 20:57:51 +0500

    Time: 63 ms (79.41%), Space: 59.1 MB (8.82%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.js
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.js
@@ -1,21 +1,39 @@
 // faster because of using linked list
-class ListNode {
-  constructor(node, s = '', next = null) {
-    this.node = node;
-    this.s = s;
-    this.next = next;
-  }
-}
+// class ListNode {
+//   constructor(node, s = '', next = null) {
+//     this.node = node;
+//     this.s = s;
+//     this.next = next;
+//   }
+// }
+// const smallestFromLeaf = (root) => {
+//   let res = '',
+//       head = new ListNode(root);
+//   while (head) {
+//     const node = head.node,
+//           s = String.fromCharCode(97 + node.val) + head.s;
+//     head = head.next;
+//     if (node.left || node.right) {
+//       if (node.left) head = new ListNode(node.left, s, head);
+//       if (node.right) head = new ListNode(node.right, s, head);
+//     } else if (!res || res > s) {
+//       res = s;
+//     }
+//   }
+//   return res;
+// };
+
 const smallestFromLeaf = (root) => {
   let res = '',
-      head = new ListNode(root);
+      head = [root, '', null],
+      node,
+      s;
   while (head) {
-    const node = head.node,
-          s = String.fromCharCode(97 + node.val) + head.s;
-    head = head.next;
+    [node, s, head] = head;
+    s = String.fromCharCode(97 + node.val) + s;
     if (node.left || node.right) {
-      if (node.left) head = new ListNode(node.left, s, head);
-      if (node.right) head = new ListNode(node.right, s, head);
+      if (node.left) head = [node.left, s, head];
+      if (node.right) head = [node.right, s, head];
     } else if (!res || res > s) {
       res = s;
     }
