dd7cf34de4edd74895e84dc496d1f72e7f64f4aa
Alexander Kolobov <a@da.am>
2024-04-17 20:10:11 +0500

    Time: 47 ms (33.96%), Space: 17.7 MB (53.12%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
@@ -1,15 +1,30 @@
+# class Solution:
+#   def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+#     res = None
+#     queue = [(root, '')]
+#     while queue:
+#       node, s = queue.pop()
+#       s = chr(97 + node.val) + s
+#       if node.left or node.right:
+#         if node.left:
+#           queue.append((node.left, s))
+#         if node.right:
+#           queue.append((node.right, s))
+#       elif not res or res > s:
+#         res = s
+#     return res
+
 class Solution:
   def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
-    res = None
-    queue = [(root, '')]
-    while queue:
-      node, s = queue.pop()
+    self.res = None
+    def tr(node, s):
       s = chr(97 + node.val) + s
       if node.left or node.right:
         if node.left:
-          queue.append((node.left, s))
+          tr(node.left, s)
         if node.right:
-          queue.append((node.right, s))
-      elif not res or res > s:
-        res = s
-    return res
+          tr(node.right, s)
+      elif not self.res or self.res > s:
+        self.res = s
+    tr(root, '')
+    return self.res
