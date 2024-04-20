edb9df105807fc5218a9c2f5502a3c5a45e5699e
Alexander Kolobov <a@da.am>
2024-04-21 03:15:47 +0500

    Time: 63 ms (85.56%), Space: 52.9 MB (51.45%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,18 +3,18 @@
  * @return {number}
  */
 const numIslands = function(grid) {
-  const flood = (y, x) => {
+  const dfs = (y, x) => {
     grid[y][x] = '2';
-    if (grid[y][x - 1] === '1') flood(y, x - 1);
-    if (grid[y][x + 1] === '1') flood(y, x + 1);
-    if (grid[y - 1] && grid[y - 1][x] === '1') flood(y - 1, x);
-    if (grid[y + 1] && grid[y + 1][x] === '1') flood(y + 1, x);
+    if (grid[y][x - 1] === '1') dfs(y, x - 1);
+    if (grid[y][x + 1] === '1') dfs(y, x + 1);
+    if (grid[y - 1] && grid[y - 1][x] === '1') dfs(y - 1, x);
+    if (grid[y + 1] && grid[y + 1][x] === '1') dfs(y + 1, x);
   }
   let count = 0;
   for (const y in grid) {
     for (const x in grid[y]) {
       if (grid[y][x]!=='1') continue;
-      flood(+y, +x);
+      dfs(+y, +x);
       ++count;
     }
   }
