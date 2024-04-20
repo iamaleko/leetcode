e2ec3a4bb303e2cabdab111a34a258dd96ed9675
Alexander Kolobov <a@da.am>
2024-04-21 03:34:32 +0500

    Time: 122 ms (13.16%), Space: 59.9 MB (23.66%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -1,7 +1,38 @@
-/**
- * @param {character[][]} grid
- * @return {number}
- */
+class ListNode {
+  constructor(data, prev, next) {
+    this.data = data
+    this.prev = prev
+    this.next = next
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+  append(data) {
+    const node = new ListNode(data, this.tail, null);
+    if (this.tail) this.tail.next = node;
+    this.tail = node;
+    if (!this.head) this.head = node;
+    this.size++
+  }
+  popleft() {
+    const node = this.head;
+    if (this.head.next) {
+      this.head.next.prev = null;    
+      this.head = this.head.next;
+    } else {
+      this.head = null
+      this.tail = null
+    }
+    this.size--
+    return node.data;
+  }
+}
+
 const numIslands = function(grid) {
   let count = 0;
   for (let y in grid) {
@@ -11,28 +42,28 @@ const numIslands = function(grid) {
       x *= 1
       y *= 1
 
-      const queue = [];
-      queue.push([y, x])
+      const list = new LinkedList()
+      list.append([y, x])
       grid[y][x] = '2';
     
-      while (queue.length) {
-        let [y, x] = queue.pop();
+      while (list.size) {
+        let [y, x] = list.popleft();
         
         if (x > 0 && grid[y][x - 1] === '1') {
           grid[y][x - 1] = '2';
-          queue.push([y, x - 1]);
+          list.append([y, x - 1]);
         }
         if (y > 0 && grid[y - 1][x] === '1') {
           grid[y - 1][x] = '2';
-          queue.push([y - 1, x]);
+          list.append([y - 1, x]);
         }
         if (x < grid[0].length - 1 && grid[y][x + 1] === '1') {
           grid[y][x + 1] = '2';
-          queue.push([y, x + 1]);
+          list.append([y, x + 1]);
         }
         if (y < grid.length - 1 && grid[y + 1][x] === '1') {
           grid[y + 1][x] = '2';
-          queue.push([y + 1, x]);
+          list.append([y + 1, x]);
         }
       }
 
