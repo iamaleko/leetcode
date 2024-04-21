fdf3e7bd2e334e0f8e0f6e09b331f10acb9a1bfd
Alexander Kolobov <a@da.am>
2024-04-22 00:22:22 +0500

    Time: 1459 ms (73.67%), Space: 108.8 MB (58.93%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,54 +1,54 @@
 # union find, good article https://yuminlee2.medium.com/union-find-algorithm-ffa9cd7d2dba
-class UnionFind:
-  def __init__(self):
-    self.parent = {}
-    self.rank = {}
-  def add(self, i: int) -> int:
-    parent = self.find(i)
-    if parent == None:
-      self.parent[i] = i
-      parent = i
-      self.rank[parent] = 1
-    return parent
-  def find(self, i: int) -> int | None:
-    if i in self.parent:
-      while i != self.parent[i]:
-        self.parent[i] = self.parent[self.parent[i]]
-        i = self.parent[i]
-      return i
-    else:
-      return None
-  def union(self, i1: int, i2: int) -> int | bool:
-    parent1 = self.find(i1)
-    parent2 = self.find(i2)
-    if parent1 == None or parent2 == None:
-      return False
-    elif parent1 == parent2:
-      return parent1
-    elif self.parent[parent1] < self.parent[parent2]:
-      self.parent[parent1] = parent2
-      self.rank[parent2] += self.rank[parent1]
-      del self.rank[parent1]
-      return parent2
-    else:
-      self.parent[parent2] = parent1
-      self.rank[parent1] += self.rank[parent2]
-      del self.rank[parent2]
-      return parent1
-class Solution:
-  def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-    if source == destination:
-      return True
-    dsu = UnionFind()
-    for a, b in edges:
-      dsu.union(dsu.add(a), dsu.add(b))
-    return dsu.find(source) == dsu.find(destination)
+# class UnionFind:
+#   def __init__(self):
+#     self.parent = {}
+#     self.rank = {}
+#   def add(self, i: int) -> int:
+#     parent = self.find(i)
+#     if parent == None:
+#       self.parent[i] = i
+#       parent = i
+#       self.rank[parent] = 1
+#     return parent
+#   def find(self, i: int) -> int | None:
+#     if i in self.parent:
+#       while i != self.parent[i]:
+#         self.parent[i] = self.parent[self.parent[i]]
+#         i = self.parent[i]
+#       return i
+#     else:
+#       return None
+#   def union(self, i1: int, i2: int) -> int | bool:
+#     parent1 = self.find(i1)
+#     parent2 = self.find(i2)
+#     if parent1 == None or parent2 == None:
+#       return False
+#     elif parent1 == parent2:
+#       return parent1
+#     elif self.parent[parent1] < self.parent[parent2]:
+#       self.parent[parent1] = parent2
+#       self.rank[parent2] += self.rank[parent1]
+#       del self.rank[parent1]
+#       return parent2
+#     else:
+#       self.parent[parent2] = parent1
+#       self.rank[parent1] += self.rank[parent2]
+#       del self.rank[parent2]
+#       return parent1
+# class Solution:
+#   def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+#     if source == destination:
+#       return True
+#     dsu = UnionFind()
+#     for a, b in edges:
+#       dsu.union(dsu.add(a), dsu.add(b))
+#     return dsu.find(source) == dsu.find(destination)
 
 # graph + bfs
 # class Solution:
 #   def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-    # if source == destination:
-    #   return True
+#     if source == destination:
+#       return True
 #     graph = {}
 #     for a, b in edges:
 #       if a in graph:
@@ -71,3 +71,33 @@ class Solution:
 #             visited.add(neighbour)
 #             queue.append(neighbour)
 #     return False
+
+# union find inline
+class Solution:
+  def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+    if source == destination:
+      return True
+    # build disjoint sets
+    parent = {}
+    for a, b in edges:
+      if a not in parent:
+        parent[a] = a
+      if b not in parent:
+        parent[b] = b
+      while parent[a] != a:
+        a = parent[a]
+      while parent[b] != b:
+        b = parent[b]
+      if a != b:
+        parent[a] = b
+    # check appearance in any set
+    if source not in parent or destination not in parent:
+      return False
+    # check if in one set
+    while parent[source] != source:
+      source = parent[source]
+    while parent[destination] != destination:
+      destination = parent[destination]
+    return source == destination
+    
+    
