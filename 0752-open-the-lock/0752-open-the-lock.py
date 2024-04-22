7eacb4864b30bae290f91860fc0d539efd40f695
Alexander Kolobov <a@da.am>
2024-04-22 18:18:17 +0500

    Time: 1692 ms (8%), Space: 18.5 MB (15.3%) - LeetHub

---
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -0,0 +1,37 @@
+class Solution:
+  def openLock(self, deadends: List[str], target: str) -> int:
+    dp = {}
+    for end in deadends:
+      dp[end] = -1
+
+    def toKey(a,b,c,d):
+      return str(a) + str(b) + str(c) + str(d)
+
+    queue = deque([(0,0,0,0,0)])
+
+    while queue:
+      a,b,c,d,step = queue.popleft()
+      
+      key = toKey(a,b,c,d)
+      if key in dp and dp[key] <= step:
+        continue
+      dp[key] = step
+
+      if a < 9: queue.append((a+1,b,c,d,step + 1))
+      if b < 9: queue.append((a,b+1,c,d,step + 1))
+      if c < 9: queue.append((a,b,c+1,d,step + 1))
+      if d < 9: queue.append((a,b,c,d+1,step + 1))
+      if a > 0: queue.append((a-1,b,c,d,step + 1))
+      if b > 0: queue.append((a,b-1,c,d,step + 1))
+      if c > 0: queue.append((a,b,c-1,d,step + 1))
+      if d > 0: queue.append((a,b,c,d-1,step + 1))
+      if a == 9: queue.append((0,b,c,d,step + 1))
+      if b == 9: queue.append((a,0,c,d,step + 1))
+      if c == 9: queue.append((a,b,0,d,step + 1))
+      if d == 9: queue.append((a,b,c,0,step + 1))
+      if a == 0: queue.append((9,b,c,d,step + 1))
+      if b == 0: queue.append((a,9,c,d,step + 1))
+      if c == 0: queue.append((a,b,9,d,step + 1))
+      if d == 0: queue.append((a,b,c,9,step + 1))
+      
+    return dp[target] if target in dp else -1
