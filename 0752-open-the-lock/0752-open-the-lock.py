cab3cabf6c9eea4f67b6d285193b8f92c91a8fd0
Alexander Kolobov <a@da.am>
2024-04-22 18:43:31 +0500

    Time: 782 ms (20.24%), Space: 18.4 MB (15.3%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -8,21 +8,17 @@ class Solution:
 
     while queue:
       a,b,c,d,step = queue.popleft()
-      
-      key = (a,b,c,d)
-      if key in dp:
-        continue
-      dp[key] = step
-
-      step += 1
-      queue.append((abs((a + 1) % 10), b, c, d, step))
-      queue.append((abs((a - 1) % 10), b, c, d, step))
-      queue.append((a, abs((b + 1) % 10), c, d, step))
-      queue.append((a, abs((b - 1) % 10), c, d, step))
-      queue.append((a, b, abs((c + 1) % 10), d, step))
-      queue.append((a, b, abs((c - 1) % 10), d, step))
-      queue.append((a, b, c, abs((d + 1) % 10), step))
-      queue.append((a, b, c, abs((d - 1) % 10), step))
+      if (a,b,c,d) not in dp:
+        dp[(a,b,c,d)] = step
+        step += 1
+        queue.append((abs((a + 1) % 10), b, c, d, step))
+        queue.append((abs((a - 1) % 10), b, c, d, step))
+        queue.append((a, abs((b + 1) % 10), c, d, step))
+        queue.append((a, abs((b - 1) % 10), c, d, step))
+        queue.append((a, b, abs((c + 1) % 10), d, step))
+        queue.append((a, b, abs((c - 1) % 10), d, step))
+        queue.append((a, b, c, abs((d + 1) % 10), step))
+        queue.append((a, b, c, abs((d - 1) % 10), step))
     
     target = tuple([int(d) for d in target])
     return dp[target] if target in dp else -1
