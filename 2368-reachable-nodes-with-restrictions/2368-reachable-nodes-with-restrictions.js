be72e24407ae99c25095c03f635a0e04f732f437
Alexander Kolobov <a@da.am>
2024-04-22 23:09:01 +0500

    Time: 268 ms (99.28%), Space: 95.3 MB (100%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.js
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.js
@@ -1,37 +1,24 @@
 const reachableNodes = (n, edges, restricted) => {
   restricted = new Set(restricted);
-  const parents = {}, rank = {}
+  const parents = {}, rank = {};
+  // create disjoint sets
+  while (n--) {
+    parents[n] = n;
+    rank[n] = 1;
+  }
+  // union sets
   for (let [a, b] of edges) {
     if (restricted.has(a) || restricted.has(b)) continue;
-    if (a in parents) {
-      while (a !== parents[a]) {
-        parents[a] = parents[parents[a]]
-        a = parents[a]
-      }
-    } else {
-      parents[a] = a;
-      rank[a] = 1
-    }
-    if (b in parents) {
-      while (b !== parents[b]) {
-        parents[b] = parents[parents[b]]
-        b = parents[b]
-      }
-    } else {
-      parents[b] = b;
-      rank[b] = 1
-    }
+    while (a !== parents[a]) a = parents[a] = parents[parents[a]];
+    while (b !== parents[b]) b = parents[b] = parents[parents[b]];
     if (a !== b) {
-      if (rank[a] > rank[b]) [a, b] = [b, a]
-      parents[a] = b
-      rank[b] += rank[a]
-      delete rank[a]
+      if (rank[a] > rank[b]) [a, b] = [b, a];
+      parents[a] = b;
+      rank[b] += rank[a];
+      delete rank[a];
     }
   }
-  if (0 in parents) {
-    z = 0
-    while (z !== parents[z]) z = parents[z];
-    return rank[z];
-  }
-  return 1;
+  n = 0
+  while (n !== parents[n]) n = parents[n];
+  return rank[n];
 };
