2b2e66ceef40e2abd0567d6ee21a4d88bdc71d9c
Alexander Kolobov <a@da.am>
2024-04-25 01:04:44 +0500

    Time: 38 ms (29.68%), Space: 16.5 MB (85.13%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,8 +1,19 @@
+# class Solution:
+#   def __init__(self):
+#     self.ls = [0,1,1]
+#     for i in range(3, 38):
+#       self.ls.append(self.ls[i - 3] + self.ls[i - 2] + self.ls[i - 1])
+#   def tribonacci(self, n: int) -> int:
+#     return self.ls[n]
+
 class Solution:
-  def __init__(self):
-    self.ls = [0,1,1]
-    for i in range(3, 38):
-      self.ls.append(self.ls[i - 3] + self.ls[i - 2] + self.ls[i - 1])
   def tribonacci(self, n: int) -> int:
-    return self.ls[n]
+    if n == 0: return 0
+    if n < 3: return 1
+    a = 0
+    b = 1
+    c = 1
+    for i in range(3, n + 1):
+      a, b, c = b, c, a + b + c
+    return c
         
