4d5821dda3aa2a926f609cce283ef33637a534dc
Alexander Kolobov <a@da.am>
2024-04-26 00:31:15 +0400

    Time: 1141 ms (57.67%), Space: 22.5 MB (8.03%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
@@ -1,11 +1,25 @@
+# class Solution:
+#   def longestIdealString(self, s: str, k: int) -> int:
+#     last = {}
+#     dp = [1 for i in range(len(s))]
+#     for i in range(len(s) - 1, -1, -1):
+#       for chr in last:
+#         if abs(ord(s[i]) - ord(chr)) <= k and dp[last[chr]] + 1 > dp[i]:
+#           dp[i] = dp[last[chr]] + 1
+#       last[s[i]] = i
+#     return max(dp)
+
 class Solution:
   def longestIdealString(self, s: str, k: int) -> int:
     last = {}
     dp = [1 for i in range(len(s))]
     for i in range(len(s) - 1, -1, -1):
-      for chr in last:
-        if abs(ord(s[i]) - ord(chr)) <= k and dp[last[chr]] + 1 > dp[i]:
-          dp[i] = dp[last[chr]] + 1
-      last[s[i]] = i
+      a = s[i]
+      for j in range(ord(a) - k, ord(a) + k + 1):
+        b = chr(j)
+        if b in last and dp[last[b]] + 1 > dp[i]:
+          dp[i] = dp[last[b]] + 1
+      last[a] = i
+    print(dp)
     return max(dp)
         
