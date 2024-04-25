3f33407a2cac04ebc53b0166fa12fedbed564c1a
Alexander Kolobov <a@da.am>
2024-04-26 00:51:29 +0400

    Time: 414 ms (70.07%), Space: 17.3 MB (68.61%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
@@ -9,16 +9,24 @@
 #       last[s[i]] = i
 #     return max(dp)
 
+# class Solution:
+#   def longestIdealString(self, s: str, k: int) -> int:
+#     last = {}
+#     dp = [1 for i in range(len(s))]
+#     for i in range(len(s) - 1, -1, -1):
+#       for j in range(ord(s[i]) - k, ord(s[i]) + k + 1):
+#         if chr(j) in last and dp[last[chr(j)]] + 1 > dp[i]:
+#           dp[i] = dp[last[chr(j)]] + 1
+#       last[s[i]] = i
+#     return max(dp)
+
 class Solution:
   def longestIdealString(self, s: str, k: int) -> int:
-    last = {}
-    dp = [1 for i in range(len(s))]
+    last = [0 for _ in range(ord('z') + k + 1)]
     for i in range(len(s) - 1, -1, -1):
-      a = ord(s[i])
-      for j in range(a - k, a + k + 1):
-        b = chr(j)
-        if b in last and dp[last[b]] + 1 > dp[i]:
-          dp[i] = dp[last[b]] + 1
-      last[s[i]] = i
-    return max(dp)
-        
+      longest = 0
+      for j in range(ord(s[i]) - k, ord(s[i]) + k + 1):
+        if last[j] > longest:
+          longest = last[j]
+      last[ord(s[i])] = longest + 1
+    return max(last)
