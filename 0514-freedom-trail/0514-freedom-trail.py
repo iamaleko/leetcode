dfafed321f1a9dd8b71690a739d5c8a45be3d346
Alexander Kolobov <a@da.am>
2024-04-28 00:20:38 +0400

    Time: 93 ms (79.15%), Space: 16.3 MB (100%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.py
+++ 0514-freedom-trail/0514-freedom-trail.py
@@ -11,15 +11,9 @@ class Solution:
     for i, letter in enumerate(ring):
       m[letter].append(i)
 
-    spent = {}
-    for rp in m[key[0]]:
-      if rp in spent:
-        spent[rp] = min(cost(0, rp), spent[rp])
-      else:
-        spent[rp] = cost(0, rp)
-    # print(0, spent)
+    spent = {0: 0}
 
-    for kp in range(1, len(key)):
+    for kp in range(len(key)):
       _spent = {}
       for rp in m[key[kp]]:
         for prp in spent:
@@ -27,7 +21,6 @@ class Solution:
             _spent[rp] = min(spent[prp] + cost(prp, rp), _spent[rp])
           else:
             _spent[rp] = spent[prp] + cost(prp, rp)
-      # print(kp, _spent)
       spent = _spent
 
     return min(spent.values())
