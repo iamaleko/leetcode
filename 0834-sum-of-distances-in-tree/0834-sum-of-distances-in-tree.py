bcbb81a66648961dd5c6bd764718a44dd1d624fc
Alexander Kolobov <a@da.am>
2024-04-29 00:13:54 +0400

    Time: 901 ms (11.32%), Space: 44.7 MB (51.75%) - LeetHub

---
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.py
@@ -0,0 +1,60 @@
+class Solution:
+  def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
+    # build graph
+    tree = defaultdict(set)
+    for a, b in edges:
+      tree[a].add(b)
+      tree[b].add(a)
+
+    # detect root
+    root = 0
+    for val in tree:
+      if len(tree[root]) < len(tree[val]):
+        root = val
+
+    # build tree from detected root
+    children = [set() for _ in range(n)]
+    parents = [None] * n
+    q = deque([root])
+    while q:
+      i = q.popleft()
+      for val in tree[i]:
+        children[i].add(val)
+        parents[val] = i
+        tree[val].remove(i)
+        q.append(val)
+
+    # calc answer and nodes count from leaves to root
+    ans = [0] * n
+    nodes = [0] * n
+    visited = [0] * n
+    q = deque()
+    for i in range(len(children)):
+      if not len(children[i]):
+        q.append(i)
+    while q:
+      i = q.popleft()
+      nodes[i] += 1
+      if i != root:
+        parent = parents[i]
+        visited[parent] += 1
+        ans[parent] += ans[i] + nodes[i]
+        nodes[parent] += nodes[i]
+        if visited[parent] == len(children[parent]):
+          q.append(parent)
+
+    # reroot, calc other nodes from root to leaves
+    q.clear()
+    for child in children[root]:
+      q.append(child)
+    while q:
+      i = q.popleft()
+      inds = nodes[i]
+      ians = ans[i]
+      pnds = nodes[root]
+      pans = ans[parents[i]]
+      ans[i] += (pans - (ans[i] + nodes[i])) + (pnds - inds)
+      for child in children[i]:
+        q.append(child)
+    
+    return ans
