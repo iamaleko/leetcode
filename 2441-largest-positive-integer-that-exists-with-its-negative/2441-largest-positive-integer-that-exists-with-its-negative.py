b82bd8c170f4ffddaa2e35fabf4382341667e297
Alexander Kolobov <a@da.am>
2024-05-02 17:32:40 +0400

    Time: 106 ms (72.32%), Space: 16.8 MB (77.07%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.py
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.py
@@ -4,8 +4,9 @@ class Solution:
     for n in nums:
       if n < 0:
         st.add(-n)
+    m = -1
     for n in nums:
-      if n > 0 and n in st:
-        return n
-    return -1
+      if n > 0 and n in st and m < n:
+        m = n
+    return m
         
