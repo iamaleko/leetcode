3bede70587aaae87677ddd22b8ecc3e286bea751
Alexander Kolobov <a@da.am>
2024-05-04 00:03:49 +0400

    Time: 39 ms (25.46%), Space: 16.4 MB (91.86%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.py
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -3,13 +3,11 @@ class Solution:
     if a and b:
       a = [int(e) for e in a.split('.')]
       b = [int(e) for e in b.split('.')]
-      i = 0
-      while i < len(a) or i < len(b):
+      for i in range(max(len(a), len(b))):
         an = a[i] if i < len(a) else 0
         bn = b[i] if i < len(b) else 0
         if an < bn:
           return -1
         elif an > bn:
           return 1
-        i += 1
     return 0    
