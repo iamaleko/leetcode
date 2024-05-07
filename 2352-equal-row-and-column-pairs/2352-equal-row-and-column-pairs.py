b1f1101abba568017691fdb81d19014f92c3aa6c
Alexander Kolobov <a@da.am>
2024-05-07 20:58:39 +0400

    Time: 374 ms (85.88%), Space: 21.6 MB (31.49%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,15 +1,28 @@
+# class Solution:
+#   def equalPairs(self, grid: List[List[int]]) -> int:
+#     res = 0
+#     d = defaultdict(int)
+#     for row in grid:
+#       key = str(row)
+#       d[key] += 1
+#     for x in range(len(grid[0])):
+#       col = []
+#       for row in grid:
+#         col.append(row[x])
+#       key = str(col)
+#       res += d[key]
+#     return res
+
 class Solution:
   def equalPairs(self, grid: List[List[int]]) -> int:
     res = 0
     d = defaultdict(int)
     for row in grid:
-      key = str(row)
-      d[key] += 1
+      d[tuple(row)] += 1
     for x in range(len(grid[0])):
       col = []
       for row in grid:
         col.append(row[x])
-      key = str(col)
-      res += d[key]
+      res += d[tuple(col)]
     return res
 
