c50e031af751c504c7a3d355f312c840aed55ef8
Alexander Kolobov <a@da.am>
2024-05-10 22:51:49 +0400

    Time: 547 ms (77.29%), Space: 16.8 MB (91.19%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.py
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.py
@@ -1,8 +1,25 @@
+# class Solution:
+#   def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+#     ls = []
+#     for i in range(len(arr) - 1, -1, -1):
+#       for j in range(i):
+#         ls.append(([arr[j],arr[i]],arr[j]/arr[i]))
+#     ls.sort(key = lambda x: x[1])
+#     return ls[k - 1][0]
+
 class Solution:
   def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-    ls = []
-    for i in range(len(arr) - 1, -1, -1):
-      for j in range(i):
-        ls.append(([arr[j],arr[i]],arr[j]/arr[i]))
-    ls.sort(key = lambda x: x[1])
-    return ls[k - 1][0]
+    h = []
+    for b, n in enumerate(arr):
+      heappush(h, (1 / n, 0, b))
+    while k:
+      _, a, b = heappop(h)
+      k -= 1
+      if not k:
+        return [arr[a], arr[b]]
+      if a < b - 1:
+        heappush(h, (arr[a + 1] / arr[b], a + 1, b))
+
+      
+
+    
