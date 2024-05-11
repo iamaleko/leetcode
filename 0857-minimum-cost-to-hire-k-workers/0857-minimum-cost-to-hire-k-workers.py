e44c0392cba7f6d5c646ffdfb1c786b09cc4ba39
Alexander Kolobov <a@da.am>
2024-05-11 23:12:03 +0400

    Time: 156 ms (54.6%), Space: 19 MB (22.85%) - LeetHub

--- 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
@@ -8,16 +8,23 @@ class Solution:
     ratio.sort(key = lambda x: x[0])
 
     ans = math.inf
-    h = []
-    qsum = 0
+    h = [] # max heap for workers based on their quality
+    qsum = 0 # sum of workers quality to the left of base worker
     # fill heap and quality sum
-    for i in range(k - 1):
-      qsum += ratio[i][1]
-      heappush(h, -ratio[i][1])
+    # for i in range(k - 1):
+    #   qsum += ratio[i][1]
+    #   heappush(h, -ratio[i][1])
+    for worker in ratio[0:k - 1]:
+      qsum += worker[1]
+      heappush(h, -worker[1])
     # move forward and remove most expensive worker so far
-    for i in range(k - 1, len(ratio)):
-      ans = min(ans, ratio[i][2] + qsum * ratio[i][0])
-      qsum += heappop(h) + ratio[i][1]
-      heappush(h, -ratio[i][1])
+    # for i in range(k - 1, len(ratio)):
+    #   ans = min(ans, ratio[i][2] + qsum * ratio[i][0])
+    #   qsum += heappop(h) + ratio[i][1]
+    #   heappush(h, -ratio[i][1])
+    for worker in ratio[k - 1:]:
+      ans = min(ans, worker[2] + qsum * worker[0])
+      qsum += heappop(h) + worker[1]
+      heappush(h, -worker[1])
 
     return ans
