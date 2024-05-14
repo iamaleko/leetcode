e3fa0a2ec5fd8c3fd5a348f172c25fbfdc86019e
Alexander Kolobov <a@da.am>
2024-05-14 19:43:55 +0400

    Time: 1077 ms (96.37%), Space: 16.6 MB (71.32%) - LeetHub

---
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -0,0 +1,23 @@
+class Solution:
+  def getMaximumGold(self, grid: List[List[int]]) -> int:
+    mx = len(grid[0]) - 1
+    my = len(grid) - 1
+    self.ans = 0
+
+    def dfs(y, x, s):
+      t = grid[y][x]
+      s += t
+      grid[y][x] = 0
+      if y > 0 and grid[y - 1][x]: dfs(y - 1, x, s)
+      if y < my and grid[y + 1][x]: dfs(y + 1, x, s)
+      if x > 0 and grid[y][x - 1]: dfs(y, x - 1, s)
+      if x < mx and grid[y][x + 1]: dfs(y, x + 1, s)
+      grid[y][x] = t
+      if self.ans < s:
+        self.ans = s
+
+    for y in range(my + 1):
+      for x in range(mx + 1):
+        dfs(y, x, 0)
+
+    return self.ans
