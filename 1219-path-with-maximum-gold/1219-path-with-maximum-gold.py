b24d1fdb99ff5410f278dbfa8885df7e95f06703
Alexander Kolobov <a@da.am>
2024-05-14 19:49:46 +0400

    Time: 946 ms (97.46%), Space: 16.7 MB (15.06%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -8,16 +8,21 @@ class Solution:
       t = grid[y][x]
       s += t
       grid[y][x] = 0
-      if y > 0 and grid[y - 1][x]: dfs(y - 1, x, s)
-      if y < my and grid[y + 1][x]: dfs(y + 1, x, s)
-      if x > 0 and grid[y][x - 1]: dfs(y, x - 1, s)
-      if x < mx and grid[y][x + 1]: dfs(y, x + 1, s)
+      if y > 0 and grid[y - 1][x]:
+        dfs(y - 1, x, s)
+      if y < my and grid[y + 1][x]:
+        dfs(y + 1, x, s)
+      if x > 0 and grid[y][x - 1]:
+        dfs(y, x - 1, s)
+      if x < mx and grid[y][x + 1]:
+        dfs(y, x + 1, s)
       grid[y][x] = t
       if self.ans < s:
         self.ans = s
 
     for y in range(my + 1):
       for x in range(mx + 1):
-        dfs(y, x, 0)
+        if grid[y][x]:
+          dfs(y, x, 0)
 
     return self.ans
