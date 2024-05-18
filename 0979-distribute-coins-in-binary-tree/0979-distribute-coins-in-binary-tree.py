3ff364eaf9e08eef5ed2d0379b0a19b3785347b4
Alexander Kolobov <a@da.am>
2024-05-18 19:11:48 +0400

    Time: 43 ms (23.31%), Space: 16.8 MB (9.41%) - LeetHub

--- 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.py
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.py
@@ -1,6 +1,6 @@
 class Solution:
   def distributeCoins(self, root: Optional[TreeNode]) -> int:
-    need = {}
+    need = { None: None }
     parent = {}
     leafs = deque()
 
@@ -18,42 +18,26 @@ class Solution:
     steps = 0
     while leafs:
       leaf = leafs.popleft()
-      needLeft = need[leaf.left] if leaf.left else None
-      needRight = need[leaf.right] if leaf.right else None
-      needLeaf = deque()
-
-
-      while needLeft:
-        if leaf.val:
-          steps += needLeft.popleft()
-          leaf.val -= 1
-        else:
-          for n in needLeft:
-            needLeaf.append(n + 1)
-          break
-
-      while needRight:
-        if leaf.val:
-          steps += needRight.popleft()
-          leaf.val -= 1
-        else:
-          for n in needRight:
-            needLeaf.append(n + 1)
-          break
+      left = need[leaf.left] if leaf.left else None
+      right = need[leaf.right] if leaf.right else None
+      
+      need[leaf] = deque()
+      
+      for side in [left, right, deque([0])]:
+        while side:
+          if leaf.val:
+            steps += side.popleft()
+            leaf.val -= 1
+          else:
+            for n in side:
+              need[leaf].append(n + 1)
+            break
 
       if leaf.val > 0:
-        leaf.val -= 1
-        if leaf.val:
-          steps += leaf.val
-          parent[leaf].val += leaf.val
-      else:
-        needLeaf.append(1)
-
-      need[leaf] = needLeaf
+        steps += leaf.val
+        parent[leaf].val += leaf.val
       
-      if leaf in parent:
-        if not parent[leaf].left or parent[leaf].left in need:
-          if not parent[leaf].right or parent[leaf].right in need:
-            leafs.append(parent[leaf])
+      if leaf in parent and parent[leaf].left in need and parent[leaf].right in need:
+        leafs.append(parent[leaf])
       
     return steps
