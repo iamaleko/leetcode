a39cf67946907f4c348d3646ec79bc44e81fed26
Alexander Kolobov <a@da.am>
2024-05-21 16:13:42 +0400

    Time: 68 ms (38.05%), Space: 17 MB (16.91%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -1,8 +1,23 @@
 class Solution:
-  def subsetXORSum(self, nums: List[int]) -> int:
-    def rec(acc, i):
-      if i == len(nums):
-        return acc
-      else:
-        return rec(acc ^ nums[i], i + 1) + rec(acc, i + 1)
-    return rec(0, 0)
+  def subsetXORSum(self, nums: List[int]) -> List[List[int]]:
+    subs = [[]]
+    for num in nums:
+      for i in range(len(subs)):
+        subs.append(subs[i][:] + [num])
+    
+    ans = 0
+    for sub in subs:
+      xor = 0
+      for num in sub:
+        xor ^= num
+      ans += xor
+    return ans
+
+# class Solution:
+#   def subsetXORSum(self, nums: List[int]) -> int:
+#     def rec(acc, i):
+#       if i == len(nums):
+#         return acc
+#       else:
+#         return rec(acc ^ nums[i], i + 1) + rec(acc, i + 1)
+#     return rec(0, 0)
