5dd88d41b351014272d4e47fab0c28f3bdd1a98b
Alexander Kolobov <a@da.am>
2024-05-23 23:33:55 +0400

    Time: 1882 ms (72.91%), Space: 17.1 MB (13.49%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
@@ -13,35 +13,37 @@
 
 #     return len(subsets)
 
-# class Solution:
-#   def beautifulSubsets(self, nums, k):
-#     nums.sort()
-#     path = [0] * 1001
-#     def backtrack(i):
-#       ans = 0
-#       for j in range(i + 1, len(nums)):
-#         if path[nums[j] - k] == 0:
-#           path[nums[j]] += 1
-#           ans += 1 + backtrack(j)
-#           path[nums[j]] -= 1
-#       return ans
-#     return backtrack(-1)
-
 class Solution:
   def beautifulSubsets(self, nums, k):
     nums.sort()
-    path = set()
-    ln = len(nums)
-    
+    path = [0] * 1001
+    n = len(nums)
+
     def backtrack(i):
       ans = 0
-      for i in range(i, ln):
-        if nums[i] - k not in path:
-          if nums[i] in path:
-            ans += 1 + backtrack(i + 1)
-          else:
-            path.add(nums[i])
-            ans += 1 + backtrack(i + 1)
-            path.remove(nums[i])
+      for j in range(i, n):
+        if path[nums[j] - k] == 0:
+          path[nums[j]] += 1
+          ans += 1 + backtrack(j + 1)
+          path[nums[j]] -= 1
       return ans
     return backtrack(0)
+
+# class Solution:
+#   def beautifulSubsets(self, nums, k):
+#     nums.sort()
+#     path = set()
+#     n = len(nums)
+    
+#     def backtrack(i):
+#       ans = 0
+#       for i in range(i, n):
+#         if nums[i] - k not in path:
+#           if nums[i] in path:
+#             ans += 1 + backtrack(i + 1)
+#           else:
+#             path.add(nums[i])
+#             ans += 1 + backtrack(i + 1)
+#             path.remove(nums[i])
+#       return ans
+#     return backtrack(0)
