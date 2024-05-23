f8196a8ed4f492896c4af67d57adb08dfc2f0ce7
Alexander Kolobov <a@da.am>
2024-05-23 23:25:14 +0400

    Time: 1903 ms (73.02%), Space: 16.5 MB (96.46%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
@@ -1,14 +1,31 @@
+# class Solution:
+#   def beautifulSubsets(self, nums: List[int], k: int) -> int:
+#     nums.sort()
+#     subsets = []
+
+#     for n in nums:
+#       for i in range(len(subsets)):
+#         if (n - k) not in subsets[i]:
+#           subset = subsets[i].copy()
+#           subset.add(n)
+#           subsets.append(subset)
+#       subsets.append(set([n]))
+
+#     return len(subsets)
+
 class Solution:
-  def beautifulSubsets(self, nums: List[int], k: int) -> int:
+  def beautifulSubsets(self, nums, k):
     nums.sort()
-    subsets = []
-
-    for n in nums:
-      for i in range(len(subsets)):
-        if (n - k) not in subsets[i]:
-          subset = subsets[i].copy()
-          subset.add(n)
-          subsets.append(subset)
-      subsets.append(set([n]))
-
-    return len(subsets)
+    path = set()
+    def backtrack(i):
+      ans = 0
+      for j in range(i + 1, len(nums)):
+        if nums[j] - k not in path:
+          if nums[j] in path:
+            ans += 1 + backtrack(j)
+          else:
+            path.add(nums[j])
+            ans += 1 + backtrack(j)
+            path.remove(nums[j])
+      return ans
+    return backtrack(-1)
