08812f1baab3c0d7fa91d07aae079539952087e1
Alexander Kolobov <a@da.am>
2024-05-23 23:27:36 +0400

    Time: 1879 ms (73.19%), Space: 16.6 MB (84.32%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
@@ -13,16 +13,33 @@
 
 #     return len(subsets)
 
+# class Solution:
+#   def beautifulSubsets(self, nums, k):
+#     nums.sort()
+#     path = [0] * 1001
+#     def backtrack(i):
+#       ans = 0
+#       for j in range(i + 1, len(nums)):
+#         if path[nums[j] - k] == 0:
+#           path[nums[j]] += 1
+#           ans += 1 + backtrack(j)
+#           path[nums[j]] -= 1
+#       return ans
+#     return backtrack(-1)
+
 class Solution:
   def beautifulSubsets(self, nums, k):
     nums.sort()
-    path = [0] * 1001
+    path = set()
     def backtrack(i):
       ans = 0
       for j in range(i + 1, len(nums)):
-        if path[nums[j] - k] == 0:
-          path[nums[j]] += 1
-          ans += 1 + backtrack(j)
-          path[nums[j]] -= 1
+        if nums[j] - k not in path:
+          if nums[j] in path:
+            ans += 1 + backtrack(j)
+          else:
+            path.add(nums[j])
+            ans += 1 + backtrack(j)
+            path.remove(nums[j])
       return ans
     return backtrack(-1)
