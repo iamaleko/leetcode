36424be371d7d2e4889db23210ae243bf793e4b2
Alexander Kolobov <a@da.am>
2024-05-25 23:12:41 +0400

    Time: 29 ms (90.21%), Space: 16.3 MB (99.87%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -3,12 +3,10 @@ class Solution:
     ans = []
     def backtrack(i, sentence):
       if i == len(s):
-        ans.append(sentence[1:])
-      else:
-        for word in words:
-          j = i + len(word)
-          if s[i:j] == word:
-            backtrack(j, sentence + ' ' + s[i:j])
+        return ans.append(sentence[1:])
+      for word in words:
+        if s[i:i + len(word)] == word:
+          backtrack(i + len(word), sentence + ' ' + s[i:i + len(word)])
     backtrack(0, '')
     return ans
         
