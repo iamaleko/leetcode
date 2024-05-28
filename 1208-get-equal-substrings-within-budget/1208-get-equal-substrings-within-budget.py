2772121d27a3c31fe466e1233ad79344eaa52f91
Alexander Kolobov <a@da.am>
2024-05-28 22:10:49 +0400

    Time: 51 ms (96.29%), Space: 21.2 MB (5.91%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
@@ -30,12 +30,13 @@
 # Running sum optimized
 class Solution:
   def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+    n = len(s)
     cumsum = [0]
     ans = 0
     l = 0
-    s += ' '
-    t += ' '
-    for r in range(len(s)):
+    s += 'a'
+    t += 'a'
+    for r in range(n + 1):
       cumsum.append(cumsum[-1] + abs(ord(s[r]) - ord(t[r])))
       while cumsum[r] - cumsum[l] > maxCost:
         l += 1
