7a116bb0cbebc51147e22052fb2eb1c3dfd8a043
Alexander Kolobov <a@da.am>
2024-05-28 21:59:31 +0400

    Time: 61 ms (74.86%), Space: 21 MB (6.32%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
@@ -14,14 +14,30 @@
 #     return ans
 
 # Running sum
+# class Solution:
+#   def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+#     cumsum = 0
+#     arr = [0] + [cumsum := abs(ord(s[i]) - ord(t[i])) + cumsum for i in range(len(s))]
+#     ans = 0
+#     l = 0
+#     for r in range(len(arr)):
+#       while arr[r] - arr[l] > maxCost:
+#         l += 1
+#       if r - l > ans:
+#         ans = r - l
+#     return ans
+
+# Running sum optimized
 class Solution:
   def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-    cumsum = 0
-    arr = [0] + [cumsum := abs(ord(s[i]) - ord(t[i])) + cumsum for i in range(len(s))]
+    n = len(s)
+    cumsum = [0] * (n + 1)
     ans = 0
     l = 0
-    for r in range(len(arr)):
-      while arr[r] - arr[l] > maxCost:
+    for r in range(n + 1):
+      if r < n:
+        cumsum[r + 1] = cumsum[r] + abs(ord(s[r]) - ord(t[r]))
+      while cumsum[r] - cumsum[l] > maxCost:
         l += 1
       if r - l > ans:
         ans = r - l
