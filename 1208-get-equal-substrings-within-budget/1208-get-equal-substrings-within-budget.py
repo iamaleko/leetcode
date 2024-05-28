924db31cf323ee9805b02e617395b0ce86419928
Alexander Kolobov <a@da.am>
2024-05-28 21:58:40 +0400

    Time: 62 ms (70.88%), Space: 22 MB (5.91%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
@@ -1,13 +1,28 @@
+# Sliding window
+# class Solution:
+#   def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+#     ans = 0
+#     l = 0
+#     cost = 0
+#     for r in range(len(t)):
+#       cost += abs(ord(s[r]) - ord(t[r]))
+#       while cost > maxCost:
+#         cost -= abs(ord(s[l]) - ord(t[l]))
+#         l += 1
+#       if r - l + 1 > ans:
+#         ans = r - l + 1
+#     return ans
+
+# Running sum
 class Solution:
   def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+    cumsum = 0
+    arr = [0] + [cumsum := abs(ord(s[i]) - ord(t[i])) + cumsum for i in range(len(s))]
     ans = 0
     l = 0
-    cost = 0
-    for r in range(len(t)):
-      cost += abs(ord(s[r]) - ord(t[r]))
-      while cost > maxCost:
-        cost -= abs(ord(s[l]) - ord(t[l]))
+    for r in range(len(arr)):
+      while arr[r] - arr[l] > maxCost:
         l += 1
-      if r - l + 1 > ans:
-        ans = r - l + 1
+      if r - l > ans:
+        ans = r - l
     return ans
