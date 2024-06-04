2f1159e8bf721731a6ccea9ad4103a3b9865ed58
Alexander Kolobov <a@da.am>
2024-06-04 20:07:02 +0400

    Time: 34 ms (76.19%), Space: 16.4 MB (99.86%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,14 +1,25 @@
+# class Solution:
+#   def longestPalindrome(self, s: str) -> int:
+#     d = defaultdict(int)
+#     for letter in s:
+#       d[letter] += 1
+#     ans = 0
+#     addition = 0
+#     for letter in d:
+#       if d[letter] & 1:
+#         addition = 1
+#         ans += d[letter] - 1
+#       else:
+#         ans += d[letter]
+#     return ans + addition
+
 class Solution:
   def longestPalindrome(self, s: str) -> int:
-    d = defaultdict(int)
-    for letter in s:
-      d[letter] += 1
+    m = 0
     ans = 0
-    addition = 0
-    for letter in d:
-      if d[letter] & 1:
-        addition = 1
-        ans += d[letter] - 1
-      else:
-        ans += d[letter]
-    return ans + addition
+    for letter in s:
+      bit = 1 << (ord(letter) - 65)
+      if m & bit:
+        ans += 2
+      m ^= bit
+    return ans + int(bool(m))
