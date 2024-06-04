b0903e994bc51d9c2218e09f082f24f8afe0be30
Alexander Kolobov <a@da.am>
2024-06-05 02:37:56 +0400

    Time: 389 ms (100%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,1 +1,6 @@
-select max(num) num from (select num, count(1) cnt from MyNumbers group by num) sub where cnt = 1
+-- select max(num) num from (select num, count(1) cnt from MyNumbers group by num) sub where cnt = 1
+
+select coalesce(
+  (select num from MyNumbers group by num having count(1) = 1 order by num desc limit 1),
+  null
+) num
