992c4d24467bc78af87b7a0a69ff89c042f24b04
Alexander Kolobov <a@da.am>
2024-06-06 19:23:48 +0400

    Time: 167 ms (45.08%), Space: 18.7 MB (10.4%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.py
+++ 0876-hand-of-straights/0876-hand-of-straights.py
@@ -1,19 +1,35 @@
+# class Solution:
+#   def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+#     if groupSize == 1:
+#       return True
+
+#     sets = defaultdict(list)
+#     hand.sort()
+
+#     for card in hand:
+#       size = sets[card].pop() + 1 if len(sets[card]) else 1
+#       if size < groupSize:
+#         sets[card + 1].append(size)
+
+#     for waited in sets:
+#       if len(sets[waited]):
+#         return False
+
+#     return True
+      
 class Solution:
   def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-    if groupSize == 1:
-      return True
-
-    sets = defaultdict(list)
+    sets = {}
     hand.sort()
 
     for card in hand:
-      size = sets[card].pop() + 1 if len(sets[card]) else 1
+      waited = card in sets
+      size = sets[card].pop() + 1 if waited else 1
       if size < groupSize:
+        if card + 1 not in sets:
+          sets[card + 1] = []
         sets[card + 1].append(size)
+      if waited and len(sets[card]) == 0:
+        del sets[card]
 
-    for waited in sets:
-      if len(sets[waited]):
-        return False
-
-    return True
-      
+    return len(sets) == 0
