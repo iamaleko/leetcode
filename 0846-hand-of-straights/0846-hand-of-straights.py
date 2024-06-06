c40885ea152777d459ca650cc3daeeccf7ed98f0
Alexander Kolobov <a@da.am>
2024-06-06 19:22:18 +0400

    Time: 182 ms (26.83%), Space: 18.3 MB (74.24%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -1,17 +1,27 @@
 class Solution:
   def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+    if groupSize == 1:
+      return True
+
     sets = {}
     heapify(hand)
 
     while hand:
       card = heappop(hand)
-      size = sets[card].pop() + 1 if card in sets else 1
-      if size < groupSize:
+      if card in sets:
+        size = sets[card].pop() + 1
+        if size < groupSize:
+          if card + 1 not in sets:
+            sets[card + 1] = [size]
+          else:
+            sets[card + 1].append(size)
+        if len(sets[card]) == 0:
+          del sets[card]
+      else:
         if card + 1 not in sets:
-          sets[card + 1] = []
-        sets[card + 1].append(size)
-      if card in sets and len(sets[card]) == 0:
-        del sets[card]
+          sets[card + 1] = [1]
+        else:
+          sets[card + 1].append(1)
 
     return len(sets) == 0
       
