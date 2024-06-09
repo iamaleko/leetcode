bd5047648b3bb709cb2b3f94117dcea4e1b19246
Alexander Kolobov <a@da.am>
2024-06-09 21:28:46 +0400

    Time: 219 ms (90.65%), Space: 21.4 MB (55.53%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.py
@@ -1,15 +1,28 @@
+# class Solution:
+#   def subarraysDivByK(self, nums: List[int], k: int) -> int:
+#     # running sum
+#     nums = [0] + nums
+#     for i in range(1, len(nums)):
+#       nums[i] += nums[i - 1]
+
+#     # divisible subarrays
+#     ans = 0
+#     m = defaultdict(int)
+#     for i in range(1, len(nums)):
+#       m[nums[i - 1] % k] += 1
+#       ans += m[nums[i] % k]
+    
+#     return ans
+
 class Solution:
   def subarraysDivByK(self, nums: List[int], k: int) -> int:
-    # running sum
-    nums = [0] + nums
-    for i in range(1, len(nums)):
-      nums[i] += nums[i - 1]
-
-    # divisible subarrays
     ans = 0
+    cum = 0
     m = defaultdict(int)
-    for i in range(1, len(nums)):
-      m[nums[i - 1] % k] += 1
-      ans += m[nums[i] % k]
+    m[0] = 1
+    for num in nums:
+      cum += num
+      ans += m[cum % k]
+      m[cum % k] += 1
     
     return ans
