f0b2ebbe191ab27a7638e44b005ecb961069bad5
Alexander Kolobov <a@da.am>
2024-06-10 22:19:25 +0400

    Time: 59 ms (33.85%), Space: 49.2 MB (32.16%) - LeetHub

--- 1051-height-checker/1051-height-checker.js
+++ 1051-height-checker/1051-height-checker.js
@@ -1,5 +1,5 @@
 const heightChecker = (h) => {
-  const e = h.slice().sort((a,b) => a - b)
+  const e = [...h].sort((a, b) => a - b)
   let ans = 0;
   for (let i = 0; i < h.length; i++) if (h[i] !== e[i]) ans++;
   return ans;
