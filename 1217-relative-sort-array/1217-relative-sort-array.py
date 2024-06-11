bcbe47c226bb1997d563db33a0483a45fb9fb4b8
Alexander Kolobov <a@da.am>
2024-06-12 00:22:41 +0400

    Time: 42 ms (50%), Space: 16.7 MB (66.46%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.py
+++ 1217-relative-sort-array/1217-relative-sort-array.py
@@ -1,10 +1,7 @@
 class Solution:
   def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-    m = {}
-    for i, num in enumerate(arr2):
-      m[num] = i
-    maxVal = max(arr2)
-    arr1.sort(key=lambda num: m[num] - maxVal if num in m else num)
+    m = {val: i for i, val in enumerate(arr2)}
+    arr1.sort(key=lambda num: (num not in m, m[num] if num in m else num))
     return arr1
 
         
