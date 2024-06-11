467cef2fb03bd5d027e8ec04053b06981888b2ab
Alexander Kolobov <a@da.am>
2024-06-12 00:27:24 +0400

    Time: 43 ms (43.54%), Space: 16.5 MB (93.33%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -1,7 +1,10 @@
 class Solution:
   def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
     m = {val: i for i, val in enumerate(arr2)}
-    arr1.sort(key=lambda num: (num not in m, m[num] if num in m else num))
+    arr1.sort(key=lambda num: (
+      notInArr2 := num not in m,
+      num if notInArr2 else m[num]
+    ))
     return arr1
 
         
