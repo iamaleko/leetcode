5678526f6138aea5c0396991327ba866a9c850e6
Alexander Kolobov <a@da.am>
2024-06-15 16:22:38 +0400

    Time: 2469 ms (5.02%), Space: 45 MB (5.02%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -1,21 +1,20 @@
 class Solution:
   def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-    projects = []
-    for i in range(len(capital)):
-      projects.append((profits[i], capital[i]))
-
+    projects = list(zip(profits, capital))
     projects.sort(key = lambda x: (-x[0], x[1]))
     
     p = 0
-    while k and projects:
-      if projects[p][1] > w:
+    n = len(projects)
+    while k and n:
+      while p < n and projects[p][1] > w:
         p += 1
-        if p == len(projects):
-          return w
-      else:
+      if p < n:
         w += projects[p][0]
         projects.pop(p)
         k -= 1
+        n -= 1
         p = 0
+      else:
+        return w
 
     return w
