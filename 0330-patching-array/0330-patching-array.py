78ee83717ae02f54cda78449016c2199453e2ad2
Alexander Kolobov <a@da.am>
2024-06-16 19:38:50 +0400

    Time: 53 ms (82.17%), Space: 16.7 MB (27.97%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.py
@@ -0,0 +1,52 @@
+# class Solution:
+#   def minPatches(self, nums: List[int], n: int) -> int:
+#     # sums set
+#     sums = set()
+#     for num in nums:
+#       extention = set()
+#       for item in sums:
+#         extention.add(item + num)
+#       extention.add(num)
+#       sums.update(extention)
+    
+#     h = list(sums)
+#     heapify(h)
+
+#     ans = 0
+#     peak = 0
+#     for i in range(1, n + 1):
+#       while h and h[0] < i:
+#         peak = heappop(h)
+
+#       if h and i == h[0]: # ok
+#         continue
+#       else: # need patch
+#         patch = i
+#         peak = patch
+#         extention = set()
+#         for item in sums:
+#           extention.add(item + patch)
+#           heappush(h, item + patch)
+#         extention.add(patch)
+#         sums.update(extention)
+#         ans += 1
+#         # print(i, peak, h[0] if h else 0, sums, 'add patch', patch)
+        
+#     return ans
+
+class Solution:
+  def minPatches(self, nums: List[int], n: int) -> int:
+    ans = 0
+    s = 0
+
+    for num in nums:
+      while s < n and num - s > 1:
+        s += s + 1
+        ans += 1
+      s += num
+      last = num
+    
+    while s < n:
+      s += s + 1
+      ans += 1
+    return ans
