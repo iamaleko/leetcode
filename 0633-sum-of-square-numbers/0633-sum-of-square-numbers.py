efc846aa3b433ecc18648e2bca6e6e406aa5be3b
Alexander Kolobov <a@da.am>
2024-06-18 01:24:25 +0400

    Time: 99 ms (50.16%), Space: 16.5 MB (79.36%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -3,8 +3,11 @@ class Solution:
     if not c:
       return True
     r = floor(math.sqrt(c))
-    for r in range(r // 2, r + 1):
-      if math.sqrt(c - r ** 2) % 1 == 0:
+    l = 0
+    while r > l:
+      l = math.sqrt(c - r ** 2)
+      if l % 1 == 0:
         return True
+      r -= 1
     return False
         
