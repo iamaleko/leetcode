c863c503f267d0cf7588289f41e01faf035ceb76
Alexander Kolobov <a@da.am>
2024-06-18 01:20:42 +0400

    Time: 189 ms (10.59%), Space: 16.6 MB (39.36%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,12 @@
+class Solution:
+  def judgeSquareSum(self, c: int) -> bool:
+    if not c:
+      return True
+    r = floor(math.sqrt(c))
+    while r:
+      l = math.sqrt(c - r ** 2)
+      if l % 1 == 0:
+        return True
+      r -= 1
+    return False
+        
