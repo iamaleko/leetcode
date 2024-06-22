5e3356b75db8525b9a545210ca78ac9dd6656cb4
Alexander Kolobov <a@da.am>
2024-06-22 19:43:40 +0400

    Time: 613 ms (60.9%), Space: 24.3 MB (19.98%) - LeetHub

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -0,0 +1,13 @@
+class Solution:
+  def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+    s = 0
+    rs = [0] + [s := s + (1 if num % 2 else 0) for num in nums]
+    d = defaultdict(int)
+    ans = 0
+    for num in rs:
+      if num - k in d:
+        ans += d[num - k]
+      d[num] += 1
+    return ans
+
+        
