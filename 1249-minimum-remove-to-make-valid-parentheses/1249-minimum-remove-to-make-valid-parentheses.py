0453565eb61e60771c8e03f4c2cd87fd0f387b29
Alexander Kolobov <a@da.am>
2024-06-22 20:48:51 +0400

    Time: 88 ms (39.92%), Space: 18 MB (85.79%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,26 +1,16 @@
 class Solution:
   def minRemoveToMakeValid(self, s: str) -> str:
-    ans = ''
-    opened = 0
-    for ch in s:
-      if ch == '(':
-        opened += 1
-      elif ch == ')':
-        if opened:
-          opened -= 1    
-        else:
-          continue
-      ans += ch
-    s = ans[::-1]
-    ans = ''
-    opened = 0
-    for ch in s:
-      if ch == ')':
-        opened += 1
-      elif ch == '(':
-        if opened:
-          opened -= 1    
-        else:
-          continue
-      ans += ch
-    return ans[::-1]
+    def fix(s, op, cl):
+      ans = ''
+      lv = 0
+      for ch in s:
+        if ch == op:
+          lv += 1
+        elif ch == cl:
+          if lv:
+            lv -= 1    
+          else:
+            continue
+        ans += ch
+      return ans
+    return fix(fix(s,'(',')')[::-1],')','(')[::-1]
