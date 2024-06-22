e09877e0598cb3559a6c7a14992348ccc0e8a175
Alexander Kolobov <a@da.am>
2024-06-22 20:06:23 +0400

    Time: 604 ms (43.61%), Space: 23 MB (52.52%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -0,0 +1,9 @@
+class Solution:
+  def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+    for i in range(len(points)):
+      points[i] = (math.sqrt(points[i][0]**2 + points[i][1]**2), points[i])
+    heapify(points)
+    res = []
+    for i in range(k):
+      res.append(heappop(points)[1])
+    return res
