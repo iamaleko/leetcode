1ca72a8e37eb74601effb86d820c33bebd18a7d4
Alexander Kolobov <a@da.am>
2024-06-22 20:48:53 +0400

    Time: 101 ms (22.81%), Space: 17.9 MB (92.46%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
@@ -1,16 +1,16 @@
 class Solution:
+  def fix(self, s, op, cl):
+    ans = ''
+    lv = 0
+    for ch in s:
+      if ch == op:
+        lv += 1
+      elif ch == cl:
+        if lv:
+          lv -= 1    
+        else:
+          continue
+      ans += ch
+    return ans
   def minRemoveToMakeValid(self, s: str) -> str:
-    def fix(s, op, cl):
-      ans = ''
-      lv = 0
-      for ch in s:
-        if ch == op:
-          lv += 1
-        elif ch == cl:
-          if lv:
-            lv -= 1    
-          else:
-            continue
-        ans += ch
-      return ans
-    return fix(fix(s,'(',')')[::-1],')','(')[::-1]
+    return self.fix(self.fix(s,'(',')')[::-1],')','(')[::-1]
