c8d4635aaf7041586128699a46d52be2e2cf6ebb
Alexander Kolobov <a@da.am>
2024-06-25 01:47:09 +0400

    Time: 799 ms (56.67%), Space: 21.3 MB (18%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.py
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.py
@@ -8,25 +8,16 @@ class Solution:
       if h and h[0] == i:
         flipped = not flipped
         h.popleft()
-      can_flip = i <= len(nums) - k
-      if nums[i] == 0 and flipped: # update
-        # nums[i] = 1 # remove!
-        # print(i, 'update', flipped, h) # remove!
-        continue
-      elif ((nums[i] == 0 and not flipped) or (nums[i] == 1 and flipped)): # need flip
-        if can_flip:
+      if ((nums[i] == 0 and not flipped) or (nums[i] == 1 and flipped)): # need flip
+        if i <= len(nums) - k: # can flip
           flips += 1
           flipped = not flipped
           if h and h[-1] == i + k:
             h.pop()
           else:
             h.append(i + k)
-          # nums[i] = 1 # remove!
-          # print(i, 'update and change state', flipped, h) # remove!
         else:
-          # print(i, 'failed', flipped, h) # remove!
           return -1
-      # print(nums)
     return flips
 
 
