b8bab7a33b30003d793edc2646a1fb9c5dbf83c1
Alexander Kolobov <a@da.am>
2024-06-25 01:41:53 +0400

    Time: 827 ms (26%), Space: 21.2 MB (26.67%) - LeetHub

---
+++ 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.py
@@ -0,0 +1,45 @@
+class Solution:
+  def minKBitFlips(self, nums: List[int], k: int) -> int:
+    flips = 0
+    flipped = False
+    h = []
+
+    for i in range(len(nums)):
+      while h and h[0] == i:
+        flipped = not flipped
+        heappop(h)
+      can_flip = i <= len(nums) - k
+      if nums[i] == 0 and flipped: # update
+        # nums[i] = 1 # remove!
+        # print(i, 'update', flipped, h) # remove!
+        continue
+      elif ((nums[i] == 0 and not flipped) or (nums[i] == 1 and flipped)): # need flip
+        if can_flip:
+          flips += 1
+          flipped = not flipped
+          heappush(h, i + k)
+          # nums[i] = 1 # remove!
+          # print(i, 'update and change state', flipped, h) # remove!
+        else:
+          # print(i, 'failed', flipped, h) # remove!
+          return -1
+      # print(nums)
+    return flips
+
+
+#  TLE
+# class Solution:
+#   def minKBitFlips(self, nums: List[int], k: int) -> int:
+#     flips = 0
+#     def flip(j):
+#       for i in range(j, j + k):
+#         nums[i] ^= 1
+#     for i in range(len(nums)):
+#       can_flip = i <= len(nums) - k
+#       if nums[i] == 0:
+#         if can_flip:
+#           flips += 1
+#           flip(i)
+#         else:
+#           return -1
+#     return flips
