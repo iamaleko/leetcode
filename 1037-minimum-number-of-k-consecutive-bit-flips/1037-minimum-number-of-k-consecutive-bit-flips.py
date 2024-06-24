62e018daaed1fb2a1c937124719116c3d78ee5ef
Alexander Kolobov <a@da.am>
2024-06-25 01:44:50 +0400

    Time: 813 ms (33.33%), Space: 21.3 MB (22%) - LeetHub

--- 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.py
+++ 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.py
@@ -5,7 +5,7 @@ class Solution:
     h = deque()
 
     for i in range(len(nums)):
-      while h and h[0] == i:
+      if h and h[0] == i:
         flipped = not flipped
         h.popleft()
       can_flip = i <= len(nums) - k
@@ -17,7 +17,10 @@ class Solution:
         if can_flip:
           flips += 1
           flipped = not flipped
-          h.append(i + k)
+          if h and h[-1] == i + k:
+            h.pop()
+          else:
+            h.append(i + k)
           # nums[i] = 1 # remove!
           # print(i, 'update and change state', flipped, h) # remove!
         else:
