1b217fda4e35b8a26816ab8c5a5e8634f9055216
Alexander Kolobov <a@da.am>
2024-07-03 19:35:14 +0400

    Time: 72 ms (93.75%), Space: 57.4 MB (91.67%) - LeetHub

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.js
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.js
@@ -54,12 +54,16 @@ var minDifference = function(nums) {
   const s = [Infinity, Infinity, Infinity, Infinity],
         l = [-Infinity, -Infinity, -Infinity, -Infinity];
   for (let p, i = 0; i < nums.length; i++) {
-    p = 0;
-    while (s[p] < nums[i]) p++;
-    if (p < 5) s.splice(p, 0, nums[i]), s.pop();
-    p = 0;
-    while (l[p] > nums[i]) p++;
-    if (p < 5) l.splice(p, 0, nums[i]), l.pop();
+    if (s[3] > nums[i]) {
+      p = 0;
+      while (s[p] < nums[i]) p++;
+      s.splice(p, 0, nums[i]), s.pop();
+    }
+    if (l[3] < nums[i]) {
+      p = 0;
+      while (l[p] > nums[i]) p++;
+      l.splice(p, 0, nums[i]), l.pop();
+    }
   }
   return Math.min(l[0] - s[3], l[1] - s[2], l[2] - s[1], l[3] - s[0])
 };
