12f46d6a76a0b34085dee05d7759cf81a17e98ad
Alexander Kolobov <a@da.am>
2024-07-03 19:30:45 +0400

    Time: 70 ms (93.75%), Space: 57.6 MB (87.5%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.js
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.js
@@ -14,37 +14,6 @@
 //   )
 // };
 
-// /**
-//  * @param {number[]} nums
-//  * @return {number}
-//  */
-// var minDifference = function(nums) {
-//   if (nums.length <= 3) return 0
-  
-//   let sa = Infinity,
-//       sb = Infinity,
-//       sc = Infinity,
-//       sd = Infinity,
-//       la = -Infinity,
-//       lb = -Infinity,
-//       lc = -Infinity,
-//       ld = -Infinity;
-//   for (let i = 0; i < nums.length; i++) {
-//     let val = nums[i]
-//     if (sa >= val) {[val, sa] = [sa, val]}
-//     if (sb >= val) {[val, sb] = [sb, val]}
-//     if (sc >= val) {[val, sc] = [sc, val]}
-//     if (sd >= val) {sd = val}
-//     val = nums[i]
-//     if (la <= val) {[val, la] = [la, val]}
-//     if (lb <= val) {[val, lb] = [lb, val]}
-//     if (lc <= val) {[val, lc] = [lc, val]}
-//     if (ld <= val) {ld = val}
-//   }
-
-//   return Math.min(la - sd, lb - sc, lc - sb, ld - sa)
-// };
-
 /**
  * @param {number[]} nums
  * @return {number}
@@ -52,11 +21,42 @@
 var minDifference = function(nums) {
   if (nums.length <= 3) return 0
   
-  let s = new Array(4).fill(Infinity),
-      l = new Array(4).fill(-Infinity);
+  let sa = Infinity,
+      sb = Infinity,
+      sc = Infinity,
+      sd = Infinity,
+      la = -Infinity,
+      lb = -Infinity,
+      lc = -Infinity,
+      ld = -Infinity;
   for (let i = 0; i < nums.length; i++) {
-    for (let j = 0, val = nums[i]; j < 5; j++) if (s[j] >= val) [val, s[j]] = [s[j], val];
-    for (let j = 0, val = nums[i]; j < 5; j++) if (l[j] <= val) [val, l[j]] = [l[j], val];
+    let val = nums[i]
+    if (sa >= val) {[val, sa] = [sa, val]}
+    if (sb >= val) {[val, sb] = [sb, val]}
+    if (sc >= val) {[val, sc] = [sc, val]}
+    if (sd >= val) {sd = val}
+    val = nums[i]
+    if (la <= val) {[val, la] = [la, val]}
+    if (lb <= val) {[val, lb] = [lb, val]}
+    if (lc <= val) {[val, lc] = [lc, val]}
+    if (ld <= val) {ld = val}
   }
-  return Math.min(l[0] - s[3], l[1] - s[2], l[2] - s[1], l[3] - s[0])
+
+  return Math.min(la - sd, lb - sc, lc - sb, ld - sa)
 };
+
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var minDifference = function(nums) {
+//   if (nums.length <= 3) return 0
+  
+//   let s = new Array(4).fill(Infinity),
+//       l = new Array(4).fill(-Infinity);
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = 0, val = nums[i]; j < 5; j++) if (s[j] >= val) [val, s[j]] = [s[j], val];
+//     for (let j = 0, val = nums[i]; j < 5; j++) if (l[j] <= val) [val, l[j]] = [l[j], val];
+//   }
+//   return Math.min(l[0] - s[3], l[1] - s[2], l[2] - s[1], l[3] - s[0])
+// };
