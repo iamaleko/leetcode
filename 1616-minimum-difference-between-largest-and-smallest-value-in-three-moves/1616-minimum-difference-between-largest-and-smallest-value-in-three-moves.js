8369724e0cd03bb61f384a4ccbeaffccfcefeb6f
Alexander Kolobov <a@da.am>
2024-07-03 19:00:59 +0400

    Time: 68 ms (95.83%), Space: 58.8 MB (16.67%) - LeetHub

---
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.js
@@ -0,0 +1,46 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var minDifference = function(nums) {
+//   if (nums.length <= 3) return 0
+//   nums.sort((a, b) => a - b)
+//   const r = nums.length - 1;
+//   return Math.min(
+//     nums[r - 0] - nums[3],
+//     nums[r - 1] - nums[2],
+//     nums[r - 2] - nums[1],
+//     nums[r - 3] - nums[0]
+//   )
+// };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minDifference = function(nums) {
+  if (nums.length <= 3) return 0
+  
+  let sa = Infinity,
+      sb = Infinity,
+      sc = Infinity,
+      sd = Infinity,
+      la = -Infinity,
+      lb = -Infinity,
+      lc = -Infinity,
+      ld = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    let val = nums[i]
+    if (sa >= val) {[val, sa] = [sa, val]}
+    if (sb >= val) {[val, sb] = [sb, val]}
+    if (sc >= val) {[val, sc] = [sc, val]}
+    if (sd >= val) {sd = val}
+    val = nums[i]
+    if (la <= val) {[val, la] = [la, val]}
+    if (lb <= val) {[val, lb] = [lb, val]}
+    if (lc <= val) {[val, lc] = [lc, val]}
+    if (ld <= val) {ld = val}
+  }
+
+  return Math.min(la - sd, lb - sc, lc - sb, ld - sa)
+};
