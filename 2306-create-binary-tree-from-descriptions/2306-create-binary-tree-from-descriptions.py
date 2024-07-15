6a8a1a11d29984fc485e36b5d3ba7a97f2e5a12f
Alexander Kolobov <a@da.am>
2024-07-15 20:42:41 +0400

    Time: 1539 ms (86.87%), Space: 25.9 MB (50.17%) - LeetHub

--- 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.py
+++ 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.py
@@ -1,39 +1,22 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
   def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
     roots = {}
     nodes = {}
 
     for parent, child, is_left in descriptions:
-      # find or create parent
-      if parent not in nodes and parent not in roots:
+      if parent not in nodes:
         roots[parent] = TreeNode(parent)
         nodes[parent] = roots[parent]
-        # print(f"add {parent} to roots")
-      parent = nodes[parent]
 
-      # find or create child
-      if child not in nodes and child not in roots:
+      if child not in nodes:
         nodes[child] = TreeNode(child)
-        # print(f"add {child} to nodes")
-      child = nodes[child]
+        
+      if child in roots:
+        del roots[child]
 
-      # remove child from roots
-      if child.val in roots:
-        # print(f"remove {child.val} from roots")
-        del roots[child.val]
-
-      # link nodes
       if is_left:
-        parent.left = child
-        # print(f"link {child.val} to {parent.val} as left")
+        nodes[parent].left = nodes[child]
       else:
-        parent.right = child
-        # print(f"link {child.val} to {parent.val} as right")
+        nodes[parent].right = nodes[child]
 
     return next(iter(roots.values()))
