d3795b3a9c50b1f4f65e1b8e67d2041dbd4d7495
Alexander Kolobov <a@da.am>
2024-07-18 18:27:54 +0400

    Time: 189 ms (23.54%), Space: 17.1 MB (22.83%) - LeetHub

--- 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.py
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.py
@@ -1,15 +1,58 @@
+# class Solution:
+#   def countPairs(self, root: TreeNode, distance: int) -> int:
+#     # build parent links and find leafs
+#     q = deque([root])
+#     parent = {}
+#     leafs = deque()
+#     while q:
+#       node = q.popleft()
+#       if node.left:
+#         parent[node.left] = node
+#         q.append(node.left)
+#       if node.right:
+#         parent[node.right] = node
+#         q.append(node.right)
+#       elif not node.left:
+#         leafs.append(node)
+    
+#     # traverse tree from leafs to root and count leaf paths
+#     dist = { None: 1 }
+#     ans = 0
+#     while leafs:
+#       node = leafs.popleft()
+#       if node.left and node.right:
+#         for l in dist[node.left]:
+#           for r in dist[node.right]:
+#             if l + r <= distance:
+#               ans += 1
+#         dist[node] = [e + 1 for e in dist.pop(node.left) + dist.pop(node.right) if e + 1 < distance]
+#       elif node.left:
+#         dist[node] = [e + 1 for e in dist.pop(node.left) if e + 1 < distance]
+#       elif node.right:
+#         dist[node] = [e + 1 for e in dist.pop(node.right) if e + 1 < distance]
+#       else:
+#         dist[node] = [1]
+#       if node in parent and parent[node].left in dist and parent[node].right in dist:
+#         leafs.append(parent[node])
+
+#     return ans
+
+# O(n)
 class Solution:
   def countPairs(self, root: TreeNode, distance: int) -> int:
     # build parent links and find leafs
+    root.val = 0
     q = deque([root])
     parent = {}
     leafs = deque()
     while q:
       node = q.popleft()
       if node.left:
+        node.left.val = node.val + 1
         parent[node.left] = node
         q.append(node.left)
       if node.right:
+        node.right.val = node.val + 1
         parent[node.right] = node
         q.append(node.right)
       elif not node.left:
@@ -23,15 +66,15 @@ class Solution:
       if node.left and node.right:
         for l in dist[node.left]:
           for r in dist[node.right]:
-            if l + r <= distance:
+            if (l - node.val) + (r - node.val) <= distance:
               ans += 1
-        dist[node] = [e + 1 for e in dist.pop(node.left) + dist.pop(node.right) if e + 1 < distance]
+        dist[node] = dist.pop(node.left) + dist.pop(node.right)
       elif node.left:
-        dist[node] = [e + 1 for e in dist.pop(node.left) if e + 1 < distance]
+        dist[node] = dist.pop(node.left)
       elif node.right:
-        dist[node] = [e + 1 for e in dist.pop(node.right) if e + 1 < distance]
+        dist[node] = dist.pop(node.right)
       else:
-        dist[node] = [1]
+        dist[node] = [node.val]
       if node in parent and parent[node].left in dist and parent[node].right in dist:
         leafs.append(parent[node])
 
