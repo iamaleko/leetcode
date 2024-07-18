2a2bb8d676e8baac0e1c500452d78674f03e3e0c
Alexander Kolobov <a@da.am>
2024-07-18 18:08:10 +0400

    Time: 98 ms (55.45%), Space: 17 MB (22.83%) - LeetHub

--- 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.py
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.py
@@ -21,11 +21,13 @@ class Solution:
     while leafs:
       node = leafs.popleft()
       if node.left and node.right:
-        for l in dist[node.left]:
-          for r in dist[node.right]:
+        left = dist[node.left]
+        right = dist[node.right]
+        for l in left:
+          for r in right:
             if l + r <= distance:
               ans += 1
-        dist[node] = [e + 1 for e in dist[node.left] if e + 1 < distance] + [e + 1 for e in dist[node.right] if e + 1 < distance]
+        dist[node] = [e + 1 for e in left if e + 1 < distance] + [e + 1 for e in right if e + 1 < distance]
         del dist[node.left]
         del dist[node.right]
 
