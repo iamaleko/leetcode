c5d2d9d9a2f3f73b5a6e33c5c03237f40742a864
Alexander Kolobov <a@da.am>
2024-07-19 14:23:31 +0400

    Time: 52 ms (50%), Space: 20.1 MB (50%) - LeetHub

---
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.php
@@ -0,0 +1,32 @@
+class Solution {
+  
+  /**
+    * @param Integer[][] $matrix
+    * @return Integer[]
+    */
+  function luckyNumbers($matrix) {
+    $ans = [];
+    foreach ($matrix[0] as $x => $_) {
+      $max = PHP_INT_MIN;
+      $max_y = null;
+      foreach ($matrix as $y => $_) {
+        if ($matrix[$y][$x] > $max) {
+          $max = $matrix[$y][$x];
+          $max_y = $y;
+        }
+      }
+      $min = PHP_INT_MAX;
+      $min_x = null;
+      foreach ($matrix[0] as $_x => $_) {
+        if ($matrix[$max_y][$_x] < $min) {
+          $min = $matrix[$max_y][$_x];
+          $min_x = $_x;
+        }
+      }
+      if ($min_x === $x) {
+        $ans []= $matrix[$max_y][$min_x];
+      }
+    }
+    return $ans;
+  }
+}
