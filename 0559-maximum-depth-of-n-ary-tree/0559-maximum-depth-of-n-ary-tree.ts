79565d93d2cd16faed8c2f4dc12e836430ed3aa5
Alexander Kolobov <a@da.am>
2024-07-23 20:50:07 +0400

    Time: 107 ms (8.7%), Space: 62.6 MB (5.8%) - LeetHub

---
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.ts
@@ -0,0 +1,17 @@
+/**
+ * Definition for _Node.
+ * class _Node {
+ *     val: number
+ *     children: _Node[]
+ * 
+ *     constructor(val?: number, children?: _Node[]) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.children = (children===undefined ? [] : children)
+ *     }
+ * }
+ */
+
+
+function maxDepth(root: _Node | null): number {
+  return root ? 1 + Math.max(0, ...root.children.map(child => maxDepth(child))) : 0;
+};
