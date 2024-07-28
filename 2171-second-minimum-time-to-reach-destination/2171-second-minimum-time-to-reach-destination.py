604576553e24f06581466e967393aa70e89995f7
Alexander Kolobov <a@da.am>
2024-07-28 18:23:57 +0400

    Time: 1989 ms (39.07%), Space: 29.4 MB (8.61%) - LeetHub

--- 2171-second-minimum-time-to-reach-destination/2171-second-minimum-time-to-reach-destination.py
+++ 2171-second-minimum-time-to-reach-destination/2171-second-minimum-time-to-reach-destination.py
@@ -18,35 +18,36 @@ class Solution:
       a_node, dist = q.popleft()
       for b_node in graph[a_node]:
         if b_node not in back:
-            back[b_node] = []
-        if len(back[b_node]) < 2 or back[b_node][-1][1] - back[b_node][0][1] == 0:  
+          back[b_node] = [0] # 0 index for dist difference
+        if not back[b_node][0]:
           q.append((b_node, dist + 1))
           back[b_node].append((a_node, dist + 1))
-          if b_node == n and not (len(back[b_node]) < 2 or back[b_node][-1][1] - back[b_node][0][1] == 0):
+          back[b_node][0] = back[b_node][-1][1] - back[b_node][1][1]
+          if b_node == n and back[b_node][0]:
             q.clear()
 
     # find minimum extention
+    best = back[n][1][1]
     extention = 2
     q = deque([n])
     while q:
       node = q.popleft()
       nodes = back[node]
-      if nodes[0][0] != 1: # add fastets node
-        q.append(nodes[0][0])
-      if len(nodes) >= 2:
-        if nodes[-1][1] - nodes[0][1]:
-          if extention > nodes[-1][1] - nodes[0][1]:
-            extention = nodes[-1][1] - nodes[0][1]
-            if extention == 1:
-              q.clear()
-        else:
-          for i in range(1, len(nodes)):
-            if nodes[i][0] != 1: # add similar slower nodes
-              q.append(nodes[-1][0])
+      if nodes[1][0] != 1: # add fastets node
+        q.append(nodes[1][0])
+      if nodes[0]:
+        if extention > nodes[0]:
+          extention = nodes[0]
+          if extention == 1:
+            q.clear()
+      else:
+        for i in range(2, len(nodes)):
+          if nodes[i][0] != 1: # add similar slower nodes
+            q.append(nodes[i][0])
 
     # calculate time
     ans = 0
-    for i in range(back[n][0][1] + extention):
+    for i in range(best + extention):
       if (ans // change) % 2:
         ans += (change - (ans % change))
       ans += time
