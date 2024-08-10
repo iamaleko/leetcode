4f6006fe387e7e26c5ab2899b6fd92132e476174
Alexander Kolobov <a@da.am>
2024-08-11 01:47:51 +0400

    Time: 96 ms (50%), Space: 58.5 MB (12.5%) - LeetHub

---
+++ 0959-regions-cut-by-slashes/0959-regions-cut-by-slashes.ts
@@ -0,0 +1,40 @@
+function regionsBySlashes(grid: string[]): number {
+  //  rebuild map
+  const map: number[][] = [];
+  for (let i = 0; i < grid.length; i++) {
+    const a = [], b = [], c = [];
+    map.push(a, b, c);
+    for (const ch of grid[i]) {
+      if (ch === ' ') {
+        a.push(0, 0, 0);
+        b.push(0, 0, 0);
+        c.push(0, 0, 0);
+      } else if (ch === '/') {
+        a.push(0, 0, 1);
+        b.push(0, 1, 0);
+        c.push(1, 0, 0);
+      } else if (ch === '\\') {
+        a.push(1, 0, 0);
+        b.push(0, 1, 0);
+        c.push(0, 0, 1);
+      }
+    }
+  }
+  // find regions
+  let ans = 0;
+  for (let w = map[0].length, h = map.length, x = 0, y = 0; y < h; ++x === w && (x = 0, y++)) {
+    if (map[y][x] === 0) {
+      ans++;
+      const q = [[y, x]];
+      while (q.length) {
+        const [y, x] = q.pop();
+        map[y][x] = 1;
+        if (y > 0 && !map[y - 1][x]) q.push([y - 1, x]);
+        if (y < h - 1 && !map[y + 1][x]) q.push([y + 1, x]);
+        if (x > 0 && !map[y][x - 1]) q.push([y, x - 1]);
+        if (x < w - 1 && !map[y][x + 1]) q.push([y, x + 1]);
+      }
+    }
+  }
+  return ans;
+};
