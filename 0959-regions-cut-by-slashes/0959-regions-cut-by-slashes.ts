a8ffcef95417a511e6615a26df1b820728ca9b69
Alexander Kolobov <a@da.am>
2024-08-11 03:57:51 +0400

    Time: 68 ms (100%), Space: 52.8 MB (87.5%) - LeetHub

--- 0959-regions-cut-by-slashes/0959-regions-cut-by-slashes.ts
+++ 0959-regions-cut-by-slashes/0959-regions-cut-by-slashes.ts
@@ -1,3 +1,23 @@
+function regionsBySlashes(grid: string[]): number {
+  const v: Record<number, number> = {}, w = grid[0].length, h = grid.length, t = 1 << 3, r = 1 << 2, b = 1 << 1, l = 1;
+  const fill = (y, x, s): void => {
+    s = grid[y][x] === ' ' ? t + r + b + l : grid[y][x] === '/' ? (s & t + l ? t + l : b + r) : (s & t + r ? t + r : b + l);
+    v[(y << 5) + x] |= s;
+    if (s & t && y > 0 && ~v[(y - 1 << 5) + x] & b) fill(y - 1, x, b);
+    if (s & b && y < h - 1 && ~v[(y + 1 << 5) + x] & t) fill(y + 1, x, t);
+    if (s & l && x > 0 && ~v[(y << 5) + x - 1] & r) fill(y, x - 1, r);
+    if (s & r && x < w - 1 && ~v[(y << 5) + x + 1] & l) fill(y, x + 1, l);
+  };
+  let ans = 0;
+  for (let y = 0; y < h; y++) {
+    for (let x = 0; x < w; x++) {
+      if (~v[(y << 5) + x] & t) { fill(y, x, t); ans++; }
+      if (~v[(y << 5) + x] & b) { fill(y, x, b); ans++; }
+    }
+  }
+  return ans;
+};
+
 // function regionsBySlashes(grid: string[]): number {
 //   //  rebuild map
 //   const map: number[][] = [];
@@ -38,23 +58,3 @@
 //   }
 //   return ans;
 // };
-
-function regionsBySlashes(grid: string[]): number {
-  const v: Record<number, number> = {}, w = grid[0].length, h = grid.length, t = 1 << 3, r = 1 << 2, b = 1 << 1, l = 1;
-  const fill = (y, x, s) => {
-    s = grid[y][x] === ' ' ? t + r + b + l : grid[y][x] === '/' ? (s & t + l ? t + l : b + r) : (s & t + r ? t + r : b + l);
-    v[(y << 5) + x] |= s;
-    if (s & t && y > 0 && ~v[(y - 1 << 5) + x] & b) fill(y - 1, x, b);
-    if (s & b && y < h - 1 && ~v[(y + 1 << 5) + x] & t) fill(y + 1, x, t);
-    if (s & l && x > 0 && ~v[(y << 5) + x - 1] & r) fill(y, x - 1, r);
-    if (s & r && x < w - 1 && ~v[(y << 5) + x + 1] & l) fill(y, x + 1, l);
-  };
-  let ans = 0;
-  for (let y = 0; y < h; y++) {
-    for (let x = 0; x < w; x++) {
-      if (~v[(y << 5) + x] & t) { fill(y, x, t); ans++; }
-      if (~v[(y << 5) + x] & b) { fill(y, x, b); ans++; }
-    }
-  }
-  return ans;
-};
