4e1a73b3dd64dd9babcd5f30f5061956a473a38f
Alexander Kolobov <a@da.am>
2024-08-11 03:34:06 +0400

    Time: 70 ms (100%), Space: 53.1 MB (75%) - LeetHub

--- 0959-regions-cut-by-slashes/0959-regions-cut-by-slashes.ts
+++ 0959-regions-cut-by-slashes/0959-regions-cut-by-slashes.ts
@@ -40,24 +40,20 @@
 // };
 
 function regionsBySlashes(grid: string[]): number {
-  let ans = 0;
-  const v: Record<number, number> = {}, w = grid[0].length, h = grid.length;
-  const pair = (y, x) => (y << 5) + x;
-  const t = 1 << 3, r = 1 << 2, b = 1 << 1, l = 1 << 0, a = t + r + b + l;
+  const v: Record<number, number> = {}, w = grid[0].length, h = grid.length, t = 1 << 3, r = 1 << 2, b = 1 << 1, l = 1;
   const fill = (y, x, s) => {
-    s = grid[y][x] === ' ' ? a : grid[y][x] === '/' ? (s & t + l ? t + l : b + r) : (s & t + r ? t + r : b + l)
-    v[pair(y, x)] |= s;
-    if (s & t && y > 0 && ~v[pair(y - 1, x)] & b) fill(y - 1, x, b);
-    if (s & b && y < h - 1 && ~v[pair(y + 1, x)] & t) fill(y + 1, x, t);
-    if (s & l && x > 0 && ~v[pair(y, x - 1)] & r) fill(y, x - 1, r);
-    if (s & r && x < w - 1 && ~v[pair(y, x + 1)] & l) fill(y, x + 1, l);
+    s = grid[y][x] === ' ' ? t + r + b + l : grid[y][x] === '/' ? (s & t + l ? t + l : b + r) : (s & t + r ? t + r : b + l);
+    v[(y << 5) + x] |= s;
+    if (s & t && y > 0 && ~v[(y - 1 << 5) + x] & b) fill(y - 1, x, b);
+    if (s & b && y < h - 1 && ~v[(y + 1 << 5) + x] & t) fill(y + 1, x, t);
+    if (s & l && x > 0 && ~v[(y << 5) + x - 1] & r) fill(y, x - 1, r);
+    if (s & r && x < w - 1 && ~v[(y << 5) + x + 1] & l) fill(y, x + 1, l);
   };
+  let ans = 0;
   for (let y = 0; y < h; y++) {
     for (let x = 0; x < w; x++) {
-      const key = pair(y, x);
-      if (v[key] === a) continue;
-      if (~v[key] & t) { fill(y, x, t); ans++; }
-      if (~v[key] & b) { fill(y, x, b); ans++; }
+      if (~v[(y << 5) + x] & t) { fill(y, x, t); ans++; }
+      if (~v[(y << 5) + x] & b) { fill(y, x, b); ans++; }
     }
   }
   return ans;
