9e67fa4e3752779426734f7e2b05407668d60772
Alexander Kolobov <a@da.am>
2024-08-16 18:20:22 +0400

    Time: 110 ms (100%), Space: 67.1 MB (100%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.ts
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.ts
@@ -2,18 +2,12 @@ function maxDistance(arrays: number[][]): number {
   let aMin = Infinity, aMinI = -1,
       bMin = Infinity, bMinI = -1,
       aMax = -Infinity, aMaxI = -1,
-      bMax = -Infinity, bMaxI = -1;
+      bMax = -Infinity, bMaxI = -1,
+      min: number, max: number;
   
   for (let i = 0; i < arrays.length; i++) {
-    let min = arrays[i][0],
-        max = arrays[i][0];
-    for (let j = 1; j < arrays[i].length; j++) {
-      if (min > arrays[i][j]) {
-        min = arrays[i][j];
-      } else if (max < arrays[i][j]) {
-        max = arrays[i][j];
-      }
-    }
+    min = arrays[i][0]
+    max = arrays[i][arrays[i].length - 1];
     if (aMin > min) { bMin = aMin; bMinI = aMinI; aMin = min; aMinI = i; }
     else if (bMin > min) { bMin = min; bMinI = i; }
     if (aMax < max) { bMax = aMax; bMaxI = aMaxI; aMax = max; aMaxI = i; }
