1dd0e5dcfe2f7f6445a7e01f14f2847656219589
Alexander Kolobov <a@da.am>
2024-08-19 18:58:06 +0400

    Time: 48 ms (95.45%), Space: 51 MB (68.18%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.ts
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.ts
@@ -1,8 +1,21 @@
+// function minSteps(n: number): number {
+//   if (n === 1) return 0;
+//   // power of 2
+//   if (Math.log2(n) % 1 === 0) return Math.log2(n) * 2;
+//   // prime factor
+//   for (let i = 2; i < n / 2; i++) if (n % i === 0) return i + minSteps(n / i);
+//   return n;
+// };
+
+// Factorization
 function minSteps(n: number): number {
-  if (n === 1) return 0;
-  // power of 2
-  if (Math.log2(n) % 1 === 0) return Math.log2(n) * 2;
-  // prime factor
-  for (let i = 2; i < n / 2; i++) if (n % i === 0) return i + minSteps(n / i);
-  return n;
+  let ans = 0, divider = 2;
+  while (n > 1) {
+    while (n % divider === 0) {
+      ans += divider;
+      n /= divider;
+    }
+    divider++;
+  }
+  return ans;
 };
