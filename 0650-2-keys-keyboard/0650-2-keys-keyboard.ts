24b30b55caae943821b3dc26d8b889fde811dd25
Alexander Kolobov <a@da.am>
2024-08-19 18:47:51 +0400

    Time: 52 ms (95.45%), Space: 50.3 MB (86.36%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.ts
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.ts
@@ -1,7 +1,7 @@
 function minSteps(n: number): number {
   if (n === 1) return 0;
   // power of 2
-  // if (Number.isInteger(Math.log2(n))) return Math.log2(n) * 2;
+  if (Math.log2(n) % 1 === 0) return Math.log2(n) * 2;
   // prime factor
   for (let i = 2; i < n; i++) if (n % i === 0) return i + minSteps(n / i);
   return n;
