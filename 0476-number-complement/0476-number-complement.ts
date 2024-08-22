9325c1da4c54e95d66ebe032b7a72b6de152dc8d
Alexander Kolobov <a@da.am>
2024-08-22 21:00:39 +0400

    Time: 56 ms (59.02%), Space: 50.1 MB (86.89%) - LeetHub

--- 0476-number-complement/0476-number-complement.ts
+++ 0476-number-complement/0476-number-complement.ts
@@ -1,8 +1,14 @@
 function findComplement(num: number): number {
-  let pos = 0, res = 0;
-  while (num) {
-    res |= (num & 1 ^ 1) << pos++;
-    num >>= 1;
-  }
-  return res;
+  let xor = 0;
+  while (xor < num) xor = (xor << 1) | 1;
+  return num ^ xor;
 };
+
+// function findComplement(num: number): number {
+//   let pos = 0, res = 0;
+//   while (num) {
+//     res |= (num & 1 ^ 1) << pos++;
+//     num >>= 1;
+//   }
+//   return res;
+// };
