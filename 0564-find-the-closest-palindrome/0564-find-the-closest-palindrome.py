79f66c4e8d39ca1a1670cb62c4b13b8cb5b0f3c6
Alexander Kolobov <a@da.am>
2024-08-25 21:39:22 +0400

    Time: 46 ms (5.79%), Space: 16.6 MB (67.95%) - LeetHub

--- 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.py
+++ 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.py
@@ -1,42 +1,37 @@
 class Solution:
+  def toPalindrome(self, left: int, is_odd: bool) -> int:
+    left = str(left)
+    return int(left + (left[-2::-1] if is_odd else left[::-1]))
+
   def nearestPalindromic(self, n: str) -> str:
-    if int(n) <= 10:
-      return str(int(n) - 1)
-    hl = len(n) // 2
-    left = n[0:hl]
-    right = str(left)[::-1]
-    center = n[hl] if len(n) % 2 else ''
+    ln = len(n)
+    left = int(n[0:math.ceil(ln / 2)])
+    n = int(n)
+
+    if n <= 10:
+      return str(n - 1)
+      
+    is_odd = ln % 2 == 1
     variants = []
+
     # as is
-    variants.append(left + center + right)
-    if center:
-      # center +1
-      if int(center) < 9:
-        variants.append(left + str(int(center) + 1) + right)
-      # center -1
-      if int(center) > 0:
-        variants.append(left + str(int(center) - 1) + right)
-    else:
-      # left -1
-      if int(left):
-        _left = str(int(left) + 1)
-        _right = str(_left)[::-1]
-        variants.append(_left + _right)
-      # left +1
-      if int(left) > 1:
-        _left = str(int(left) - 1)
-        _right = str(_left)[::-1]
-        variants.append(_left + _right)
+    variants.append(self.toPalindrome(left, is_odd))
+    # +1
+    variants.append(self.toPalindrome(left + 1, is_odd))
+    # -1
+    variants.append(self.toPalindrome(left - 1, is_odd))
     # pos +1
-    variants.append('1' + ('0' * (len(n) - 1)) + '1')
+    variants.append(int('1' + ('0' * (ln - 1)) + '1'))
     # pos -1
-    variants.append('9' * (len(n) - 1))
-    min = math.inf
-    ans = ''
+    variants.append(int('9' * (ln - 1)))
+
+    min_diff = math.inf
+    ans = math.inf
     for variant in variants:
-      diff = abs(int(variant) - int(n))
-      if diff and (min > diff or (min == diff and ans and int(ans) > int(variant))):
-        min = diff
+      diff = abs(variant - n)
+      if diff and (min_diff > diff or (min_diff == diff and ans > variant)):
+        min_diff = diff
         ans = variant
-    return ans
+        
+    return str(ans)
     
