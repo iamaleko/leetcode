57362a67a5383c7171558802ab9e52366800e0d0
Alexander Kolobov <a@da.am>
2024-08-27 17:37:46 +0400

    Time: 585 ms (17.65%), Space: 87.2 MB (5.88%) - LeetHub

---
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.ts
@@ -0,0 +1,24 @@
+function maxProbability(n: number, edges: number[][], succProb: number[], start_node: number, end_node: number): number {
+  // build graph
+  const graph: Record<number, Record<number, number>> = {};
+  for (let i = 0; i < edges.length; i++) {
+    const [a, b] = edges[i];
+    if (!graph.hasOwnProperty(a)) graph[a] = {};
+    if (!graph.hasOwnProperty(b)) graph[b] = {};
+    graph[a][b] = succProb[i];
+    graph[b][a] = succProb[i];
+  }
+  // BFS
+  const reached: Record<string, number> = { [start_node]: 1 };
+  const queue: string[] = [String(start_node)];
+  while (queue.length) {
+    const node = queue.shift();
+    for (const _node in graph[node]) {
+      if (!reached.hasOwnProperty(_node) || reached[_node] < reached[node] * graph[node][_node]) {
+        reached[_node] = reached[node] * graph[node][_node];
+        queue.push(_node);
+      }
+    }
+  }
+  return reached.hasOwnProperty(end_node) ? reached[end_node] : 0;
+};
