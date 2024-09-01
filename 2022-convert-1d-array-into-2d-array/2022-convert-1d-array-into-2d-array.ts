fbaa3ae33aa9dc2e792afed50552ae2ea741b19d
Alexander Kolobov <a@da.am>
2024-09-01 17:18:00 +0400

    Time: 194 ms (22.45%), Space: 68.5 MB (97.96%) - LeetHub

---
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.ts
@@ -0,0 +1,10 @@
+function construct2DArray(original: number[], m: number, n: number): number[][] {
+  if (original.length !== m * n) return [];
+  const grid: number[][] = new Array(m).fill(0).map(() => new Array(n));
+  for (let i = 0, y = 0; y < m; y++) {
+    for (let x = 0; x < n; x++) {
+      grid[y][x] = original[i++];
+    }  
+  }
+  return grid;
+};
