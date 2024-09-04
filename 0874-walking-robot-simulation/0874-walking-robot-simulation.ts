ea026d483f262b29f3da0985db0ea569b7924ef3
Alexander Kolobov <a@da.am>
2024-09-05 02:57:20 +0400

    Time: 226 ms (9.52%), Space: 77.9 MB (9.52%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.ts
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.ts
@@ -6,6 +6,7 @@ enum Direction {
 };
 
 function robotSim(commands: number[], obstacles: number[][]): number {
+
   // build index
   const y_index: Record<number, number[]> = {},
         x_index: Record<number, number[]> = {};
@@ -17,55 +18,43 @@ function robotSim(commands: number[], obstacles: number[][]): number {
   }
   for (const x in y_index) y_index[x].sort((a, b) => a - b);
   for (const y in x_index) x_index[y].sort((a, b) => a - b);
+
   // search next obstacle
   const search = (x: number, y: number, d: Direction): number | undefined => {
-    const index = d === Direction.Up || d === Direction.Down ? y_index[x] : x_index[y];
+    let [index, t] = d === Direction.Up || d === Direction.Down ? [y_index[x], y] : [x_index[y], x];
     if (index === undefined) return undefined;
     let l = 0,
         r = index.length - 1,
-        c:number,
-        t = d === Direction.Up ? y :
-            d === Direction.Down ? y :
-            d === Direction.Right ? x : x;
-    // console.log(index, t)
+        c:number;
+    if (!x && !y) d === Direction.Up || d === Direction.Right ? t++ : t--;
     while (l <= r) {
       c = l + r >>> 1;
-      // if (d === Direction.Up || d === Direction.Right) {
-        if (index[c] < t) { l = c + 1; } else { r = c - 1; }
-      // } else {
-      //   if (index[c] < t) { r = c - 1; } else { l = c + 1; }
-      // }
+      if (index[c] < t) { l = c + 1; } else { r = c - 1; }
     }
     return d === Direction.Up || d === Direction.Right ? index[l] : index[r];
   }
+
   // perform moves
   let max = 0, x = 0, y = 0, d = Direction.Up;
-  // x = 23
-  // y = -2
-  // d = Direction.Down;
-  // console.log('start', x, y, d);
   for (const command of commands) {
     if (command === -2) { // left
-      // @ts-ignore
-      d = d === Direction.Up ? Direction.Left : d === Direction.Left ? Direction.Down : d === Direction.Down ? Direction.Right : Direction.Up;
-      // console.log('turn left', x, y, d);
+      d = d === Direction.Up ? Direction.Left :
+          d === Direction.Left ? Direction.Down :
+          d === Direction.Down ? Direction.Right : Direction.Up;
     } else if (command === -1) { // right
-      // @ts-ignore
-      d = d === Direction.Up ? Direction.Right : d === Direction.Right ? Direction.Down : d === Direction.Down ? Direction.Left : Direction.Up;
-      // console.log('turn right', x, y, d);
+      d = d === Direction.Up ? Direction.Right :
+          d === Direction.Right ? Direction.Down :
+          d === Direction.Down ? Direction.Left : Direction.Up;
     } else {
-      let n = search(x, y, d);
-      if (!x && !y && n === 0) n = undefined;
+      let n = search(x,y,d);
       switch (d) {
         case Direction.Up: y = n === undefined ? y + command : Math.min(y + command, n - 1); break;
         case Direction.Down: y = n === undefined ? y - command : Math.max(y - command, n + 1); break;
         case Direction.Right: x = n === undefined ? x + command : Math.min(x + command, n - 1); break;
         case Direction.Left: x = n === undefined ? x - command : Math.max(x - command, n + 1); break;
       }
-      // console.log('move to', x, y, d, [command, n]);
       if (x * x + y * y > max) max = x * x + y * y;
     }
   }
-  // console.log('stop', x, y, d);
   return max;
 };
