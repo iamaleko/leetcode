de8180039c76a88d6e42efc633244cb5dd2cb4c6
Alexander Kolobov <a@da.am>
2024-09-15 15:50:00 +0400

    Time: 34 ms (40%), Space: 6.9 MB (73.33%) - LeetHub

---
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.go
@@ -0,0 +1,19 @@
+func findTheLongestSubstring(s string) int {
+  v := map[rune]int{ rune('a'): 1, rune('e'): 2, rune('i'): 4, rune('o'): 8, rune('u'): 16 }
+  m := map[int]int{ 0: -1 }
+  mask := 0
+  ans := 0
+  for i, r := range s {
+    if bit, ok := v[r]; ok {
+      mask ^= bit
+    }
+    if pos, ok := m[mask]; ok {
+      if ans < i - pos {
+        ans = i - pos
+      }
+    } else {
+      m[mask] = i
+    }
+  }
+  return ans
+}
