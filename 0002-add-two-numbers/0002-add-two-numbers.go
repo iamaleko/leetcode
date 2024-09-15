da6f8de095fd29391c277e9974ffdcadf3061f70
Alexander Kolobov <a@da.am>
2024-09-16 01:44:51 +0400

    Time: 9 ms (46.39%), Space: 4.5 MB (92.49%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.go
+++ 0002-add-two-numbers/0002-add-two-numbers.go
@@ -8,21 +8,24 @@
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
   if l1.Val == 0 && l1.Next == nil { return l2 }
   if l2.Val == 0 && l2.Next == nil { return l1 }
-  head := &ListNode{ Val: 0, Next: nil }
+  head := &ListNode{}
   tail := head
   mem := 0
 
   for l1 != nil || l2 != nil || mem > 0 {
     sum := mem
-    if l1 != nil { sum += l1.Val }
-    if l2 != nil { sum += l2.Val }
+    if l1 != nil {
+      sum += l1.Val
+      l1 = l1.Next
+    }
+    if l2 != nil {
+      sum += l2.Val
+      l2 = l2.Next
+    }
 
     mem = sum / 10
-    tail.Next = &ListNode{ Val: sum % 10, Next: nil }
+    tail.Next = &ListNode{ Val: sum % 10 }
     tail = tail.Next
-
-    if l1 != nil { l1 = l1.Next }
-    if l2 != nil { l2 = l2.Next }
   }
 
   return head.Next
