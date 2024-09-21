a57a479fc4d5a4ab3635be9c8dd85f809bde635e
Alexander Kolobov <a@da.am>
2024-09-21 22:00:53 +0400

    Time: 86 ms (25%), Space: 58.3 MB (91.67%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.ts
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.ts
@@ -1,12 +1,11 @@
 function lexicalOrder(n: number): number[] {
   const ans: number[] = new Array(n).fill(1);
-  for (let i = 1; i < n; i++) {
-    if (ans[i - 1] * 10 <= n) {
-      ans[i] = ans[i - 1] * 10;
+  for (let i = 1, d = 1; i < n; i++) {
+    if (d * 10 <= n) {
+      ans[i] = (d *= 10);
     } else {
-      ans[i] = ans[i - 1]
-      while (ans[i] % 10 === 9 || ans[i] + 1 > n) ans[i] = ans[i] / 10 | 0
-      ans[i] += 1;
+      while (d % 10 === 9 || d + 1 > n) d = d / 10 | 0
+      ans[i] = (d += 1);
     }
   } 
   return ans;
