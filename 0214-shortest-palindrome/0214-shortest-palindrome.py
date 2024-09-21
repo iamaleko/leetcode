9cf738c45f8824b7e95784c5dfcf42e553f34ea9
Alexander Kolobov <a@da.am>
2024-09-22 01:04:42 +0400

    Time: 64 ms (62.49%), Space: 17.4 MB (32.79%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -1,10 +1,19 @@
 class Solution:
-    def shortestPalindrome(self, s: str) -> str:
-        length = len(s)
-        reversed_string = s[::-1]  # Reverse the string
+  def shortestPalindrome(self, s: str) -> str:
+    if s == s[::-1]:
+      return s
 
-        # Iterate through the string to find the longest palindromic prefix
-        for i in range(length):
-            if s[: length - i] == reversed_string[i:]:
-                return reversed_string[:i] + s
-        return ""
+    p = len(s) // 2
+    right, left = s[p:], s[0:p][::-1]
+    print(left, right)
+
+    while left and right:
+      if right.startswith(left):
+        break
+      if (left[0] + right).startswith(left):
+        # print(1)
+        return right[::-1] + left[0] + right
+      right = left[0] + right
+      left = left[1:]
+
+    return right[::-1] + right
