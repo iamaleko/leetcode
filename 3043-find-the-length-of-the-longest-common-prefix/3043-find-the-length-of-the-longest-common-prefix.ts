3f73f9d84fc31b77d0460edd4473ecd804bbb1a1
Alexander Kolobov <a@da.am>
2024-09-24 17:54:36 +0400

    Time: 226 ms (92.86%), Space: 74.5 MB (71.43%) - LeetHub

---
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.ts
@@ -0,0 +1,25 @@
+type Trie = {
+  [key: `${number}` ]: Trie;
+  level: number
+};
+
+function longestCommonPrefix(arr1: number[], arr2: number[]): number {
+  const trie: Trie = { level: 1 };
+  for (const num of arr1) {
+    let node = trie;
+    for (const d of String(num)) {
+      if (!(d in node)) node[d] = { level: node.level + 1 };
+      node = node[d];
+    }
+  }
+  let ans = 0;
+  for (const num of arr2) {
+    let node = trie;
+    for (const d of String(num)) {
+      if (!(d in node)) break;
+      if (ans < node.level) ans = node.level;
+      node = node[d];
+    }
+  }
+  return ans;
+};
