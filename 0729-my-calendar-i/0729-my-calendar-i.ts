62700dbbb5ef10fe7add42f69a4cf33dd65756bf
Alexander Kolobov <a@da.am>
2024-09-26 05:42:16 +0400

    Time: 148 ms (62.74%), Space: 60.2 MB (50.98%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.ts
+++ 0729-my-calendar-i/0729-my-calendar-i.ts
@@ -1,17 +1,16 @@
 type Booking = { start: number, end: number };
 
 class MyCalendar {
-  #bookings: Booking[];
-
+  private _bookings: Booking[];
   constructor() {
-    this.#bookings = [];
+    this._bookings = [];
   }
 
-  #search(n: number): number {
-    let l = 0, r = this.#bookings.length - 1;
+  private _search(n: number): number {
+    let l = 0, r = this._bookings.length - 1;
     while (l <= r) {
       const c = l + r >>> 1;
-      if (this.#bookings[c].end <= n) {
+      if (this._bookings[c].end <= n) {
         l = c + 1;
       } else {
         r = c - 1;
@@ -21,9 +20,9 @@ class MyCalendar {
   }
 
   book(start: number, end: number): boolean {
-    const i = this.#search(start);
-    if (i !== this.#bookings.length && this.#bookings[i].start < end) return false;
-    this.#bookings.splice(i, 0, { start: start, end: end });
+    const i = this._search(start);
+    if (i !== this._bookings.length && this._bookings[i].start < end) return false;
+    this._bookings.splice(i, 0, { start: start, end: end });
     return true;
   }
 }
