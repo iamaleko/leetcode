98c8e262a138beac9f4f6d55b25553126fba6200
Alexander Kolobov <a@da.am>
2024-10-01 21:20:49 +0400

    Time: 573 ms (5.07%), Space: 30.6 MB (27.88%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.py
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.py
@@ -1,13 +1,16 @@
 class Solution:
   def canArrange(self, arr, k):
     m = defaultdict(list)
+    n = 0
     for i in range(len(arr)):
       key = arr[i] % k
-      alt = k - arr[i] % k if key else 0
+      alt = k - key if key else 0
       if alt in m:
         j = m[alt].pop()
+        n -= 1
         if not m[alt]:
           del m[alt]
       else:
         m[key].append(i)
-    return False if len(m) else True
+        n += 1
+    return n == 0
