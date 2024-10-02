2884eb79dd13e9509d27d04a69b6f534e6199c12
Alexander Kolobov <a@da.am>
2024-10-02 21:49:37 +0400

    Time: 215 ms (23.21%), Space: 81.4 MB (25%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.ts
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.ts
@@ -5,24 +5,22 @@
 // };
 
 const radixSort = (arr: number[]): number[] => {
-  const s = (arr: number[], p: number): number[] => {
+  const s = (arr: number[], p: number, sign: boolean = false): number[] => {
     let l: number[] = [], r: number[] = [];
-    for (const num of arr) (num & 1 << p ? r : l).push(num);
-    return p-- ? (l.length > 1 ? s(l, p) : l).concat(r.length > 1 ? s(r, p) : r) : l.concat(r);
+    if (sign) {
+      for (const num of arr) (num < 0 ? l : r).push(num);
+    } else {
+      for (const num of arr) (num & 1 << p ? r : l).push(num);
+      p--;
+    }
+    return p > -1 ? (l.length > 1 ? s(l, p) : l).concat(r.length > 1 ? s(r, p) : r) : l.concat(r);
   };
-  let min = Math.min(...arr),
-      max = Math.max(...arr, -min),
-      l: number[] = [],
-      r: number[] = [],
-      p = -1;
+  let min = Math.min(...arr), max = Math.max(...arr, -min), p = -1;
   while (max) max >>= 1, p++;
-  for (const num of arr) (num < 0 ? l : r).push(num);
-  return (l.length > 1 ? s(l, p) : l).concat(r.length > 1 ? s(r, p) : r);
+  return s(arr, p, min < 0);
 }
-
 function arrayRankTransform(arr: number[]): number[] {
   const rank: Record<number, number> = {};
-  // [...new Set(arr)].sort((a, b) => a - b).forEach((v, i) => { rank[v] = i + 1 });
   radixSort([...new Set(arr)]).forEach((v, i) => { rank[v] = i + 1 });
   return arr.map((v) => rank[v]);
 };
