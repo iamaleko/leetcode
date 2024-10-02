15e706886992106e4b8907ded298788e074cecd1
Alexander Kolobov <a@da.am>
2024-10-03 00:50:28 +0400

    Time: 179 ms (42.86%), Space: 81 MB (26.79%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.ts
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.ts
@@ -38,7 +38,9 @@ const radixSort = (arr: number[]): void => {
       while (++i < r) arr[i] & b ? st[p++] = arr[i] : p && (arr[i - p] = arr[i]);
       b >>= 1;
     }
-    if (p) arr.splice(r - p, p, ...st);
+    i = 0;
+    while (i < p) arr[r - p + i] = st[i++];
+    // if (p) arr.splice(r - p, p, ...st);
     if (!b) return;
     if (r - p - l > 1) sort(l, r - p, b);
     if (p > 1) sort(r - p, r, b);
