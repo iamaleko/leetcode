6ab629ee0506224ad22ef7cb1a3a81b6ec82a7ea
Alexander Kolobov <a@da.am>
2024-10-03 19:04:27 +0400

    Time: 119 ms (87.5%), Space: 71.3 MB (100%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.ts
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.ts
@@ -1,14 +1,13 @@
 function minSubarray(nums: number[], p: number): number {
-  const sum = nums.reduce((v, a) => a + v);
-  if (sum % p === 0) return 0;
+  const remove = nums.reduce((v, a) => a + v) % p;
+  if (remove === 0) return 0;
   const m = new Map<number, number>();
   m.set(0, -1);
   let ans = nums.length, rs = 0;
   for (let i = 0; i < nums.length; i++) {
     rs += nums[i];
-    let remove = (rs - sum % p) % p;
-    if (m.has(remove)) {
-      const j = m.get(remove);
+    if (m.has((rs - remove) % p)) {
+      const j = m.get((rs - remove) % p);
       if (ans > i - j) ans = i - j;
     }
     m.set(rs % p, i);
