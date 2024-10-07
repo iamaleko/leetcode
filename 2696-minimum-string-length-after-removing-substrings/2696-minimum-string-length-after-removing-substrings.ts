3c37efea44ef909b6220511a579176ccebf068f2
Alexander Kolobov <a@da.am>
2024-10-07 17:42:30 +0400

    Time: 94 ms (50%), Space: 56.9 MB (29.03%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.ts
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.ts
@@ -1,25 +1,13 @@
 function minLength(s: string): number {
-  let ab: number = -1;
-  let cd: number = -1;
+  let i = -1;
   while (true) {
-    let pair: string;
-    if (ab === -1) ab = s.indexOf('AB');
-    if (ab >= 0) {
-      s = s.slice(0, ab) + s.slice(ab + 2);
-      pair = s.slice(ab, ab + 2);
-      ab = pair === 'AB' ? ab : -1;
-      cd = pair === 'CD' ? cd : -1;
-      continue;
-    }
-    if (cd === -1) cd = s.indexOf('CD');
-    if (cd >= 0) {
-      s = s.slice(0, cd) + s.slice(cd + 2);
-      pair = s.slice(cd, cd + 2);
-      ab = pair === 'AB' ? ab : -1;
-      cd = pair === 'CD' ? cd : -1;
-      continue;
-    }
-    break;
+    if (i === -1) i = s.indexOf('AB');
+    if (i === -1) i = s.indexOf('CD');
+    if (i === -1) break;
+
+    s = s.slice(0, i) + s.slice(i + 2);
+    let pair = s.slice(i, i + 2);
+    if (pair !== 'AB' && pair !== 'CD') i = -1;
   }
   return s.length;
 };
