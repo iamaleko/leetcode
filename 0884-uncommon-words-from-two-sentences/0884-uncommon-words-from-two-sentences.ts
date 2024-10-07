ec01dfdc3acebc91ea24f8df196eb2a093b37568
Alexander Kolobov <a@da.am>
2024-10-08 01:45:17 +0400

    Time: 59 ms (54.59%), Space: 51.8 MB (29.32%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.ts
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.ts
@@ -1,12 +1,13 @@
 function uncommonFromSentences(s1: string, s2: string): string[] {
-  const words = (s1 + ' ' + s2).split(' '),
-        m = new Map<string, number>();
-  for (const word of words) {
-    m.set(word, (m.get(word) ?? 0) + 1);
+  const m: Record<string, number> = {};
+  for (const word of (s1 + ' ' + s2).split(' ')) {
+    if (m.hasOwnProperty(word)) {
+      m[word]++;
+    } else {
+      m[word] = 1;
+    }
   }
   const ans: string[] = [];
-  for (const [word, count] of m.entries()) {
-    if (count === 1) ans.push(word);
-  }
+  for (const word in m) if (m[word] === 1) ans.push(word);
   return ans;
 };
