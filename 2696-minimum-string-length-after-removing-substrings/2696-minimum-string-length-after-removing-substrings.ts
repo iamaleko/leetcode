5904dd11813be266a357f42a44232bbb1b83c448
Alexander Kolobov <a@da.am>
2024-10-07 17:43:58 +0400

    Time: 86 ms (87.1%), Space: 54.5 MB (90.32%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.ts
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.ts
@@ -1,13 +1,29 @@
 function minLength(s: string): number {
-  let i = -1;
-  while (true) {
-    if (i === -1) i = s.indexOf('AB');
-    if (i === -1) i = s.indexOf('CD');
-    if (i === -1) break;
-
-    s = s.slice(0, i) + s.slice(i + 2);
-    let pair = s.slice(i, i + 2);
-    if (pair !== 'AB' && pair !== 'CD') i = -1;
+  const st: string[] = [];
+  let p = -1;
+  for (let ch of s) {
+    if (
+      ch === 'B' && st[p] === 'A' ||
+      ch === 'D' && st[p] === 'C'
+    ) {
+      p--;
+    } else {
+      st[++p] = ch;
+    }
   }
-  return s.length;
+  return p + 1;
 };
+
+// function minLength(s: string): number {
+//   let i = -1;
+//   while (true) {
+//     if (i === -1) i = s.indexOf('AB');
+//     if (i === -1) i = s.indexOf('CD');
+//     if (i === -1) break;
+
+//     s = s.slice(0, i) + s.slice(i + 2);
+//     let pair = s.slice(i, i + 2);
+//     if (pair !== 'AB' && pair !== 'CD') i = -1;
+//   }
+//   return s.length;
+// };
