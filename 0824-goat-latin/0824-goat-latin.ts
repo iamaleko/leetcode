1a9b20233aae9bb5a8412ee385605ecec7af9929
Alexander Kolobov <a@da.am>
2024-10-09 17:28:49 +0400

    Time: 67 ms (9.73%), Space: 51.7 MB (49.56%) - LeetHub

--- 0824-goat-latin/0824-goat-latin.ts
+++ 0824-goat-latin/0824-goat-latin.ts
@@ -2,6 +2,6 @@ function toGoatLatin(sentence: string): string {
   const vovels = new Set<string>(['a','e','i','o','u','A','E','I','O','U']);
   return sentence
     .split(' ')
-    .map((w, i) => (vovels.has(w[0]) ? w : w.slice(1) + w[0]) + 'm'.padEnd(i + 3, 'a'))
+    .map((w, i) => (vovels.has(w[0]) ? w : w.slice(1) + w[0]) + 'm' + 'a'.repeat(i + 2))
     .join(' ');
 };
