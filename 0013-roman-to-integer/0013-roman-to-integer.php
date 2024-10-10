f02671ee8a1e069684b0e789ee91f0bca34710b1
Alexander Kolobov <a@da.am>
2024-10-10 20:15:01 +0400

    Time: 12 ms (76.87%), Space: 19.2 MB (97.82%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.php
+++ 0013-roman-to-integer/0013-roman-to-integer.php
@@ -4,8 +4,7 @@ class Solution {
     $map = [ 'I' => 1, 'V' => 5, 'X' => 10, 'L' => 50, 'C' => 100, 'D' => 500, 'M' => 1000 ];
     for ($i = 0; $i < strlen($s); $i++) {
       if ($i && $map[$s[$i]] > $map[$s[$i - 1]]) {
-        $ans -= $map[$s[$i - 1]];
-        $ans += $map[$s[$i]] - $map[$s[$i - 1]];
+        $ans += $map[$s[$i]] - $map[$s[$i - 1]] * 2;
       } else {
         $ans += $map[$s[$i]];
       }
