876684e9550bacb3e33bb1a3428e1eb49d934d61
Alexander Kolobov <a@da.am>
2024-10-10 21:52:40 +0400

    Time: 57 ms (58.7%), Space: 51 MB (87.31%) - LeetHub

--- 0066-plus-one/0066-plus-one.ts
+++ 0066-plus-one/0066-plus-one.ts
@@ -1,13 +1,13 @@
 function plusOne(digits: number[]): number[] {
   let carry = 1;
   for (let i = digits.length - 1; i >= 0; i--) {
-    digits[i] += carry;
-    carry = 0;
-    if (digits[i] > 9) {
-      carry = digits[i] / 10 | 0;
-      digits[i] %= 10;
+    if (digits[i] + carry > 9) {
+      carry = (digits[i] + carry) / 10 | 0;
+      digits[i] = (digits[i] + carry) % 10;
+    } else {
+      digits[i] += carry;
+      return digits;
     }
-    if (!carry) break;
   }
   if (carry) digits.unshift(carry);
   return digits;
