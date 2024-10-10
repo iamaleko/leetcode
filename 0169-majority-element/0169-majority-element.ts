0a49c7f80054543a0fa1c619897920327d361095
Alexander Kolobov <a@da.am>
2024-10-10 17:46:02 +0400

    Time: 62 ms (72.34%), Space: 53.9 MB (60.76%) - LeetHub

--- 0169-majority-element/0169-majority-element.ts
+++ 0169-majority-element/0169-majority-element.ts
@@ -1,10 +1,26 @@
 function majorityElement(nums: number[]): number {
-  const freq: Record<number, number> = {};
-  for (const num of nums) freq[num] = (freq[num] || 0) + 1;
-  let ans = 0, max = 0;
-  for (const num in freq) if (freq[num] > max) {
-    ans = Number(num);
-    max = freq[num];
+  let candidate = nums[0], count = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== candidate) {
+      if (count === 1) {
+        candidate = nums[i];
+      } else {
+        count--;
+      }
+    } else {
+      count++;
+    }
   }
-  return ans;
+  return candidate;
 };
+
+// function majorityElement(nums: number[]): number {
+//   const freq: Record<number, number> = {};
+//   for (const num of nums) freq[num] = (freq[num] || 0) + 1;
+//   let ans = 0, max = 0;
+//   for (const num in freq) if (freq[num] > max) {
+//     ans = Number(num);
+//     max = freq[num];
+//   }
+//   return ans;
+// };
