f9d08562c72b79e860cdebe6a77a4bf19467e7ff
Alexander Kolobov <a@da.am>
2024-10-10 21:31:32 +0400

    Time: 76 ms (13.82%), Space: 52.2 MB (53.95%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSymmetric(root: TreeNode | null): boolean {
+  const traverse = (left: TreeNode, right: TreeNode) => {
+    if (left === null && right === null) return true;
+    if (left && right && left.val === right.val) {
+      return traverse(left.left, right.right) && traverse(left.right, right.left);
+    }
+    return false;
+  }
+  return root ? traverse(root.left, root.right) : true;
+};
