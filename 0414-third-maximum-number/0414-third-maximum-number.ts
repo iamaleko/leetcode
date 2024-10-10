f1c38ca753c1097bc26897e17f3b99a38ab2fe33
Alexander Kolobov <a@da.am>
2024-10-10 15:36:21 +0400

    Time: 64 ms (43.1%), Space: 53.4 MB (19.31%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.ts
@@ -0,0 +1,19 @@
+function thirdMax(nums: number[]): number {
+  const max: number[] = [];
+  for (let num of nums) {
+    for (let i = 0; i < 3; i++) {
+      if (max[i] === undefined) {
+        max[i] = num;
+        break;
+      } else if (max[i] === num) {
+        break;
+      } if (max[i] < num) {
+        max.splice(i, 0, num);
+        if (max.length > 3) max.pop();
+        break;
+      }
+    }
+  }
+  console.log(max)
+  return max[2] !== undefined ? max[2] : max[0];
+};
