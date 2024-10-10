2ad66d1bc3fbfeeb3af00e4623fe0f01c1dbbac5
Alexander Kolobov <a@da.am>
2024-10-10 17:16:59 +0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.ts
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.ts
@@ -1,23 +1,11 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
-
+// O(n) PASS
 function findTarget(root: TreeNode | null, k: number): boolean {
   const set = new Set<number>();
   const traverse = (node: TreeNode): boolean => {
     if (set.has(k - node.val)) return true;
     set.add(node.val);
-    return !!node.left && traverse(node.left) || !!node.right && traverse(node.right);
+    return node.left !== null && traverse(node.left) ||
+           node.right !== null && Math.abs(node.val - k) > 0 && traverse(node.right);
   }
   return root && traverse(root);
 };
