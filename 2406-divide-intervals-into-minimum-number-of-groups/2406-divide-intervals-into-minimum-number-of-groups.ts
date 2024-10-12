3c5657f80b4b22da7356a866db6b1004ffe7aa0c
Alexander Kolobov <a@da.am>
2024-10-12 16:53:41 +0400

    Time: 255 ms (100%), Space: 79.4 MB (100%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.ts
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.ts
@@ -1,11 +1,11 @@
-type HeapNode = [number, ...number[]];
+type HeapNode = number;
 class Heap {
   static push(heap: HeapNode[], item: HeapNode): void {
     heap.push(item);
     let i = heap.length - 1;
     while (i) {
       const p = i - 1 >>> 1;
-      if (p === i || heap[p][0] <= heap[i][0]) break;
+      if (p === i || heap[p] <= heap[i]) break;
       [heap[p], heap[i]] = [heap[i], heap[p]];
       i = p;
     }
@@ -17,8 +17,8 @@ class Heap {
       const l = i * 2 + 1,
             r = i * 2 + 2;
       let t = i;
-      if (l < heap.length && heap[t][0] > heap[l][0]) t = l;
-      if (r < heap.length && heap[t][0] > heap[r][0]) t = r;
+      if (l < heap.length && heap[t] > heap[l]) t = l;
+      if (r < heap.length && heap[t] > heap[r]) t = r;
       if (t === i) break;
       [heap[t], heap[i]] = [heap[i], heap[t]];
       i = t;
@@ -28,18 +28,14 @@ class Heap {
 
 function minGroups(intervals: number[][]): number {
   if (!intervals.length) return 0;
-
   intervals.sort((a, b) => a[0] - b[0]);
-  const heap: HeapNode[] = [];
-  let groups = 0;
-  for (const [from, to] of intervals) {
-    if (heap.length && heap[0][0] < from) { // extend group
-      Heap.poppush(heap, [to]);
-    } else { // create new group
-      groups++;
-      Heap.push(heap, [to]);
+  const heap: HeapNode[] = [intervals[0][1]];
+  for (let i = 1; i < intervals.length; i++) {
+    if (heap[0] < intervals[i][0]) {
+      Heap.poppush(heap, intervals[i][1]);
+    } else {
+      Heap.push(heap, intervals[i][1]);
     }
   }
-
-  return groups;
+  return heap.length;
 };
