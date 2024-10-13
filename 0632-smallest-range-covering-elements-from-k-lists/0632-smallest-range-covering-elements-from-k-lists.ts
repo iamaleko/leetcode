e180b7bb947925e4691f24eb729b18eb8bc58894
Alexander Kolobov <a@da.am>
2024-10-13 18:10:40 +0400

    Time: 84 ms (100%), Space: 57.8 MB (100%) - LeetHub

---
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.ts
@@ -0,0 +1,35 @@
+function smallestRange(nums: number[][]): number[] {
+  // create sorted points structure
+  const m = new Map<number, Set<number>>();
+  for (let i = 0; i < nums.length; i++) {
+    for (const num of nums[i]) {
+      if (m.has(num)) {
+        m.get(num).add(i);
+      } else {
+        m.set(num, new Set([i]));
+      }
+    }
+  }
+  const points = Array.from(m.entries());
+  points.sort((a, b) => a[0] - b[0]);
+
+  // iterate using window
+  const represented = new Array(nums.length).fill(0);
+  let lower = points[0][0],
+      upper = points[points.length - 1][0],
+      missed = nums.length;
+
+  for (let l = 0, r = 0; r < points.length; r++) {
+    for (const i of points[r][1]) if (represented[i]++ === 0) missed--;
+    while (!missed) {
+      if (upper - lower > points[r][0] - points[l][0]) {
+        upper = points[r][0]
+        lower = points[l][0]
+      }
+      for (const i of points[l][1]) if (represented[i]-- === 1) missed++;
+      l++;
+    }
+  }
+
+  return [lower, upper];
+};
