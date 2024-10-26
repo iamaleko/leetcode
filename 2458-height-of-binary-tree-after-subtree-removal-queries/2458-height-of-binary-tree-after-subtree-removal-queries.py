458c7a895909c73002d1911544255f396ee4c80e
Alexander Kolobov <a@da.am>
2024-10-27 01:05:14 +0400

    Time: 119 ms (100%), Space: 55.4 MB (73.03%) - LeetHub

--- 2458-height-of-binary-tree-after-subtree-removal-queries/2458-height-of-binary-tree-after-subtree-removal-queries.py
+++ 2458-height-of-binary-tree-after-subtree-removal-queries/2458-height-of-binary-tree-after-subtree-removal-queries.py
@@ -1,47 +1,33 @@
 class Solution:
-    def treeQueries(
-        self, root: Optional[TreeNode], queries: List[int]
-    ) -> List[int]:
-        max_height_after_removal = [0] * 100001
-        self.current_max_height = 0
+  def treeQueries(self, root: Optional[TreeNode], queries: List[int]) -> List[int]:
+    max_height_after_removal = [0] * 100001
+    self.current_max_height = 0
 
-        def _traverse_left_to_right(node, current_height):
-            if not node:
-                return
+    def _ltr(node, current_height):
+      max_height_after_removal[node.val] = self.current_max_height
+      if current_height > self.current_max_height:
+        self.current_max_height = current_height
+      current_height += 1
+      if node.left:
+        _ltr(node.left, current_height)
+      if node.right:
+        _ltr(node.right, current_height)
 
-            # Store the maximum height if this node were removed
-            max_height_after_removal[node.val] = self.current_max_height
+    def _rtl(node, current_height):
+      if self.current_max_height > max_height_after_removal[node.val]:
+        max_height_after_removal[node.val] = self.current_max_height
+      if current_height > self.current_max_height:
+        self.current_max_height = current_height
+      current_height += 1
+      if node.right:
+        _rtl(node.right, current_height)
+      if node.left:
+        _rtl(node.left, current_height)
 
-            # Update the current maximum height
-            self.current_max_height = max(
-                self.current_max_height, current_height
-            )
+    if root:
+      _ltr(root, 0)
+      self.current_max_height = 0
+    if root:
+      _rtl(root, 0)
 
-            # Traverse left subtree first, then right
-            _traverse_left_to_right(node.left, current_height + 1)
-            _traverse_left_to_right(node.right, current_height + 1)
-
-        def _traverse_right_to_left(node, current_height):
-            if not node:
-                return
-
-            # Update the maximum height if this node were removed
-            max_height_after_removal[node.val] = max(
-                max_height_after_removal[node.val], self.current_max_height
-            )
-
-            # Update the current maximum height
-            self.current_max_height = max(
-                current_height, self.current_max_height
-            )
-
-            # Traverse right subtree first, then left
-            _traverse_right_to_left(node.right, current_height + 1)
-            _traverse_right_to_left(node.left, current_height + 1)
-
-        _traverse_left_to_right(root, 0)
-        self.current_max_height = 0  # Reset for the second traversal
-        _traverse_right_to_left(root, 0)
-
-        # Process queries and build the result list
-        return [max_height_after_removal[q] for q in queries]
+    return [max_height_after_removal[q] for q in queries]
