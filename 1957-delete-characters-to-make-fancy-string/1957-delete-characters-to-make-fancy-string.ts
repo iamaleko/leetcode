a24a2df8a26c8ad28fb4fee04907329c97b892d7
Alexander Kolobov <a@da.am>
2024-11-02 01:15:06 +0400

    Time: 67 ms (50%), Space: 74.8 MB (6.25%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.ts
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.ts
@@ -1,8 +1,8 @@
 function makeFancyString(s: string): string {
   const st: string[] = [];
-  for (let i = 0, p = -1, skip = ''; i < s.length; i++) {
+  for (let i = 0, skip = ''; i < s.length; i++) {
     if (skip === s[i]) continue;
-    skip = st[p++] === s[i] ? s[i] : '';
+    skip = st.at(-1) === s[i] ? s[i] : '';
     st.push(s[i]);
   }
   return st.join('');
