2395541c76af2db7754bf08a7932f6eb94688718
Alexander Kolobov <a@da.am>
2024-11-02 01:09:21 +0400

    Time: 76 ms (50%), Space: 74.4 MB (12.5%) - LeetHub

---
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.ts
@@ -0,0 +1,8 @@
+function makeFancyString(s: string): string {
+  let st: string[] = [];
+  for (let i = 0; i < s.length; i++) {
+    if (st.at(-2) === s[i] && st.at(-1) === s[i]) continue;
+    st.push(s[i]);
+  }
+  return st.join('');
+};
