1d303289ae14184d806212d67f6f9696e8b9d5d1
Alexander Kolobov <a@da.am>
2024-11-06 00:07:30 +0400

    Time: 35 ms (100%), Space: 69.4 MB (66.67%) - LeetHub

---
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.ts
@@ -0,0 +1,25 @@
+function minChanges(s: string): number {
+  const parts: [number, number][] = [];
+  let last = '', count = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== last) {
+      if (count) parts.push([last === '1' ? 1 : 0, count])
+      last = s[i];
+      count = 0;
+    }
+    count++;
+  }
+  if (count) parts.push([last === '1' ? 1 : 0, count]);
+  // console.log(parts)
+  
+  let ans = 0;
+  for (let i = 1; i < parts.length; i++) {
+    if (parts[i - 1][1] % 2 !== 0) {
+      parts[i - 1][1]++;
+      parts[i][1]--;
+      ans++;
+    }
+  }
+
+  return ans;
+};
