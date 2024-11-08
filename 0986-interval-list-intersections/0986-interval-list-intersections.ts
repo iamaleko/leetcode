eb5cbe5e2c2680875763016359636e8a09761aa2
Alexander Kolobov <a@da.am>
2024-11-09 00:13:20 +0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0986-interval-list-intersections/0986-interval-list-intersections.ts
@@ -0,0 +1,23 @@
+function intervalIntersection(a: number[][], b: number[][]): number[][] {
+  const ans: number[][] = [];
+  let ap = 0, bp = 0;
+  main: while (ap < a.length && bp < b.length) {
+    while (a[ap][1] < b[bp][0]) {
+      if (++ap >= a.length) break main;
+    }
+    while (b[bp][1] < a[ap][0]) {
+      if (++bp >= b.length) break main;
+    }
+    if (b[bp][0] > a[ap][1]) continue;
+    const int = [Math.max(a[ap][0], b[bp][0]), Math.min(a[ap][1], b[bp][1])];
+    // console.log(a[ap], b[bp], int)
+    if (ans.length && ans.at(-1)[1] > int[0]) {
+      ans.at(-1)[1] = int[1];
+    } else {
+      ans.push(int);
+    }
+    a[ap][1] > b[bp][1] ? bp++ : ap++;
+    console.log(ans)
+  }
+  return ans;
+};
