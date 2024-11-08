07b4a4cce4f0f0801bf86effa646cdef550c8012
Alexander Kolobov <a@da.am>
2024-11-08 21:22:26 +0400

    Time: 7 ms (61.06%), Space: 68 MB (25.92%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -4,8 +4,8 @@ function productExceptSelf(nums: number[]): number[] {
         ans = new Array(nums.length).fill(0);
   for (let product = 1, i = 0; i < nums.length; i++) ltr[i] = product *= nums[i];
   for (let product = 1, i = nums.length - 1; i >= 0; i--) rtl[i] = product *= nums[i];
-  for (let i = 0; i < nums.length; i++) {
-    ans[i] = (i ? ltr[i - 1] : 1) * (i + 1 < nums.length ? rtl[i + 1] : 1);
-  }
+  for (let i = 1; i < nums.length - 1; i++) ans[i] = ltr[i - 1] * rtl[i + 1];
+  ans[0] = rtl[1];
+  ans[nums.length - 1] = ltr[nums.length - 2];
   return ans;
 };
