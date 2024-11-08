64e77b475472b6e4cb24b26406cc655ae964cec0
Alexander Kolobov <a@da.am>
2024-11-08 23:14:57 +0400

    Time: 22 ms (11.11%), Space: 62.9 MB (81.17%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.ts
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.ts
@@ -1,36 +1,52 @@
+// class BSTIterator {
+//   root: TreeNode | null;
+//   iterator: Generator;
+//   curr: IteratorResult<unknown, any>;
+//   constructor(root: TreeNode | null) {
+//     this.root = root;
+//     this.iterator = this.generator();
+//     this.curr = this.iterator.next();
+//   }
+//   *generator(node: TreeNode | null = this.root) {
+//     if (node) {
+//       if (node.left) yield *this.generator(node.left);
+//       yield node.val;
+//       if (node.right) yield *this.generator(node.right);
+//     }
+//   }
+//   next(): number {
+//     const val = this.curr.value;
+//     this.curr = this.iterator.next();
+//     return val;
+//   }
+//   hasNext(): boolean {
+//     return !this.curr.done;
+//   }
+// }
+
+// Using ES6 generator
 class BSTIterator {
-  root: TreeNode | null;
-  iterator: Generator;
-  curr: IteratorResult<unknown, any>;
+  private _generator: Generator;
+  private _result: IteratorResult<unknown, any>;
 
   constructor(root: TreeNode | null) {
-    this.root = root;
-    this.iterator = this.generator();
-    this.curr = this.iterator.next();
-  }
-
-  *generator(node: TreeNode | null = this.root) {
-    if (node) {
-      if (node.left) yield *this.generator(node.left);
-      yield node.val;
-      if (node.right) yield *this.generator(node.right);
+    function *generator(node: TreeNode | null = root) {
+      if (node) {
+        if (node.left) yield *generator(node.left);
+        yield node.val;
+        if (node.right) yield *generator(node.right);
+      }
     }
+    this._generator = generator();
+    this._result = this._generator.next();
   }
-
+  
   next(): number {
-    const val = this.curr.value;
-    this.curr = this.iterator.next();
-    return val;
+    const value = this._result.value;
+    this._result = this._generator.next();
+    return value;
   }
-
   hasNext(): boolean {
-    return !this.curr.done;
+    return !this._result.done;
   }
 }
-
-/**
- * Your BSTIterator object will be instantiated and called as such:
- * var obj = new BSTIterator(root)
- * var param_1 = obj.next()
- * var param_2 = obj.hasNext()
- */
