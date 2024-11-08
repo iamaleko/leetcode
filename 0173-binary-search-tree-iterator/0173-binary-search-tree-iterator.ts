b1f6b56ff299d77b522c1a5b6f970133dc3299a7
Alexander Kolobov <a@da.am>
2024-11-08 23:16:51 +0400

    Time: 10 ms (91.36%), Space: 63.5 MB (31.38%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.ts
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.ts
@@ -1,52 +1,46 @@
-// class BSTIterator {
-//   root: TreeNode | null;
-//   iterator: Generator;
-//   curr: IteratorResult<unknown, any>;
-//   constructor(root: TreeNode | null) {
-//     this.root = root;
-//     this.iterator = this.generator();
-//     this.curr = this.iterator.next();
-//   }
-//   *generator(node: TreeNode | null = this.root) {
-//     if (node) {
-//       if (node.left) yield *this.generator(node.left);
-//       yield node.val;
-//       if (node.right) yield *this.generator(node.right);
-//     }
-//   }
-//   next(): number {
-//     const val = this.curr.value;
-//     this.curr = this.iterator.next();
-//     return val;
-//   }
-//   hasNext(): boolean {
-//     return !this.curr.done;
-//   }
-// }
-
-// Using ES6 generator
 class BSTIterator {
-  private _generator: Generator;
-  private _result: IteratorResult<unknown, any>;
-
-  constructor(root: TreeNode | null) {
-    function *generator(node: TreeNode | null = root) {
-      if (node) {
-        if (node.left) yield *generator(node.left);
-        yield node.val;
-        if (node.right) yield *generator(node.right);
-      }
+  private _stack: TreeNode[];
+  private _fill(node: TreeNode) {
+    while (node) {
+      this._stack.push(node);
+      node = node.left;
     }
-    this._generator = generator();
-    this._result = this._generator.next();
   }
-  
+  constructor(root: TreeNode | null) {
+    this._stack = [];
+    if (root) this._fill(root);
+  }
   next(): number {
-    const value = this._result.value;
-    this._result = this._generator.next();
-    return value;
+    const node = this._stack.pop() as TreeNode;
+    if (node.right) this._fill(node.right);
+    return node.val;
   }
   hasNext(): boolean {
-    return !this._result.done;
+    return this._stack.length > 0;
   }
 }
+
+// Using ES6 generator
+// class BSTIterator {
+//   private _generator: Generator;
+//   private _result: IteratorResult<unknown, any>;
+//   constructor(root: TreeNode | null) {
+//     function *generator(node: TreeNode | null = root) {
+//       if (node) {
+//         if (node.left) yield *generator(node.left);
+//         yield node.val;
+//         if (node.right) yield *generator(node.right);
+//       }
+//     }
+//     this._generator = generator();
+//     this._result = this._generator.next();
+//   }
+//   next(): number {
+//     const value = this._result.value;
+//     this._result = this._generator.next();
+//     return value;
+//   }
+//   hasNext(): boolean {
+//     return !this._result.done;
+//   }
+// }
