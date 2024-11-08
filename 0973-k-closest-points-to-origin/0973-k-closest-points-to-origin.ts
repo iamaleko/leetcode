14ebbdca76b031a1997eb1577547720ba03ddf95
Alexander Kolobov <a@da.am>
2024-11-08 20:38:28 +0400

    Time: 46 ms (70.8%), Space: 65.1 MB (71.85%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.ts
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.ts
@@ -1,6 +1,6 @@
 function kClosest(points: number[][], k: number): number[][] {
-  return points.map((v) => [v[0], v[1], v[0] ** 2 + v[1] ** 2])
-         .sort((a, b) => a[2] - b[2])
+  return points.map(([x, y]) => [x, y, x ** 2 + y ** 2])
+         .sort(([,,a], [,,b]) => a - b)
          .slice(0, k)
-         .map((v) => v.slice(0, 2));
+         .map(([x, y]) => [x, y]);
 };
