ee092502f00f55ef0e06d462ee2861be6c0364ca
Alexander Kolobov <a@da.am>
2024-11-08 21:06:04 +0400

    Time: 106 ms (29.93%), Space: 70.7 MB (25.68%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.ts
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.ts
@@ -1,6 +1,14 @@
 function kClosest(points: number[][], k: number): number[][] {
-  return points.map(([x, y]) => [x, y, x ** 2 + y ** 2])
-         .sort(([,,a], [,,b]) => a - b)
-         .slice(0, k)
-         .map(([x, y]) => [x, y]);
+  const heap = MinPriorityQueue.from(points.map(([x, y]) => [[x, y], x ** 2 + y ** 2]));
+  const ans: number[][] = [];
+  k = Math.min(points.length, k);
+  while (ans.length < k) ans.push(heap.dequeue().element)
+  return ans;
 };
+
+// function kClosest(points: number[][], k: number): number[][] {
+//   return points.map(([x, y]) => [x, y, x ** 2 + y ** 2])
+//          .sort(([,,a], [,,b]) => a - b)
+//          .slice(0, k)
+//          .map(([x, y]) => [x, y]);
+// };
