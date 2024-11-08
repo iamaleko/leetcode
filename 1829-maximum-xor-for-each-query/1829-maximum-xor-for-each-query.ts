79c18d5aa47b885fbe97640cd86dd1f6e6c1113b
Alexander Kolobov <a@da.am>
2024-11-08 19:52:01 +0400

    Time: 9 ms (100%), Space: 76.2 MB (75%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.ts
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.ts
@@ -1,17 +1,17 @@
-// function getMaximumXor(nums: number[], maximumBit: number): number[] {
-//   const n = nums.length,
-//         ans: number[] = new Array(n).fill(0),
-//         mask = (1 << maximumBit) - 1;
-//   for (let xor = 0, i = 0; i < n; ans[n - ++i] = xor & mask ^ mask) xor ^= nums[i];
-//   return ans;
-// };
-
 function getMaximumXor(nums: number[], maximumBit: number): number[] {
   const n = nums.length,
         ans: number[] = new Array(n).fill(0),
         mask = (1 << maximumBit) - 1;
-  let xor = 0;
-  for (let i = 0; i < n; i++) xor ^= nums[i];
-  for (let i = 0; i < n; xor ^= nums[n - ++i]) ans[i] = xor & mask ^ mask;
+  for (let xor = 0, i = 0; i < n; ans[n - ++i] = xor & mask ^ mask) xor ^= nums[i];
   return ans;
 };
+
+// function getMaximumXor(nums: number[], maximumBit: number): number[] {
+//   const n = nums.length,
+//         ans: number[] = new Array(n).fill(0),
+//         mask = (1 << maximumBit) - 1;
+//   let xor = 0;
+//   for (let i = 0; i < n; i++) xor ^= nums[i];
+//   for (let i = 0; i < n; xor ^= nums[n - ++i]) ans[i] = xor & mask ^ mask;
+//   return ans;
+// };
