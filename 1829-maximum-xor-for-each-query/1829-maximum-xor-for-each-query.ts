f40f2865a0e269f21caf57e694576874f83e04f0
Alexander Kolobov <a@da.am>
2024-11-08 19:50:34 +0400

    Time: 9 ms (100%), Space: 76.1 MB (75%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.ts
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.ts
@@ -1,3 +1,11 @@
+// function getMaximumXor(nums: number[], maximumBit: number): number[] {
+//   const n = nums.length,
+//         ans: number[] = new Array(n).fill(0),
+//         mask = (1 << maximumBit) - 1;
+//   for (let xor = 0, i = 0; i < n; ans[n - ++i] = xor & mask ^ mask) xor ^= nums[i];
+//   return ans;
+// };
+
 function getMaximumXor(nums: number[], maximumBit: number): number[] {
   const n = nums.length,
         ans: number[] = new Array(n).fill(0),
