af798b2f59f98c3fd7cd46ab6a4f354f799849b2
Alexander Kolobov <a@da.am>
2024-11-09 22:17:33 +0400

    Time: 1 ms (100%), Space: 52.2 MB (66.67%) - LeetHub

---
+++ 3133-minimum-array-end/3133-minimum-array-end.ts
@@ -0,0 +1,31 @@
+// Tips (but i'v almost got it myself)
+function minEnd(n: number, x: number): number {
+  const bits: number[] = [];
+  n--;
+  while (x || n) {
+    if (x & 1) {
+      bits.push(1);
+    } else {
+      bits.push(n & 1 ? 1 : 0);
+      n >>=1;
+    }
+    x >>= 1;
+  }
+  return parseInt(bits.reverse().join(''), 2);
+};
+
+// Math TLE
+// function minEnd(n: number, x: number): number {
+//   let num = x, int32 = 2 ** 31;
+//   for (let i = 1; i < n; i++) {
+//     num++;
+//     if (num > int32 - 1) {
+//       num = Math.trunc(num / int32) * int32 + (num & int32 - 1 | x);
+//     } else {
+//       num |= x;
+//     }
+//   }
+//   return num;
+// };
+
+
