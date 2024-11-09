5a0406d549b44f81a667d6ae01eb0653f488814a
Alexander Kolobov <a@da.am>
2024-11-09 23:52:54 +0400

    Time: 2 ms (100%), Space: 52.5 MB (33.33%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.ts
+++ 3133-minimum-array-end/3133-minimum-array-end.ts
@@ -1,26 +1,45 @@
-// Tips (but i'v almost got it myself)
+// Bit manipulation above 32 bits limit
+function setBitAtPos(mask: number, pos: number, val: number): number {
+  return (Math.trunc(mask / 2 ** ++pos) * 2 + val) * 2 ** --pos + mask % 2 ** pos;
+}
 function minEnd(n: number, x: number): number {
-  const bits: number[] = [];
+  let ans = 0, pos = 0;
   n--;
   while (x || n) {
     if (x & 1) {
-      bits.push(1);
+      ans = setBitAtPos(ans, pos++, 1);
     } else {
-      bits.push(n & 1);
-      n >>=1;
+      ans = setBitAtPos(ans, pos++, n & 1);
+      n >>= 1;
     }
     x >>= 1;
   }
-  return parseInt(bits.reverse().join(''), 2);
+  return ans;
 };
+
+// Tips (but i'v almost got it myself)
+// function minEnd(n: number, x: number): number {
+//   const bits: number[] = [];
+//   n--;
+//   while (x || n) {
+//     if (x & 1) {
+//       bits.push(1);
+//     } else {
+//       bits.push(n & 1);
+//       n >>= 1;
+//     }
+//     x >>= 1;
+//   }
+//   return parseInt(bits.reverse().join(''), 2);
+// };
 
 // Math TLE
 // function minEnd(n: number, x: number): number {
-//   let num = x, int32 = 2 ** 31;
+//   let num = x;
 //   for (let i = 1; i < n; i++) {
 //     num++;
-//     if (num > int32 - 1) {
-//       num = Math.trunc(num / int32) * int32 + (num & int32 - 1 | x);
+//     if (num > 2 ** 31 - 1) {
+//       num = Math.trunc(num / 2 ** 31) * 2 ** 31 + (num & 2 ** 31 - 1 | x);
 //     } else {
 //       num |= x;
 //     }
