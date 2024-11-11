8a54eb7603127bdc29a860012da58cef11f352b0
Alexander Kolobov <a@da.am>
2024-11-11 21:08:22 +0400

    Time: 47 ms (100%), Space: 52.3 MB (100%) - LeetHub

--- 2601-prime-subtraction-operation/2601-prime-subtraction-operation.ts
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.ts
@@ -6,17 +6,15 @@ function primeSubOperation(nums: number[]): boolean {
     for (let j = 2; j < i; j++) if (i % j === 0) continue main;
     primes.push(i);
   }
-  console.log(primes)
   for (let i = nums.length - 2; i >= 0; i--) {
-    if (nums[i] < nums[i + 1]) continue;
-    if (!primes.length) return false;
-    let p = 0;
-    while (primes[p] >= nums[i] || nums[i] - primes[p] >= nums[i + 1]) {
-      p++;
-      if (p === primes.length || nums[i] - primes[i] <= 0) return false;
+    if (nums[i] >= nums[i + 1]) {
+      if (!primes.length) return false;
+      let p = 0;
+      while (primes[p] >= nums[i] || nums[i] - primes[p] >= nums[i + 1]) {
+        if (++p === primes.length || nums[i] - primes[p] <= 0) return false;
+      }
+      nums[i] -= primes[p];
     }
-    nums[i] -= primes[p];
-    // console.log(i, nums);
   }
   return true;
 };
