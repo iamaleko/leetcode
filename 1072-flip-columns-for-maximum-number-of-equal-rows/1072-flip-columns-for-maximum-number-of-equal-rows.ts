b4d5a7344c9b74a95d3c181f9ea5c592e9dc06d5
Alexander Kolobov <a@da.am>
2024-11-22 15:22:47 +0400

    Time: 83 ms (28.57%), Space: 62.2 MB (37.5%) - LeetHub

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.ts
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.ts
@@ -1,16 +1,7 @@
 function maxEqualRowsAfterFlips(m: number[][]): number {
   const count: Record<string, number> = {};
   for (let y = 0; y < m.length; y++) {
-    let unset = '', set = '';
-    for (let x = 0; x <m[y].length ; x++) {
-      if (m[y][x]) {
-        unset += '1';
-        set += '0';
-      } else {
-        unset += '0';
-        set += '1';
-      }
-    }
+    const unset = m[y].join(), set = m[y].map((v) => v ^ 1).join();
     count.hasOwnProperty(unset) ? count[unset]++ : count[unset] = 1;
     count.hasOwnProperty(set) ? count[set]++ : count[set] = 1;
   }
@@ -18,3 +9,25 @@ function maxEqualRowsAfterFlips(m: number[][]): number {
   for (const key in count) if (count[key] > ans) ans = count[key];
   return ans;
 };
+
+// Pass, beats 57%, O(m * n) 
+// function maxEqualRowsAfterFlips(m: number[][]): number {
+//   const count: Record<string, number> = {};
+//   for (let y = 0; y < m.length; y++) {
+//     let unset = '', set = '';
+//     for (let x = 0; x <m[y].length ; x++) {
+//       if (m[y][x]) {
+//         unset += '1';
+//         set += '0';
+//       } else {
+//         unset += '0';
+//         set += '1';
+//       }
+//     }
+//     count.hasOwnProperty(unset) ? count[unset]++ : count[unset] = 1;
+//     count.hasOwnProperty(set) ? count[set]++ : count[set] = 1;
+//   }
+//   let ans = 0;
+//   for (const key in count) if (count[key] > ans) ans = count[key];
+//   return ans;
+// };
