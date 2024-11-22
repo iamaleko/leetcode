e5b05ab06a1b348de0008d8fb31f9cc441cefccf
Alexander Kolobov <a@da.am>
2024-11-22 21:59:47 +0400

    Time: 337 ms (27.12%), Space: 37.1 MB (8.85%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -1,57 +1,76 @@
+# Fails on negative integers
+# class Solution:
+#   def shortestSubarray(self, nums: List[int], k: int) -> int:
+#     if sum(nums) < k:
+#       return -1
+
+#     rs = [0]
+#     ans = len(nums)
+    
+#     for num in nums:
+#       l = 0
+#       r = len(rs) - 1
+#       num += rs[-1]
+#       while l <= r:
+#         c = l + r >> 1
+#         if num - rs[c] < k:
+#           r = c - 1
+#         else:
+#           l = c + 1
+#       if r >= 0:
+#         ans = min(len(rs) - r, ans)
+#       rs.append(num)
+
+#     return ans
+
+# TLE 91/98
+# class Solution:
+#   def shortestSubarray(self, nums: List[int], k: int) -> int:
+#     s = 0
+#     rs = [[0, -1]]
+#     ans = -1
+    
+#     for i in range(len(nums)):
+#       if nums[i] >= k:
+#         return 1
+#       s += nums[i]
+#       rs.append([s, i])
+      
+#       # bubble sort
+#       p = len(rs) - 1
+#       while (p and rs[p][0] < rs[p-1][0]):
+#         if rs[p - 1][1] < rs[p][1]:
+#           rs[p - 1][1] = rs[p][1]
+#         rs[p], rs[p-1] = rs[p-1], rs[p]
+#         p -= 1
+
+#       l, r = 0, len(rs) - 2
+#       while l <= r:
+#         c = l + r >> 1
+#         if s - rs[c][0] >= k:
+#           l = c + 1
+#           if ans == -1 or ans > i - rs[c][1]:
+#             ans = i - rs[c][1]
+#         else:
+#           r = c - 1
+
+#     return ans
+
 class Solution:
-    def shortestSubarray(self, nums: List[int], k: int) -> int:
-        n = len(nums)
+  def shortestSubarray(self, nums: List[int], k: int) -> int:
+    s = 0
+    ans = -1
+    h = [(0,-1)]
+    
+    for i in range(len(nums)):
+      if nums[i] >= k:
+        return 1
 
-        # Stack-like list to store cumulative sums and their indices
-        cumulative_sum_stack = [(0, -1)]
+      s += nums[i]
+      while s - h[0][0] >= k:
+        if ans == -1 or ans > i - h[0][1]:
+          ans = i - h[0][1]
+        heappop(h)
+      heappush(h, (s,i))
 
-        running_cumulative_sum = 0
-        shortest_subarray_length = float("inf")
-
-        for i in range(n):
-            # Update cumulative sum
-            running_cumulative_sum += nums[i]
-
-            # Remove entries from stack that are larger than current cumulative sum
-            while (
-                cumulative_sum_stack
-                and running_cumulative_sum <= cumulative_sum_stack[-1][0]
-            ):
-                cumulative_sum_stack.pop()
-
-            # Add current cumulative sum and index to stack
-            cumulative_sum_stack.append((running_cumulative_sum, i))
-
-            candidate_index = self._find_candidate_index(
-                cumulative_sum_stack, running_cumulative_sum - k
-            )
-
-            # If a valid candidate is found, update the shortest subarray length
-            if candidate_index != -1:
-                shortest_subarray_length = min(
-                    shortest_subarray_length,
-                    i - cumulative_sum_stack[candidate_index][1],
-                )
-
-        # Return -1 if no valid subarray found
-        return (
-            shortest_subarray_length
-            if shortest_subarray_length != float("inf")
-            else -1
-        )
-
-    # Binary search to find the largest index where cumulative sum is <= target
-    def _find_candidate_index(
-        self, nums: List[Tuple[int, int]], target: int
-    ) -> int:
-        left, right = 0, len(nums) - 1
-
-        while left <= right:
-            mid = left + (right - left) // 2
-
-            if nums[mid][0] <= target:
-                left = mid + 1
-            else:
-                right = mid - 1
-
-        return right
+    return ans
