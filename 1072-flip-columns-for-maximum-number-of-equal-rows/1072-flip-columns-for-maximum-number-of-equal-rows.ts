4246b289acbdb6ab5127699869631e263793be57
Alexander Kolobov <a@da.am>
2024-11-22 15:23:41 +0400

    Time: 60 ms (57.14%), Space: 60.2 MB (50%) - LeetHub

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.ts
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.ts
@@ -1,7 +1,7 @@
 function maxEqualRowsAfterFlips(m: number[][]): number {
   const count: Record<string, number> = {};
   for (let y = 0; y < m.length; y++) {
-    const unset = m[y].join(), set = m[y].map((v) => v ^ 1).join();
+    const unset = m[y].join(''), set = m[y].map((v) => v ^ 1).join('');
     count.hasOwnProperty(unset) ? count[unset]++ : count[unset] = 1;
     count.hasOwnProperty(set) ? count[set]++ : count[set] = 1;
   }
