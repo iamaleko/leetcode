b75475d215b75f47a5727e63eda8dc555d094dd7
Alexander Kolobov <a@da.am>
2024-11-27 14:52:32 +0400

    Time: 67 ms (98.12%), Space: 18 MB (8.31%) - LeetHub

--- 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
@@ -7,6 +7,15 @@ class GraphNode:
 class Solution:
   graph = None
 
+  def buildGraph(self, n: int) -> None:
+    self.graph = GraphNode(0, 0)
+    self.map = {0: self.graph}
+    node = self.graph
+    for i in range(1, n):
+      self.map[i] = GraphNode(i, node.dist + 1)
+      node.joints.add(self.map[i])
+      node = self.map[i]
+
   def updateGraph(self, node: GraphNode, dist: int) -> None:
     if node.dist > dist:
       node.dist = dist
@@ -14,22 +23,13 @@ class Solution:
         self.updateGraph(joint, dist + 1)
 
   def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
-    # build graph and index
-    self.graph = GraphNode(0, 0)
-    m = {0: self.graph}
-    node = self.graph
-    for i in range(1, n):
-      m[i] = GraphNode(i, node.dist + 1)
-      node.joints.add(m[i])
-      node = m[i]
-
-    # update graph
+    self.buildGraph(n)
     ans = []
     for a, b in queries:
-      if m[b] not in m[a].joints:
-        m[a].joints.add(m[b])
-        self.updateGraph(m[b], m[a].dist + 1)
-        ans.append(m[n - 1].dist)
+      if self.map[b] not in self.map[a].joints:
+        self.map[a].joints.add(self.map[b])
+        self.updateGraph(self.map[b], self.map[a].dist + 1)
+        ans.append(self.map[n - 1].dist)
 
     return ans
         
