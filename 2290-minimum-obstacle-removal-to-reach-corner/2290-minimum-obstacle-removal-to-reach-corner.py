5fa4a0a7d26040cd15cbe21c90a36d0becb263c6
Alexander Kolobov <a@da.am>
2024-11-28 20:59:48 +0400

    Time: 543 ms (100%), Space: 31.2 MB (99.45%) - LeetHub

---
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -0,0 +1,53 @@
+class Solution:
+  def minimumObstacles(self, grid: List[List[int]]) -> int:
+    ans, my, mx = 0, len(grid) - 1, len(grid[0]) - 1
+    
+    EMPTY = 0
+    OBSTACLE = 1
+    VISITED = 2
+
+    queue = deque([(0,0)])
+    grid[0][0] = VISITED
+    border = deque()
+
+    while queue:
+      while queue:
+        y, x = queue.popleft()
+
+        if y == my and x == mx:
+          return ans
+
+        if y > 0:
+          if grid[y - 1][x] == EMPTY:
+            queue.append((y - 1, x))
+          elif grid[y - 1][x] == OBSTACLE:
+            border.append((y - 1, x))
+          grid[y - 1][x] = VISITED
+        if y < my:
+          if grid[y + 1][x] == EMPTY:
+            queue.append((y + 1, x))
+          elif grid[y + 1][x] == OBSTACLE:
+            border.append((y + 1, x))
+          grid[y + 1][x] = VISITED
+        if x > 0:
+          if grid[y][x - 1] == EMPTY:
+            queue.append((y, x - 1))
+          elif grid[y][x - 1] == OBSTACLE:
+            border.append((y, x - 1))
+          grid[y][x - 1] = VISITED
+        if x < mx:
+          if grid[y][x + 1] == EMPTY:
+            queue.append((y, x + 1))
+          elif grid[y][x + 1] == OBSTACLE:
+            border.append((y, x + 1))
+          grid[y][x + 1] = VISITED
+      
+      if border:
+        while border:
+          y, x = border.popleft()
+          queue.append((y, x))
+        ans += 1
+      
+
+
+        
