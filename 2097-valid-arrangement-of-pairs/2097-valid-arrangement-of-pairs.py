65f8424564600cf55fd0d14372952ea4880eaa31
Alexander Kolobov <a@da.am>
2024-12-02 16:47:28 +0400

    Time: 1483 ms (5.03%), Space: 114.3 MB (22.14%) - LeetHub

--- 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.py
+++ 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.py
@@ -8,79 +8,66 @@ class Node:
     self.r = r
 
 class Solution:
-  head = None
+  ltr: Dict | None = None
+  rtl: Dict | None = None
 
-  def removeFromGraph(self, l: int, r: int):
-    if l in self.ltr:
-      self.ltr[l].remove(r)
-      if not len(self.ltr[l]):
-        del self.ltr[l]
-    if r in self.rtl:
-      self.rtl[r].remove(l)
-      if not len(self.rtl[r]):
-        del self.rtl[r]
-
-  def buildPath(self, node: Node) -> None:
-    # build forward
-    _next = node.next
-    while node.r in self.ltr:
-      l, r = node.r, next(iter(self.ltr[node.r]))
-      self.removeFromGraph(l, r)
-      successor = Node(l, r)
-      node.next = successor
-      successor.prev = node
-      node = successor
-    node.next = _next
-    
-    # build center
-    if node.prev:
-      self.buildPath(node.prev)
-    else:
-      # build backward
-      last = node
-      while node.l in self.rtl:
-        l, r = next(iter(self.rtl[node.l])), node.l
-        self.removeFromGraph(l, r)
-        predecessor = Node(l, r)
-        node.prev = predecessor
-        predecessor.next = node
-        node = predecessor
-      # if last.prev:
-      #   self.buildPath(last.prev)
-
-  def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
-    # build graph
+  def buildGraph(self, pairs: List[List[int]]) -> None:
     self.ltr = {}
     self.rtl = {}
     for l, r in pairs:
-      if l not in self.ltr: self.ltr[l] = set()
-      if r not in self.rtl: self.rtl[r] = set()
-      self.ltr[l].add(r)
-      self.rtl[r].add(l)
-    
-    # find starting point in open graph
+      if l in self.ltr:
+        self.ltr[l].add(r)
+      else:
+        self.ltr[l] = set([r])
+      if r in self.rtl:
+        self.rtl[r].add(l)
+      else:
+        self.rtl[r] = set([l])
+
+  def defineStartingNode(self, pairs: List[List[int]]) -> Node:
+    # node with more incoming edges than outgoing
     for l, r in pairs:
       if len(self.ltr[l]) > (len(self.rtl[l]) if l in self.rtl else 0):
-        self.removeFromGraph(l, r)
-        self.head = Node(l, r)
-        break
+        return self.buildNode(l, r)
+    return self.buildNode(pairs[0][0], pairs[0][1])
 
-    # find starting point in cyclic graph
-    if not self.head:
-      l,r = pairs[0]
-      self.removeFromGraph(l, r)
-      self.head = Node(l, r)
+  def buildNode(self, l: int, r: int) -> Node:
+    self.ltr[l].remove(r)
+    if not len(self.ltr[l]):
+      del self.ltr[l]
+    self.rtl[r].remove(l)
+    if not len(self.rtl[r]):
+      del self.rtl[r]
+    return Node(l, r)
 
-    # find path
-    self.buildPath(self.head)
+  def buildPath(self, node: Node) -> None:
+    # build forward
+    tail = node.next
+    while node.r in self.ltr:
+      node.next = self.buildNode(node.r, next(iter(self.ltr[node.r])))
+      node.next.prev = node
+      node = node.next
+    node.next = tail
+    
+    # build center
+    if node.prev:
+      return self.buildPath(node.prev)
+
+    # build backward (in case if there are multiple starting points in open graph)
+    while node.l in self.rtl:
+      node.prev = self.buildNode(next(iter(self.rtl[node.l])), node.l)
+      node.prev.next = node
+      node = node.prev
+    return node
+
+  def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
+    self.buildGraph(pairs)
+    node = self.defineStartingNode(pairs)
+    node = self.buildPath(node)
 
     # build answer
     ans = []
-    while self.head.prev:
-      self.head = self.head.prev
-    while self.head:
-      ans.append([self.head.l, self.head.r])
-      self.head = self.head.next
-
+    while node:
+      ans.append([node.l, node.r])
+      node = node.next
     return ans
-
