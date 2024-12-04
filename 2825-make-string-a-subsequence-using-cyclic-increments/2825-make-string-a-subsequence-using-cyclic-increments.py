d11faf7b839bad5b89549e4581645d121dd687af
Alexander Kolobov <a@da.am>
2024-12-04 14:48:41 +0400

    Time: 71 ms (74.31%), Space: 17.9 MB (5.69%) - LeetHub

--- 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
@@ -1,11 +1,8 @@
 class Solution:
-  def next(self, ch: str) -> str:
-    return 'a' if ch == 'z' else chr(ord(ch) + 1)
-
   def canMakeSubsequence(self, a: str, b: str) -> bool:
     n, j = len(a), 0
     for i in range(len(b)):
-      while j < n and a[j] != b[i] and self.next(a[j]) != b[i]:
+      while j < n and a[j] != b[i] and ('a' if a[j] == 'z' else chr(ord(a[j]) + 1)) != b[i]:
         j += 1
       if j == n:
         return False
