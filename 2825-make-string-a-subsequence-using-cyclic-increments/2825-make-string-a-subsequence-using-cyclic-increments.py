03931b08f8577e478ac3386d4ba24a16ca6e8283
Alexander Kolobov <a@da.am>
2024-12-04 14:47:55 +0400

    Time: 79 ms (56.25%), Space: 17.7 MB (31.28%) - LeetHub

--- 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
@@ -5,11 +5,9 @@ class Solution:
   def canMakeSubsequence(self, a: str, b: str) -> bool:
     n, j = len(a), 0
     for i in range(len(b)):
+      while j < n and a[j] != b[i] and self.next(a[j]) != b[i]:
+        j += 1
       if j == n:
         return False
-      while a[j] != b[i] and self.next(a[j]) != b[i]:
-        j += 1
-        if j == n:
-          return False
       j += 1
     return True
