524c970daf7609a7d2c62f2d463120884da14252
Alexander Kolobov <a@da.am>
2024-12-10 21:27:17 +0400

    Time: 19 ms (54.32%), Space: 17.3 MB (13.91%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
@@ -2,11 +2,11 @@ class Solution:
   def maximumLength(self, s: str) -> int:
     count, ans = 1, -1
     d = defaultdict(int)
-    d[f"{s[0]}1"] = 1
+    d[(1 << 5) + ord(s[0]) - 96] = 1
     for i in range(1, len(s)):
       count = count + 1 if s[i] == s[i - 1] else 1
       for offset in range(min(count, 3)):
-        key = f"{s[i]}{count - offset}"
+        key = (count - offset << 5) + ord(s[i]) - 96
         d[key] += 1
         if d[key] > 2:
           ans = max(ans, count - offset)
