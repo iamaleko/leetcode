85705e4615bff1963c994b79a7ec087ebc1c1a22
Alexander Kolobov <a@da.am>
2024-12-10 21:16:29 +0400

    Time: 18 ms (55.68%), Space: 17.5 MB (5.52%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
@@ -5,8 +5,9 @@ class Solution:
     for i in range(len(s)):
       count = count + 1 if i and s[i] == s[i - 1] else 1
       for offset in range(min(count, 3)):
-        d[f"{s[i]}{count - offset}"] += 1
-        if ans < count - offset and d[f"{s[i]}{count - offset}"] > 2:
+        key = f"{s[i]}{count - offset}"
+        d[key] += 1
+        if ans < count - offset and d[key] > 2:
           ans = count - offset
     return ans
         
