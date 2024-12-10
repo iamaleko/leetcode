11ba460c3bddd1df0b9e0877bdc88f09884ca209
Alexander Kolobov <a@da.am>
2024-12-10 21:43:57 +0400

    Time: 18 ms (55.68%), Space: 17.5 MB (5.52%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
@@ -1,19 +1,13 @@
 class Solution:
   def maximumLength(self, s: str) -> int:
-    count, ans = 1, -1
-    mem = [0] * 1626 # (50 << 5) + ord('z') - 96
+    cnt, ans, mem = 1, -1, [0] * 1626
     mem[(1 << 5) + ord(s[0]) - 96] = 1
     for i in range(1, len(s)):
-      count = count + 1 if s[i] == s[i - 1] else 1
-      for t in [count - x for x in range(min(count, 3))]:
-        if mem[key := (t << 5) + ord(s[i]) - 96] == 2:
-          if ans < t: ans = t
+      cnt = cnt + 1 if s[i] == s[i - 1] else 1
+      for j in [cnt - x for x in range(min(cnt, 3))]:
+        if mem[key := (j << 5) + ord(s[i]) - 96] == 2:
+          if ans < j: ans = j
         else:
           mem[key] += 1
-      # for offset in range(min(count := count + 1 if s[i] == s[i - 1] else 1, 3)):
-      #   if mem[key := (count - offset << 5) + ord(s[i]) - 96] == 2:
-      #     if ans < count - offset: ans = count - offset
-      #   else:
-      #     mem[key] += 1
     return ans
         
