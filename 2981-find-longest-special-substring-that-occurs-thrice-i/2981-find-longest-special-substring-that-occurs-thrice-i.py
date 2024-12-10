1a05ea2c5b3718bb00446db1207a0200f89f8e2d
Alexander Kolobov <a@da.am>
2024-12-10 21:22:15 +0400

    Time: 23 ms (41.08%), Space: 17.4 MB (9.27%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
@@ -2,8 +2,9 @@ class Solution:
   def maximumLength(self, s: str) -> int:
     count, ans = 1, -1
     d = defaultdict(int)
-    for i in range(len(s)):
-      count = count + 1 if i and s[i] == s[i - 1] else 1
+    d[f"{s[0]}1"] = 1
+    for i in range(1, len(s)):
+      count = count + 1 if s[i] == s[i - 1] else 1
       for offset in range(min(count, 3)):
         key = f"{s[i]}{count - offset}"
         d[key] += 1
