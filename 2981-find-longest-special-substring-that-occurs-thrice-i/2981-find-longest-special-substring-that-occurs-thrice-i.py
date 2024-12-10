ebdead668ced41afa0eccbcca186040103f1e8b6
Alexander Kolobov <a@da.am>
2024-12-10 21:43:10 +0400

    Time: 11 ms (75.95%), Space: 17.4 MB (5.52%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
@@ -4,10 +4,16 @@ class Solution:
     mem = [0] * 1626 # (50 << 5) + ord('z') - 96
     mem[(1 << 5) + ord(s[0]) - 96] = 1
     for i in range(1, len(s)):
-      for offset in range(min(count := count + 1 if s[i] == s[i - 1] else 1, 3)):
-        if mem[key := (count - offset << 5) + ord(s[i]) - 96] == 2:
-          if ans < count - offset: ans = count - offset
+      count = count + 1 if s[i] == s[i - 1] else 1
+      for t in [count - x for x in range(min(count, 3))]:
+        if mem[key := (t << 5) + ord(s[i]) - 96] == 2:
+          if ans < t: ans = t
         else:
           mem[key] += 1
+      # for offset in range(min(count := count + 1 if s[i] == s[i - 1] else 1, 3)):
+      #   if mem[key := (count - offset << 5) + ord(s[i]) - 96] == 2:
+      #     if ans < count - offset: ans = count - offset
+      #   else:
+      #     mem[key] += 1
     return ans
         
