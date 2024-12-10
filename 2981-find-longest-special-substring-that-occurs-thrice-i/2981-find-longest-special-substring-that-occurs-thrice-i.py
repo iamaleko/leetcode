3c6e05cab5bc370e84989fd4feb597b39daad656
Alexander Kolobov <a@da.am>
2024-12-10 21:33:40 +0400

    Time: 4 ms (95.14%), Space: 17.3 MB (9.27%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
@@ -1,14 +1,13 @@
 class Solution:
   def maximumLength(self, s: str) -> int:
     count, ans = 1, -1
-    mem = [0] * ((50 << 5) + ord('z') - 96)
+    mem = [0] * 1626 # (50 << 5) + ord('z') - 96
     mem[(1 << 5) + ord(s[0]) - 96] = 1
     for i in range(1, len(s)):
-      count = count + 1 if s[i] == s[i - 1] else 1
-      for offset in range(min(count, 3)):
-        key = (count - offset << 5) + ord(s[i]) - 96
-        mem[key] += 1
-        if mem[key] > 2:
+      for offset in range(min(count := count + 1 if s[i] == s[i - 1] else 1, 3)):
+        if mem[key := (count - offset << 5) + ord(s[i]) - 96] == 2:
           ans = max(ans, count - offset)
+        else:
+          mem[key] += 1
     return ans
         
