61b59ee5288b4199e98df34b8da1d37731c4f381
Alexander Kolobov <a@da.am>
2024-12-13 18:47:19 +0400

    Time: 166 ms (97.82%), Space: 32 MB (97.75%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.py
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.py
@@ -1,11 +1,11 @@
 class Solution:
   def findScore(self, nums: List[int]) -> int:
-    marked = set()
+    n, score = len(nums) - 1, 0
     indexes = sorted(list(range(len(nums))), key = lambda x: nums[x])
-    score = 0
     for i in indexes:
-      if i not in marked:
-        marked.update((i - 1, i, i + 1))
+      if nums[i]:
+        if i: nums[i - 1] = 0
+        if i < n: nums[i + 1] = 0
         score += nums[i]
     return score
         
