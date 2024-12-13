955b78c554897285db482490463eb232245db9f0
Alexander Kolobov <a@da.am>
2024-12-14 02:46:37 +0400

    Time: 47 ms (98.04%), Space: 17.3 MB (54.67%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -1,15 +1,12 @@
-from functools import lru_cache
-
 class Solution:
-  @lru_cache(maxsize=None)
   def traverse(self, i: int, val: int) -> int:
-    # self.mem[self.pos[i][1]] = val
+    self.mem[self.pos[i][1]] = val
     res = val
     for j in range(i + 1, len(self.pos)):
       if self.pos[i][1] <= self.pos[j][0]:
         l, r = self.pos[j]
-        # if r not in self.mem or self.mem[r] > val - r + l:
-        res = min(res, self.traverse(j, val - r + l))
+        if r not in self.mem or self.mem[r] > val - r + l:
+          res = min(res, self.traverse(j, val - r + l))
     return res
 
   def minExtraChar(self, s: str, words: list[str]) -> int:
