f97cb30825b8e6d1b6667764ec1af80b68584b24
Alexander Kolobov <a@da.am>
2024-12-14 02:17:02 +0400

    Time: 44 ms (98.17%), Space: 17.8 MB (24.41%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -6,19 +6,17 @@ class Solution:
       while True:
         i = s.find(word, i)
         if i == -1: break
-        pos.append((i, i + len(word), len(word)))
+        pos.append((i, i + len(word)))
         i += 1
     pos.sort()
 
     mem = {}
     def traverse(i: int, j: int, val: int) -> int:
       mem[j] = val
-      res = val
       for i in range(i, len(pos)):
-        l, r, n = pos[i]
-        if j <= l:
-          if r in mem and mem[r] <= val - n: continue
-          res = min(res, traverse(i + 1, r, val - n))
-      return res
+        l, r = pos[i]
+        if j <= l and (r not in mem or mem[r] > mem[j] - (r - l)):
+          val = min(val, traverse(i + 1, r, mem[j] - (r - l)))
+      return val
 
     return traverse(0, 0, len(s))
