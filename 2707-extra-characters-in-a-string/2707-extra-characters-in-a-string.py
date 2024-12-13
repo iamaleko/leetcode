dd809f774b56e70a7e80a3f792fcde2702d33d3c
Alexander Kolobov <a@da.am>
2024-12-14 02:14:21 +0400

    Time: 31 ms (99.48%), Space: 17.7 MB (27.56%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -12,14 +12,13 @@ class Solution:
 
     mem = {}
     def traverse(i: int, j: int, val: int) -> int:
-      if j in mem and mem[j] <= val:
-        return math.inf
-
       mem[j] = val
+      res = val
       for i in range(i, len(pos)):
         l, r = pos[i]
         if j <= l:
-          val = min(val, traverse(i + 1, r, mem[j] - (r - l)))
-      return val
+          if r in mem and mem[r] <= val - (r - l): continue
+          res = min(res, traverse(i + 1, r, val - (r - l)))
+      return res
 
     return traverse(0, 0, len(s))
