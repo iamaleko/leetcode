d18addcc1242004240305ff2404f6830d3ae829e
Alexander Kolobov <a@da.am>
2024-12-14 02:42:11 +0400

    Time: 1295 ms (5.1%), Space: 46.7 MB (5.29%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -1,24 +1,24 @@
 class Solution:
-  def minExtraChar(self, s: str, words: List[str]) -> int:
-    pos = [(0, 0)]
+  @cache
+  def traverse(self, i: int, val: int) -> int:
+    # self.mem[self.pos[i][1]] = val
+    res = val
+    for j in range(i + 1, len(self.pos)):
+      if self.pos[i][1] <= self.pos[j][0]:
+        l, r = self.pos[j]
+        # if r not in self.mem or self.mem[r] > val - r + l:
+        res = min(res, self.traverse(j, val - r + l))
+    return res
+
+  def minExtraChar(self, s: str, words: list[str]) -> int:
+    self.pos = [(0, 0)]
     for word in words:
       i = 0
       while True:
         i = s.find(word, i)
         if i == -1: break
-        pos.append((i, i + len(word)))
+        self.pos.append((i, i + len(word)))
         i += 1
-    pos.sort()
-
-    mem = {}
-    def traverse(i: int, val: int) -> int:
-      mem[pos[i][1]] = val
-      res = val
-      for j in range(i + 1, len(pos)):
-        if pos[i][1] <= pos[j][0]:
-          l, r = pos[j]
-          if r not in mem or mem[r] > val - r + l:
-            res = min(res, traverse(j, val - r + l))
-      return res
-
-    return traverse(0, len(s))
+    self.pos.sort()
+    self.mem = {}
+    return self.traverse(0, len(s))
