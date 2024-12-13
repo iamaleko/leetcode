1ea973688ef1c12dba4a317ebd6c354020f7c334
Alexander Kolobov <a@da.am>
2024-12-13 21:31:59 +0400

    Time: 0 ms (100%), Space: 17.3 MB (12.36%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,10 +1,27 @@
-from functools import cmp_to_key
-
 class Solution:
+  def mergeSort(self, a: List[str], l: int, r: int) -> List[str]:
+    if r - l == 2:
+      if a[l] + a[r - 1] < a[r - 1] + a[l]:
+        a[r - 1], a[l] = a[l], a[r - 1]
+    elif r - l > 2:
+      i = l + r >> 1
+      self.mergeSort(a, l, i)
+      self.mergeSort(a, i, r)
+      b = a[l:i]
+      for j in range(len(b)):
+        while i < r and b[j] + a[i] < a[i] + b[j]:
+          a[l] = a[i]
+          i += 1
+          l += 1
+        a[l] = b[j]
+        j += 1
+        l += 1
+
   def largestNumber(self, nums: List[int]) -> str:
-    if not max(nums):
+    if max(nums):
+      nums = list(map(str, nums))
+      self.mergeSort(nums, 0, len(nums))
+      return ''.join(nums)
+    else:
       return '0'
-    nums = list(map(str, nums))
-    nums.sort(key=cmp_to_key(lambda a, b: 1 if a + b < b + a else -1))
-    print(nums)
-    return ''.join(nums)
+        
