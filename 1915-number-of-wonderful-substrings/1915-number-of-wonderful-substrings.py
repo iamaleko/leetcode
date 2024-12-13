a9bb130c148ed8e36b7bae76cebec918a117ac13
Alexander Kolobov <a@da.am>
2024-12-13 19:40:04 +0400

    Time: 1064 ms (92.4%), Space: 18.1 MB (12.11%) - LeetHub

--- 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.py
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.py
@@ -1,17 +1,17 @@
 class Solution:
   def wonderfulSubstrings(self, word: str) -> int:
-    count = [0] * (1 << 10)
-    ans, mask = 0, 0
+    ans, mask, count = 0, 0, [0] * (1 << 10)
+    d = dict(zip("abcdefghij", range(10)))
     count[mask] += 1
     for ch in word:
-      mask ^= 1 << (ord(ch) - 97)
+      # mask ^= 1 << (ord(ch) - 97)
+      mask ^= 1 << d[ch]
       ans += count[mask]
       for i in range(10):
         ans += count[mask ^ (1 << i)]
       count[mask] += 1
     return ans
     
-
 # TLE 58/88
 # class Solution:
 #   def wonderfulSubstrings(self, word: str) -> int:
