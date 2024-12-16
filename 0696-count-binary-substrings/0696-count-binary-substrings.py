bfcd61e52896b9bdd7a9c3b3646de6fd3cb63c06
Alexander Kolobov <a@da.am>
2024-12-16 20:38:29 +0400

    Time: 51 ms (96.29%), Space: 17.8 MB (10.04%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.py
+++ 0696-count-binary-substrings/0696-count-binary-substrings.py
@@ -1,11 +1,11 @@
 class Solution:
   def countBinarySubstrings(self, s: str) -> int:
     ans, l, c = 0, 0, 0
-    for r in range(len(s)):
+    for r in range(n := len(s)):
       if c != l and s[r] != s[c]:
         ans += c - l if c - l < r - c else r - c
         l, c = c, r
       elif c == l and s[r] != s[l]:
         c = r
-    return ans + min(c - l, len(s) - c)
+    return ans + (c - l if c - l < n - c else n - c)
         
