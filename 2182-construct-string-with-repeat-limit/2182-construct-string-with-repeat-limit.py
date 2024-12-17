f921be98e3343875288ea5dc8464d223a8aed3f6
Alexander Kolobov <a@da.am>
2024-12-17 20:30:34 +0400

    Time: 302 ms (31.89%), Space: 20 MB (6.29%) - LeetHub

--- 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
@@ -3,27 +3,15 @@ class Solution:
     count = [0] * 26
     for ch in s:
       count[ord(ch) - 97] += 1
-
-    ans = []
-    last = -1
-    repeat = 0
-
-    p = len(count) - 1
-    while p > -1 and not count[p]: p -= 1
-
-    while p > -1:
-      code = 0
-      # print(count, last, p, repeat, chr(p + 97), repeat == limit and last == p)
-      if repeat == limit and last == p:
-        j = p - 1
-        while j > -1 and not count[j]: j -= 1
-        if j == -1: break
-        code = j
-        count[j] -= 1
-      else:
-        code = p
-        count[p] -= 1
-        while p > -1 and not count[p]: p -= 1
+    ans, last, repeat, p = [], -1, 0, 25
+    while True:
+      while p >= 0 and not count[p]: p -= 1
+      code = p
+      if repeat == limit and code == last:
+        code = p - 1
+        while code >= 0 and not count[code]: code -= 1
+      if code < 0: break
+      count[code] -= 1
       if last != code:
         last = code
         repeat = 0
