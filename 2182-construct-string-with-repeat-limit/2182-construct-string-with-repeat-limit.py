6a7a86cee3512a4aa2b9e07d00a4af470385ff81
Alexander Kolobov <a@da.am>
2024-12-17 20:47:17 +0400

    Time: 296 ms (34.06%), Space: 20.2 MB (5.59%) - LeetHub

--- 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
@@ -1,23 +1,17 @@
 class Solution:
   def repeatLimitedString(self, s: str, limit: int) -> str:
     count, codes, last, repeat, code = [0] * 123, [], -1, 0, 122
-    for ch in s:
-      count[ord(ch)] += 1
+    for ch in s: count[ord(ch)] += 1
     while True:
       while code >= 0 and not count[code]: code -= 1
-      if repeat == limit and code == last:
-        alt = code - 1
-        while alt >= 0 and not count[alt]: alt -= 1
-        if alt < 0: break
-        count[alt] -= 1
-        repeat = 1
-        codes.append(alt)
-        last = alt
-      else:
-        if code < 0: break
-        count[code] -= 1
-        repeat = repeat + 1 if last == code else 1
-        codes.append(code)
-        last = code
+      add = code
+      if repeat == limit and add == last:
+        add -= 1
+        while add >= 0 and not count[add]: add -= 1
+      if add < 0: break
+      count[add] -= 1
+      repeat = repeat + 1 if last == add else 1
+      codes.append(add)
+      last = add
     return ''.join([chr(code) for code in codes])
         
