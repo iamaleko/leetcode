80c165ce80ef0d7ab7ad8bf81c2cf9afbe0fbc09
Alexander Kolobov <a@da.am>
2024-12-18 19:55:24 +0400

    Time: 4 ms (21.42%), Space: 17.8 MB (10.22%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,26 +1,32 @@
+# O(n)
 class Solution:
-  # O(n log n)
   def finalPrices(self, prices: List[int]) -> List[int]:
     mst = []
-    for i in range(len(prices) - 1, -1, -1):
-      price = prices[i]
-
-      # binary search in monotonic stack
-      l, r = 0, len(mst) - 1
-      while l <= r:
-        c = l + r >> 1
-        if mst[c] <= price:
-          l = c + 1
-        else:
-          r = c - 1
-      if r >= 0:
-        prices[i] -= mst[r]
-
-      # add to monotonic stack
-      while mst and mst[-1] >= price:
-        mst.pop()
-      mst.append(price)
-      
+    for i in range(len(prices)):
+      while mst and prices[mst[-1]] >= prices[i]:
+        prices[mst.pop()] -= prices[i]
+      mst.append(i)
     return prices
+
+
+# O(n log n)
+# class Solution:
+#   def finalPrices(self, prices: List[int]) -> List[int]:
+#     mst = []
+#     for i in range(len(prices) - 1, -1, -1):
+#       price = prices[i]
+#       l, r = 0, len(mst) - 1
+#       while l <= r:
+#         c = l + r >> 1
+#         if mst[c] <= price:
+#           l = c + 1
+#         else:
+#           r = c - 1
+#       if r >= 0:
+#         prices[i] -= mst[r]
+#       while mst and mst[-1] >= price:
+#         mst.pop()
+#       mst.append(price)
+#     return prices
 
         
