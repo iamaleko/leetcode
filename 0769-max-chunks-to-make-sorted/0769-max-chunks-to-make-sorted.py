2d5e7d8ca450fc3b8d7cf0946e3fdc6b32c8486a
Alexander Kolobov <a@da.am>
2024-12-19 07:00:47 +0400

    Time: 0 ms (100%), Space: 17.7 MB (14.45%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.py
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.py
@@ -1,11 +1,11 @@
 class Solution:
   def maxChunksToSorted(self, arr: List[int]) -> int:
     m = {num: i for i, num in enumerate(arr)}
-    ans, l, r = 0, 0, 0
+    ans, r = 0, 0
     for i, num in enumerate(arr):
-      if (num == i and l == i and r == i) or (num >= l and num <= r and i == r):
+      if i == r and num <= r:
         ans += 1
-        r = l = i + 1
+        r = i + 1
       elif num > r:
         r = num
     return ans
