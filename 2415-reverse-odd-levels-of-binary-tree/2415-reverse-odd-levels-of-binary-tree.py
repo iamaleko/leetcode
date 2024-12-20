61551eef263fc29d69baaa9af78b5e99fe2695f4
Alexander Kolobov <a@da.am>
2024-12-20 21:54:42 +0400

    Time: 6 ms (99.48%), Space: 22.9 MB (5.08%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
@@ -1,14 +1,14 @@
 class Solution:
   def swap(self, left: TreeNode, right: TreeNode) -> None:
     left.val, right.val = right.val, left.val
-    if left.left and left.left.left:
-      self.swap(left.left.left, right.right.right)
-      self.swap(left.left.right, right.right.left)
-      self.swap(left.right.left, right.left.right)
-      self.swap(left.right.right, right.left.left)
+    if left.left == None or left.left.left == None: return
+    self.swap(left.left.left, right.right.right)
+    self.swap(left.left.right, right.right.left)
+    self.swap(left.right.left, right.left.right)
+    self.swap(left.right.right, right.left.left)
 
   def reverseOddLevels(self, root: TreeNode) -> TreeNode:
-    if root.left:
-      self.swap(root.left, root.right)
+    if root.left == None: return root
+    self.swap(root.left, root.right)
     return root
         
