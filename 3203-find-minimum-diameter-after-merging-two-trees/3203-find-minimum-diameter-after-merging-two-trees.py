3f8e07b8645730e0beb3c56ca2a069b6285d93cb
Alexander Kolobov <a@da.am>
2024-12-24 17:33:37 +0400

    Time: 683 ms (31.72%), Space: 102.6 MB (42.07%) - LeetHub

--- 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.py
+++ 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.py
@@ -6,28 +6,32 @@ class Solution:
       graph[b].add(a)
     return graph
 
-  def pealGraph(self, graph: Dict[int, int]) -> int:
-    ans = -1
-    leafs = set()
+  def pealGraph(self, graph: Dict[int, int]) -> (int, int):
+    leafs, path = set(), {}
     for node in graph:
       if len(graph[node]) <= 1:
         leafs.add(node)
+        path[node] = (0, 0)
     while leafs:
-      ans += 1
       stems = set()
       for leaf in leafs:
         for parent in graph[leaf]:
           graph[parent].remove(leaf)
           if len(graph[parent]) <= 1:
+            if parent in path:
+              longest = max(path[leaf][0] + 1, path[parent][0])
+              diam = max(path[parent][1], path[parent][0] + longest)
+              path[parent] = (longest, diam)
+            else:
+              path[parent] = (path[leaf][0] + 1, path[leaf][0] + 1)
+            if not len(graph[parent]):
+              return path[parent]
             stems.add(parent)
-        del graph[leaf]
       leafs = stems
-    return max(ans, 0)
+    return (0, 0)
 
   def minimumDiameterAfterMerge(self, edges1: List[List[int]], edges2: List[List[int]]) -> int:
-    graph1 = self.buildGraph(edges1)
-    graph2 = self.buildGraph(edges2)
-    max1 = self.pealGraph(graph1)
-    max2 = self.pealGraph(graph2)
-    return max1 + max2 + 1
+    longest1, diam1 = self.pealGraph(self.buildGraph(edges1))
+    longest2, diam2 = self.pealGraph(self.buildGraph(edges2))
+    return max(diam1, diam2, longest1 + longest2 + 1)
         
