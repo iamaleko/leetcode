8bdf6cf1ecccaa72a24def20a6b742934f8bd69e
Alexander Kolobov <a@da.am>
2024-12-28 14:25:29 +0400

    Time: 42 ms (45.56%), Space: 26.4 MB (22.5%) - LeetHub

--- 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.py
+++ 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.py
@@ -4,21 +4,18 @@ class Solution:
     for i in range(1, len(nums)):
       nums[i] += nums[i - 1]
     # dp
-    dp = [[(0,0),(0,0,0),(0,0,0,0)] for _ in nums]
-    for r in range(k - 1, len(nums)):
+    dp = [[(0,0),(0,0,0),(0,0,0,0)]] * len(nums)
+    dp[k - 1][0] = (nums[k - 1], 0)
+    for r in range(k, len(nums)):
+      curr = dp[r] = dp[r - 1][:]
       l = r - k + 1
       a = nums[r] - nums[l - 1] if l else nums[r]
-      if not r:
-        dp[r][0] = (a, l)
-      else:
-        last = dp[r - 1]
-        dp[r] = last[:]
-        if last[0][0] < a: dp[r][0] = (a, l)
-        if l > k - 1:
-          prev = dp[l - 1]
-          b = prev[0][0] + a
-          if last[1][0] < b: dp[r][1] = (b, prev[0][1], l)
-          c = prev[1][0] + a
-          if last[2][0] < c: dp[r][2] = (c, prev[1][1], prev[1][2], l)
+      if curr[0][0] < a: curr[0] = (a, l)
+      if l > k - 1:
+        prev = dp[l - 1]
+        b = prev[0][0] + a
+        if curr[1][0] < b: curr[1] = (b, prev[0][1], l)
+        c = prev[1][0] + a
+        if curr[2][0] < c: curr[2] = (c, prev[1][1], prev[1][2], l)
       
     return dp[-1][2][1:]
