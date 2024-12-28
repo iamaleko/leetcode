974b8cfdcfd226b726803f55df922c8869474c6c
Alexander Kolobov <a@da.am>
2024-12-29 03:06:02 +0400

    Time: 31 ms (55.07%), Space: 24.8 MB (26.03%) - LeetHub

--- 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.py
+++ 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.py
@@ -1,18 +1,14 @@
 class Solution:
   def maxSumOfThreeSubarrays(self, nums: List[int], k: int) -> List[int]:
-    # running sum
-    for i in range(1, len(nums)):
+    for i in range(1, n := len(nums)):
       nums[i] += nums[i - 1]
-    # dp
-    dpa = [(0,0)] * len(nums)
-    dpb = [(0,0,0)] * len(nums)
-    dpc = [(0,0,0,0)] * len(nums)
+    dpa, dpb, dpc = [(0,0)] * n, [(0,0,0)] * n, [(0,0,0,0)] * n
     dpa[k - 1] = (nums[k - 1], 0)
-    for r in range(k, len(nums)):
-      l = r - k + 1
-      a = nums[r] - nums[l - 1] if l else nums[r]
-      dpa[r] = (a, l) if dpa[r - 1][0] < a else dpa[r - 1]
+    for r in range(k, n):
+      rr, ll, l = r - 1, r - k, r - k + 1
+      a = nums[r] - nums[ll] if l else nums[r]
+      dpa[r] = (a, l) if dpa[rr][0] < a else dpa[rr]
       if l >= k:
-        dpb[r] = (dpa[l - 1][0] + a, dpa[l - 1][1], l) if dpb[r - 1][0] < dpa[l - 1][0] + a else dpb[r - 1]
-        dpc[r] = (dpb[l - 1][0] + a, dpb[l - 1][1], dpb[l - 1][2], l) if dpc[r - 1][0] < dpb[l - 1][0] + a else dpc[r - 1]
+        dpb[r] = (dpa[ll][0] + a, dpa[ll][1], l) if dpb[rr][0] < dpa[ll][0] + a else dpb[rr]
+        dpc[r] = (dpb[ll][0] + a, dpb[ll][1], dpb[ll][2], l) if dpc[rr][0] < dpb[ll][0] + a else dpc[rr]
     return dpc[-1][1:]
