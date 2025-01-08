82c925524c1da9ec6111d3ac8f0a342eed7ce6c9
Alexander Kolobov <a@da.am>
2025-01-08 09:57:39 +0400

    Time: 9 ms (66.41%), Space: 18 MB (6.47%) - LeetHub

--- 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.py
+++ 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.py
@@ -4,7 +4,8 @@ class Solution:
     for i in range(len(words)):
       l = len(words[i])
       for j in range(i + 1, len(words)):
-        if words[j][:l] == words[i] and words[j][-l:] == words[i]:
+        # if words[j][:l] == words[i] and words[j][-l:] == words[i]:
+        if words[j].startswith(words[i]) and words[j].endswith(words[i]):
           ans += 1
     return ans
         
