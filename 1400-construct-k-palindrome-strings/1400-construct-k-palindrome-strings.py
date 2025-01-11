4db997c6a33c6a40649bea18a56a458db8177de7
Alexander Kolobov <a@da.am>
2025-01-11 21:29:16 +0400

    Time: 32 ms (53.3%), Space: 18.1 MB (16.3%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.py
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.py
@@ -1,19 +1,19 @@
-class Solution:
-  def canConstruct(self, s: str, k: int) -> bool:
-    if len(s) < k: return False
-    m = 0
-    for b in bytes(s, 'utf-8'): m ^= 1 << b
-    while m := m & m - 1: k -= 1
-    return k > 0
-
 # class Solution:
 #   def canConstruct(self, s: str, k: int) -> bool:
-#     if len(s) >= k:
-#       odd = 0
-#       for v in Counter(s).values():
-#         if v & 1: odd += 1
-#         if odd > k: break
-#       else:
-#         return True
-#     return False
+#     if len(s) < k: return False
+#     m = 0
+#     for b in bytes(s, 'utf-8'): m ^= 1 << b
+#     while m := m & m - 1: k -= 1
+#     return k > 0
+
+class Solution:
+  def canConstruct(self, s: str, k: int) -> bool:
+    if len(s) >= k:
+      odd = 0
+      for v in Counter(s).values():
+        if v & 1: odd += 1
+        if odd > k: break
+      else:
+        return True
+    return False
         
