5153d66a854e8b56e5f6ec59f78cd09916e73601
Alexander Kolobov <a@da.am>
2025-01-11 22:09:15 +0400

    Time: 1010 ms (6.75%), Space: 679.9 MB (5.29%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
@@ -5,4 +5,3 @@ class Solution:
       if i - zero >= -1: dp[i] += 1 if i - zero == -1 else dp[i - zero]
       if i - one >= -1: dp[i] += 1 if i - one == -1 else dp[i - one]
     return int(sum(dp[low - 1:high]) % (10 ** 9 + 7))
-
