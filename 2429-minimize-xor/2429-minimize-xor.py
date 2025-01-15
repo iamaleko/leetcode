1a255b017040f6532161657b81bb774724ca83d4
Alexander Kolobov <a@da.am>
2025-01-15 15:03:50 +0400

    Time: 0 ms (100%), Space: 18 MB (14.4%) - LeetHub

--- 2429-minimize-xor/2429-minimize-xor.py
+++ 2429-minimize-xor/2429-minimize-xor.py
@@ -1,12 +1,13 @@
 class Solution:
   def minimizeXor(self, num1: int, num2: int) -> int:
-    ans, bits, p = 0, 0, 30
-    while num2: bits, num2 = bits + 1, num2 & num2 - 1
-    p = 30
-    while p and bits:
+    n, b, p = 0, 0, 30
+    while num2:
+      b += 1
+      num2 &= num2 - 1
+    while p and b:
       p -= 1
-      if num1 & (1 << p) or bits == p + 1:
-        bits -= 1
-        ans |= (1 << p)
-    return ans
+      if num1 & (1 << p) or b == p + 1:
+        b -= 1
+        n |= (1 << p)
+    return n
         
