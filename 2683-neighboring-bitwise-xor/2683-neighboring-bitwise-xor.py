fa783de9372a674418a8ab2a6de94daa19ead6da
Alexander Kolobov <a@da.am>
2025-01-17 19:49:55 +0400

    Time: 43 ms (67.15%), Space: 22.3 MB (48.9%) - LeetHub

--- 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.py
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.py
@@ -1,7 +1,12 @@
+# class Solution:
+#   def doesValidArrayExist(self, derived: List[int]) -> bool:
+#     for start, current in ((0, 0), (1, 1)):
+#       for num in derived: current ^= num
+#       if current == start: return True
+#     return False
+
 class Solution:
   def doesValidArrayExist(self, derived: List[int]) -> bool:
-    for i in range(1):
-      s = i
-      for num in derived: i ^= num
-      if i == s: return True
-    return False
+    xor = 0
+    for num in derived: xor ^= num
+    return xor ^ 0 == 0 or xor ^ 1 == 1
