1360e27584bead74cb78b81a0019c0ed9130b8bd
Alexander Kolobov <a@da.am>
2025-01-21 22:48:43 +0400

    Time: 0 ms (100%), Space: 11.5 MB (42.86%) - LeetHub

--- 2017-grid-game/2017-grid-game.go
+++ 2017-grid-game/2017-grid-game.go
@@ -1,18 +1,14 @@
-import (
-  "math"
-)
-
 func gridGame(grid [][]int) int64 {
-  var left, right, ans float64 = 0, 0, math.Inf(1)
+  left, right, ans := 0, 0, math.MaxInt
   for _, num := range grid[0] {
-    right += float64(num)
+    right += num
   }
-  for i := range len(grid[0]) {
-    right -= float64(grid[0][i])
-    if val := math.Max(left, right); val < ans {
+  for i := range grid[0] {
+    right -= grid[0][i]
+    if val := max(left, right); val < ans {
       ans = val
     }
-    left += float64(grid[1][i])
+    left += grid[1][i]
   }
   return int64(ans)
 }
