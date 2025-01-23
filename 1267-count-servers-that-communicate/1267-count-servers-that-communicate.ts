86c39f61bbaa40bfd60da98964192c7014d05307
Alexander Kolobov <a@da.am>
2025-01-23 16:56:42 +0400

    Time: 17 ms (12.5%), Space: 58.3 MB (6.25%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.ts
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.ts
@@ -27,21 +27,3 @@ function countServers(grid: number[][]): number {
   }
   return ans.size
 };
-
-// class Solution:
-//   def countServers(self, grid: List[List[int]]) -> int:
-//     rows = [set() for _ in range(len(grid))]
-//     cols = [set() for _ in range(len(grid[0]))]
-//     for y in range(len(grid)):
-//       for x in range(len(grid[0])):
-        // if grid[y][x] == 1:
-        //   rows[y].add((y, x))
-        //   cols[x].add((y, x))
-    // s = set()
-    // for row in rows:
-    //   if len(row) > 1:
-    //     s.update(row)
-    // for col in cols:
-    //   if len(col) > 1:
-    //     s.update(col)
-    // return len(s)
