4c0072a92c57818295b6b595264d2266d79e2565
Alexander Kolobov <a@da.am>
2025-01-24 21:35:39 +0400

    Time: 23 ms (78.78%), Space: 54 MB (57.42%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.ts
@@ -0,0 +1,20 @@
+function bubbleFromCenter(s: string, left: number, right: number): string {
+  while (left >= 0 && right < s.length && s[left] === s[right]) {
+    left--;
+    right++;
+  }
+  return s.slice(left + 1, right)
+}
+
+function longestPalindrome(s: string): string {
+  let ans = '';
+    
+  for (let i = 0; i < s.length; i++) {
+    const a = bubbleFromCenter(s, i, i),
+          b = bubbleFromCenter(s, i, i + 1),
+          t = a.length > b.length ? a : b;
+    if(t.length > ans.length) ans = t
+  }
+
+  return ans
+};
