721cabea5a20da42460ee5b0fec654d95012e1e6
Alexander Kolobov <a@da.am>
2025-01-24 21:45:00 +0400

    Time: 28 ms (63.58%), Space: 54 MB (57.42%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.ts
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.ts
@@ -1,16 +1,16 @@
-function bubbleFromCenter(s: string, left: number, right: number): string {
-  while (left >= 0 && right < s.length && s[left] === s[right]) {
-    left--;
-    right++;
+function bubbleFromCenter(s: string, l: number, r: number): string {
+  while (l > -1 && r < s.length && s[l] === s[r]) {
+    l--;
+    r++;
   }
-  return s.slice(left + 1, right)
+  return s.slice(l + 1, r)
 }
 
 function longestPalindrome(s: string): string {
   let ans = ''; 
   for (let i = 0; i < s.length; i++) {
     const a = bubbleFromCenter(s, i, i),
-          b = s[i] === s[i + 1] ? bubbleFromCenter(s, i, i + 1) : '',
+          b = bubbleFromCenter(s, i, i + 1),
           t = a.length > b.length ? a : b;
     if(t.length > ans.length) ans = t
   }
