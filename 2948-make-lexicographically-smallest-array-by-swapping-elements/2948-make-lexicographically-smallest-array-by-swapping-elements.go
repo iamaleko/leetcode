eb7495abd974a44669ea79f9a96a3dc4ba0c7006
Alexander Kolobov <a@da.am>
2025-01-26 01:06:56 +0400

    Time: 212 ms (20%), Space: 27.4 MB (0%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.go
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.go
@@ -5,12 +5,12 @@ import (
 func lexicographicallySmallestArray(nums []int, limit int) []int {
   // split into backward sorted groups and build group index
   sorted := slices.Clone(nums)
-  slices.SortFunc(sorted, func(a int, b int) int { return b - a })
+  slices.SortFunc(sorted, func(a int, b int) int { return a - b })
   groups := [][]int{ []int{ sorted[0] } }
   index := map[int]int{ sorted[0]: 0 }
   for i := 1; i < len(sorted); i++ {
     j := len(groups) - 1
-    if groups[j][len(groups[j]) - 1] - sorted[i] <= limit {
+    if sorted[i] - groups[j][len(groups[j]) - 1] <= limit {
       groups[j] = append(groups[j], sorted[i])
     } else {
       groups = append(groups, []int{ sorted[i] })
@@ -21,8 +21,8 @@ func lexicographicallySmallestArray(nums []int, limit int) []int {
   // replace elements in input array
   for i := 0; i < len(nums); i++ {
     j := index[nums[i]]
-    nums[i] = groups[j][len(groups[j]) - 1]
-    groups[j] = groups[j][:len(groups[j]) - 1]
+    nums[i] = groups[j][0]
+    groups[j] = groups[j][1:]
   }
 
   return nums
