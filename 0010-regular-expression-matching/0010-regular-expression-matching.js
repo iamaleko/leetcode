89d3f460f11ea3a44acb5757b8dfbe9fbc023bc3
Alexander Kolobov <a@da.am>
2025-01-26 21:56:35 +0400

    Time: 15 ms (49.02%), Space: 56 MB (5.88%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.js
+++ 0010-regular-expression-matching/0010-regular-expression-matching.js
@@ -1,35 +1,37 @@
 const isMatch = (s, p) => {
+    // tokenize pattern, remove duplicates
     const tokens = [];
-    for (let char of p) {
-        if (char === '.') {
-            char = null;
-        }
+    for (let i = 0, j = -1; i < p.length; ++i) {
+        const char = p[i] === '.' ? null : p[i];
         if (char === '*') {
-            if (
-                tokens[tokens.length - 1].char === tokens[tokens.length - 2]?.char &&
-                tokens[tokens.length - 2]?.count === null
-            ) { // skip duplicate
+            if ( j > 0 && tokens[j].char === tokens[j - 1]?.char && tokens[j - 1].count === null) { // skip duplicate
                 tokens.pop();
+                --j;
             } else {
-                tokens[tokens.length - 1].count = null;
+                tokens[j].count = null;
             }
-        } else {
-            tokens.push({char: char, count: 1});
-        }
+        } else if (j > -1 && tokens[j].char === char && tokens[j].count !== null && p[i + 1] !== '*') { // skip duplicate
+			++tokens[j].count;
+		} else {
+			tokens.push({char: char, count: 1});
+			++j;
+		}
     }
     
+    // match string to tokens
     const match = (str, tokens, pointer, count = null) => {
-        const token = tokens[pointer];
-        const tokenCount = count || token?.count;
+        const token = tokens[pointer],
+              tokenCount = count || token?.count;
         
         if (!token) {
-            return !str.length; // tokens and pattern must ends in the same time
+            return !str.length; // tokens and pattern must ends at the same position
         }
         
         if (tokenCount === null) { // any match
             if (match(str, tokens, pointer + 1)) { // try skip token
                 return true;
             }
+            
             let i = str.length + 1;
             while (--i) {
                 if (match(str, tokens, pointer, i)) { // try same token but greedy descending count
@@ -45,8 +47,9 @@ const isMatch = (s, p) => {
             if (token.char === null) { // continue testing
                 return match(str.substr(tokenCount), tokens, pointer + 1);
             } else {
-                let substr = str.substr(0, tokenCount);
-                let pattern = token.char.padStart(tokenCount, token.char);
+                let substr = str.substr(0, tokenCount),
+                    pattern = token.char.padStart(tokenCount, token.char);
+                
                 if (substr !== pattern) { // not matched
                     return false;
                 } else { // continue testing
