457fe7fc92891b654f0beb2943e083d02d6fa44d
Alexander Kolobov <a@da.am>
2025-01-29 02:03:56 +0400

    Time: 68 ms (21.77%), Space: 9.6 MB (30.38%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.go
+++ 1462-course-schedule-iv/1462-course-schedule-iv.go
@@ -2,14 +2,15 @@ import (
   "maps"
 )
 
-func flatten(node int, graph *map[int]map[int]bool, flattened *map[int]bool) map[int]bool {
+func flatten(node int, graph *map[int]map[int]bool, flattened *map[int]bool) *map[int]bool {
+  ancestors := (*graph)[node]
   if !(*flattened)[node] {
-    for ancestor := range maps.Keys((*graph)[node]) {
-      maps.Insert((*graph)[node], maps.All(flatten(ancestor, graph, flattened)))
+    for ancestor := range maps.Keys(ancestors) {
+      maps.Insert(ancestors, maps.All(*flatten(ancestor, graph, flattened)))
     }
     (*flattened)[node] = true   
   }
-  return (*graph)[node]
+  return &ancestors
 }
 
 func checkIfPrerequisite(numCourses int, prerequisites [][]int, queries [][]int) []bool {
@@ -28,7 +29,7 @@ func checkIfPrerequisite(numCourses int, prerequisites [][]int, queries [][]int)
   ans := make([]bool, len(queries))
   flattened := map[int]bool{}
   for i, pair := range queries {
-    ans[i] = flatten(pair[1], &graph, &flattened)[pair[0]]
+    ans[i] = (*flatten(pair[1], &graph, &flattened))[pair[0]]
   }
 
   return ans
