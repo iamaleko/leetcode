9e31725caf2efa53c6c53a8b2982a3976cc2160b
Alexander Kolobov <a@da.am>
2025-01-28 21:20:31 +0400

    Time: 18 ms (0%), Space: 28 MB (0%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.go
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.go
@@ -1,26 +1,21 @@
-func collectFish(y int, x int, my int, mx int, grid *[][]int) int {
-  ans := 0
-  yst, xst := []int{y}, []int{x}
-  for len(xst) > 0 {
-    y, x := yst[0], xst[0]
-    yst, xst = yst[1:], xst[1:]
-    ans += (*grid)[y][x]
-    (*grid)[y][x] = 0
-    if y > 0 && (*grid)[y - 1][x] > 0 { yst, xst = append(yst, y - 1), append(xst, x) }
-    if y < my && (*grid)[y + 1][x] > 0 { yst, xst = append(yst, y + 1), append(xst, x) }
-    if x > 0 && (*grid)[y][x - 1] > 0 { yst, xst = append(yst, y), append(xst, x - 1) }
-    if x < mx && (*grid)[y][x + 1] > 0 { yst, xst = append(yst, y), append(xst, x + 1) }
-  }
-  return ans
-}
-
 func findMaxFish(grid [][]int) int {
   ans := 0
   my, mx := len(grid) - 1, len((grid)[0]) - 1
   for y := range grid {
     for x := range grid[y] {
       if grid[y][x] != 0 {
-        ans = max(ans, collectFish(y, x, my, mx, &grid))
+        sum, yst, xst := 0, []int{y}, []int{x}
+        for len(xst) > 0 {
+          y, x := yst[0], xst[0]
+          yst, xst = yst[1:], xst[1:]
+          sum += grid[y][x]
+          grid[y][x] = 0
+          if y > 0 && grid[y - 1][x] > 0 { yst, xst = append(yst, y - 1), append(xst, x) }
+          if y < my && grid[y + 1][x] > 0 { yst, xst = append(yst, y + 1), append(xst, x) }
+          if x > 0 && grid[y][x - 1] > 0 { yst, xst = append(yst, y), append(xst, x - 1) }
+          if x < mx && grid[y][x + 1] > 0 { yst, xst = append(yst, y), append(xst, x + 1) }
+        }
+        ans = max(ans, sum)
       }
     }
   }
