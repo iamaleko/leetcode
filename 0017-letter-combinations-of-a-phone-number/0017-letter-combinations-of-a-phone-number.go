cd035b25aa05b84909eef644381e190a0ef3da15
Alexander Kolobov <a@da.am>
2025-01-29 20:17:43 +0400

    Time: 0 ms (100%), Space: 4.1 MB (17.18%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.go
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.go
@@ -1,20 +1,19 @@
-var dict = []string{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"}
-
-func build(ans *[]string, rns *[]rune, prefix string, i int) {
-  for _, rn := range dict[(*rns)[i] - '0'] {
-    if i - 1 >= 0 {
-      build(ans, rns, string(rn) + prefix, i - 1)
-    } else {
-      *ans = append(*ans, string(rn) + prefix)
-    }
-  }
-}
-
 func letterCombinations(digits string) []string {
   ans := []string{}
   if len(digits) > 0 {
-    rns := []rune(digits)
-    build(&ans, &rns, "", len(digits) - 1)
+    dict := []string{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"}
+    runes := []rune(digits)
+    var build func(prefix string, i int)
+    build = func(prefix string, i int) {
+      for _, rn := range dict[runes[i] - '0'] {
+        if i > 0 {
+          build(string(rn) + prefix, i - 1)
+        } else {
+          ans = append(ans, string(rn) + prefix)
+        }
+      }
+    }
+    build("", len(digits) - 1)
   }
   return ans
 }
