0c3f37e35d50f820712175469874bf1fb9febcfb
Alexander Kolobov <a@da.am>
2025-01-29 19:45:43 +0400

    Time: 23 ms (59.86%), Space: 9.5 MB (79.44%) - LeetHub

--- 0015-3sum/0015-3sum.go
+++ 0015-3sum/0015-3sum.go
@@ -1,24 +1,27 @@
 import "slices"
 
 func threeSum(nums []int) [][]int {
-  n := len(nums)-1
-  ans := [][]int{}
-  slices.Sort(nums)
-  for i := range n-1 {
-    if i > 0 && nums[i] == nums[i-1] { continue }
-    target, j, k := -nums[i], i+1, n
-    for j < k {
-      switch {
-        case nums[j] + nums[k] == target:
+	n := len(nums) - 1
+	ans := [][]int{}
+	slices.Sort(nums)
+	for i := range n - 1 {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		j, k := i+1, n
+		for j < k {
+			switch sum := nums[j] + nums[k] + nums[i]; {
+        case sum == 0:
           ans = append(ans, []int{nums[i], nums[j], nums[k]})
-          j++
-          for j<k && nums[j] == nums[j-1] { j++ }
-        case nums[j] + nums[k] < target:
+          for j++; j < k && nums[j] == nums[j-1]; {
+            j++
+          }
+        case sum < 0:
           j++
         default:
           k--
-      }
-    }
-  }
-  return ans
+			}
+		}
+	}
+	return ans
 }
