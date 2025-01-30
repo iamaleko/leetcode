31dc78e9f6ea0f30a8c5f784a22519bc00f6558c
Alexander Kolobov <a@da.am>
2025-01-30 21:21:20 +0400

    Time: 0 ms (100%), Space: 4.6 MB (74.14%) - LeetHub

--- 0018-4sum/0018-4sum.go
+++ 0018-4sum/0018-4sum.go
@@ -1,29 +1,29 @@
 import (
-  "slices"
+	"slices"
 )
 
 func fourSum(nums []int, target int) [][]int {
-  slices.Sort(nums)
-  ans := [][]int{}
-  n := len(nums)
-  if n > 3 {
-    for a := 0; a < n - 3; a++ {
-      if nums[a] + nums[a+1] + nums[a+2] + nums[a+3] > target { break }
-      if nums[a] + nums[n-1] + nums[n-2] + nums[n-3] < target || a > 0 && nums[a] == nums[a-1] { continue }
-      for b := a + 1; b < n - 2; b++ {
-        if nums[a] + nums[b] + nums[b+1] + nums[b+2] > target { break }
-        if nums[a] + nums[b] + nums[n-1] + nums[n-2] < target || b > a + 1 && nums[b] == nums[b-1] { continue }
-        for c := b + 1; c < n - 1; c++ {
-          if nums[a] + nums[b] + nums[c] + nums[c+1] > target { break }
-          if nums[a] + nums[b] + nums[c] + nums[n-1] < target || c > b + 1 && nums[c] == nums[c-1] { continue }
-          for d := c + 1; d < n; d++ {
-            if nums[a] + nums[b] + nums[c] + nums[d] > target { break }
-            if nums[a] + nums[b] + nums[c] + nums[d] < target || d > c+1 && nums[d] == nums[d-1] { continue }
-            ans = append(ans, []int{nums[a], nums[b], nums[c], nums[d]})
-          }
-        }
-      }
-    }
-  }
-  return ans
+	slices.Sort(nums)
+	ans := [][]int{}
+	n := len(nums)
+	if n > 3 {
+		for a := 0; a < n-3; a++ {
+			if nums[a]+nums[a+1]+nums[a+2]+nums[a+3] > target { break }
+			if nums[a]+nums[n-1]+nums[n-2]+nums[n-3] < target || a > 0 && nums[a] == nums[a-1] { continue }
+			for b := a + 1; b < n-2; b++ {
+				if nums[a]+nums[b]+nums[b+1]+nums[b+2] > target { break }
+				if nums[a]+nums[b]+nums[n-1]+nums[n-2] < target || b > a+1 && nums[b] == nums[b-1] { continue }
+				for c := b + 1; c < n-1; c++ {
+					if nums[a]+nums[b]+nums[c]+nums[c+1] > target { break }
+					if nums[a]+nums[b]+nums[c]+nums[n-1] < target || c > b+1 && nums[c] == nums[c-1] { continue }
+					for d := c + 1; d < n; d++ {
+						if nums[a]+nums[b]+nums[c]+nums[d] > target { break }
+						if nums[a]+nums[b]+nums[c]+nums[d] < target || d > c+1 && nums[d] == nums[d-1] { continue }
+						ans = append(ans, []int{nums[a], nums[b], nums[c], nums[d]})
+					}
+				}
+			}
+		}
+	}
+	return ans
 }
