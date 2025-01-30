ecbdbd055e5b72f819b115889e43aab58b383efe
Alexander Kolobov <a@da.am>
2025-01-31 01:34:49 +0400

    Time: 5 ms (49.75%), Space: 6.8 MB (63.23%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.go
@@ -0,0 +1,53 @@
+import (
+  "slices"
+	"container/heap"
+)
+
+type ListNodeHeap []*ListNode
+
+func (h ListNodeHeap) Less(i, j int) bool {
+	return h[i].Val < h[j].Val
+}
+
+func (h ListNodeHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h ListNodeHeap) Len() int {
+	return len(h)
+}
+
+func (h *ListNodeHeap) Pop() (v any) {
+	*h, v = (*h)[:h.Len()-1], (*h)[h.Len()-1]
+	return
+}
+
+func (h *ListNodeHeap) Push(v any) {
+	*h = append(*h, v.(*ListNode))
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeKLists(lists []*ListNode) *ListNode {
+  lists = slices.DeleteFunc(lists, func(el *ListNode) bool { return el == nil })
+  var h = new(ListNodeHeap)
+  *h = ListNodeHeap(lists)
+	heap.Init(h)
+	head := &ListNode{}
+  tail := head
+  for h.Len() > 0 {
+    node := heap.Pop(h).(*ListNode)
+    tail.Next = node
+    tail = tail.Next
+    if node.Next != nil {
+      heap.Push(h, node.Next)
+      node.Next = nil
+    }
+	}
+	return head.Next
+}
