a21c9090aa8deb6f612cf665db31cc3d67b33ce4
Alexander Kolobov <a@da.am>
2025-01-30 21:04:39 +0400

    Time: 217 ms (10%), Space: 4.9 MB (34.83%) - LeetHub

--- 0018-4sum/0018-4sum.go
+++ 0018-4sum/0018-4sum.go
@@ -7,15 +7,17 @@ func fourSum(nums []int, target int) [][]int {
   ans := [][]int{}
   n := len(nums)
   for a := 0; a < n - 3; a++ {
-    if a > 0 && nums[a] == nums[a-1] { continue }
-    for b := a + 1; b < n - 2; b++ {
-      if b > a+1 && nums[b] == nums[b-1] { continue }
-      for c := b + 1; c < n - 1; c++ {
-        if c > b+1 && nums[c] == nums[c-1] { continue }
-        for d := c + 1; d < n; d++ {
-          if d > c+1 && nums[d] == nums[d-1] { continue }
-          if nums[a] + nums[b] + nums[c] + nums[d] == target {
-            ans = append(ans, []int{nums[a], nums[b], nums[c], nums[d]})
+    if a == 0 || nums[a] != nums[a-1] {
+      for b := a + 1; b < n - 2; b++ {
+        if b == a + 1 || nums[b] != nums[b-1] {
+          for c := b + 1; c < n - 1; c++ {
+            if c == b + 1 || nums[c] != nums[c-1] {
+              for d := c + 1; d < n; d++ {
+                if (d == c+1 || nums[d] != nums[d-1]) && nums[a] + nums[b] + nums[c] + nums[d] == target {
+                  ans = append(ans, []int{nums[a], nums[b], nums[c], nums[d]})
+                }
+              }
+            }
           }
         }
       }
