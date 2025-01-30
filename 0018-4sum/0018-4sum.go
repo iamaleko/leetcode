bcf9b6a774b76a9e95c1aa8176ac2a40d4cd5181
Alexander Kolobov <a@da.am>
2025-01-30 21:05:20 +0400

    Time: 113 ms (13.97%), Space: 4.7 MB (74.14%) - LeetHub

--- 0018-4sum/0018-4sum.go
+++ 0018-4sum/0018-4sum.go
@@ -6,15 +6,24 @@ func fourSum(nums []int, target int) [][]int {
   slices.Sort(nums)
   ans := [][]int{}
   n := len(nums)
-  for a := 0; a < n - 3; a++ {
-    if a == 0 || nums[a] != nums[a-1] {
-      for b := a + 1; b < n - 2; b++ {
-        if b == a + 1 || nums[b] != nums[b-1] {
-          for c := b + 1; c < n - 1; c++ {
-            if c == b + 1 || nums[c] != nums[c-1] {
-              for d := c + 1; d < n; d++ {
-                if (d == c+1 || nums[d] != nums[d-1]) && nums[a] + nums[b] + nums[c] + nums[d] == target {
-                  ans = append(ans, []int{nums[a], nums[b], nums[c], nums[d]})
+  if n > 3 {
+    for i := range n {
+      if nums[0] + nums[1] + nums[2] + nums[i] > target {
+        nums = nums[:i]
+        n = i
+        break
+      }
+    }
+    for a := 0; a < n - 3; a++ {
+      if a == 0 || nums[a] != nums[a-1] {
+        for b := a + 1; b < n - 2; b++ {
+          if b == a + 1 || nums[b] != nums[b-1] {
+            for c := b + 1; c < n - 1; c++ {
+              if c == b + 1 || nums[c] != nums[c-1] {
+                for d := c + 1; d < n; d++ {
+                  if (d == c+1 || nums[d] != nums[d-1]) && nums[a] + nums[b] + nums[c] + nums[d] == target {
+                    ans = append(ans, []int{nums[a], nums[b], nums[c], nums[d]})
+                  }
                 }
               }
             }
