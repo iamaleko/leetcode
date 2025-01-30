150712a833c14810a1c8b2457e1aacc7583dc357
Alexander Kolobov <a@da.am>
2025-01-30 21:12:43 +0400

    Time: 89 ms (17.24%), Space: 4.9 MB (40.69%) - LeetHub

--- 0018-4sum/0018-4sum.go
+++ 0018-4sum/0018-4sum.go
@@ -15,21 +15,19 @@ func fourSum(nums []int, target int) [][]int {
       }
     }
     for a := 0; a < n - 3; a++ {
+      if a > 0 && nums[a] == nums[a-1] { continue }
       if nums[a] + nums[a+1] + nums[a+2] + nums[a+3] > target { break }
-      if a == 0 || nums[a] != nums[a-1] {
-        for b := a + 1; b < n - 2; b++ {
-          if nums[a] + nums[b] + nums[b+1] + nums[b+2] > target { break }
-          if b == a + 1 || nums[b] != nums[b-1] {
-            for c := b + 1; c < n - 1; c++ {
-              if nums[a] + nums[b] + nums[c] + nums[c+1] > target { break }
-              if c == b + 1 || nums[c] != nums[c-1] {
-                for d := c + 1; d < n; d++ {
-                  if nums[a] + nums[b] + nums[c] + nums[d] > target { break }
-                  if (d == c+1 || nums[d] != nums[d-1]) && nums[a] + nums[b] + nums[c] + nums[d] == target {
-                    ans = append(ans, []int{nums[a], nums[b], nums[c], nums[d]})
-                  }
-                }
-              }
+      for b := a + 1; b < n - 2; b++ {
+        if b > a + 1 && nums[b] == nums[b-1] { continue }
+        if nums[a] + nums[b] + nums[b+1] + nums[b+2] > target { break }
+        for c := b + 1; c < n - 1; c++ {
+          if c > b + 1 && nums[c] == nums[c-1] { continue }
+          if nums[a] + nums[b] + nums[c] + nums[c+1] > target { break }
+          for d := c + 1; d < n; d++ {
+            if d > c+1 && nums[d] == nums[d-1] { continue }
+            if nums[a] + nums[b] + nums[c] + nums[d] > target { break }
+            if nums[a] + nums[b] + nums[c] + nums[d] == target {
+              ans = append(ans, []int{nums[a], nums[b], nums[c], nums[d]})
             }
           }
         }
