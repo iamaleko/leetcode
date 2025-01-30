883e28cd54c09a897d8a4acdbbbe44877d1f4f96
Alexander Kolobov <a@da.am>
2025-01-30 21:12:29 +0400

    Time: 71 ms (18.97%), Space: 4.6 MB (74.14%) - LeetHub

--- 0018-4sum/0018-4sum.go
+++ 0018-4sum/0018-4sum.go
@@ -21,8 +21,10 @@ func fourSum(nums []int, target int) [][]int {
           if nums[a] + nums[b] + nums[b+1] + nums[b+2] > target { break }
           if b == a + 1 || nums[b] != nums[b-1] {
             for c := b + 1; c < n - 1; c++ {
+              if nums[a] + nums[b] + nums[c] + nums[c+1] > target { break }
               if c == b + 1 || nums[c] != nums[c-1] {
                 for d := c + 1; d < n; d++ {
+                  if nums[a] + nums[b] + nums[c] + nums[d] > target { break }
                   if (d == c+1 || nums[d] != nums[d-1]) && nums[a] + nums[b] + nums[c] + nums[d] == target {
                     ans = append(ans, []int{nums[a], nums[b], nums[c], nums[d]})
                   }
