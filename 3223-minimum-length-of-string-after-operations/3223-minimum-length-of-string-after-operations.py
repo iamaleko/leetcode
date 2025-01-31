d430aacfabde13b3ee9c2c0e4988e3f9621fd4e7
Alexander Kolobov <a@da.am>
2025-01-31 23:33:40 +0400

    Time: 142 ms (67.59%), Space: 19 MB (6.64%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.py
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.py
@@ -1,17 +1,7 @@
 class Solution:
-    def minimumLength(self, s: str) -> int:
-        # Step 1: Count the frequency of each character in the string
-        char_frequency_map = Counter(s)
-
-        # Step 2: Calculate the number of characters to delete
-        delete_count = 0
-        for frequency in char_frequency_map.values():
-            if frequency % 2 == 1:
-                # If frequency is odd, delete all except one
-                delete_count += frequency - 1
-            else:
-                # If frequency is even, delete all except two
-                delete_count += frequency - 2
-
-        # Step 3: Return the minimum length after deletions
-        return len(s) - delete_count
+  def minimumLength(self, s: str) -> int:
+    ans = len(s)
+    for n in Counter(s).values():
+      if n > 2:
+        ans -= n - 1 if n & 1 else n - 2
+    return ans
