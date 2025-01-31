2e6d775297e23498860f57262fe279dca8510c7a
Alexander Kolobov <a@da.am>
2025-01-31 21:22:54 +0400

    Time: 65 ms (93.88%), Space: 10.6 MB (87.76%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.go
+++ 0827-making-a-large-island/0827-making-a-large-island.go
@@ -3,11 +3,10 @@ func largestIsland(grid [][]int) int {
 	hasWater := false
 	ans := 0
 	n := len(grid)
-  type Offset struct{ Y, X int }
-  dir := []*Offset { &Offset{-1, 0}, &Offset{1, 0}, &Offset{0, -1}, &Offset{0, 1} }
+	offsets := []*struct{ Y, X int }{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
 
 	// find islands
-  yq, xq, p := []int{}, []int{}, -1
+	yq, xq, p := []int{}, []int{}, -1
 	for y := range n {
 		for x := range n {
 			if grid[y][x] == 0 {
@@ -21,8 +20,8 @@ func largestIsland(grid [][]int) int {
 					y, x := yq[p], xq[p]
 					yq, xq = yq[:p], xq[:p]
 					p--
-					for _, offset := range dir {
-            yo, xo := y + offset.Y, x + offset.X
+					for _, offset := range offsets {
+						yo, xo := y+offset.Y, x+offset.X
 						if yo > -1 && yo < n && xo > -1 && xo < n && grid[yo][xo] == 1 {
 							islandSize++
 							grid[yo][xo] = islandId
@@ -32,7 +31,7 @@ func largestIsland(grid [][]int) int {
 					}
 				}
 				islandSizes[islandId] = islandSize
-        ans = max(ans, islandSize)
+				ans = max(ans, islandSize)
 				islandId += 1
 			}
 		}
@@ -45,14 +44,14 @@ func largestIsland(grid [][]int) int {
 			for x := range n {
 				if grid[y][x] == 0 {
 					joinedSize, addedIslandIds := 1, map[int]bool{}
-					for _, cell := range []struct{ Y, X int }{{y - 1, x}, {y + 1, x}, {y, x - 1}, {y, x + 1}} {
-						if cell.Y > -1 && cell.Y < n && cell.X > -1 && cell.X < n && grid[cell.Y][cell.X] > 0 &&
-							addedIslandIds[grid[cell.Y][cell.X]] == false {
-							joinedSize += islandSizes[grid[cell.Y][cell.X]]
-							addedIslandIds[grid[cell.Y][cell.X]] = true
+					for _, offset := range offsets {
+						yo, xo := y+offset.Y, x+offset.X
+						if yo > -1 && yo < n && xo > -1 && xo < n && grid[yo][xo] > 0 && addedIslandIds[grid[yo][xo]] == false {
+							joinedSize += islandSizes[grid[yo][xo]]
+							addedIslandIds[grid[yo][xo]] = true
 						}
 					}
-          ans = max(ans, joinedSize)
+					ans = max(ans, joinedSize)
 				}
 			}
 		}
