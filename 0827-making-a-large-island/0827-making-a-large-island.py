4fa1139a4a4fdd0b70f2d5af88f332a196d45ae7
Alexander Kolobov <a@da.am>
2025-01-31 20:35:52 +0400

    Time: 667 ms (92.34%), Space: 27.2 MB (90.95%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.py
+++ 0827-making-a-large-island/0827-making-a-large-island.py
@@ -1,12 +1,10 @@
 class Solution:
   def largestIsland(self, grid: List[List[int]]) -> int:
-    sizes = {}
-    island, blank, ans = 2, 0, 0
-    my, mx = len(grid)-1, len(grid[0])-1
+    sizes, island, blank, ans, n = {}, 2, 0, 0, len(grid)
 
     # find islands
-    for y in range(my+1):
-      for x in range(mx+1):
+    for y in range(n):
+      for x in range(n):
         if grid[y][x] == 0:
           blank = 1
         elif grid[y][x] == 1:
@@ -15,8 +13,8 @@ class Solution:
           q = deque([(y, x)])
           while q:
             c, d = q.pop()
-            for a, b in [(c-1, d), (c+1, d), (c, d-1), (c,d+1)]:
-              if a >= 0 and a <= my and b >= 0 and b <= mx and grid[a][b] == 1:
+            for a, b in [(c-1, d), (c+1, d), (c, d-1), (c, d+1)]:
+              if a > -1 and a < n and b > -1 and b < n and grid[a][b] == 1:
                 size += 1
                 grid[a][b] = island
                 q.append((a, b))
@@ -27,13 +25,14 @@ class Solution:
     # check connections
     if blank:
       ans += 1
-      for y in range(my+1):
-        for x in range(mx+1):
+      for y in range(n):
+        for x in range(n):
           if grid[y][x] == 0:
             size, added = 1, set()
             for a, b in [(y-1, x), (y+1, x), (y, x-1), (y, x+1)]:
-              if a >= 0 and a <= my and b >= 0 and b <= mx and grid[a][b] and grid[a][b] not in added:
+              if a > -1 and a < n and b > -1 and b < n and grid[a][b] and grid[a][b] not in added:
                 size += sizes[grid[a][b]]
                 added.add(grid[a][b])
             if size > ans: ans = size
+            
     return ans
