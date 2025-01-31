e4c5836488c0b8602abe565769f71d5709d10ecd
Alexander Kolobov <a@da.am>
2025-01-31 23:42:08 +0400

    Time: 112 ms (20.5%), Space: 9.6 MB (12.94%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.go
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.go
@@ -3,10 +3,8 @@ func minimumLength(s string) int {
   for _, rn := range []rune(s) { count[rn]++ }
   ans := len(s)
   for _, n := range count {
-    if n > 2 {
-      ans -= n - 1
-      if n & 1 == 0 { ans++ }
-    }
+    ans -= n - 1
+    if n & 1 == 0 { ans++ }
   }
   return ans
 }
