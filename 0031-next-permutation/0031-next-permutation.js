88374d7a39db56f3b8decff428d7d2fd72b3cf56
Alexander Kolobov <a@da.am>
2025-02-02 02:15:14 +0400

    Time: 0 ms (100%), Space: 51.1 MB (81.28%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const nextPermutation = (nums) => {
+  let j = nums.length - 1, i = nums.length - 2, max = nums[j], st = [j];
+  while (i > -1) {
+    if (nums[i] < max) {
+      j = st.length - 1;
+      while (nums[i] < nums[st[j]]) j--;
+      j = st[j+1]
+      break;
+    } else if (nums[i] > max) {
+      max = nums[i];
+      st.push(i);
+    }
+    i--;
+  }
+  if (i < 0) {
+    nums.sort((a, b) => a - b);
+  } else {
+    [nums[j], nums[i]] = [nums[i], nums[j]];
+    nums.splice(i+1, nums.length, ...nums.slice(i+1).sort((a, b) => a - b));
+  }
+};
