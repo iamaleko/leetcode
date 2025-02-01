86d8c2a4ad4e6f605cf1ccad918797451656da48
Alexander Kolobov <a@da.am>
2025-02-02 01:18:19 +0400

    Time: 1332 ms (34.36%), Space: 9.6 MB (23.7%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.go
@@ -0,0 +1,24 @@
+func findSubstring(s string, words []string) []int {
+  total := len(words)
+  chunk := len(words[0])
+  count := map[string]int{}
+  found := map[string]int{}
+  ans := []int{}
+  for _, word := range words {
+    count[word]++
+  }
+  for i, m := 0, len(s) - total * chunk; i <= m; i++ {
+    found = map[string]int{}
+    added := 0
+    for j, m := i, i + total * chunk - chunk; j <= m; j += chunk {
+      word := s[j: j + chunk]
+      if found[word] + 1 > count[word] { break }
+      found[word]++
+      added++
+    }
+    if added == total {
+      ans = append(ans, i)
+    }
+  }
+  return ans
+}
