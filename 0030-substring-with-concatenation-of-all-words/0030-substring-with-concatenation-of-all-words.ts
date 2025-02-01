6ad933a1a58f36d598767abdbf2ce2a59942e01f
Alexander Kolobov <a@da.am>
2025-02-02 00:44:52 +0400

    Time: 1313 ms (38.09%), Space: 58.2 MB (49.52%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.ts
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.ts
@@ -5,17 +5,16 @@ function findSubstring(s: string, words: string[]): number[] {
   const ans: number[] = [];
   for (const word of words) map.set(word, (map.get(word) ?? 0) + 1)
   main: for (let i = 0, m = s.length - size * chunk; i <= m; i++) {
-    if (map.has(s.slice(i, i + chunk))) {
-      const count = new Map();
-      for (let j = i; j < i + size * chunk; j += chunk) {
-        const word = s.slice(j, j + chunk);
-        count.set(word, (count.get(word) ?? 0) + 1)
-      }
-      for (const [key, val] of map) {
-        if (count.get(key) !== val) continue main;
-      }
-      ans.push(i);
+    const count = new Map();
+    for (let j = i; j < i + size * chunk; j += chunk) {
+      const word = s.slice(j, j + chunk);
+      if (!map.has(word)) continue main;
+      count.set(word, (count.get(word) ?? 0) + 1)
     }
+    for (const [key, val] of map) {
+      if (count.get(key) !== val) continue main;
+    }
+    ans.push(i);
   }
   return ans;
 };
