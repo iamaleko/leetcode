83ea7bb5906410e13d2797a56b1dc7598040015f
Alexander Kolobov <a@da.am>
2025-02-02 01:10:13 +0400

    Time: 1364 ms (38.09%), Space: 57.7 MB (78.1%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.ts
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.ts
@@ -1,21 +1,21 @@
 function findSubstring(s: string, words: string[]): number[] {
-  const size = words.length;
+  const total = words.length;
   const chunk = words[0].length;
-  const map = new Map<string, number>();
   const count = new Map<string, number>();
+  const found = new Map<string, number>();
   const ans: number[] = [];
-  for (const word of words) map.set(word, (map.get(word) ?? 0) + 1)
-  main: for (let i = 0, m = s.length - size * chunk; i <= m; i++) {
-    count.clear();
-    let found = 0;
-    for (let j = i, m = i + size * chunk; j + chunk <= m; j += chunk) {
+  for (const word of words) count.set(word, (count.get(word) ?? 0) + 1)
+  main: for (let i = 0, m = s.length - total * chunk; i <= m; i++) {
+    found.clear();
+    let added = 0;
+    for (let j = i, m = i + total * chunk; j + chunk <= m; j += chunk) {
       const word = s.substr(j, chunk);
-      const val = (count.get(word) ?? 0) + 1;
-      if (val > (map.get(word) ?? 0)) continue main;
-      count.set(word, val)
-      found++
+      const val = (found.get(word) ?? 0) + 1;
+      if (val > (count.get(word) ?? 0)) continue main;
+      found.set(word, val)
+      added++
     }
-    if (found === size) {
+    if (added === total) {
       ans.push(i);
     }
   }
