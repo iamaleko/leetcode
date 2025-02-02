1f698790c8a27a8d3d0ffeba79af817950d4478a
Alexander Kolobov <a@da.am>
2025-02-03 00:58:00 +0400

    Time: 0 ms (100%), Space: 52.2 MB (52.26%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.ts
@@ -0,0 +1,25 @@
+function searchRange(nums: number[], target: number): number[] {
+  if (!nums.length || nums[0] > target) return [-1, -1];
+  let c1: number, c2: number, l1 = 0, l2 = 0, r1 = nums.length - 1, r2 = nums.length - 1;
+  while (l1 < r1 || l2 < r2) {
+    if (l1 < r1) {
+      c1 = l1 + r1 >> 1;
+      if (nums[c1] < target) {
+        l1 = c1 + 1;
+      } else {
+        r1 = c1 - 1;
+      }
+    }
+    if (l2 < r2) {
+      c2 = l2 + r2 >> 1;
+      if (nums[c2] > target) {
+        r2 = c2 - 1;
+      } else {
+        l2 = c2 + 1;
+      }
+    }
+  }
+  if (nums[l1] !== target) l1++;
+  if (nums[l2] !== target) l2--;
+  return nums[l1] !== target ? [-1, -1] : [l1, l2];
+};
