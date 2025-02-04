407a42ccc958b078c05bbd95bf6e752d8efb3fe9
Alexander Kolobov <a@da.am>
2025-02-04 21:17:45 +0400

    Time: 0 ms (100%), Space: 51.5 MB (16.67%) - LeetHub

--- 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.ts
+++ 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.ts
@@ -2,9 +2,10 @@ function maxAscendingSum(nums: number[]): number {
   let ans = nums[0], sum = nums[0];
   for (let i = 1; i < nums.length; i++) {
     if (nums[i-1] >= nums[i]) {
-      [ans, sum] = [Math.max(ans, sum), 0];
+      if (sum > ans) ans = sum;
+      sum = 0;
     }
     sum += nums[i];
   }
-  return Math.max(ans, sum);
+  return sum > ans ? sum : ans;
 };
