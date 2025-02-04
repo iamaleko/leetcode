e71a40638bf7a57473bdc5981428a7fd193a7322
Alexander Kolobov <a@da.am>
2025-02-05 01:45:50 +0400

    Time: 2 ms (90.41%), Space: 8.4 MB (94.52%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.go
+++ 0697-degree-of-an-array/0697-degree-of-an-array.go
@@ -1,24 +1,22 @@
 func findShortestSubArray(nums []int) int {
   type Item struct {
     First int
-    Last int
     Count int
   }
   m := map[int]*Item{}
-  ans, count := 1, 1
+  ans, count := 0, 1
   for i, num := range nums {
     if _, ok := m[num]; ok {
       m[num].Count++
-      m[num].Last = i
       if m[num].Count > count {
-        ans = m[num].Last - m[num].First + 1
+        ans = i - m[num].First
         count = m[num].Count
       } else if m[num].Count == count {
-        ans = min(ans, m[num].Last - m[num].First + 1)
+        ans = min(ans, i - m[num].First)
       }
     } else {
-      m[num] = &Item{ i, i, 1 }
+      m[num] = &Item{ i, 1 }
     }
   }
-  return ans
+  return ans + 1
 }
