65e195ae5be0b110ae84844e398ab80eeea5e434
Alexander Kolobov <a@da.am>
2025-02-04 22:27:50 +0400

    Time: 1 ms (71.7%), Space: 53.3 MB (44.93%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.ts
+++ 0042-trapping-rain-water/0042-trapping-rain-water.ts
@@ -1,25 +1,13 @@
-function trap(arr: number[]): number {
-  if (arr.length < 2) return 0;
-  let sum = 0;
-  for (let i = 1; i < arr.length; ++i) {
-    // find first level decrease
-    if (arr[i] < arr[i - 1]) {
-      // find level encrease or max level from below
-      let j = i, mi = i;
-      for (; arr[j] < arr[i - 1] && j < arr.length; ++j) {
-        if (arr[j] >= arr[mi]) {
-          mi = j;
-        }
-      }
-      if (j < arr.length) mi = j;
-      
-      // add water in between
-      if (mi - i > 0) {
-        const min = Math.min(arr[i - 1], arr[mi]);
-        for (let k = i; k < mi; ++k) if (arr[k] < min) sum += min - arr[k];
-        i = mi - 1;    
-      }
-    }
+function trap(height: number[]): number {
+  const n = height.length;
+  const r = height.slice();
+  for (let i = n-2; i >= 0; i--) {
+    r[i] = Math.max(r[i], r[i+1]);
   }
-  return sum;
+  let ans = 0;
+  for (let l = 0, i = 0; i < n; i++) {
+    l = Math.max(height[i], l);
+    ans += Math.max(0, Math.min(l, r[i]) - height[i]);
+  }
+  return ans;
 };
