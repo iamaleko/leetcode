57aae5082975738994821e8b52057431d92c9f86
Alexander Kolobov <a@da.am>
2025-02-04 23:38:22 +0400

    Time: 1 ms (44.27%), Space: 8.4 MB (75.96%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.go
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.go
@@ -14,18 +14,9 @@ func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
     }
     return a == nil || a.Val == b.Val && check(a.Left, b.Left) && check(a.Right, b.Right)
   }
-  q := []*TreeNode{root}
-  for len(q) > 0 {
-    if check(q[0], subRoot) {
-      return true
-    }
-    if q[0].Left != nil {
-      q = append(q, q[0].Left)
-    }
-    if q[0].Right != nil {
-      q = append(q, q[0].Right)
-    }
-    q = q[1:]
+  var traverse func(a *TreeNode) bool
+  traverse = func(a *TreeNode) bool {
+    return check(a, subRoot) || a != nil && (traverse(a.Left) || traverse(a.Right))
   }
-  return false
+  return traverse(root)
 }
