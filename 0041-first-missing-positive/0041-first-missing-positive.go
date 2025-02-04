e69baf5765b06fe7f7d84615a6bfa1474134238e
Alexander Kolobov <a@da.am>
2025-02-04 21:39:57 +0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.go
@@ -0,0 +1,27 @@
+func firstMissingPositive(nums []int) int {
+  min, n := math.MaxInt, len(nums)
+  for _, num := range nums {
+    if num > 0 && num < min {
+      min = num
+    }
+  } 
+  if min > 1 {
+    return 1
+  }
+  for i, _ := range nums {
+    for nums[i]-1 != i {
+      if nums[i]-1 < 0 || nums[i]-1 >= n {
+        nums[i] = 0
+        break
+      } else {
+        nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
+      }
+    }
+  }
+  for i, num := range nums {
+    if num == 0 {
+      return i+1
+    }
+  }
+  return n+1
+}
