d69390062d53ac868fc67e01bb56b23c2789a561
Alexander Kolobov <a@da.am>
2025-02-04 21:44:08 +0400

    Time: 0 ms (100%), Space: 9.8 MB (83.21%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.go
+++ 0041-first-missing-positive/0041-first-missing-positive.go
@@ -1,10 +1,9 @@
 func firstMissingPositive(nums []int) int {
   n := len(nums)
   for i, _ := range nums {
-    for nums[i]-1 != i {
+    for nums[i] != 0 && nums[i]-1 != i {
       if nums[i]-1 < 0 || nums[i]-1 >= n || nums[nums[i]-1] == nums[i] {
         nums[i] = 0
-        break
       } else {
         nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
       }
