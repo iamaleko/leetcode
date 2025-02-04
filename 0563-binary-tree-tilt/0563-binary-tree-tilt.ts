62f818be53ce08c5e3939830bc775fecd4f91c5f
Alexander Kolobov <a@da.am>
2025-02-04 23:21:17 +0400

    Time: 3 ms (27.78%), Space: 56.4 MB (11.11%) - LeetHub

---
+++ 0563-binary-tree-tilt/0563-binary-tree-tilt.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function findTilt(root: TreeNode | null): number {
+  const traverse = (node: TreeNode | null) => {
+    if (node === null) return [0, 0]
+    const [leftTiltSum, leftValSum] = traverse(node.left);
+    const [rightTiltSum, rightValSum] = traverse(node.right);
+    return [
+      Math.abs(leftValSum - rightValSum) + leftTiltSum + rightTiltSum,
+      leftValSum + node.val + rightValSum,
+    ];
+  }
+  return traverse(root)[0];
+};
