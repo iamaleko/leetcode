45ac65971ef6e2895e48467e24abe707e498355a
Alexander Kolobov <a@da.am>
2025-02-04 21:48:00 +0400

    Time: 4 ms (73.64%), Space: 59.1 MB (70.03%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.ts
+++ 0041-first-missing-positive/0041-first-missing-positive.ts
@@ -1,61 +1,18 @@
 function firstMissingPositive(nums: number[]): number {
-  let n = nums.length,
-    pointer = 0,
-    proceed: number | null = null,
-    swap = false,
-    found: number | null = null;
-
-  while (pointer <= n) {
-    const val = nums[pointer] === null ? null : nums[pointer] - 1;
-
-    if (swap) {
-      // swap in progress
-      if (
-        val === null || // empty
-        val === pointer || // same
-        (val < 0 || val >= n) // outside range
-      ) {
-        nums[pointer] = pointer + 1;
-        if (found === pointer) {
-          // check next
-          found = null;
-          pointer++;
-        } else {
-          // proceed after swap ends
-          pointer = proceed;
-        }
-        proceed = null;
-        swap = false;
-      } else {
-        // inside range
-        if (found === pointer) {
-          proceed = found + 1;
-          found = null;
-        }
-        nums[pointer] = pointer + 1;
-        pointer = val;
-      }
-    } else {
-      if (val === pointer) {
-        // on it's place
-        pointer++;
-      } else if (
-        val === null || // empty
-        (val < 0 || val >= n) // outside range
-      ) {
-        if (found === null) found = pointer;
-        nums[pointer] = null;
-        pointer++;
-      } else {
-        // inside range
-        if (found === null) found = pointer;
-        nums[pointer] = null;
-        proceed = pointer + 1;
-        pointer = val;
-        swap = true;
-      }
-    }
-  }
-
-  return found === null ? n : found + 1;
+  const n = nums.length;
+	for (let i = 0; i < n; i++) {
+		while (nums[i] !== 0 && nums[i]-1 !== i) {
+			if (nums[i]-1 < 0 || nums[i]-1 >= n || nums[nums[i]-1] == nums[i]) {
+				nums[i] = 0;
+			} else {
+				[nums[nums[i]-1], nums[i]] = [nums[i], nums[nums[i]-1]];
+			}
+		}
+	}
+	for (let i = 0; i < n; i++) {
+		if (nums[i] === 0) {
+			return i + 1;
+		}
+	}
+	return n + 1;
 };
