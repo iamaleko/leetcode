02dd087ccfb28434353427cd13669d239dbb8e09
Alexander Kolobov <a@da.am>
2025-02-04 21:52:18 +0400

    Time: 3 ms (80.36%), Space: 58.7 MB (85.79%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.ts
+++ 0041-first-missing-positive/0041-first-missing-positive.ts
@@ -1,14 +1,14 @@
 function firstMissingPositive(nums: number[]): number {
   const n = nums.length;
 	for (let i = 0; i < n; i++) {
-		while (nums[i] !== 0 && nums[i]-1 !== i) {
-			if (nums[i]-1 < 0 || nums[i]-1 >= n || nums[nums[i]-1] == nums[i]) {
+		while (nums[i] && nums[i]-1 !== i) {
+			if (nums[i]-1 < 0 || nums[i]-1 >= n || nums[nums[i]-1] === nums[i]) {
 				nums[i] = 0;
 			} else {
 				[nums[nums[i]-1], nums[i]] = [nums[i], nums[nums[i]-1]];
 			}
 		}
 	}
-  let i = nums.indexOf(0);
+  const i = nums.indexOf(0);
 	return i<0 ? n+1 : i+1;
 };
