94c8575a864e9f2a1494a9b9b46b1ac853ad6d9b
Alexander Kolobov <a@da.am>
2025-02-06 22:08:31 +0400

    Time: 148 ms (71.43%), Space: 42.5 MB (57.14%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.go
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.go
@@ -1,13 +1,7 @@
 func tupleSameProduct(nums []int) int {
-  d := map[int]bool{}
-  for _, num := range nums {
-    d[num] = true
-  }
-  ans := 0
-  m := map[int]int{}
-  var k int
-  for i := 0; i < len(nums)-1; i++ {
-    for j := i+1; j < len(nums); j++ {
+  ans, k, n, m := 0, 0, len(nums), map[int]int{}
+  for i := 0; i < n-1; i++ {
+    for j := i+1; j < n; j++ {
       k = nums[i] * nums[j]
       if m[k] > 0 {
         ans += 8 * m[k]
