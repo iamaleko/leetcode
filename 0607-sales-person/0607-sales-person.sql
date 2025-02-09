f82f6ec7c79ce33addcee9fa77792c091c331976
Alexander Kolobov <a@da.am>
2025-02-10 00:39:05 +0400

    Time: 3614 ms (8.73%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,3 +1,7 @@
-select name from SalesPerson left join (
+-- select name from SalesPerson left join (
+--   select distinct sales_id from Orders left join Company using(com_id) where name = "RED"
+-- ) sub using(sales_id) where sub.sales_id is null
+
+select name from SalesPerson where sales_id not in (
   select distinct sales_id from Orders left join Company using(com_id) where name = "RED"
-) sub using(sales_id) where sub.sales_id is null
+)
