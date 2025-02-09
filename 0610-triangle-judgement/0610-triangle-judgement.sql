b0664ade66baabe71495804d920bc0a025b45d29
Alexander Kolobov <a@da.am>
2025-02-10 00:45:53 +0400

    Time: 349 ms (86.49%), Space: 0B (100%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,1 +1,2 @@
-select x, y, z, if(x<y+z and y<x+z and z<y+x, "Yes", "No") as triangle from Triangle
+-- select x, y, z, if(x<y+z and y<x+z and z<y+x, "Yes", "No") as triangle from Triangle
+select x, y, z, if(greatest(x,y,z) < least(x+y, y+z, z+x), "Yes", "No") as triangle from Triangle
