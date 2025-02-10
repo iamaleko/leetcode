7bcf7dd9432960c979185c353f4178cb7f3ecb91
Alexander Kolobov <a@da.am>
2025-02-10 21:29:57 +0400

    Time: 1 ms (73.53%), Space: 3.8 MB (88.24%) - LeetHub

--- 0806-number-of-lines-to-write-string/0806-number-of-lines-to-write-string.go
+++ 0806-number-of-lines-to-write-string/0806-number-of-lines-to-write-string.go
@@ -1,7 +1,7 @@
 func numberOfLines(widths []int, s string) []int {
 	width := 0
 	line := 1
-	for _, rn := range s {
+	for _, rn := range []rune(s) {
 		width += widths[rn-'a']
 		if width > 100 {
 			line++
