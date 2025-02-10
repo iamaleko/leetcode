f834e426b6e2f079a4f81056cb3f74a4ccc26b63
Alexander Kolobov <a@da.am>
2025-02-10 20:04:12 +0400

    Time: 0 ms (100%), Space: 4.1 MB (93.33%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.go
+++ 3174-clear-digits/3174-clear-digits.go
@@ -1,17 +1,30 @@
 func clearDigits(s string) string {
-  ans := []rune{}
-  runes := []rune(s)
-  remove := 0
-  slices.Reverse(runes)
-  for _, rn := range runes  {
-    if unicode.IsDigit(rn) {
-      remove++
-    } else if remove != 0 {
-      remove--
-    } else {
-      ans = append(ans, rn)
-    }
-  }
-  slices.Reverse(ans)
-  return string(ans)
+	runes := []rune(s)
+	offset := 0
+	for i, rn := range runes {
+		if unicode.IsDigit(rn) {
+			offset += 2
+		} else if offset > 0 {
+			runes[i-offset] = rn
+		}
+	}
+	return string(runes[:len(runes)-offset])
 }
+
+// func clearDigits(s string) string {
+//   ans := []rune{}
+//   runes := []rune(s)
+//   remove := 0
+//   slices.Reverse(runes)
+//   for _, rn := range runes  {
+//     if unicode.IsDigit(rn) {
+//       remove++
+//     } else if remove != 0 {
+//       remove--
+//     } else {
+//       ans = append(ans, rn)
+//     }
+//   }
+//   slices.Reverse(ans)
+//   return string(ans)
+// }
