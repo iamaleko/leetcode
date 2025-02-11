c832072f1709948492e5715f10959db11631aac8
Alexander Kolobov <a@da.am>
2025-02-12 00:59:03 +0400

    Time: 35 ms (5.41%), Space: 23.5 MB (8.11%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.go
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.go
@@ -12,35 +12,24 @@ func removeOccurrences(s string, part string) string {
       // save state
       matches[i] = maps.Clone(match)
       // check all candidates
-      k := i
       for start := range match {
         if s[i] == part[match[start]] {
           match[start]++
           if match[start] == m {
             skip[start] = i + 1
-            // fmt.Println("match", start, skip[start])
-            k = max(-1, start - 2)
-            // fmt.Println("go to", k + 1, matches[k + 1])
-            delete(match, start)
+            i = max(-1, start - 2)
+            match = maps.Clone(matches[i + 1])
+            break
           }
         } else {
           delete(match, start)
-        }
-      }
-      if i != k {
-        i = k
-        if matches[i + 1] != nil {
-          match = maps.Clone(matches[i + 1])
-        } else {
-          match = map[int]int{}
         }
       }
 		} else {
       i = skip[i]-1
 		}
 	}
-  // fmt.Println(matches)
-  // fmt.Println(skip)
+  // build result string
   ans := []byte{}
   for i := 0; i < n; i++ {
     if skip[i] == 0 {
