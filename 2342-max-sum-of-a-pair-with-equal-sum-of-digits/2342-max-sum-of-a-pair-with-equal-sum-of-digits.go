31becfa057c6ff7ac1c8973925af9e415c844a5d
Alexander Kolobov <a@da.am>
2025-02-13 20:17:23 +0400

    Time: 8 ms (92.73%), Space: 10.5 MB (70.91%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.go
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.go
@@ -1,19 +1,19 @@
 func maximumSum(nums []int) int {
-  m := [82]int{}
+	arr := [82]int{}
 	ans := -1
-  var s, n int
+	var s, n int
 	for _, num := range nums {
 		s, n = 0, num
 		for n > 0 {
 			s += n % 10
 			n /= 10
 		}
-    if m[s] != 0 && ans < num+m[s] {
-      ans = num + m[s]
-    }
-    if m[s] < num {
-      m[s] = num
-    }
+		if arr[s] != 0 && ans < num+arr[s] {
+			ans = num + arr[s]
+		}
+		if arr[s] < num {
+			arr[s] = num
+		}
 	}
 	return ans
 }
