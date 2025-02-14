3582bcd21ec3744d15f0085ed2b59d08d413cff5
Alexander Kolobov <a@da.am>
2025-02-14 23:00:09 +0400

    Time: 24 ms (88.46%), Space: 18.6 MB (42.31%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.go
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.go
@@ -1,37 +1,36 @@
 type ProductOfNumbers struct {
   Nums []int
   ZeroIndex int
-  LastIndex int
 }
 
 func Constructor() ProductOfNumbers {
-  return ProductOfNumbers{ ZeroIndex: -1, LastIndex: -1 }
+  return ProductOfNumbers{ ZeroIndex: -1 }
 }
 func (this *ProductOfNumbers) Add(num int)  {
   if num == 0 {
-    this.ZeroIndex = this.LastIndex + 1
+    this.ZeroIndex = len(this.Nums)
     this.Nums = append(this.Nums, 0)
-  } else if this.LastIndex >= 0 {
-    if this.LastIndex == this.ZeroIndex {
+  } else if len(this.Nums) > 0 {
+    if this.Nums[len(this.Nums)-1] == 0 {
       this.Nums = append(this.Nums, num)
     } else {
-      this.Nums = append(this.Nums, num * this.Nums[this.LastIndex])
+      this.Nums = append(this.Nums, num * this.Nums[len(this.Nums)-1])
     }
   } else {
     this.Nums = append(this.Nums, num)
   }
-  this.LastIndex++
 }
 func (this *ProductOfNumbers) GetProduct(k int) int {
-  dividerIndex := this.LastIndex - k
+  lastIndex := len(this.Nums) - 1
+  dividerIndex := len(this.Nums) - k - 1
   // stream is empty
-  if this.LastIndex < 0 {
+  if lastIndex < 0 {
     return 0
   }
   // all stream is requested
   if dividerIndex < 0 {
     if this.ZeroIndex == -1 {
-      return this.Nums[this.LastIndex]
+      return this.Nums[lastIndex]
     }
     return 0
   }
@@ -40,7 +39,7 @@ func (this *ProductOfNumbers) GetProduct(k int) int {
     return 0
   }
   if this.ZeroIndex == dividerIndex {
-    return this.Nums[this.LastIndex]
+    return this.Nums[lastIndex]
   }
-  return this.Nums[this.LastIndex] / this.Nums[dividerIndex]
+  return this.Nums[lastIndex] / this.Nums[dividerIndex]
 }
