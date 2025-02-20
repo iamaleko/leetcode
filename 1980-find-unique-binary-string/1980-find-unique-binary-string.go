eacd4b37339424e3700534747f9882a99c51cce4
Alexander Kolobov <a@da.am>
2025-02-20 21:16:38 +0400

    Time: 0 ms (100%), Space: 4.1 MB (96.3%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.go
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.go
@@ -1,11 +1,7 @@
 func findDifferentBinaryString(nums []string) string {
   bytes := []byte{}
   for i := range(len(nums)) {
-    if nums[i][i] == 49 {
-      bytes = append(bytes, 48)
-    } else {
-      bytes = append(bytes, 49)
-    }
+    bytes = append(bytes, nums[i][i] ^ 1) // 48 is "0", 49 is "1", also 48 ^ 1 == 49 and 49 ^ 1 == 48
   }
   return string(bytes)
 }
