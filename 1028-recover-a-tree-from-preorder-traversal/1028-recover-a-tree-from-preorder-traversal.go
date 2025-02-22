3c4d3343d99b8ce343abfbba6f3a27f585a68459
Alexander Kolobov <a@da.am>
2025-02-22 17:36:01 +0400

    Time: 0 ms (100%), Space: 7.3 MB (0%) - LeetHub

---
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.go
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func recoverFromPreorder(traversal string) *TreeNode {
+	var depth, value int
+	var depths, values []int
+	traversal += "-"
+	for i, rn := range traversal {
+		if rn == '-' {
+			if traversal[i-1] != '-' {
+				depths, values = append(depths, depth), append(values, value)
+				depth, value = 0, 0
+			}
+			depth++
+		} else {
+			value = value*10 + int(rn-'0')
+		}
+	}
+	m := len(values)-1
+	var traverse func(p int) (*TreeNode, int)
+	traverse = func(p int) (*TreeNode, int) {
+    node, childDepth := &TreeNode{Val: values[p]}, depths[p]+1
+		if p < m && node.Left == nil && depths[p+1] == childDepth {
+			node.Left, p = traverse(p+1)
+		}
+		if p < m && node.Right == nil && depths[p+1] == childDepth {
+			node.Right, p = traverse(p+1)
+		}
+		return node, p
+	}
+  root, _ := traverse(0)
+	return root
+}
