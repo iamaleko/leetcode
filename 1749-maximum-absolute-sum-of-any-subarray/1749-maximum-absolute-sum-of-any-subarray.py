e06a6906121048a659058f25df539699f180b7c4
Alexander Kolobov <a@da.am>
2025-02-26 21:22:26 +0400

    Time: 20 ms (91.46%), Space: 28.5 MB (63.9%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.py
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.py
@@ -1,18 +1,24 @@
 class Solution:
   def maxAbsoluteSum(self, nums: List[int]) -> int:
-    minSum = 0
-    maxSum = 0
-    ans = 0
-    for num in nums:
-      minSum += num
-      if minSum > 0:
-        minSum = 0
-      else:
-        ans = max(ans, abs(minSum))
-      maxSum += num
-      if maxSum < 0:
-        maxSum = 0
-      else:
-        ans = max(ans, abs(maxSum))
-    return ans
+    for i in range(1, len(nums)):
+      nums[i] += nums[i - 1]
+    nums += [0]
+    return max(nums) - min(nums)
         
+# class Solution:
+#   def maxAbsoluteSum(self, nums: List[int]) -> int:
+#     minSum = 0
+#     maxSum = 0
+#     ans = 0
+#     for num in nums:
+#       minSum += num
+#       if minSum > 0:
+#         minSum = 0
+#       else:
+#         ans = max(ans, abs(minSum))
+#       maxSum += num
+#       if maxSum < 0:
+#         maxSum = 0
+#       else:
+#         ans = max(ans, abs(maxSum))
+#     return ans
