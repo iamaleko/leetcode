cbe1efe1eddba00423532f9a56f3d12f3bc7c4b2
Alexander Kolobov <a@da.am>
2025-02-27 22:10:14 +0400

    Time: 4186 ms (5.3%), Space: 182.2 MB (5.3%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.py
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.py
@@ -1,17 +1,21 @@
 class Solution:
   def lenLongestFibSubseq(self, nums: List[int]) -> int:
     n = len(nums)
-    m = {}
+    m = defaultdict(dict)
     ans = 0
     for i in range(n):
+
       for j in range(i):
         s = nums[j] + nums[i]
-        if s not in m: m[s] = []
-        m[s].append((nums[i], 2))
+        if nums[i] not in m[s]:
+          m[s][nums[i]] = 2
+
       if nums[i] in m:
-        for num, count in m[nums[i]]:
+        for num in m[nums[i]]:
+          count = m[nums[i]][num] + 1
           s = num + nums[i]
-          if s not in m: m[s] = []
-          m[s].append((nums[i], count + 1))
-          if count >= ans: ans = count + 1
+          if nums[i] not in m[s] or m[s][nums[i]] < count:
+            m[s][nums[i]] = count
+            if count >= ans: ans = count
+
     return ans
