8b6b081d985fc12dd4a02abfaa21452b6aa9c275
Alexander Kolobov <a@da.am>
2025-02-27 22:10:50 +0400

    Time: 4293 ms (5.3%), Space: 181.9 MB (5.3%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.py
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.py
@@ -4,18 +4,15 @@ class Solution:
     m = defaultdict(dict)
     ans = 0
     for i in range(n):
-
       for j in range(i):
         s = nums[j] + nums[i]
         if nums[i] not in m[s]:
           m[s][nums[i]] = 2
-
       if nums[i] in m:
         for num in m[nums[i]]:
           count = m[nums[i]][num] + 1
           s = num + nums[i]
           if nums[i] not in m[s] or m[s][nums[i]] < count:
             m[s][nums[i]] = count
-            if count >= ans: ans = count
-
+            ans = max(ans, count)
     return ans
