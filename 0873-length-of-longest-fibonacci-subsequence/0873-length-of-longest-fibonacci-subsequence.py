691c455b3180d205b722a780c0d975d2c0d88389
Alexander Kolobov <a@da.am>
2025-02-27 22:10:02 +0400

    Time: 8079 ms (5.3%), Space: 151.5 MB (5.3%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.py
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.py
@@ -4,27 +4,14 @@ class Solution:
     m = {}
     ans = 0
     for i in range(n):
-      # print('+', nums[i])
-
       for j in range(i):
         s = nums[j] + nums[i]
-        if s not in m:
-          m[s] = []
-        m[s] += [(nums[i], 2)]
-        # print('start waiting', s, '(',nums[j],'->',nums[i],')', 2)
-
+        if s not in m: m[s] = []
+        m[s].append((nums[i], 2))
       if nums[i] in m:
         for num, count in m[nums[i]]:
           s = num + nums[i]
-          count += 1
-          if s not in m:
-            m[s] = []
-          m[s] += [(nums[i], count)]
-          # print('continue waiting', s, '(',num,'->',nums[i],')', count)
-          if count > ans:
-            ans = count
-      # print(nums[i], m)
+          if s not in m: m[s] = []
+          m[s].append((nums[i], count + 1))
+          if count >= ans: ans = count + 1
     return ans
-
-
-# 4 -> 14 -> 18 -> 32 -> 50
