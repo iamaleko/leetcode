39f1074e260fbe820db9681007abff30456426bc
Alexander Kolobov <a@da.am>
2025-03-05 00:36:36 +0400

    Time: 95 ms (8.82%), Space: 17.9 MB (18.43%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.py
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.py
@@ -2,17 +2,16 @@ class Solution:
   def __init__(self):
     self.powers = []
 
-  def find(self, n: int, m: int) -> bool:
-    for i in range(m, len(self.powers)):
-      if self.powers[i] > n:
-        break
+  def find(self, n: int, i: int) -> bool:
+    while self.powers[i] <= n:
       if self.powers[i] == n or self.find(n - self.powers[i], i + 1):
         return True
+      i += 1
     return False
 
   def checkPowersOfThree(self, n: int) -> bool:
     if not self.powers:
-      limit = floor(math.log(1e7) / math.log(3))
+      limit = ceil(math.log(1e7) / math.log(3))
       for power in range(limit + 1):
         self.powers.append(3 ** power)
     return self.find(n,0)
