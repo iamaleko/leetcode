f56eac63084fe2baf86ed2743242950468d05881
Alexander Kolobov <a@da.am>
2025-03-08 01:06:44 +0400

    Time: 1161 ms (7.41%), Space: 4 MB (96.3%) - LeetHub

---
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.go
@@ -0,0 +1,41 @@
+import (
+  "math"
+)
+
+func isPrime(num int) bool {
+  fl := float64(num)
+  delimiter := 2.0
+  for delimiter < fl {
+    if (fl / delimiter) == math.Trunc(fl / delimiter) {
+      return false
+    }
+    delimiter++
+  }
+  return true
+}
+
+func closestPrimes(left int, right int) []int {
+  prime := 0
+  ans := []int{-1, -1}
+  best := right
+  if left == 1 {
+    left++
+  }
+  samples := 200
+  for left <= right {
+    if isPrime(left) {
+      if prime > 0 && left - prime < best {
+        best = left - prime
+        ans[0] = prime
+        ans[1] = left
+      }
+      samples--
+      if samples <= 0 {
+        return ans
+      }
+      prime = left
+    }
+    left++
+  }
+  return ans
+}
