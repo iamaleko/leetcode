668cdffd2b8f9e211bc2e0ea931038f54f4fca66
Alexander Kolobov <a@da.am>
2025-03-08 13:39:08 +0400

    Time: 0 ms (100%), Space: 4 MB (18.18%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.go
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.go
@@ -1,19 +1,18 @@
 func minimumRecolors(blocks string, k int) int {
-  ans := k
-  whites := 0
+	ans, whites := k,0
   k--
-  for i := range blocks {
-    if blocks[i] == 'W' {
-      whites++
-    }
-    if i >= k {
-      if ans > whites {
-        ans = whites
-      }
+	for i := range blocks {
+		if blocks[i] == 'W' {
+			whites++
+		}
+		if i >= k {
+			if ans > whites {
+				ans = whites
+			}
       if blocks[i-k] == 'W' {
-        whites--
-      }
-    }
-  }
-  return ans
+				whites--
+			}
+		}
+	}
+	return ans
 }
