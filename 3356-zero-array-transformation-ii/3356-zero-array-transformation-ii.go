cfdfcd75ce757aefba03ae53309379127d248a6b
Alexander Kolobov <a@da.am>
2025-03-14 00:54:41 +0400

    Time: 7 ms (47.37%), Space: 22.3 MB (78.95%) - LeetHub

--- 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.go
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.go
@@ -3,16 +3,13 @@ func minZeroArray(nums []int, queries [][]int) int {
   apply := make([]int, n)
   var p, d, l, r, v int
   for i, num := range nums {
-    for num - (d + apply[i]) > 0 {
+    for num - d - apply[i] > 0 {
       if p == m {
         return -1
       }
       l, r, v = queries[p][0], queries[p][1] + 1, queries[p][2]
       if r > i {
-        if i > l {
-          l = i
-        }
-        apply[l] += v
+        apply[max(l, i)] += v
         if r < n {
           apply[r] -= v
         }
