1503cb4974084163a8ff0aed72c00caad728737f
Alexander Kolobov <a@da.am>
2025-03-16 03:10:34 +0400

    Time: 1256 ms (73.11%), Space: 25.3 MB (22.53%) - LeetHub

--- 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
+++ 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
@@ -1,10 +1,12 @@
 class Solution:
   def countOfSubstrings(self, w: str, k: int) -> int:
     ans, n, d = 0, len(w), {'a': 0, 'e': 1, 'i': 2, 'o': 3, 'u': 4}
-    c, v = [n] * n, [-1] * 5
+    c, v, f = [n] * n, [-1] * 5, 0
     cl, cr, wl = 0, 0, 0
     for wr in range(n):
       if w[wr] in d:
+        if v[d[w[wr]]] == -1:
+          f += 1
         v[d[w[wr]]] = wr
       else:
         c[cr] = wr
@@ -13,9 +15,10 @@ class Solution:
         if w[wl] in d:
           if wl == v[d[w[wl]]]:
             v[d[w[wl]]] = -1
+            f -= 1
         else:
           cl += 1
         wl += 1
-      if cr - cl == k and min(v) > -1:
+      if cr - cl == k and f == 5:
         ans += 1 + min(c[cl], min(v)) - wl
     return ans
