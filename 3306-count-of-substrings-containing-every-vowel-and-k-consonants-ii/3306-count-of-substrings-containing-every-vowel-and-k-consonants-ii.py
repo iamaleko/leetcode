609b7b0aff5b34555719ab662059c898404ad387
Alexander Kolobov <a@da.am>
2025-03-16 02:49:26 +0400

    Time: 1708 ms (58.13%), Space: 25 MB (25.04%) - LeetHub

--- 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
+++ 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
@@ -1,23 +1,22 @@
 class Solution:
   def countOfSubstrings(self, w: str, k: int) -> int:
+    n = len(w)
     d = {'a': 0, 'e': 1, 'i': 2, 'o': 3, 'u': 4}
-    vovel = [-1,-1,-1,-1,-1]
-    cons = []
+    consonants, vovels = [], [-1,-1,-1,-1,-1]
     ans = 0
-    l = 0
-    for r in range(len(w)):
+    c, l = 0, 0
+    for r in range(n):
       if w[r] in d:
-        vovel[d[w[r]]] = r
+        vovels[d[w[r]]] = r
       else:
-        cons.append(r)
-      while len(cons) > k:
+        consonants.append(r)
+      while len(consonants) - c > k:
         if w[l] in d:
-          if l == vovel[d[w[l]]]:
-            vovel[d[w[l]]] = -1
+          if l == vovels[d[w[l]]]:
+            vovels[d[w[l]]] = -1
         else:
-          heappop(cons)
+          c += 1
         l += 1
-      if len(cons) == k and min(vovel) > -1:
-        ans += 1 + min(cons[0] if len(cons) else len(w), min(vovel)) - l 
-        # print(l, cons[0] if len(cons) else inf, min(vovel))
+      if len(consonants) - c == k and min(vovels) > -1:
+        ans += 1 + min(consonants[c] if c < len(consonants) else n, min(vovels)) - l 
     return ans
