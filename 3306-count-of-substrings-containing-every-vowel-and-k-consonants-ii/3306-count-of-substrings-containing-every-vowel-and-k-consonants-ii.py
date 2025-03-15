6776a5ff0fc7d111cb58088834b7753cc1b77e47
Alexander Kolobov <a@da.am>
2025-03-16 02:54:47 +0400

    Time: 1507 ms (67.59%), Space: 25.2 MB (22.53%) - LeetHub

--- 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
+++ 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
@@ -3,20 +3,22 @@ class Solution:
     n = len(w)
     d = {'a': 0, 'e': 1, 'i': 2, 'o': 3, 'u': 4}
     consonants, vovels = [], [-1,-1,-1,-1,-1]
+    consonantsL, consonantsR = 0, 0
     ans = 0
-    c, l = 0, 0
+    l = 0
     for r in range(n):
       if w[r] in d:
         vovels[d[w[r]]] = r
       else:
         consonants.append(r)
-      while len(consonants) - c > k:
+        consonantsR += 1
+      while consonantsR - consonantsL > k:
         if w[l] in d:
           if l == vovels[d[w[l]]]:
             vovels[d[w[l]]] = -1
         else:
-          c += 1
+          consonantsL += 1
         l += 1
-      if len(consonants) - c == k and min(vovels) > -1:
-        ans += 1 + min(consonants[c] if c < len(consonants) else n, min(vovels)) - l 
+      if consonantsR - consonantsL == k and min(vovels) > -1:
+        ans += 1 + min(consonants[consonantsL] if consonantsL < consonantsR else n, min(vovels)) - l 
     return ans
