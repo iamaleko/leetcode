c89bf51741a61b0079ffb272309799c0d7dc9e7e
Alexander Kolobov <a@da.am>
2025-03-16 03:11:51 +0400

    Time: 1185 ms (73.96%), Space: 25.5 MB (5.6%) - LeetHub

--- 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
+++ 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
@@ -1,13 +1,12 @@
 class Solution:
   def countOfSubstrings(self, w: str, k: int) -> int:
     ans, n, d = 0, len(w), {'a': 0, 'e': 1, 'i': 2, 'o': 3, 'u': 4}
-    c, v, f = [n] * n, [-1] * 5, 0
+    c, v, m = [n] * n, [-1] * 5, -1
     cl, cr, wl = 0, 0, 0
     for wr in range(n):
       if w[wr] in d:
-        if v[d[w[wr]]] == -1:
-          f += 1
         v[d[w[wr]]] = wr
+        m = min(v)
       else:
         c[cr] = wr
         cr += 1
@@ -15,10 +14,10 @@ class Solution:
         if w[wl] in d:
           if wl == v[d[w[wl]]]:
             v[d[w[wl]]] = -1
-            f -= 1
+            m = -1
         else:
           cl += 1
         wl += 1
-      if cr - cl == k and f == 5:
-        ans += 1 + min(c[cl], min(v)) - wl
+      if cr - cl == k and m > -1:
+        ans += 1 + min(c[cl], m) - wl
     return ans
