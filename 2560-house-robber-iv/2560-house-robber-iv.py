ed64d63da7b07021c27255bad6b81c025e8a779c
Alexander Kolobov <a@da.am>
2025-03-15 22:16:43 +0400

    Time: 284 ms (70.59%), Space: 28.6 MB (50%) - LeetHub

---
+++ 2560-house-robber-iv/2560-house-robber-iv.py
@@ -0,0 +1,48 @@
+class Solution:
+  def check(self, n: int) -> bool:
+    a, b = 0, 0
+    for num in self.nums:
+      if num <= n:
+        a, b = b, a + 1
+        if b == self.k:
+          return True
+      elif b > a:
+        a = b
+    return False
+    
+  def minCapability(self, nums: List[int], k: int) -> int:
+    self.nums = nums[:]
+    self.k = k
+    nums.sort()
+    l, r = 0, len(nums) - 1
+    while l < r:
+      c = l + r >> 1
+      if self.check(nums[c]):
+        r = c
+      else:
+        l = c + 1
+    return nums[r]
+
+# TLE 50/64
+# class Solution:
+#   def update(self, m, n, r):
+#     if r == len(m):
+#       m.append(n)
+#     elif m[r] > n:
+#       m[r] = n
+#   def minCapability(self, nums: List[int], k: int) -> int:
+#     if k == 1:
+#       return min(nums)
+#     m2, m1, m0 = [0], [0], [0]
+#     for i in range(1, len(nums)):
+#       m2, m1, m0 = m1, m0, m2
+#       if i > 0:
+#         self.update(m0, nums[i-1], 1)
+#       if i > 1:
+#         self.update(m0, max(nums[i], nums[i-2]), 2)
+#       for r, n in enumerate(m1):
+#         self.update(m0, n, r)
+#       for r, n in enumerate(m2):
+#         if r + 1 <= k:
+#           self.update(m0, max(n, nums[i]), r + 1)
+#     return m0[k] 
