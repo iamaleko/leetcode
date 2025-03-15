86a77e8d2eeb61f370baefa0d46d719f07fd2633
Alexander Kolobov <a@da.am>
2025-03-16 03:09:20 +0400

    Time: 1468 ms (68.98%), Space: 25.4 MB (10.95%) - LeetHub

--- 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
+++ 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
@@ -1,21 +1,21 @@
 class Solution:
   def countOfSubstrings(self, w: str, k: int) -> int:
     ans, n, d = 0, len(w), {'a': 0, 'e': 1, 'i': 2, 'o': 3, 'u': 4}
-    cIndex, vIndex = [], [-1] * 5
-    cLeft, cRight, wLeft = 0, 0, 0
-    for wRight in range(n):
-      if w[wRight] in d:
-        vIndex[d[w[wRight]]] = wRight
+    c, v = [n] * n, [-1] * 5
+    cl, cr, wl = 0, 0, 0
+    for wr in range(n):
+      if w[wr] in d:
+        v[d[w[wr]]] = wr
       else:
-        cIndex.append(wRight)
-        cRight += 1
-      while cRight - cLeft > k:
-        if w[wLeft] in d:
-          if wLeft == vIndex[d[w[wLeft]]]:
-            vIndex[d[w[wLeft]]] = -1
+        c[cr] = wr
+        cr += 1
+      while cr - cl > k:
+        if w[wl] in d:
+          if wl == v[d[w[wl]]]:
+            v[d[w[wl]]] = -1
         else:
-          cLeft += 1
-        wLeft += 1
-      if cRight - cLeft == k and min(vIndex) > -1:
-        ans += 1 + min(cIndex[cLeft] if cLeft < cRight else n, min(vIndex)) - wLeft 
+          cl += 1
+        wl += 1
+      if cr - cl == k and min(v) > -1:
+        ans += 1 + min(c[cl], min(v)) - wl
     return ans
