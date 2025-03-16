6de6e59a1d500424be24008a06c280182e42a711
Alexander Kolobov <a@da.am>
2025-03-16 15:30:59 +0400

    Time: 494 ms (59.01%), Space: 21.6 MB (97.36%) - LeetHub

--- 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.py
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.py
@@ -4,8 +4,8 @@ class Solution:
     h = [(rank, rank, 1, ranks[rank]) for rank in ranks]
     heapify(h)
     while cars > 0:
-      time, rank, n, count = heappop(h)
+      time, rank, n, count = h[0]
       cars -= count
       n += 1
-      heappush(h, (rank * n * n, rank, n, count))
+      heappushpop(h, (rank * n * n, rank, n, count))
     return time
