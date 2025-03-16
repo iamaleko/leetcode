aa1f72f1606aedeb5680db78dfcb68c5de1bbe3f
Alexander Kolobov <a@da.am>
2025-03-16 15:39:09 +0400

    Time: 811 ms (45.29%), Space: 21.6 MB (97.36%) - LeetHub

--- 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.py
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.py
@@ -1,11 +1,10 @@
 class Solution:
   def repairCars(self, rank: List[int], cars: int) -> int:
     ranks = Counter(rank)
-    h = [(rank, rank, 1) for rank in ranks]
+    h = [(rank, rank) for rank in ranks]
     heapify(h)
     while cars > 0:
-      ans, rank, repaired = h[0]
+      ans, rank = h[0]
       cars -= ranks[rank]
-      repaired += 1
-      heappushpop(h, (rank * repaired ** 2, rank, repaired))
-    return ans
+      heappushpop(h, (rank * (math.sqrt(ans / rank) + 1) ** 2, rank))
+    return int(ans)
