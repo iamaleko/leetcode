e016f1288af75748327e8c3d024b2833ee4eeed7
Alexander Kolobov <a@da.am>
2025-03-18 21:29:40 +0400

    Time: 165 ms (21.65%), Space: 33.6 MB (26.22%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.py
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.py
@@ -1,37 +1,37 @@
 # n * 32
-class Solution:
-  def longestNiceSubarray(self, nums: List[int]) -> int:
-    ans = 0
-    prev, curr = [-1] * 32, [-1] * 32
-    l = -1
-    for r, num in enumerate(nums):
-      b = 0
-      while num:
-        if num & 1:
-          prev[b] = curr[b]
-          curr[b] = r
-          if prev[b] > l:
-            l = prev[b]
-        num >>= 1
-        b += 1
-      if r - l > ans:
-        ans = r - l
-    return ans
-
-# n * n
 # class Solution:
 #   def longestNiceSubarray(self, nums: List[int]) -> int:
 #     ans = 0
-#     masks = {}
-#     n = len(nums)
-#     for i in range(n):
-#       for j in list(masks.keys()):
-#         if nums[i] & masks[j]:
-#           ans = max(ans, i - j)
-#           del masks[j]
-#         else:
-#           masks[j] |= nums[i]
-#       masks[i] = nums[i]
-#     for j in masks:
-#       ans = max(ans, n - j)
+#     prev, curr = [-1] * 32, [-1] * 32
+#     l = -1
+#     for r, num in enumerate(nums):
+#       b = 0
+#       while num:
+#         if num & 1:
+#           prev[b] = curr[b]
+#           curr[b] = r
+#           if prev[b] > l:
+#             l = prev[b]
+#         num >>= 1
+#         b += 1
+#       if r - l > ans:
+#         ans = r - l
 #     return ans
+
+# n * n
+class Solution:
+  def longestNiceSubarray(self, nums: List[int]) -> int:
+    ans = 0
+    masks = {}
+    n = len(nums)
+    for i in range(n):
+      for j in list(masks.keys()):
+        if nums[i] & masks[j]:
+          ans = max(ans, i - j)
+          del masks[j]
+        else:
+          masks[j] |= nums[i]
+      masks[i] = nums[i]
+    for j in masks:
+      ans = max(ans, n - j)
+    return ans
