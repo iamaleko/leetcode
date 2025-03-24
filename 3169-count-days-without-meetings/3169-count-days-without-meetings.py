26a0a369e59fb9942ea930759a444ea9cf1dcabb
Alexander Kolobov <a@da.am>
2025-03-24 22:15:06 +0400

    Time: 143 ms (77.33%), Space: 52.9 MB (55.42%) - LeetHub

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.py
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.py
@@ -2,9 +2,8 @@ class Solution:
   def countDays(self, days: int, meetings: List[List[int]]) -> int:
     if not meetings:
       return days
-    meetings.sort(key=lambda x: (x[0],x[1]))
-    ans = 0
-    day = 0
+    meetings.sort()
+    ans, day = 0, 0
     for start, end in meetings:
       if day < start - 1:
         ans += start - day - 1
