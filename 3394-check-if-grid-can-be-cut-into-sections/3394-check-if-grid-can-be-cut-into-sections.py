2698eb16d6e3d4b292ecff1340bfe8d46a79b7f9
Alexander Kolobov <a@da.am>
2025-03-25 21:24:16 +0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.py
@@ -0,0 +1,22 @@
+class Solution:
+  def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
+    # hor
+    rectangles.sort(key=lambda x: (x[0],x[2]))
+    cuts, cut = 0, 0
+    for l,_,r,_ in rectangles:
+      if cut and cut <= l:
+        cuts += 1
+        if cuts == 2:
+          return True
+      cut = r
+    # ver
+    rectangles.sort(key=lambda x: (x[1],x[3]))
+    cuts, cut = 0, 0
+    for _,l,_,r in rectangles:
+      if cut and cut <= l:
+        cuts += 1
+        if cuts == 2:
+          return True
+      cut = r
+    return False
+        
