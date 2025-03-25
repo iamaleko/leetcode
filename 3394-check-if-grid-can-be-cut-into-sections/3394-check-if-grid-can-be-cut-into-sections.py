974c2dc312ca87890b9b753c2f4d81837614901b
Alexander Kolobov <a@da.am>
2025-03-25 21:26:11 +0400

    Time: 348 ms (79.05%), Space: 83.6 MB (70.72%) - LeetHub

--- 3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.py
+++ 3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.py
@@ -4,9 +4,9 @@ class Solution:
     rectangles.sort(key=lambda x: (x[0],x[2]))
     cuts, cut = 0, 0
     for l,_,r,_ in rectangles:
-      if cut and cut <= l:
+      if cut <= l:
         cuts += 1
-        if cuts == 2:
+        if cuts == 3:
           return True
       if r > cut:
         cut = r
@@ -14,9 +14,9 @@ class Solution:
     rectangles.sort(key=lambda x: (x[1],x[3]))
     cuts, cut = 0, 0
     for _,l,_,r in rectangles:
-      if cut and cut <= l:
+      if cut <= l:
         cuts += 1
-        if cuts == 2:
+        if cuts == 3:
           return True
       if r > cut:
         cut = r
