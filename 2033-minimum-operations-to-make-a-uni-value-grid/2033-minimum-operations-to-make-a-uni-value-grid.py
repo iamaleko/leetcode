aff0e1512e710a5ff40cce4276eef9e2bd10db6d
Alexander Kolobov <a@da.am>
2025-03-26 23:09:26 +0400

    Time: 156 ms (57.46%), Space: 38.8 MB (91.23%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.py
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.py
@@ -1,14 +1,12 @@
 class Solution:
   def minOperations(self, grid: List[List[int]], x: int) -> int:
-    arr = [num for row in grid for num in row]
-    if (n := len(arr)) == 1:
-      return 0
-    arr.sort()
-    if arr[0] == arr[-1]:
-      return 0
-    med, ans = arr[n // 2] if n & 1 else ((arr[(n // 2) - 1] + arr[n // 2]) // 2 - arr[0]) // x * x + arr[0], 0
-    for num in arr:
-      if abs(med - num) % x:
-        return -1
-      ans += abs(med - num) // x
+    arr, ans = [num for row in grid for num in row], 0
+    if (n := len(arr)) > 1:
+      arr.sort()
+      if arr[0] < arr[-1]:
+        med = arr[n // 2] if n & 1 else ((arr[(n // 2) - 1] + arr[n // 2]) // 2 - arr[0]) // x * x + arr[0]
+        for num in arr:
+          if abs(med - num) % x:
+            return -1
+          ans += abs(med - num) // x
     return ans
