a7880b2c4e5e71bfd90b026ba298740324ec3151
Alexander Kolobov <a@da.am>
2025-03-26 23:09:36 +0400

    Time: 144 ms (74.56%), Space: 38.8 MB (91.23%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.py
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.py
@@ -4,7 +4,7 @@ class Solution:
     if (n := len(arr)) > 1:
       arr.sort()
       if arr[0] < arr[-1]:
-        med = arr[n // 2] if n & 1 else ((arr[(n // 2) - 1] + arr[n // 2]) // 2 - arr[0]) // x * x + arr[0]
+        med = arr[n // 2] if n & 1 else ((arr[n // 2 - 1] + arr[n // 2]) // 2 - arr[0]) // x * x + arr[0]
         for num in arr:
           if abs(med - num) % x:
             return -1
