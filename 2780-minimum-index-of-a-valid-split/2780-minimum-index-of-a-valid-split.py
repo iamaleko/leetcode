b8b9844fc8225d3d4322a70d7c41f11b93c7a4f6
Alexander Kolobov <a@da.am>
2025-03-27 20:17:49 +0400

    Time: 39 ms (96.04%), Space: 31.9 MB (85.15%) - LeetHub

--- 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
@@ -10,11 +10,11 @@ class Solution:
       else:
         c -= 1
     # find split, running sum
-    l,r,n = 1 if nums[0] == x else 0,0,len(nums)
-    for i in range(1, n):
+    l,r,n = 0,0,len(nums)
+    for i in range(n):
       if nums[i] == x:
         r += 1
-    for i in range(1, n):
+    for i in range(n):
       if l > i // 2 and r > (n - i) // 2:
         return i - 1
       if nums[i] == x:
