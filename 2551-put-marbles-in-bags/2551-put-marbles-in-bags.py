bfb319f0a6b670720f5c161eb9ca9ad027abe7ef
Alexander Kolobov <a@da.am>
2025-03-31 23:42:12 +0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.py
@@ -0,0 +1,15 @@
+class Solution:
+  def putMarbles(self, weights: List[int], k: int) -> int:
+    h = []
+    n = len(weights)
+    for i in range(1, n):
+      s.append((weights[i-1] + weights[i], i))
+    heapify(h)
+    s = 0
+    splits
+    while s < k:
+
+      s += 1
+    print(splits)
+
+        
