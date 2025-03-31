39e73969a9b26c61c0418649723b20254e59a114
Alexander Kolobov <a@da.am>
2025-04-01 00:10:11 +0400

    Time: 125 ms (80.49%), Space: 30.7 MB (16.67%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.py
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.py
@@ -1,3 +1,3 @@
 class Solution:
   def putMarbles(self, w: List[int], k: int) -> int:
-    return 0 if k == 1 else sum((w := sorted([w[i] + w[i + 1] for i in range(len(w) - 1)]))[-k + 1:]) - sum(w[:k - 1])
+    return 0 if k == 1 else sum((w := sorted([a + b for a, b in zip(w[:-1], w[1:])]))[-k + 1:]) - sum(w[:k - 1])
