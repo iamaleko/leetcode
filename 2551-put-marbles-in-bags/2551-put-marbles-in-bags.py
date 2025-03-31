edd8e91e4fc50e69f75b86077df2acd01b24479e
Alexander Kolobov <a@da.am>
2025-03-31 23:51:53 +0400

    Time: 1069 ms (5.29%), Space: 49.5 MB (5.69%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.py
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.py
@@ -1,15 +1,21 @@
 class Solution:
   def putMarbles(self, weights: List[int], k: int) -> int:
-    h = []
+    mn, mx = [], []
     n = len(weights)
     for i in range(1, n):
-      s.append((weights[i-1] + weights[i], i))
-    heapify(h)
-    s = 0
-    splits
-    while s < k:
-
-      s += 1
-    print(splits)
+      s = weights[i-1] + weights[i]
+      mn.append((s, i))
+      mx.append((-s, i))
+    heapify(mn)
+    heapify(mx)
+    mns, mxs = [], []
+    while len(mns) < k - 1:
+      mns.append(heappop(mn)[1])
+      mxs.append(heappop(mx)[1])
+    a, b = weights[0] + weights[-1], weights[0] + weights[-1]
+    for i, j in zip(sorted(mns), sorted(mxs)):
+      a += weights[i - 1] + weights[i]
+      b += weights[j - 1] + weights[j]
+    return abs(a - b)
 
         
