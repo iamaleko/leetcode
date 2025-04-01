5797a3cb9b618d53082eb743b7de621149e9721a
Alexander Kolobov <a@da.am>
2025-04-01 23:19:11 +0400

    Time: 69 ms (85.01%), Space: 60.9 MB (50.81%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -1,10 +1,14 @@
 class Solution:
   def mostPoints(self, q: List[List[int]]) -> int:
     n = len(q)
-    score = [0] * (n + 1)
+    score = [0] * n
+    ans = 0
     for i, [p, s] in enumerate(q):
       if i and score[i] < score[i-1]:
         score[i] = score[i-1]
-      if score[j := min(n, i + s + 1)] < score[i] + p:
-        score[j] = score[i] + p
-    return score[n]
+      if i + s + 1 < n:
+        if score[i + s + 1] < score[i] + p:
+          score[i + s + 1] = score[i] + p
+      elif ans < score[i] + p:
+        ans = score[i] + p
+    return ans
