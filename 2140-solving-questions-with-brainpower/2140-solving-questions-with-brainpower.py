76f53059eb5bc51479f9ab92224dc58ff055e6d6
Alexander Kolobov <a@da.am>
2025-04-01 23:13:53 +0400

    Time: 127 ms (46.82%), Space: 60.8 MB (66.88%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -4,6 +4,6 @@ class Solution:
     score = [0] * (n + 1)
     for i in range(n):
       s = min(n, i + q[i][1] + 1)
+      if i: score[i] = max(score[i], score[i-1])
       score[s] = max(score[s], score[i] + q[i][0])
     return score[n]
-        
