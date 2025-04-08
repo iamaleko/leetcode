3ee8b1d32a02add9c1f597f6904a49129c8a0490
Alexander Kolobov <a@da.am>
2025-04-08 21:34:48 +0400

    Time: 0 ms (100%), Space: 17.9 MB (48.49%) - LeetHub

--- 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -1,13 +1,23 @@
 class Solution:
   def minimumOperations(self, nums: List[int]) -> int:
-    count = Counter(nums)
-    ans = 0
-    for i in range(0, len(nums), 3):
-      if len(count) == len(nums) - i:
-        break
-      ans += 1
-      for j in range(i, min(len(nums), i + 3)):
-        count[nums[j]] -= 1
-        if not count[nums[j]]: del count[nums[j]]
-    return ans
+    m, i = {}, -1
+    for j, num in enumerate(nums):
+      if num in m:
+        if m[num] > i:
+          i = m[num]
+      m[num] = j
+    return math.ceil((i + 1) / 3) if i >= 0 else 0
+
+# class Solution:
+#   def minimumOperations(self, nums: List[int]) -> int:
+#     count = Counter(nums)
+#     ans = 0
+#     for i in range(0, len(nums), 3):
+#       if len(count) == len(nums) - i:
+#         break
+#       ans += 1
+#       for j in range(i, min(len(nums), i + 3)):
+#         count[nums[j]] -= 1
+#         if not count[nums[j]]: del count[nums[j]]
+#     return ans
         
