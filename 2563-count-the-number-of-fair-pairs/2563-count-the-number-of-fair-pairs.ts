4a5517dae957ebc33ae1db148e978c1405fc25fe
Alexander Kolobov <a@da.am>
2025-04-20 03:29:08 +0400

    Time: 106 ms (78.95%), Space: 63.1 MB (100%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.ts
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.ts
@@ -9,14 +9,14 @@ function countFairPairs(nums: number[], lower: number, upper: number): number {
     } else {
       let ll = l, rr = r, c: number;
       while (ll <= rr) {
-        c = ll + rr >>>1;
+        c = ll + rr >>> 1;
         if (nums[c] + nums[r] <= upper) {
           ll = c + 1;
         } else {
           rr = c - 1;
         }
       }
-      ans += Math.min(ll,r) - l;
+      ans += ll > r ? r - l : ll - l;
       r--;
     }
   }
