6b7f17a4873930d563dc01998cf1fd9e32bacf2e
Alexander Kolobov <a@da.am>
2025-04-20 20:44:59 +0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.py
@@ -0,0 +1,16 @@
+class Solution:
+  def numRabbits(self, answers: List[int]) -> int:
+    count = {}
+    ans = 0
+    for answer in answers:
+      if answer + 1 not in count:
+        count[answer + 1] = 1
+      elif answer + 1 in count:
+        count[answer + 1] += 1
+        if count[answer + 1] == answer + 1:
+          ans += answer + 1
+          del count[answer + 1]
+    ans += sum(count.keys())
+    return ans
+
+        
