b99aa9d0b79eba02e30e76408d11a6e1a8d48715
Alexander Kolobov <a@da.am>
2025-05-03 15:15:33 +0400

    Time: 75 ms (24.47%), Space: 18.4 MB (66.24%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -1,17 +1,22 @@
 class Solution:
   def minDominoRotations(self, t: List[int], b: List[int]) -> int:
-    ans = 0
-    for i in range(1, len(t)):
-      if b[i]!= t[i] and b[0] != t[0] and (t[0] == b[i] or b[0] == t[i]):
-        ans += 1
-        b[i],t[i] = t[i],b[i]
-      if t[0] == t[i]:
-        if b[0] != b[i]:
-          b[0] = -1
-      if b[0] == b[i]:
-        if t[0] != t[i]:
-          t[0] = -1
-      if t[0] != t[i] and b[0] != b[i] or t[0] == -1 and b[0] == -1:
+    cnt = defaultdict(int)
+    for num in t:
+      cnt[num] += 1
+    for num in b:
+      cnt[num] += 1
+    num = -1
+    for k in cnt:
+      if cnt[k] >= len(t):
+        num = k
+        break
+    a, k = 0, 0
+    for i in range(len(t)):
+      if t[i] != num and b[i] != num:
         return -1
-    return ans
+      if t[i] != num:
+        a += 1
+      if b[i] != num:
+        k += 1
+    return min(a, k)
         
