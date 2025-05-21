2e9b0bc86f75551991eb58887da4699313632e8e
Alexander Kolobov <a@da.am>
2025-05-21 21:20:04 +0400

    Time: 103 ms (76.88%), Space: 21.1 MB (51.03%) - LeetHub

--- 0838-push-dominoes/0838-push-dominoes.py
+++ 0838-push-dominoes/0838-push-dominoes.py
@@ -1,14 +1,14 @@
 class Solution(object):
   def pushDominoes(self, dominoes):
-    ans = ''
+    ans = []
     l = -1
     for r in range(len(dominoes := 'L' + dominoes + 'R')):
       if dominoes[r] != '.':
         if n := r - l - 1:
           if dominoes[l] == 'L':
-            ans += ('L' if dominoes[r] == 'L' else '.') * n
+            ans += [('L' if dominoes[r] == 'L' else '.') * n]
           else:
-            ans += 'R' * n if dominoes[r] == 'R' else 'R' * (n // 2) + '.' * (n & 1) + 'L' * (n // 2)
+            ans += ['R' * n if dominoes[r] == 'R' else 'R' * (n // 2) + '.' * (n & 1) + 'L' * (n // 2)]
         ans += dominoes[r]
         l = r
-    return ans[1:-1]
+    return ''.join(ans[1:-1])
