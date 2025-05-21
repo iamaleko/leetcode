df30e810531e6c9dff98a63e78b29a21efc81083
Alexander Kolobov <a@da.am>
2025-05-21 22:06:02 +0400

    Time: 39 ms (99.31%), Space: 24.4 MB (15.93%) - LeetHub

--- 2901-longest-unequal-adjacent-groups-subsequence-ii/2901-longest-unequal-adjacent-groups-subsequence-ii.py
+++ 2901-longest-unequal-adjacent-groups-subsequence-ii/2901-longest-unequal-adjacent-groups-subsequence-ii.py
@@ -3,19 +3,14 @@ class Solution:
     ans = []
     m = dict()
     for i, word in enumerate(words):
-      # find best chain
-      best = []
-      for j in range(len(word)):
-        key = word[0:j] + '.' + word[j+1:]
+      best, keys = [], [word[0:j] + '.' + word[j+1:] for j in range(len(word))]
+      for key in keys:
         if key in m and groups[i] != groups[m[key][-1]] and len(m[key]) > len(best):
           best = m[key]
       best = best[:] + [i]
-      # update chains
-      for j in range(len(word)):
-        key = word[0:j] + '.' + word[j+1:]
+      for key in keys:
         if key not in m or len(m[key]) < len(best):
           m[key] = best
-      # update ans
       if len(best) > len(ans):
         ans = best
     return list(map(lambda i: words[i], ans))
