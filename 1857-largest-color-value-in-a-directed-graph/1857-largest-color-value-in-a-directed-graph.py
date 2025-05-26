da0f0a410fc47e4e751d438f1530522591454772
Alexander Kolobov <a@da.am>
2025-05-26 23:21:14 +0400

    Time: 1464 ms (93.03%), Space: 91.9 MB (51.24%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.py
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.py
@@ -1,16 +1,12 @@
 class Solution:
   def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
     n = len(colors)
-    graph = {}
+    graph = defaultdict(set)
     cache = [0] * (26 * n)
     groups = [ord(color) - 97 for color in colors]
     indegree = [0] * n
 
     for a, b in edges:
-      if a == b:
-        return -1
-      if b not in graph:
-        graph[b] = set()
       graph[b].add(a)
       indegree[a] += 1
       
@@ -21,23 +17,20 @@ class Solution:
 
     if not leaves:
       return -1
-
-    def merge(a, b):
-      a *= 26
-      b *= 26
-      for i in range(26):
-        if cache[a + i] < cache[b + i]:
-          cache[a + i] = cache[b + i]
     
     while leaves:
       node = leaves.popleft()
       cache[node * 26 + groups[node]] += 1
       if node in graph:
+        np = node * 26
         for parent in graph[node]:
           indegree[parent] -= 1
           if indegree[parent] < 0:
             return -1
-          merge(parent, node)
+          pp = parent * 26
+          for i in range(26):
+            if cache[pp + i] < cache[np + i]:
+              cache[pp + i] = cache[np + i]
           if indegree[parent] == 0:
            leaves.append(parent)
       cache[node * 26 + groups[node]] -= 1
