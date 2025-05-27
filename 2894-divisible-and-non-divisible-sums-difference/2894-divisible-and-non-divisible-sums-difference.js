747c3cb4929632e7ceae169d169ce1599f417481
Alexander Kolobov <a@da.am>
2025-05-27 22:35:47 +0400

    Time: 1 ms (36.79%), Space: 54.1 MB (78.93%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
@@ -3,15 +3,11 @@
  * @param {number} m
  * @return {number}
  */
-const differenceOfSums = (n, m) => {
-  let a = 0, b = 0;
+var differenceOfSums = function(n, m) {
+  let ans = 0;
   while (n) {
-    if (n % m) {
-      a += n;
-    } else {
-      b += n;
-    }
-    --n;
+    ans += n % m ? n : -n;
+    n--;
   }
-  return a - b;
+  return ans;
 };
