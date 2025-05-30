b368293b5e2be2bc502d96b233ec9c1a35dd2004
Alexander Kolobov <a@da.am>
2025-05-30 22:54:34 +0400

    Time: 81 ms (95.02%), Space: 40.8 MB (68.33%) - LeetHub

---
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
@@ -0,0 +1,26 @@
+class Solution:
+  def closestMeetingNode(self, edges: List[int], a: int, b: int) -> int:
+    if a == b:
+      return a
+    level, levels = 0, {}
+    while a > -1:
+      if a in levels:
+        break
+      levels[a] = level
+      a = edges[a]
+      level += 1
+    level = 0
+    visited = set()
+    ans, best = -1, inf
+    while b > -1:
+      if b in visited:
+        break
+      visited.add(b)
+      if b in levels:
+        score = max(levels[b], level)
+        if best > score or best == score and ans > b:
+          ans = b
+          best = score
+      b = edges[b]
+      level += 1
+    return ans
