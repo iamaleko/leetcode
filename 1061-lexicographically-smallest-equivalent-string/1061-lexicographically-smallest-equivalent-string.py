bcb835731c5309898ff6d27034db2f27c1c43b09
Alexander Kolobov <a@da.am>
2025-06-06 00:12:16 +0400

    Time: 3 ms (93.94%), Space: 18 MB (41.03%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -1,26 +1,25 @@
 class Solution:
   def smallestEquivalentString(self, a: str, b: str, s: str) -> str:
-    parent = {}
+    parent, ans = {}, ''
     for l, r in zip(a, b):
-      if l not in parent:
-        parent[l] = l
-      if r not in parent:
-        parent[r] = r
-      while parent[l] != l:
-        l = parent[parent[l]]
-      while parent[r] != r:
-        r = parent[parent[r]]
-      if l < r:
-        parent[r] = l
+      if l in parent:
+        while parent[l] != l:
+          l = parent[parent[l]]
       else:
-        parent[l] = r
-    # print(parent)
-    ans = []
+        parent[l] = l
+      if r in parent:
+        while parent[r] != r:
+          r = parent[parent[r]]
+      else:
+        parent[r] = r
+      if l > r:
+        l, r = r, l
+      parent[r] = l
     for k in s:
       if k in parent:
         while parent[k] != k:
           k = parent[parent[k]]
-      ans.append(k)
-    return ''.join(ans)
+      ans += k
+    return ans
 
         
