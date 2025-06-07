4d476f20c0b627c3b14f043e96e9dd0e83a12cf1
Alexander Kolobov <a@da.am>
2025-06-07 19:04:05 +0400

    Time: 549 ms (34.48%), Space: 20.5 MB (76.21%) - LeetHub

--- 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.py
+++ 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -1,34 +1,19 @@
 class Solution:
-    def robotWithString(self, s: str) -> str:
-        cnt = Counter(s)
-        stack = []
-        res = []
-        minCharacter = "a"
-        for c in s:
-            stack.append(c)
-            cnt[c] -= 1
-            while minCharacter != "z" and cnt[minCharacter] == 0:
-                minCharacter = chr(ord(minCharacter) + 1)
-            while stack and stack[-1] <= minCharacter:
-                res.append(stack.pop())
-        return "".join(res)
-
-# class Solution:
-#   def robotWithString(self, s: str) -> str:
-#     st = deque()
-#     for i in range(len(s)):
-#       while st and s[st[-1]] > s[i]:
-#         st.pop()
-#       st.append(i)
-#     print(st)
-#     paper = []
-#     robot = []
-#     for i in range(len(s)):
-#       robot.append(i)
-#       if st and st[0] == i:
-#         st.popleft()
-#         while robot and (not st or robot[-1] <= st[0]):
-#           paper.append(robot.pop())
-#     return ''.join([s[i] for i in paper])
-
-    
+  def robotWithString(self, s: str) -> str:
+    st = deque()
+    for i in range(len(s)):
+      while st and st[-1] > s[i]:
+        st.pop()
+      st.append(s[i])
+    # print(st)
+    robot = []
+    paper = []
+    for i in range(len(s)):
+      if st and s[i] == st[0]:
+        st.popleft()
+        paper.append(s[i])
+      else:
+        robot.append(s[i])
+      while robot and (not st or robot[-1] <= st[0]):
+        paper.append(robot.pop())
+    return ''.join(paper)
