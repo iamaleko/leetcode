da9fbc0d900bb2561620fe5297eda8ddb1e2d952
Alexander Kolobov <a@da.am>
2025-06-15 23:41:08 +0400

    Time: 0 ms (100%), Space: 17.6 MB (99.15%) - LeetHub

--- 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.py
+++ 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.py
@@ -1,21 +1,17 @@
 class Solution:
   def maxDiff(self, num: int) -> int:
-    arr = list(str(num))
-    if len(set(arr)) == 1:
-      return int('8' * len(arr))
-    x = ''
-    y = ''
-    for i, ch in enumerate(arr):
-      if not x and int(ch) > 1:
-        x = ch
-      if not y and ch != '9':
-        y = ch
-      if x and y:
-        break
-    y = int(''.join(['9' if ch == y else ch for ch in arr]))
-    rep = '1' if arr[0] == x else '0'
-    x = int(''.join([rep if ch == x else ch for ch in arr]))
-    return y - x
+    if len(set(str(num))) == 1:
+      return int('8' * len(str(num)))
+    x, y, arr = -1, -1, list(map(int, str(num)))
+    for d in arr:
+      if x == -1 and d > 1: x = d
+      if y == -1 and d < 9: y = d
+      if x != -1 and y != -1: break
+    a, b, r = 0, 0, 1 if arr[0] == x else 0
+    for d in arr:
+      a = a * 10 + (r if d == x else d)
+      b = b * 10 + (9 if d == y else d)
+    return b - a
     
 
         
