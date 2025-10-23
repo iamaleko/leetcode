750da191c2140a3a3149368d88d6676a5091d5f3
Alexander Kolobov <a@da.am>
2025-10-24 01:01:08 +0400

    Time: 10 ms (65.52%), Space: 59 MB (79.31%) - LeetHub

--- 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.ts
+++ 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.ts
@@ -2,10 +2,7 @@ function hasSameDigits(s: string): boolean {
   const arr = s.split('').map(Number);
   let l = 0, r = arr.length;
   while (r > 2) {
-    while (l + 1 < r) {
-      arr[l] = (arr[l] + arr[l + 1]) % 10;
-      l++;
-    }
+    while (l + 1 < r) arr[l] = (arr[l] + arr[++l]) % 10;
     l = 0;
     r--;
   }
