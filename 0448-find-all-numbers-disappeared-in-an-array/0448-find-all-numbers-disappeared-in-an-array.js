da44227f592843da046c44d7582dddfbc68f834d
Alexander Kolobov <a@da.am>
2026-02-02 20:23:06 +0400

    Time: 14 ms (57.3%), Space: 73.2 MB (56.85%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -1,15 +1,7 @@
 const findDisappearedNumbers = (nums) => {
   const res = [];
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === i + 1) continue;
-    while (nums[i] !== undefined && nums[i] !== i + 1) {
-      if (nums[i] === nums[nums[i] - 1]) {
-        break;
-      }
-      let tmp = nums[i];
-      nums[i] = nums[nums[i] - 1];
-      nums[tmp - 1] = tmp;
-    }
+    while (nums[i] !== i + 1 && nums[i] !== nums[nums[i] - 1]) [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
   }
   for (let i = 0; i < nums.length; i++) if (nums[i] !== i + 1) res.push(i + 1)
   return res;
